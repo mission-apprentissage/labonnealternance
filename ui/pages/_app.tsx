698f36a32f11c0c63b24a4b96c2b8c3767201b05
Alan Le Ruyet <alan.leruyet@free.fr>
2022-10-07 17:34:59 +0200

    Version tsx de _app

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,7 +1,59 @@
-import type { AppProps } from "next/app";
+import App from "next/app";
+import React from "react";
+import HeadLaBonneAlternance from "../components/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import Providers from "../context/Providers";
+
+import "../public/styles/application.scss";
+
+//import * as Sentry from "@sentry/node";
+//import * as SentryReact from "@sentry/react";
+import PageTracker from "../components/pageTracker";
+import { getEnvFromProps } from "../utils/env";
+
+/*if (process.env.uiSentryDsn) {
+  Sentry.init({ dsn: process.env.uiSentryDsn, enabled: true, environment: process.env.env });
+  SentryReact.init({ dsn: process.env.uiSentryDsn, enabled: true, environment: process.env.env });
+}*/
+
+class LaBonneAlternance extends App {
+  static async getInitialProps(context) {
+    // récupération du hostname pour initialiser les fonts en preload
+    const { req } = context.ctx;
+    let host = "";
+    let shouldLoadAnalytics = true;
+
+    if (req?.headers?.host) {
+      host = req.headers.host;
+      host = `${host.startsWith("localhost") ? "http" : "https"}://${host}`;
+    }
+
+    // identification des exceptions au chargement d'analytics
+    if (/caller=TSA/g.test(req?.url)) {
+      shouldLoadAnalytics = false;
+    }
+
+    return { host, shouldLoadAnalytics };
+  }
+
+  render() {
+    const { Component, pageProps, host, shouldLoadAnalytics } = this.props;
+
+    const env = getEnvFromProps(this.props).env;
+
+    return (
+      <Providers env={env}>
+        <PageTracker>
+          <main className="c-app">
+            <HeadLaBonneAlternance
+              shouldLoadAnalytics={shouldLoadAnalytics}
+              publicUrl={host && process.env.publicUrl ? host : ""}
+            />
+            <Component {...pageProps} />
+          </main>
+        </PageTracker>
+      </Providers>
+    );
+  }
 }
-
-export default MyApp;
+export default LaBonneAlternance;
