3796299fdd2f5e21c50299fef3629bf61c121701
Alan Le Ruyet <alan.leruyet@free.fr>
2022-10-12 16:02:30 +0200

    Fix sitemap

---
+++ ui/next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://labonnealternance.pole-emploi.fr',
+  generateRobotsTxt: false,
+  changefreq: 'monthly',
+  exclude: ['/someerror', '/test-widget', '/stats', '/500', '/simulated-error', '/config', '/styleguide']
+}
