bb28adb94530fee27d656473f596f00db5ecbe93
Alan Le Ruyet <alan.leruyet@free.fr>
2022-10-18 10:38:43 +0200

    version module

--- server/src/http/middlewares/corsMiddleware.js
+++ server/src/http/middlewares/corsMiddleware.js
@@ -1,8 +1,8 @@
-module.exports = () => {
+export function corsMiddleware() {
   // eslint-disable-next-line no-unused-vars
   return (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, API-Key");
     next();
   };
-};
+}
