7c3086bdcb53e3e0c100e61ae6f5910b2b68f4b2
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-10-21 11:12:14 +0200

    Mises à jour mineures de libellés

--- ui/components/ItemDetail/CandidatureSpontanee/services/getSchema.js
+++ ui/components/ItemDetail/CandidatureSpontanee/services/getSchema.js
@@ -15,9 +15,9 @@ export function getInitialSchemaValues() {
 
 const commonControls = {
   fileName: Yup.string().nullable().required("⚠ La pièce jointe est obligatoire"),
-  firstName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le prénom est obligatoire."),
-  lastName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le nom est obligatoire."),
-  email: Yup.string().email("⚠ Adresse e-mail invalide.").required("⚠ L'adresse e-mail est obligatoire."),
+  firstName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le prénom est obligatoire"),
+  lastName: Yup.string().max(50, "⚠ Doit avoir 50 caractères ou moins").required("⚠ Le nom est obligatoire"),
+  email: Yup.string().email("⚠ Adresse e-mail invalide").required("⚠ L'adresse e-mail est obligatoire"),
   phone: Yup.string()
     .matches(/^[0-9]{10}$/, "⚠ Le numéro de téléphone doit avoir exactement 10 chiffres")
     .required("⚠ Le téléphone est obligatoire"),
