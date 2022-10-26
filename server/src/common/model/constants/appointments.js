1b3f884808cd415e8c7742c474b8e54eb82294e6
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-10-26 13:33:05 +0200

    feat: constants imported

---
+++ server/src/common/model/constants/appointments.js
@@ -0,0 +1,11 @@
+const mailType = {
+  CANDIDAT_HAVE_YOU_BEEN_CONTACTED: "CANDIDAT_HAVE_YOU_BEEN_CONTACTED",
+  CFA_REMINDER_RESEND_APPOINTMENT: "CFA_REMINDER_RESEND_APPOINTMENT",
+};
+
+const candidatFollowUpType = {
+  CONFIRM: "CONFIRM",
+  RESEND: "RESEND",
+};
+
+module.exports = { mailType, candidatFollowUpType };
---
+++ server/src/common/model/constants/emails.js
@@ -0,0 +1,26 @@
+const emailStatus = {
+  request: "Envoyé",
+  click: "Clické",
+  deferred: "Différé",
+  delivered: "Délivré",
+  soft_bounce: "Rejecté (soft)",
+  spam: "Spam",
+  unique_opened: "Ouverture unique",
+  hard_bounce: "Rejeté (hard)",
+  unsubscribed: "Désinscrit",
+  opened: "Ouvert",
+  invalid_email: "Email invalide",
+  blocked: "Bloqué",
+  error: "Erreur",
+};
+
+/**
+ * @description Returns email status.
+ * @param {string} status - Status stored in database
+ * @return {string}
+ */
+const getEmailStatus = (status) => emailStatus[status] || "N/C";
+
+module.exports = {
+  getEmailStatus,
+};
