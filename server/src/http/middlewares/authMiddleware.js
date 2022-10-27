428655ef728b5bd2d65b0c0ed196dadf8ef2fffd
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-10-27 16:49:58 +0200

    feat: middlewares added

---
+++ server/src/http/middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { compose } from "compose-middleware";
+import config from "../../config";
+
+export default ({ users }) => {
+  passport.use(
+    new Strategy(
+      {
+        jwtFromRequest: ExtractJwt.fromExtractors([
+          ExtractJwt.fromUrlQueryParameter("token"),
+          ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ]),
+        secretOrKey: config.auth.user.jwtSecret,
+      },
+      (jwt_payload, done) => {
+        return users
+          .getUser(jwt_payload.sub)
+          .then((user) => {
+            if (!user) {
+              return done(null, false);
+            }
+            return done(null, user);
+          })
+          .catch((err) => done(err));
+      }
+    )
+  );
+
+  return compose([
+    passport.authenticate("jwt", { session: false }),
+    async (req, res, next) => {
+      next();
+    },
+  ]);
+};
