7349cf752d6b688a218f15081e5a09a3884012e1
Kevin Barnoin <k.barnoin@gmail.com>
2022-11-04 10:56:55 +0100

    feat(server): add jobs

---
+++ server/src/jobs/lba_recruteur/api/createApiUser.js
@@ -0,0 +1,7 @@
+import { logger } from "../../../common/logger.js";
+import { Credential } from "../../../common/model/index.js";
+
+export const createApiUser = async (nom, prenom, email, organisation, scope) => {
+  const apiUser = await Credential.create({ nom, prenom, email, organisation, scope });
+  logger.info(`API-KEY : ${apiUser.apiKey}`);
+};
