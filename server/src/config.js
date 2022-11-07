201fe698bd4b31684f62d875d309774e07dec6b2
Kevin Barnoin <k.barnoin@gmail.com>
2022-11-07 14:30:29 +0100

    feat(server): remove duplicate env var

--- server/src/config.js
+++ server/src/config.js
@@ -18,17 +18,35 @@ const config = {
   mongodb: {
     uri: env
       .get("LBA_MONGODB_URI")
-      .default("mongodb://127.0.0.1:27017/doctrina?retryWrites=true&w=majority")
+      .default("mongodb://127.0. .1:27017/labonnealternance?retryWrites=true&w=majority")
       .asString(),
   },
-  private: {
-    apiKey: env.get("LBA_API_KEY").default("12345").asString(), //TODO: check usefulness
-    laBonneFormationPassword: env.get("LBA_LABONNEFORMATION_PASSWORD").default("12345").asString(),
-    mongodb: {
-      uri: env
-        .get("LBA_MONGODB_URI")
-        .default("mongodb://127.0.0.1:27017/labonnealternance?retryWrites=true&w=majority")
-        .asString(),
+  apiKey: env.get("LBA_API_KEY").default("12345").asString(), //TODO: check usefulness
+  laBonneFormationPassword: env.get("LBA_LABONNEFORMATION_PASSWORD").default("12345").asString(),
+  awsAccessKeyId: env.get("LBA_AWS_ACCESS_KEY_ID").default("1234").asString(),
+  awsSecretAccessKey: env.get("LBA_AWS_SECRET_ACCESS_KEY").default("1234").asString(),
+  catalogueUrl: env.get("LBA_CATALOGUE_URL").default("https://catalogue.intercariforef.org").asString(),
+  matcha: {
+    apiKey: env.get("LBA_MATCHA_ACCESS_KEY").default("1234").asString(),
+  },
+  apiEntrepriseKey: env.get("LBA_API_ENTREPRISE_KEY").default("1234").asString(),
+  esdClientId: env.get("LBA_ESD_CLIENT_ID").default("1234").asString(),
+  esdClientSecret: env.get("LBA_ESD_CLIENT_SECRET").default("1234").asString(),
+  serverSentryDsn: env
+    .get("LBA_SERVER_SENTRY_DSN")
+    .default("https://61156c2d46b54369b777900d475ee0f9@o154210.ingest.sentry.io/5562203")
+    .asString(),
+  secretUpdateRomesMetiers: env.get("LBA_SECRET_UPDATE_ROMES_METIERS").default("1234").asString(), //TODO: rename
+  secret1j1s: env.get("LBA_SECRET_1J1S").default("5678").asString(), //TODO: rearrange
+  secretAkto: env.get("LBA_SECRET_AKTO").default("1234").asString(), //TODO: rearrange
+  jobSlackWebhook: env.get("LBA_JOB_SLACK_WEBHOOK").default("https://hooks.slack.com/services").asString(),
+  allowedSources: env.get("LBA_ALLOWED_SOURCES").default("allowed").asString(),
+  smtp: {
+    host: env.get("LBA_SMTP_HOST").default("smtp").asString(),
+    port: env.get("LBA_SMTP_PORT").default("1025").asString(),
+    auth: {
+      user: env.get("LBA_SMTP_AUTH_USER").default("lba").asString(),
+      pass: env.get("LBA_SMTP_AUTH_PASS").default("1234").asString(),
     },
     awsAccessKeyId: env.get("LBA_AWS_ACCESS_KEY_ID").default("1234").asString(),
     awsSecretAccessKey: env.get("LBA_AWS_SECRET_ACCESS_KEY").default("1234").asString(),
