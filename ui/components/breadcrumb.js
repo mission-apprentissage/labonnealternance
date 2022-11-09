2e287ba69173b872d8aa47131114fb92f240138c
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-09 15:32:44 +0100

    Breadcrumb full chakra

--- ui/components/breadcrumb.js
+++ ui/components/breadcrumb.js
@@ -1,41 +1,44 @@
 import React from "react";
 import NextLink from "next/link";
-import { Box, Container, Link } from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, Container, Link } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
-import separator from "../public/images/breadcrumb_separator.svg";
+const BreadcrumbLinks = (props) =>
+{
+  return (props.items.map((item, index) => {
+    return (
+      <BreadcrumbItem key={index} {...props}>
+        <NextLink passHref href={{ pathname: `/${item.path}` }}>
+          <BreadcrumbLink>{item.title}</BreadcrumbLink>
+        </NextLink>
+        {index<(props.items.length-1)?<BreadcrumbSeparator />:""}
+      </BreadcrumbItem>
+    );
+  }));
+}
 
-const Breadcrumb = ({ forPage, label, items = null }) => {
+const BreadcrumbComponent = ({ forPage, label, items = null }) => {
   return (
     <Box display={["none","block"]}>
       <Container fontSize="12px" variant="responsiveContainer" pl={0} pt={4} pb={4} display="flex">
-        <NextLink href={{ pathname: "/" }} passHref>
-          <Link>Accueil</Link>
-        </NextLink>
+        <Breadcrumb separator={<ChevronRightIcon fontSize='1.25rem' color='grey.800' />}>
+          <BreadcrumbItem>
+            <NextLink href={{ pathname: "/" }} passHref>
+              <Link>Accueil</Link>
+            </NextLink>
+          </BreadcrumbItem>
 
-        {!items ? (
-          <>
-            <Box mt="4px" mx={4}>
-              <img src={separator} alt="" />
-            </Box>
-            {forPage ? <NextLink href={{ pathname: `/${forPage}` }} passHref><Link>{label}</Link></NextLink> : <div></div>}
-          </>
-        ) : (
-          <>
-            {items.map((item, index) => {
-              return (
-                <Box display="flex" key={index}>
-                  <Box mt="4px" mx={4}>
-                    <img src={separator} alt="" />
-                  </Box>
-                  <NextLink passHref href={{ pathname: `/${item.path}` }}><Link>{item.title}</Link></NextLink>
-                </Box>
-              );
-            })}
-          </>
-        )}
+          {!items ? (
+            <BreadcrumbItem isCurrentPage>
+              <NextLink href={{ pathname: `/${forPage}` }} passHref><BreadcrumbLink>{label}</BreadcrumbLink></NextLink>
+            </BreadcrumbItem>
+          ) : (
+            <BreadcrumbLinks items={items} />            
+          )}
+        </Breadcrumb>
       </Container>
     </Box>
   );
 };
 
-export default Breadcrumb;
+export default BreadcrumbComponent;
