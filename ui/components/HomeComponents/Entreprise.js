c4c839721529b9dc310159bcb0aa87e587d28d38
David Boureau <bdavidxyz@gmail.com>
2022-11-14 11:29:59 +0100

    simple grid

--- ui/components/HomeComponents/Entreprise.js
+++ ui/components/HomeComponents/Entreprise.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Flex, Image, SimpleGrid, Text, Container, Grid, GridItem, Divider} from '@chakra-ui/react'
+import { Box, Button, Flex, Image, SimpleGrid, Text, Container, Grid, GridItem, Divider, Show} from '@chakra-ui/react'
 
 import ConnectionActions from "./ConnectionActions";
 import votrebesoinImage from '../../public/images/home_pics/illu-votrebesoin.svg'
@@ -9,13 +9,8 @@ import parcoursupImage from '../../public/images/home_pics/parcoursup.svg'
 const Entreprise = () => {
   return (
     <Box as="section" p={3} mb={{ base: '2', md: '5' }} >
-      <div className="row">
-        <div className="col-12 col-md-6 d-none d-md-block">
-          <Image src={votrebesoinImage} alt="Votre besoin" />
-          <Image src={ujusImage} alt="1 jeune 1 solution" />
-          <Image src={parcoursupImage} alt="parcoursup" />
-        </div>
-        <div className="col-12 col-md-6 order-md-first">
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing='40px'>
+        <Box>
           <h1 className="c-homecomponent-title c-homecomponent-title__blue mb-3">Vous êtes une entreprise</h1>
           <h2 className="c-homecomponent-title__small mb-3 mb-lg-5">
             Diffusez simplement et gratuitement vos offres en alternance.
@@ -25,8 +20,15 @@ const Entreprise = () => {
             centres de formation pouvant vous accompagner.
           </div>
           <ConnectionActions service="entreprise" />
-        </div>
-      </div>
+        </Box>
+        <Show above='md'>
+          <Box>
+            <Image src={votrebesoinImage} alt="Votre besoin" />
+            <Image src={ujusImage} alt="1 jeune 1 solution" />
+            <Image src={parcoursupImage} alt="parcoursup" />
+          </Box>
+        </Show>
+      </SimpleGrid>
     </Box>
   );
 };
