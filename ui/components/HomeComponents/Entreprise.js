773805485867c7bfba0eaa335a1c5a304f641521
David Boureau <bdavidxyz@gmail.com>
2022-11-14 11:51:05 +0100

    feat: text as title corrected

--- ui/components/HomeComponents/Entreprise.js
+++ ui/components/HomeComponents/Entreprise.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Flex, Image, SimpleGrid, Text, Container, Grid, GridItem, Divider, Show} from '@chakra-ui/react'
+import { Box, Image, SimpleGrid, Text, Show} from '@chakra-ui/react'
 
 import ConnectionActions from "./ConnectionActions";
 import votrebesoinImage from '../../public/images/home_pics/illu-votrebesoin.svg'
@@ -11,18 +11,18 @@ const Entreprise = () => {
     <Box as="section" p={3} mb={{ base: '2', md: '5' }} >
       <SimpleGrid columns={{ sm: 1, md: 2 }} spacing='40px'>
         <Box>
-          <h1 className="c-homecomponent-title c-homecomponent-title__blue mb-3">Vous êtes une entreprise</h1>
+          <Text as="h1" fontSize={{ base: '24px', md: '32px' }} color={"#000091"} lineHeight={"40px"} fontWeight={"700"}>Vous êtes une entreprise</Text>
           <h2 className="c-homecomponent-title__small mb-3 mb-lg-5">
             Diffusez simplement et gratuitement vos offres en alternance.
           </h2>
-          <div>
+          <Box>
             Exprimez vos besoins en alternance afin d’être visible auprès des jeunes en recherche de contrat, et des
             centres de formation pouvant vous accompagner.
-          </div>
+          </Box>
           <ConnectionActions service="entreprise" />
         </Box>
-        <Show above='md'>
-          <Box>
+        <Show above="md">
+          <Box position={"relative"}>
             <Image src={votrebesoinImage} alt="Votre besoin" />
             <Image src={ujusImage} alt="1 jeune 1 solution" />
             <Image src={parcoursupImage} alt="parcoursup" />
