85a6c0631b19769dfb200f7c0e32c0c5dabbcd14
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-14 15:05:05 +0100

    Suppression imports rendus inutiles

--- ui/pages/recherche-apprentissage.js
+++ ui/pages/recherche-apprentissage.js
@@ -6,8 +6,6 @@ import { ScopeContextProvider } from "context/ScopeContext.js";
 import { NextSeo } from "next-seo";
 import { ParameterContext } from "../context/ParameterContextProvider";
 
-import Head from "next/head";
-
 const RechercheApprentissage = () => {
   const router = useRouter();
 
@@ -23,14 +21,6 @@ const RechercheApprentissage = () => {
         title="Recherche d'apprentissage | La bonne alternance | Trouvez votre alternance"
         description="Recherche d'apprentissage sur le site de La bonne alternance"
       />
-      <Head>
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
-          rel="stylesheet"
-          media="print"
-          onLoad="this.media='all'"
-        />
-      </Head>
       <ScopeContextProvider value={{ isJob: true, isTraining: true, path: "/recherche-apprentissage" }}>
         <SearchForTrainingsAndJobs />
       </ScopeContextProvider>
