8e3cc3562c7b52decac97a029d34213161ed4245
David Boureau <bdavidxyz@gmail.com>
2022-11-14 14:43:52 +0100

    feat: chakra for OffresGratuites

--- ui/components/HomeComponents/OffresGratuites.js
+++ ui/components/HomeComponents/OffresGratuites.js
@@ -1,19 +1,14 @@
 import React from "react";
 import ExternalLink from "../externalLink";
-import { Box, Text } from '@chakra-ui/react'
+import plateformesjeunesImage from '../../public/images/home_pics/illu-plateformesjeunes.svg'
+
+import { Box, Text, SimpleGrid, Image } from '@chakra-ui/react'
 
 const OffresGratuites = () => {
   return (
     <Box as="section" p={3} mb={{ base: '2', md: '5' }} >
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <img
-            className="c-homecomponent-illustration mr-3 my-3"
-            src="/images/home_pics/illu-plateformesjeunes.svg"
-            alt=""
-          />
-        </div>
-        <div className="col-12 col-md-6 pt-md-5 order-md-first">
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing='40px'>
+        <Box pl={{base: 5}}>
           <Text as="h2" variant="pageEntrepriseH2" mb={{ base: '3', lg: '5' }}>Vos offres sont diffusées gratuitement au plus près des candidats.</Text>
           <Box variant="pageEntrepriseText">
             Elles sont mises en ligne sur les sites les plus visités par les candidats en recherche d’alternance :{" "}
@@ -32,8 +27,11 @@ const OffresGratuites = () => {
             <ExternalLink className="c-homecomponent-link__inline" url="https://www.parcoursup.fr" title="Parcoursup" />{" "}
             et bien d’autres.
           </Box>
-        </div>
-      </div>
+        </Box>
+        <Box>
+          <Image src={plateformesjeunesImage} alt="Plateforme jeune" />
+        </Box>
+      </SimpleGrid>
     </Box>
   );
 };
