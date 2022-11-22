bd86bd96ab9ab0b421fedf73fa97cc62e1239f17
David Boureau <bdavidxyz@users.noreply.github.com>
2022-11-22 23:13:07 +0100

    feat: [lbac 414] home candidat chakra (#58)

--- ui/components/HomeComponents/AmeliorerLBA.js
+++ ui/components/HomeComponents/AmeliorerLBA.js
@@ -1,32 +1,40 @@
 import React from "react"
-import ExternalLink from "../externalLink"
+import { Box, Grid, GridItem, Link, Image, Text } from "@chakra-ui/react"
 
 const AmeliorerLBA = () => {
   return (
-    <section className="c-homecomponent__blue p-3 mb-5">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-support.svg" alt="" />
-        </div>
-        <div className="col-12 col-md-6 mb-5">
-          <div className="font-weight-bold mb-2">Donnez votre avis</div>
-          <h2 className="c-homecomponent-title__blue mb-4">Aidez-nous à améliorer La bonne alternance</h2>
-          La bonne alternance est un service en construction. Pour le faire évoluer, nous interrogeons régulièrement les utilisateurs du service.
-          <div className="font-weight-bold">
-            Nous vous invitions à participer à un échange en visio d’une trentaine de minutes avec un membre de notre équipe pour répondre à quelques questions et nous partager
-            votre avis.
-          </div>
-          <div className="mt-3">
-            <ExternalLink
-              className="c-homecomponent-link c-homecomponent-link__clear mt-3"
+    <Box as="section" p="8" mb="5" backgroundColor="#f5f5fe" borderRadius="10px">
+      <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }}>
+        <GridItem display="flex" justifyContent="center" alignItems="center">
+          <Image src="/images/home_pics/illu-support.svg" alt="" />
+        </GridItem>
+        <GridItem pl="8">
+          <Text fontWeight="700" my="5">Donnez votre avis</Text>
+          <Text as="h2" color="#000091" fontSize="2rem" fontWeight="500" my="5">Aidez-nous à améliorer La bonne alternance</Text>
+          <Text>
+            La bonne alternance est un service en construction. Pour le faire évoluer, nous interrogeons régulièrement les utilisateurs du service.
+          </Text>
+          <Text>
+            <strong>Nous vous invitions à participer à un échange en visio d’une trentaine de minutes avec un membre de notre équipe pour répondre à quelques questions et nous partager
+              votre avis.</strong>
+          </Text>
+          <Box mt="7">
+            <Link
+              href="https://calendly.com/rdv-labonnealternance/discussion-labonnealternance"
               aria-label="Planifier un échange avec l'équipe"
-              url="https://calendly.com/rdv-labonnealternance/discussion-labonnealternance"
               title="Je participe à l'étude"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+              color="#000091"
+              border="1px solid #000091"
+              padding="10px 24px"
+              isExternal
+            >Je participe à l'étude</Link>
+          </Box>
+          <Box mt="3">
+            &nbsp;
+          </Box>
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 export default AmeliorerLBA
--- ui/components/HowTo/HowTo.js
+++ ui/components/HowTo/HowTo.js
@@ -6,39 +6,46 @@ import howtoline1 from "../../public/images/howtoline1.svg"
 import howtoline2a from "../../public/images/howtoline2a.svg"
 import howtoline3a from "../../public/images/howtoline3a.svg"
 import howtoline3b from "../../public/images/howtoline3b.svg"
+import { Image, Text, GridItem, Grid, Container, Box, Show } from "@chakra-ui/react"
 
 const HowTo = () => {
   return (
     <>
-      <section className="c-howto container">
-        <div className="c-howto-cards card-deck">
-          <div className="card border-0 position-relative">
-            <img src={howto1} className="card-img-top" alt="" />
-            <img src={howtoline1} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line1" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">Le job de vos rêves</h5>
-              <p className="card-text">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</p>
-            </div>
-          </div>
-          <div className="card border-0  position-relative">
-            <img src={howto2} className="card-img-top" alt="" />
-            <img src={howtoline2a} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line2a" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">En un clin d’&oelig;il</h5>
-              <p className="card-text">Obtenez la liste des formations et entreprises proches de chez vous dans le domaine recherché.</p>
-            </div>
-          </div>
-          <div className="card border-0  position-relative">
-            <img src={howto3} className="card-img-top" alt="" />
-            <img src={howtoline3a} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3a" alt="" />
-            <img src={howtoline3b} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3b" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">Un contact facile</h5>
-              <p className="card-text">Contactez facilement les centres de formation ou les entreprises pour postuler </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Container variant="responsiveContainer">
+        <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap={{ base: "10", md: "24" }}>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto">
+            <Image src={howto1} alt="" zIndex="2" position="inherit"/>
+            <Show above="md">
+              <Image src={howtoline1} alt="" position="absolute" top="85px" left="-162px" />
+            </Show>
+            <Box>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">Le job de vos rêves</Text>
+              <Text as="p">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</Text>
+            </Box>
+          </GridItem>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto">
+            <Image src={howto2} alt="" zIndex="2" position="inherit" />
+            <Show above="md">
+              <Image src={howtoline2a} alt="" position="absolute" top="47px" left="-208px" />
+            </Show>
+            <Box>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">En un clin d’&oelig;il</Text>
+              <Text as="p">Obtenez la liste des formations et entreprises proches de chez vous dans le domaine recherché.</Text>
+            </Box>
+          </GridItem>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mb="12">
+            <Image src={howto3} alt="" zIndex="2" position="inherit" />
+            <Show above="md">
+              <Image src={howtoline3a} alt="" position="absolute" top="47px" left="-200px" />
+              <Image src={howtoline3b} alt="" position="absolute" top="47px" left="158px" />
+            </Show>
+            <Box>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">Un contact facile</Text>
+              <Text as="p">Contactez facilement les centres de formation ou les entreprises pour postuler </Text>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Container>
     </>
   )
 }
