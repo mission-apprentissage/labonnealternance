9e263fb9e9428d3cb0a0d5a2c3ee0a63aab277f3
David Boureau <bdavidxyz@users.noreply.github.com>
2022-11-28 17:48:23 +0100

    feat: 405 stats page (#81)

--- ui/pages/stats.js
+++ ui/pages/stats.js
@@ -4,41 +4,51 @@ import Navigation from "../components/navigation"
 import ScrollToTop from "../components/ScrollToTop"
 
 import Footer from "../components/footer"
+import { Box, Grid, GridItem, Text, Container, Divider, Link } from "@chakra-ui/react"
 
 const stats = () => (
-  <div>
+  <Box>
     <ScrollToTop />
     <Navigation />
     <Breadcrumb forPage="stats" label="Statistiques" />
 
-    <div className="c-page-container container my-0 mb-sm-5 p-5">
-      <div className="row">
-        <div className="col-12 col-md-5">
-          <h1>
-            <span className="d-block c-page-title is-color-2">Statistiques</span>
-          </h1>
-          <hr className="c-page-title-separator" align="left" />
-        </div>
-        <div className="col-12 col-md-7">
-          <h3>Statistiques</h3>
-          <p>
+    <Container p={12} my={0} mb={[0, 12]} variant="pageContainer">
+      <Grid templateColumns="repeat(12, 1fr)">
+        <GridItem px={4} colSpan={[12, 12, 12, 5]}>
+          <Box as="h1">
+            <Text as="span" display="block" mb={1} variant="editorialContentH1">
+              Statistiques
+            </Text>
+          </Box>
+          <Divider variant="pageTitleDivider" my={12} />
+        </GridItem>
+        <GridItem px={4} colSpan={[12, 12, 12, 7]}>
+          <Text as="h2" mb="2" variant="homeEditorialH2">
+            Statistiques
+          </Text>
+          <Text as="p" mb="3">
             La bonne alternance est une startup d’Etat incubée par beta.gouv. Nous développons un service à destination des publics selon les principes du{" "}
-            <a href="https://beta.gouv.fr/approche/manifeste" aira-label="Accès au site de Beta gouv">
+            <Link href="https://beta.gouv.fr/approche/manifeste" aria-label="Accès au site de Beta gouv" isExternal variant="editorialContentLink">
               Manifeste de beta.gouv
-            </a>
-          </p>
-          <p>
+            </Link>
+          </Text>
+          <Text as="p" mb="3">
             Nous mesurons l’impact de nos actions et publions en toute transparence nos statistiques que vous pouvez{" "}
-            <a href="https://datastudio.google.com/reporting/1v-Sim2qMlFSMn4n9JJWaMk8PIONvM757" aria-label="Accès aux statistiques du service">
+            <Link
+              href="https://datastudio.google.com/reporting/1v-Sim2qMlFSMn4n9JJWaMk8PIONvM757"
+              aria-label="Accès aux statistiques du service"
+              isExternal
+              variant="editorialContentLink"
+            >
               consulter ici
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div className="mb-3">&nbsp;</div>
+            </Link>
+          </Text>
+        </GridItem>
+      </Grid>
+    </Container>
+
     <Footer />
-  </div>
+  </Box>
 )
 
 export default stats
