f24d43bf53fca91cb30b6ad314fa1e0a304e3b99
David Boureau <bdavidxyz@users.noreply.github.com>
2022-11-29 11:10:53 +0100

    feat: footer (#78)

--- ui/components/footer.js
+++ ui/components/footer.js
@@ -1,134 +1,192 @@
-import Link from "next/link"
 import { useRouter } from "next/router"
 import React from "react"
-import { Col, Container, Row } from "reactstrap"
-import ExternalLink from "./externalLink"
+import { Box, Image, GridItem, Grid, Link, UnorderedList, ListItem, Flex, Divider } from "@chakra-ui/react"
 
-const Footer = (props) => {
+const Footer = () => {
   const router = useRouter()
 
   return (
-    <>
-      <nav className="c-footer c-footer--one py-4 border-bottom">
-        <Container>
-          <Row>
-            <Col className="col-12 col-lg-3">
-              <img src="/images/marianne.svg#svgView(viewBox(0 0 162 78))" alt="" width="290" height="130" />
-            </Col>
-            <Col className="col-12 col-lg-3">
-              <div className="c-footer-francerelance">
-                <img src="/images/france_relance.svg" alt="" width="81" height="81" />
-              </div>
-            </Col>
-            <Col className="col-12 col-lg-6 c-footer-text">
-              <div>La bonne alternance. Trouvez votre alternance.</div>
-              <div className="mt-4">La bonne alternance est proposée par les services suivants :</div>
-              <div className="mt-4 c-footer-official-links">
-                <ExternalLink className="c-footer-official-link" url="https://pole-emploi.fr" aria-label="Accès au site de Pôle emploi" title="pole-emploi.fr" />
-                <ExternalLink className="c-footer-official-link" url="https://gouvernement.fr" aria-label="Accès au site gouvernement.fr" title="gouvernement.fr" />
-                <ExternalLink className="c-footer-official-link" url="https://service-public.fr" aria-label="Accès au site service-public.fr" title="service-public.fr" />
-                <ExternalLink className="c-footer-official-link" url="https://data.gouv.fr" aria-label="Accès au site data.gouv" title="data.gouv.fr" />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </nav>
-      <nav className="c-footer pt-2 pb-5">
-        <Container>
-          <Row>
-            <Col className="col-12">
-              <ul className="c-footer-links">
-                <li className="c-footer-links__line">
-                  <Link href="/mentions-legales">
-                    <a className="c-footer-links__link c-footer-smallword pr-3" aria-label="Accès aux mentions légales">
-                      Mentions légales
-                    </a>
+    <Box as="footer" borderTop="1px solid" borderTopColor="info" pt={6}>
+      <Box maxWidth="1350px" mx="auto" pl={{ base: 0, lg: 6 }}>
+        <Box as="nav" pl={{ base: 8, lg: 0 }}>
+          <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }}>
+            <GridItem colSpan={{ base: 1, lg: 1 }}>
+              <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="290" height="130" />
+            </GridItem>
+            <GridItem colSpan={{ base: 1, lg: 1 }}>
+              <Box pl={{ base: 0, lg: 2 }} height="100%" display="flex" alignItems="center">
+                <Image src="/images/france_relance.svg" alt="" width="81" height="81" />
+              </Box>
+            </GridItem>
+            <GridItem colSpan={{ base: 1, lg: 2 }}>
+              <Box pl={{ base: 0, lg: 4 }} mt={{ base: 8, lg: 0 }}>
+                <Box fontSize="14px" color="grey.800">
+                  La bonne alternance. Trouvez votre alternance.
+                </Box>
+                <Box fontSize="14px" color="grey.800" mt="6">
+                  La bonne alternance est proposée par les services suivants :
+                </Box>
+                <Flex flexDirection={{ base: "column", lg: "row" }} mt="6">
+                  <Link href="https://pole-emploi.fr" aria-label="Accès au site de Pôle emploi" isExternal fontSize={14} fontWeight={700} color="grey.425" mr={4}>
+                    pole-emploi.fr
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/cgu">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès aux conditions générales d'utilisation">
-                      CGU
-                    </a>
+                  <Link href="https://gouvernement.fr" aria-label="Accès au site gouvernement.fr" isExternal fontSize={14} fontWeight={700} color="grey.425" mr={4}>
+                    gouvernement.fr
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/cookies">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la page Cookies">
-                      Cookies
-                    </a>
+                  <Link href="https://service-public.fr" aria-label="Accès au site service-public.fr" isExternal fontSize={14} fontWeight={700} color="grey.425" mr={4}>
+                    service-public.fr
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/stats">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès aux statistiques du service">
-                      Statistiques
-                    </a>
+                  <Link href="https://data.gouv.fr" aria-label="Accès au site data.gouv" isExternal fontSize={14} fontWeight={700} color="grey.425" mr={4}>
+                    data.gouv.fr
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/faq">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la foire aux questions">
-                      FAQ
-                    </a>
+                </Flex>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Box>
+      <Divider mt={6} mb={3}></Divider>
+      <Box maxWidth="1350px" mx="auto" pl={{ base: 0, lg: 6 }}>
+        <Box as="nav" pl={{ base: 8, lg: 0 }}>
+          <Grid>
+            <GridItem>
+              <UnorderedList listStyleType="none" color="grey.425" fontSize={12} marginInlineStart={0}>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/mentions-legales" aria-label="Accès aux mentions légales">
+                    Mentions légales
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/contact">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la page Contact">
-                      Contact
-                    </a>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/cgu" aria-label="Accès aux conditions générales d'utilisation">
+                    CGU
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/metiers">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la page Métiers">
-                      Métiers
-                    </a>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/cookies" aria-label="Accès à la page Cookies">
+                    Cookies
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/a-propos">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la page A propos">
-                      A propos
-                    </a>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/stats" aria-label="Accès aux statistiques du service">
+                    Statistiques
                   </Link>
-                </li>
-
-                <li className="c-footer-links__line">
-                  <Link href="/developpeurs">
-                    <a className="c-footer-links__link c-footer-smallword" aria-label="Accès à la page Développeurs">
-                      Développeurs
-                    </a>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/faq" aria-label="Accès à la foire aux questions">
+                    FAQ
                   </Link>
-                </li>
-                <li className="c-footer-links__line">
-                  <a className="c-footer-links__link c-footer-smallword">Accessibilité : non conforme</a>
-                </li>
-              </ul>
-            </Col>
-            <Col className="col-12">
-              <div className="c-footer-smallword c-footer-lastword">
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/contact" aria-label="Accès à la page Contact">
+                    Contact
+                  </Link>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/metiers" aria-label="Accès à la page Métiers">
+                    Métiers
+                  </Link>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/a-propos" aria-label="Accès à la page A propos">
+                    A propos
+                  </Link>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link href="/developpeurs" aria-label="Accès à la page Développeurs">
+                    Développeurs
+                  </Link>
+                </ListItem>
+                <ListItem
+                  mt={{ base: 3, lg: 0 }}
+                  display={{ base: "inherit", lg: "inline-block" }}
+                  borderRight={{ base: "none", lg: "1px solid" }}
+                  borderRightColor={{ base: "none", lg: "grey.300" }}
+                  paddingRight={{ base: 0, lg: 4 }}
+                  paddingLeft={{ base: 0, lg: 4 }}
+                >
+                  <Link>Accessibilité : non conforme</Link>
+                </ListItem>
+              </UnorderedList>
+            </GridItem>
+            <GridItem>
+              <Box color="grey.425" fontSize={12} mt={6} pb={12}>
                 Sauf mention contraire, tous les contenus de ce site sont sous licence{" "}
-                <ExternalLink
-                  url="https://www.etalab.gouv.fr/licence-version-2-0-de-la-licence-ouverte-suite-a-la-consultation-et-presentation-du-decret"
+                <Link
+                  href="https://www.etalab.gouv.fr/licence-version-2-0-de-la-licence-ouverte-suite-a-la-consultation-et-presentation-du-decret"
                   aria-label="Accès au site Etalab"
-                  title="etalab-2.0"
-                  withPic={<img className="ml-1" src="/images/square_link.svg" alt="Ouverture dans un nouvel onglet" />}
-                />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </nav>
-    </>
+                  textDecor="underline"
+                >
+                  etalab-2.0
+                  <Image src="/images/square_link.svg" alt="Ouverture dans un nouvel onglet" display="inline-block" pl="1" />
+                </Link>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
