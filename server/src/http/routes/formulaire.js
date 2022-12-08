bf9ef01abbaed4ef73105eb10592feaff645eed3
Kevin Barnoin <k.barnoin@gmail.com>
2022-12-08 18:40:00 +0100

    feat: fetch applications from db

--- server/src/http/routes/formulaire.js
+++ server/src/http/routes/formulaire.js
@@ -41,7 +41,9 @@ export default ({ formulaire, mailer, etablissementsRecruteur, application, user
       await Promise.all(
         result.offres.map(async (offre) => {
           let candidatures = await application.getApplication(offre._id)
-          offre.candidatures = candidatures.data.data.length > 0 ? candidatures.data.data.length : undefined
+          if (candidatures) {
+            offre.candidatures = candidatures.data.data.length > 0 ? candidatures.data.data.length : undefined
+          }
 
           return offre
         })
