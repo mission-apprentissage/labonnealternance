2c22a6c1b2abe2780e65c6aaa7f7a9282442a8dc
Kevin Barnoin <k.barnoin@gmail.com>
2022-12-12 15:21:30 +0100

    fix: applications fetching and API response

--- server/src/common/components/application.js
+++ server/src/common/components/application.js
@@ -1,5 +1,5 @@
 import { Application } from "../model/index.js"
 
 export default () => ({
-  getApplication: (offreId) => Application.findOne({ job_id: offreId }).lean(),
+  getApplication: (offreId) => Application.find({ job_id: offreId }).lean(),
 })
