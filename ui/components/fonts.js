430bd8edfb19fd6044208d350d2c6166235faf42
Alan Le Ruyet <alan.leruyet@free.fr>
2022-12-12 09:19:40 +0100

    fix: marianne font not used (bold still missing) (#108)

--- ui/components/fonts.js
+++ ui/components/fonts.js
@@ -2,11 +2,13 @@ import React from "react"
 
 const Fonts = () => {
   return (
+    /*
     <>
       <link rel="preload" href="/fonts/Marianne/Marianne-Regular.woff" as="font" type="font/woff" crossOrigin="anonymous"></link>
       <link rel="preload" href="/fonts/Marianne/Marianne-Medium.woff" as="font" type="font/woff" crossOrigin="anonymous"></link>
       <link rel="preload" href="/fonts/Marianne/Marianne-Bold.woff" as="font" type="font/woff" crossOrigin="anonymous"></link>
-    </>
+    </>*/
+    ""
   )
 }
 
