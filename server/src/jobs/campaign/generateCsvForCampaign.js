b86af868db313bc34955a0a726ee9d5c39f81270
Kevin Barnoin <k.barnoin@gmail.com>
2022-12-14 11:29:21 +0100

    feat: update script

--- server/src/jobs/campaign/generateCsvForCampaign.js
+++ server/src/jobs/campaign/generateCsvForCampaign.js
@@ -3,7 +3,7 @@ import { runScript } from "../scriptWrapper.js"
 import jeunes from "./jeunes.json" assert { type: "json" }
 
 runScript(async () => {
-  const first = jeunes.slice(0, 10)
+  const first = jeunes.slice(0, 20)
   const stat = {
     noMatch: 0,
     exactMatch: 0,
@@ -13,11 +13,29 @@ runScript(async () => {
   await Promise.all(
     jeunes.map(async (jeune) => {
       const formation = await ConvertedFormation_0.find({
-        etablissement_formateur_uai: jeune.uai_etablissement,
+        $or: [
+          {
+            etablissement_formateur_siret: jeune.siret_etablissement,
+          },
+          {
+            etablissement_gestionnaire_siret: jeune.siret_etablissement,
+          },
+        ],
+        $or: [
+          {
+            etablissement_formateur_uai: jeune.uai_etablissement,
+          },
+          {
+            etablissement_gestionnaire_uai: jeune.uai_etablissement,
+          },
+        ],
         cfd: jeune.formation_cfd,
+        tags: { $in: ["2022"] },
         published: true,
         catalogue_published: true,
       })
+
+      console.log({ jeune: jeune.formation_cfd, siret: jeune.siret_etablissement, catalogue: formation.length })
 
       switch (formation.length) {
         case 0:
