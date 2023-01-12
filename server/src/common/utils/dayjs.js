dae71de7cbcb583b59375cbde9f50c2d8b124046
Kevin Barnoin <k.barnoin@gmail.com>
2023-01-12 15:05:10 +0100

    fix: dayjs language

--- server/src/common/utils/dayjs.js
+++ server/src/common/utils/dayjs.js
@@ -1,14 +1,16 @@
 import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc.js"
-import timezone from "dayjs/plugin/timezone.js"
 import advancedFormat from "dayjs/plugin/advancedFormat.js"
-import duration from "dayjs/plugin/duration.js"
-import isSameOrAfter from "dayjs/plugin/isSameOrAfter.js"
-import isBetween from "dayjs/plugin/isBetween.js"
-import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js"
 import customParseFormat from "dayjs/plugin/customParseFormat.js"
+import duration from "dayjs/plugin/duration.js"
+import isBetween from "dayjs/plugin/isBetween.js"
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter.js"
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js"
 import isYesterday from "dayjs/plugin/isYesterday.js"
 import localizedFormat from "dayjs/plugin/localizedFormat.js"
+import timezone from "dayjs/plugin/timezone.js"
+import utc from "dayjs/plugin/utc.js"
+
+import "dayjs/locale/fr"
 
 dayjs.extend(utc)
 dayjs.extend(duration)
