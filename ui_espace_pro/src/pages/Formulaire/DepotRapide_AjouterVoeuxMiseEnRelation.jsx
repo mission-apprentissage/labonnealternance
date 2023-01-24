2d98462358775fc922a749783ad8b9f880c85cb8
Kevin Barnoin <k.barnoin@gmail.com>
2023-01-24 10:37:35 +0100

    fix: missing props passed to state

--- ui_espace_pro/src/pages/Formulaire/DepotRapide_AjouterVoeuxMiseEnRelation.jsx
+++ ui_espace_pro/src/pages/Formulaire/DepotRapide_AjouterVoeuxMiseEnRelation.jsx
@@ -55,7 +55,7 @@ const DepotRapide_AjouterVoeuxMiseEnRelation = () => {
 
     navigate("/creation/fin", {
       replace: true,
-      state: { offre, email, withDelegation: true, fromDashboard },
+      state: { offre, email, withDelegation: true, fromDashboard, userId },
     })
   }
 
