b2b6bedc0d788786d7f74606da4d363a949074cd
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-01-25 15:44:23 +0100

    feat: track delegations (#199)
    * fix(hotfix): skip proposal if no geocoordinate
    
    * fix: missing return
    
    * feat: soft running (#185)
    
    * Revert "feat: add unknown option to dropdown"
    
    This reverts commit e0f30b600b89ec15cdaf2580ad9bd95cac4c8ac1.
    
    * Revert "feat: update opco selection"
    
    This reverts commit ab881eaf0b4e0a44bb973faedb9437ec98baecba.
    
    * feat: track delegations
    
    * feat: changes after review
    
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>

--- server/src/jobs/rdv/premiumInviteOneShot.js
+++ server/src/jobs/rdv/premiumInviteOneShot.js
@@ -3,6 +3,7 @@ import { logger } from "../../common/logger.js"
 import { mailType } from "../../common/model/constants/etablissement.js"
 import { dayjs } from "../../common/utils/dayjs.js"
 import config from "../../config.js"
+import { isValidEmail } from "../../common/utils/isValidEmail.js"
 
 /**
  * @description Send a "Premium" reminder mail.
@@ -31,51 +32,60 @@ export const premiumInviteOneShot = async ({ etablissements, widgetParameters, m
   )
 
   for (const etablissement of etablissementWithParcoursup) {
-    const { messageId } = await mailer.sendEmail({
-      to: etablissement.email_decisionnaire,
-      subject: `Activez la prise de rendez-vous apprentissage sur Parcoursup`,
-      template: mailTemplate["mail-cfa-premium-invite-one-shot"],
-      data: {
-        replyTo: config.publicEmail,
-        url: config.publicUrl,
-        images: {
-          logo: `${config.publicUrlEspacePro}/assets/logo-lba.png?raw=true`,
-          logoParcoursup: `${config.publicUrlEspacePro}/assets/logo-parcoursup.png?raw=true`,
-          logoFooter: `${config.publicUrlEspacePro}/assets/logo-republique-francaise.png?raw=true`,
-          peopleLaptop: `${config.publicUrlEspacePro}/assets/people-laptop.png?raw=true`,
-          informationIcon: `${config.publicUrlEspacePro}/assets/icon-information-orange.png?raw=true`,
-        },
-        etablissement: {
-          name: etablissement.raison_sociale,
-          address: etablissement.adresse,
-          postalCode: etablissement.code_postal,
-          ville: etablissement.localite,
-          siret: etablissement.siret_formateur,
-          email: etablissement.email_decisionnaire,
-          linkToForm: `${config.publicUrlEspacePro}/form/premium/${etablissement._id}`,
-          optOutActivatedAtDate: dayjs(etablissement.opt_out_activated_at).format("DD/MM/YYYY"),
-          emailGestionnaire: etablissement.email_decisionnaire,
-        },
-        user: {
-          destinataireEmail: etablissement.email_decisionnaire,
-        },
-      },
-    })
+    try {
+      if (!isValidEmail) {
+        logger.info("Invalid email syntax.", { etablissement })
+        continue
+      }
 
-    await etablissements.updateOne(
-      { siret_formateur: etablissement.siret_formateur },
-      {
-        premium_invited_at: dayjs().toDate(),
-        $push: {
-          mailing: {
-            campaign: mailType.PREMIUM_INVITE_ONE_SHOT_2023,
-            status: null,
-            message_id: messageId,
-            email_sent_at: dayjs().toDate(),
+      const { messageId } = await mailer.sendEmail({
+        to: etablissement.email_decisionnaire,
+        subject: `Activez la prise de rendez-vous apprentissage sur Parcoursup`,
+        template: mailTemplate["mail-cfa-premium-invite-one-shot"],
+        data: {
+          replyTo: config.publicEmail,
+          url: config.publicUrl,
+          images: {
+            logo: `${config.publicUrlEspacePro}/assets/logo-lba.png?raw=true`,
+            logoParcoursup: `${config.publicUrlEspacePro}/assets/logo-parcoursup.png?raw=true`,
+            logoFooter: `${config.publicUrlEspacePro}/assets/logo-republique-francaise.png?raw=true`,
+            peopleLaptop: `${config.publicUrlEspacePro}/assets/people-laptop.png?raw=true`,
+            informationIcon: `${config.publicUrlEspacePro}/assets/icon-information-orange.png?raw=true`,
+          },
+          etablissement: {
+            name: etablissement.raison_sociale,
+            address: etablissement.adresse,
+            postalCode: etablissement.code_postal,
+            ville: etablissement.localite,
+            siret: etablissement.siret_formateur,
+            email: etablissement.email_decisionnaire,
+            linkToForm: `${config.publicUrlEspacePro}/form/premium/${etablissement._id}`,
+            optOutActivatedAtDate: dayjs(etablissement.opt_out_activated_at).format("DD/MM/YYYY"),
+            emailGestionnaire: etablissement.email_decisionnaire,
+          },
+          user: {
+            destinataireEmail: etablissement.email_decisionnaire,
           },
         },
-      }
-    )
+      })
+
+      await etablissements.updateOne(
+        { siret_formateur: etablissement.siret_formateur },
+        {
+          premium_invited_at: dayjs().toDate(),
+          $push: {
+            mailing: {
+              campaign: mailType.PREMIUM_INVITE_ONE_SHOT_2023,
+              status: null,
+              message_id: messageId,
+              email_sent_at: dayjs().toDate(),
+            },
+          },
+        }
+      )
+    } catch (error) {
+      logger.error(error)
+    }
   }
 
   logger.info("Cron #premiumInviteOneShot done.")
