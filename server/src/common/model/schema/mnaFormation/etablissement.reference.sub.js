41f2a2ff3cd1e2724c16f03b2e42e1f2ffeb9876
Kevin Barnoin <k.barnoin@gmail.com>
2023-02-01 20:15:20 +0100

    feat: update model

--- server/src/common/model/schema/mnaFormation/etablissement.reference.sub.js
+++ server/src/common/model/schema/mnaFormation/etablissement.reference.sub.js
@@ -1,35 +1,19 @@
 export const etablissementReferenceInfo = {
   etablissement_reference: {
     type: String,
-    description: "Etablissement reference  est soit formateur soit le gestionnaire",
-  },
-  etablissement_reference_type: {
-    type: String,
-    description: "Etablissement reference est un CFA ou un OF",
-  },
-  etablissement_reference_conventionne: {
-    type: String,
-    description: "Etablissement reference est conventionné ou pas",
-  },
-  etablissement_reference_declare_prefecture: {
-    type: String,
-    description: "Etablissement reference est déclaré en prefecture",
-  },
-  etablissement_reference_datadock: {
-    type: String,
-    description: "Etablissement reference est connu de datadock",
+    description: "Etablissement reference est soit formateur soit le gestionnaire",
   },
   etablissement_reference_published: {
     type: Boolean,
     description: "Etablissement reference est publié",
   },
-  etablissement_reference_catalogue_published: {
-    type: Boolean,
-    description: "Etablissement reference entre dans le catalogue",
-  },
-  rncp_etablissement_reference_habilite: {
+  etablissement_reference_habilite_rncp: {
     type: Boolean,
     description: "Etablissement reference est habilité RNCP ou pas",
+  },
+  etablissement_reference_certifie_qualite: {
+    type: Boolean,
+    description: "Etablissement reference est certifié Qualité",
   },
   etablissement_reference_date_creation: {
     type: Date,
