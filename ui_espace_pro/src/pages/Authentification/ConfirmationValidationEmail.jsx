d392d3aa69f310a186d28b0a3a690ce4d3d6847e
Kevin Barnoin <k.barnoin@gmail.com>
2023-02-01 09:41:33 +0100

    feat: add message upon user awaiting

--- ui_espace_pro/src/pages/Authentification/ConfirmationValidationEmail.jsx
+++ ui_espace_pro/src/pages/Authentification/ConfirmationValidationEmail.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, Spinner, Text, useBoolean, useToast } from "@chakra-ui/react"
+import { Box, Flex, Heading, Link, Spinner, Text, useBoolean } from "@chakra-ui/react"
 import { useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { validationCompte } from "../../api"
@@ -6,48 +6,44 @@ import { AUTHTYPE } from "../../common/contants"
 import useAuth from "../../common/hooks/useAuth"
 import { AuthentificationLayout } from "../../components"
 
-/**
- *
- * Validate email & redirect to admin
- */
+const EmailValide = () => (
+  <Box pt={["6w", "12w"]} px={["6", "8"]}>
+    <Heading fontSize="28px" as="h1">
+      Merci ! Votre adresse email est bien confirmée.
+    </Heading>
+    <Text fontSize="18px">Nos équipes se chargent à présent de valider votre compte. Vous serez notifié par email dès que ce sera fait. À bientôt sur La bonne alternance !</Text>
+    <Text fontSize="18px">Vous allez être redirigé automatiquement dans 5 secondes.</Text>
+  </Box>
+)
 
 const EmailInvalide = () => (
-  <>
-    <Box pt={["6w", "12w"]} px={["6", "8"]}>
-      <Heading fontSize={["32px", "40px"]} as="h1">
-        Mail invalide
-      </Heading>
-      <Text fontSize={["16px", "22px"]}>
-        La validation de votre email à échoué. Merci de{" "}
-        <Link pl={2} href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Authentification%20LBAR%20-%20Mail%20invalide" textDecoration="underline">
-          Contacter l'équipe La bonne alternance
-        </Link>
-      </Text>
-    </Box>
-  </>
+  <Box pt={["6w", "12w"]} px={["6", "8"]}>
+    <Heading fontSize="28px" as="h1">
+      Mail invalide
+    </Heading>
+    <Text fontSize="18px">
+      La validation de votre email à échoué. Merci de{" "}
+      <Link pl={2} href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Authentification%20LBAR%20-%20Mail%20invalide" textDecoration="underline">
+        Contacter l'équipe La bonne alternance
+      </Link>
+    </Text>
+  </Box>
 )
 
 export default (props) => {
   const [isValid, setIsValid] = useBoolean(true)
+  const [isAwaitingValidation, setIsAwaitingValidation] = useBoolean(false)
   const { id } = useParams()
   const navigate = useNavigate()
   const [auth, setAuth] = useAuth()
-  const toast = useToast()
 
   useEffect(() => {
     // get user from params coming from email link
     validationCompte({ id })
       .then(({ data }) => {
         if (data?.isUserAwaiting) {
-          toast({
-            title: "Merci d’avoir confirmé votre email !",
-            description: "Nos équipes vont maintenant pouvoir valider votre compte.",
-            position: "top-right",
-            status: "success",
-            duration: 8000,
-            isClosable: true,
-          })
-          window.location.replace("/")
+          setIsAwaitingValidation(true)
+          setTimeout(() => window.location.replace("/"), 5000)
         }
         if (data?.token) {
           setAuth(data?.token)
@@ -83,13 +79,18 @@ export default (props) => {
 
   return (
     <>
-      {isValid && (
+      {isValid && !isAwaitingValidation && (
         <Box>
           <Flex justify="center" align="center" h="100vh" direction="column">
             <Spinner thickness="4px" speed="0.5s" emptyColor="gray.200" color="bluefrance.500" size="xl" />
             <Text>Verification en cours...</Text>
           </Flex>
         </Box>
+      )}
+      {isAwaitingValidation && (
+        <AuthentificationLayout>
+          <EmailValide />
+        </AuthentificationLayout>
       )}
       {!isValid && (
         <AuthentificationLayout>
