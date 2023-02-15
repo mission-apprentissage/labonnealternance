68277f5a1ecf140e44f1acd7ee480aa593121edb
Kevin Barnoin <k.barnoin@gmail.com>
2023-02-15 20:51:28 +0100

    fix: temporary fix (build ok/dev ko)

--- server/src/common/mongodb.ts
+++ server/src/common/mongodb.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
-import { mongoosePagination, PaginationModel } from "mongoose-paginate-ts"
+import { mongoosePagination, Pagination } from "mongoose-paginate-ts"
 import config from "../config.js"
 
 export const mongooseInstance = mongoose
 export const { model, Schema } = mongoose
-const Pagination = PaginationModel
 export { mongoosePagination, Pagination }
+
 export const connectToMongo = (mongoUri = config.mongodb.uri, mongooseInst = null) => {
   return new Promise((resolve, reject) => {
     console.log(`MongoDB: Connection to ${mongoUri}`)
