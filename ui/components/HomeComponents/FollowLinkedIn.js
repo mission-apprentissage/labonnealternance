b8c38b683262142dabb16b29c81e451a23b475c1
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2023-02-16 16:21:50 +0100

    Update FollowLinkedIn.js (#241)

--- ui/components/HomeComponents/FollowLinkedIn.js
+++ ui/components/HomeComponents/FollowLinkedIn.js
@@ -8,7 +8,7 @@ const FollowLinkedIn = () => {
       <Grid as="section" py={6} bg="bluefrance.200" mb="24" templateColumns={{ base: "1fr", lg: "repeat(12, 1fr)" }} borderRadius={10}>
         <GridItem colSpan={{ base: 1, lg: 9 }} ml={{ base: 2, lg: 6 }}>
           <Text as="p" size="16px" fontWeight={700}>
-            La mission interministérielle pour l’apprentissage et les trajectoires professionnelles construit des services numériques qui facilitent les entrées en apprentissage.
+            La mission interministérielle pour l’apprentissage construit des services numériques qui facilitent les entrées en apprentissage.
           </Text>
           <Text fontWeight={700} color="bluefrance.500" fontSize={{ base: 28, md: 30, lg: 32 }} pt={2}>
             Rendez-vous sur LinkedIn pour suivre nos actualités
@@ -17,7 +17,7 @@ const FollowLinkedIn = () => {
         <GridItem mt="4" colSpan={{ base: 1, lg: 3 }} display="flex" justifyContent="center" alignItems="center">
           <Link
             href="https://www.linkedin.com/company/mission-apprentissage/posts/?feedView=all"
-            aria-label="Accès à la page Linkedin de la mission interministérielle pour l’apprentissage et les trajectoires professionnelles"
+            aria-label="Accès à la page Linkedin de la mission interministérielle pour l’apprentissage"
             bg="bluefrance.500"
             color="white"
             width={209}
