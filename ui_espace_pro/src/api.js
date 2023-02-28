6ab3c6385aa0224c29fdd367b719368800878352
Kevin Barnoin <k.barnoin@gmail.com>
2023-02-28 16:42:43 +0100

    feat: update api baseUrl

--- ui_espace_pro/src/api.js
+++ ui_espace_pro/src/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 
 const API = Axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
 })
 
 const errorHandler = (error) => {
