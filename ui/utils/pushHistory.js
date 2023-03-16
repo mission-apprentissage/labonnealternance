93acda2b9f3635168d31606a1652f26bf858b3d4
Alan Le Ruyet <alan.leruyet@free.fr>
2023-03-16 14:53:15 +0100

    feat: conservation du displayMap

--- ui/utils/pushHistory.js
+++ ui/utils/pushHistory.js
@@ -2,10 +2,10 @@ import { getItemQueryParameters } from "../utils/getItemId"
 import { getSearchQueryParameters } from "../utils/getSearchParameters"
 import { getCampaignParameters } from "../utils/campaignParameters"
 
-const pushHistory = ({ router, scopeContext, item, page, display, searchParameters, searchTimestamp, isReplace }) => {
+const pushHistory = ({ router, scopeContext, item, page, display, searchParameters, searchTimestamp, isReplace, displayMap }) => {
   let params = `${display ? `&display=${display}` : ""}${page ? `&page=${page}` : ""}${item ? `&${getItemQueryParameters(item)}` : ""}${
     searchParameters ? `&${getSearchQueryParameters(searchParameters)}` : ""
-  }${searchTimestamp ? `&s=${searchTimestamp}` : ""}${getCampaignParameters()}`
+  }${searchTimestamp ? `&s=${searchTimestamp}` : ""}${getCampaignParameters()}${displayMap===false?"&displayMap=false":""}`
 
   if (!isReplace) {
     router.push(`${scopeContext.path}${params ? `?${params}` : ""}`, undefined, {
