6bf14a80346c3df776a4e67423634bade881c23b
Alan Le Ruyet <alan.leruyet@free.fr>
2023-03-21 10:12:29 +0100

    feat: affichage systématique de tous les niveaux de filtrage par diplôme (#311)

--- ui/services/updateValuesFromJobAutoComplete.js
+++ ui/services/updateValuesFromJobAutoComplete.js
@@ -1,8 +1,6 @@
 import formikUpdateValue from "../services/formikUpdateValue"
-import updateDiplomaSelectionFromJobChange from "../services/updateDiplomaSelectionFromJobChange"
 
 // Mets à jours les valeurs de champs du formulaire Formik à partir de l'item sélectionné dans l'AutoCompleteField
-export default function updateValuesFromJobAutoComplete(item, setFieldValue, setDiplomas) {
+export default function updateValuesFromJobAutoComplete(item, setFieldValue) {
   formikUpdateValue(item, setFieldValue, "job")
-  updateDiplomaSelectionFromJobChange(item, setDiplomas)
 }
