210102e8bfcc13b9c23e4f9c0740d47e390ed7d5
Kevin Barnoin <k.barnoin@gmail.com>
2023-03-21 16:44:55 +0100

    fix: update out of scope contract duration (#313)

--- server/src/jobs/lba_recruteur/formulaire/misc/updateContractDurationToMonth.ts
+++ server/src/jobs/lba_recruteur/formulaire/misc/updateContractDurationToMonth.ts
@@ -3,16 +3,17 @@ import { Formulaire } from "../../../../common/model/index.js"
 import { asyncForEach } from "../../../../common/utils/asyncUtils.js"
 import { runScript } from "../../../scriptWrapper.js"
 
-const convertToMonth = (year) => year * 12
-
 runScript(async () => {
   logger.info("Start update contract duration job")
-  const formulaires = await Formulaire.find({})
+  const formulaires = await Formulaire.find({ "offres.duree_contrat": { $gt: 36 } })
 
   await asyncForEach(formulaires, async (form) => {
     if (!form.offres.length) return
+
     await asyncForEach(form.offres, async (offre) => {
-      offre.duree_contrat = convertToMonth(offre.duree_contrat ?? 1)
+      if (offre.duree_contrat > 36) {
+        offre.duree_contrat = 36
+      }
     })
     await form.save({ timestamps: false })
   })
