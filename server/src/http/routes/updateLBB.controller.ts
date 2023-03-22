5e6ab39283dd1c02fde574a75ff53f40b0712dc7
Kevin Barnoin <k.barnoin@gmail.com>
2023-03-22 17:00:25 +0100

    fix: merge conflict import

--- server/src/http/routes/updateLBB.controller.ts
+++ server/src/http/routes/updateLBB.controller.ts
@@ -1,12 +1,7 @@
 // @ts-nocheck
 import express from "express"
-<<<<<<<< HEAD:server/src/http/routes/updateLBB.controller.ts
-import { tryCatch } from "../middlewares/tryCatchMiddleware.js"
 import { updateContactInfo } from "../../service/lbb/updateContactInfo.js"
-========
-import { getDiplomasForJobsQuery } from "../../service/jobDiploma.js"
 import { tryCatch } from "../middlewares/tryCatchMiddleware.js"
->>>>>>>> 44bf7099 (refactor: shift to Typescript):server/src/http/routes/jobDiploma.ts
 
 export default function () {
   const router = express.Router()
