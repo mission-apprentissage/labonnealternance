794e9b2b619cb81020dbaa2624f314e28761b8a5
Alan Le Ruyet <alan.leruyet@free.fr>
2023-03-23 13:49:33 +0100

    fix: ajustement scroll vertical dans liste de résultats

--- ui/utils/mapTools.js
+++ ui/utils/mapTools.js
@@ -276,7 +276,7 @@ const onLayerClick = (e, layer, selectItemOnMap, unselectItem, unselectMapPopupI
     })
 
     unselectItem()
-    scrollToElementInContainer("resultList", getItemElement(item), 150, "smooth")
+    scrollToElementInContainer("resultList", getItemElement(item), 210, "smooth")
     setSelectedMarker(item)
   }
 }
