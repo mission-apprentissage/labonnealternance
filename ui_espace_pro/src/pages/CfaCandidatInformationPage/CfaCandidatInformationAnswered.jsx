19c684be18b10a1e79c0138003fc588f54a6dcaf
David Boureau <bdavidxyz@gmail.com>
2023-03-27 11:16:03 +0200

    fix: better margins

--- ui_espace_pro/src/pages/CfaCandidatInformationPage/CfaCandidatInformationAnswered.jsx
+++ ui_espace_pro/src/pages/CfaCandidatInformationPage/CfaCandidatInformationAnswered.jsx
@@ -13,13 +13,13 @@ export const CfaCandidatInformationAnswered = (props) => {
         <Text as="h2" fontWeight="700" color="#000091" fontSize="22px" lineHeight="36px">
           Votre réponse au candidat
         </Text>
-        <Text as="p" mt="8" fontWeight="700" color="#1E1E1E" fontSize="18px" lineHeight="28px">
+        <Text as="p" mt="7" fontWeight="700" color="#1E1E1E" fontSize="18px" lineHeight="28px">
           Votre réponse a été envoyée !
         </Text>
         <Text as="p" fontWeight="700" color="#666666" fontSize="16px" lineHeight="24px">
           Réponse envoyée le {getCurrentDate()} à {getCurrentHourMinute()}
         </Text>
-        <Text as="p" mt="8" fontWeight="400" color="#929292" fontSize="16px" lineHeight="24px">
+        <Text as="p" mt="5" fontWeight="400" color="#929292" fontSize="16px" lineHeight="24px">
           {props.msg}
         </Text>
       </Box>
