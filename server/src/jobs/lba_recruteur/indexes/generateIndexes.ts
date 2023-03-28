d6e5c2fce6384c539ec1bb5fe5d0cf39fcf1a5ff
Alan Le Ruyet <alan.leruyet@free.fr>
2023-03-28 17:43:05 +0200

    feat: generate all indexes

--- server/src/jobs/lba_recruteur/indexes/generateIndexes.ts
+++ server/src/jobs/lba_recruteur/indexes/generateIndexes.ts
@@ -1,7 +1,15 @@
-import { Formulaire } from "../../../common/model/index.js"
+import { BonnesBoites, DiplomesMetiers, DomainesMetiers, FormationCatalogue, Formulaire } from "../../../common/model/index.js"
 import { rebuildIndex } from "../../../common/utils/esUtils.js"
 
 export const generateIndexes = async () => {
   await Formulaire.syncIndexes()
   await rebuildIndex(Formulaire, { skipNotFound: true })
+  await FormationCatalogue.syncIndexes()
+  await rebuildIndex(FormationCatalogue, { skipNotFound: true })
+  await BonnesBoites.syncIndexes()
+  await rebuildIndex(BonnesBoites, { skipNotFound: true })
+  await DiplomesMetiers.syncIndexes()
+  await rebuildIndex(DiplomesMetiers, { skipNotFound: true })
+  await DomainesMetiers.syncIndexes()
+  await rebuildIndex(DomainesMetiers, { skipNotFound: true })
 }
