54bf02692b074ab06c92b38af9b249f30b23151a
David Boureau <bdavidxyz@gmail.com>
2023-03-28 20:42:39 +0200

    fix: indent code

--- ui_espace_pro/src/pages/Candidat/FormCreatePage.jsx
+++ ui_espace_pro/src/pages/Candidat/FormCreatePage.jsx
@@ -150,163 +150,161 @@ export const FormCreatePage = (props) => {
   }
 
   return (
-    <>
-      <FormLayoutComponent
-        headerText={
-          <>
-            Envoyer <br />
-            une demande de contact <br />
-            <Text textStyle="h2" as="span" color="grey.700">
-              au centre de formation
-            </Text>
-          </>
-        }
-        prefooter={
-          <>
-            <FormCreatePageFooter />
-          </>
-        }
-        bg="white"
-      >
-        {loading && <Spinner display="block" mx="auto" size="xl" mt="10rem" />}
-        {error && (
-          <Box mt="5rem" textAlign="center">
-            {error}
-          </Box>
-        )}
-        {data && (
-          <Box>
-            <Formik
-              initialValues={{
-                firstname: "",
-                lastname: "",
-                phone: "",
-                email: "",
-                motivations: "",
-              }}
-              validationSchema={Yup.object().shape({
-                firstname: Yup.string().required("Requis"),
-                lastname: Yup.string().required("Requis"),
-                phone: Yup.number().required("Requis"),
-                email: Yup.string().required("Requis"),
-                motivations: Yup.string(),
-              })}
-              onSubmit={sendNewRequest}
-            >
-              {({ status = {} }) => {
-                return (
-                  <Form>
-                    <ContactCfaComponent
-                      entrepriseRaisonSociale={data.etablissement_formateur_entreprise_raison_sociale}
-                      intitule={data.intitule_long}
-                      adresse={data.lieu_formation_adresse}
-                      codePostal={data.code_postal}
-                      ville={data.localite}
-                    />
-                    <Text textStyle="h6" color="info">
-                      Bonjour,
-                    </Text>
-                    <Text mt={7} pb={2}>
-                      Vous êtes
+    <FormLayoutComponent
+      headerText={
+        <>
+          Envoyer <br />
+          une demande de contact <br />
+          <Text textStyle="h2" as="span" color="grey.700">
+            au centre de formation
+          </Text>
+        </>
+      }
+      prefooter={
+        <>
+          <FormCreatePageFooter />
+        </>
+      }
+      bg="white"
+    >
+      {loading && <Spinner display="block" mx="auto" size="xl" mt="10rem" />}
+      {error && (
+        <Box mt="5rem" textAlign="center">
+          {error}
+        </Box>
+      )}
+      {data && (
+        <Box>
+          <Formik
+            initialValues={{
+              firstname: "",
+              lastname: "",
+              phone: "",
+              email: "",
+              motivations: "",
+            }}
+            validationSchema={Yup.object().shape({
+              firstname: Yup.string().required("Requis"),
+              lastname: Yup.string().required("Requis"),
+              phone: Yup.number().required("Requis"),
+              email: Yup.string().required("Requis"),
+              motivations: Yup.string(),
+            })}
+            onSubmit={sendNewRequest}
+          >
+            {({ status = {} }) => {
+              return (
+                <Form>
+                  <ContactCfaComponent
+                    entrepriseRaisonSociale={data.etablissement_formateur_entreprise_raison_sociale}
+                    intitule={data.intitule_long}
+                    adresse={data.lieu_formation_adresse}
+                    codePostal={data.code_postal}
+                    ville={data.localite}
+                  />
+                  <Text textStyle="h6" color="info">
+                    Bonjour,
+                  </Text>
+                  <Text mt={7} pb={2}>
+                    Vous êtes
+                    <Text color="redmarianne" as="span">
+                      *
+                    </Text>{" "}
+                    :
+                  </Text>
+                  <Field name="firstname">{({ field, meta }) => <Input placeholder="votre prénom" {...field} {...feedback(meta, "Prénom invalide")} />}</Field>
+                  <Field name="lastname">{({ field, meta }) => <Input mt={2} placeholder="votre nom" {...field} {...feedback(meta, "Nom invalide")} />}</Field>
+                  {data.intitule_long && (
+                    <Text mt={5}>
+                      Pour tout savoir de la formation{" "}
+                      <b>
+                        <u>{data.intitule_long.toUpperCase()}</u>
+                      </b>
+                      , laissez votre numéro et votre adresse email au centre de formation
                       <Text color="redmarianne" as="span">
                         *
                       </Text>{" "}
                       :
                     </Text>
-                    <Field name="firstname">{({ field, meta }) => <Input placeholder="votre prénom" {...field} {...feedback(meta, "Prénom invalide")} />}</Field>
-                    <Field name="lastname">{({ field, meta }) => <Input mt={2} placeholder="votre nom" {...field} {...feedback(meta, "Nom invalide")} />}</Field>
-                    {data.intitule_long && (
-                      <Text mt={5}>
-                        Pour tout savoir de la formation{" "}
-                        <b>
-                          <u>{data.intitule_long.toUpperCase()}</u>
-                        </b>
-                        , laissez votre numéro et votre adresse email au centre de formation
-                        <Text color="redmarianne" as="span">
-                          *
-                        </Text>{" "}
-                        :
+                  )}
+                  <Field name="phone" validate={validatePhone}>
+                    {({ field, meta }) => {
+                      return (
+                        <Box>
+                          <Input mt={2} type="tel" placeholder="votre numéro" {...field} />
+                          <Text color="red" mt={2} mb={3}>
+                            {errorPhone}
+                          </Text>
+                        </Box>
+                      )
+                    }}
+                  </Field>
+                  <Field name="email" validate={validateEmail}>
+                    {({ field, meta }) => {
+                      return <Input placeholder="votre adresse email" type="email" {...field} {...feedback(meta, "Adresse email invalide")} />
+                    }}
+                  </Field>
+                  <Text mt={5} pb={2}>
+                    Quel sujet souhaitez-vous aborder ?
+                  </Text>
+                  <Field name="motivations">
+                    {({ field, meta }) => {
+                      return <Input placeholder="période d’inscription, horaires, etc." {...field} {...feedback(meta, "Désolée, ce champs est nécessaire")} />
+                    }}
+                  </Field>
+                  <Flex mt={8} bg="#F6F6F6" py="9px" px="18px">
+                    <Box w="430px">
+                      <Text fontWeight="600">Souhaiteriez-vous recevoir des offres d’emploi en lien avec cette formation ?</Text>
+                    </Box>
+                    <Center w="150px" pl="20px">
+                      <Text
+                        as="span"
+                        pr="28px"
+                        onClick={() => setPlausibleFeedback(plausibleFeebackEnum.OUI)}
+                        fontWeight={plausibleFeedback === plausibleFeebackEnum.OUI ? "600" : "none"}
+                        sx={{ cursor: "pointer" }}
+                      >
+                        👍 Oui
                       </Text>
-                    )}
-                    <Field name="phone" validate={validatePhone}>
-                      {({ field, meta }) => {
-                        return (
-                          <Box>
-                            <Input mt={2} type="tel" placeholder="votre numéro" {...field} />
-                            <Text color="red" mt={2} mb={3}>
-                              {errorPhone}
-                            </Text>
-                          </Box>
-                        )
-                      }}
-                    </Field>
-                    <Field name="email" validate={validateEmail}>
-                      {({ field, meta }) => {
-                        return <Input placeholder="votre adresse email" type="email" {...field} {...feedback(meta, "Adresse email invalide")} />
-                      }}
-                    </Field>
-                    <Text mt={5} pb={2}>
-                      Quel sujet souhaitez-vous aborder ?
+                      <Text
+                        onClick={() => setPlausibleFeedback(plausibleFeebackEnum.NON)}
+                        fontWeight={plausibleFeedback === plausibleFeebackEnum.NON ? "600" : "none"}
+                        sx={{ cursor: "pointer" }}
+                      >
+                        👎 Non
+                      </Text>
+                    </Center>
+                  </Flex>
+                  <Button
+                    variant="unstyled"
+                    type={"submit"}
+                    loading={submitLoading}
+                    disabled={submitLoading}
+                    bg={"grey.750"}
+                    borderRadius="10px"
+                    color="#FFFFFF"
+                    w="269px"
+                    h="44px"
+                    fontWeight="700"
+                    display="block"
+                    mx={["auto", "0", "0", "0"]}
+                    mt="2rem"
+                    _hover=""
+                    textAlign="center"
+                  >
+                    Envoyer ma demande
+                  </Button>
+                  {status.error && (
+                    <Text color="#cd201f" textAlign="center" mt={8}>
+                      {status.error}
                     </Text>
-                    <Field name="motivations">
-                      {({ field, meta }) => {
-                        return <Input placeholder="période d’inscription, horaires, etc." {...field} {...feedback(meta, "Désolée, ce champs est nécessaire")} />
-                      }}
-                    </Field>
-                    <Flex mt={8} bg="#F6F6F6" py="9px" px="18px">
-                      <Box w="430px">
-                        <Text fontWeight="600">Souhaiteriez-vous recevoir des offres d’emploi en lien avec cette formation ?</Text>
-                      </Box>
-                      <Center w="150px" pl="20px">
-                        <Text
-                          as="span"
-                          pr="28px"
-                          onClick={() => setPlausibleFeedback(plausibleFeebackEnum.OUI)}
-                          fontWeight={plausibleFeedback === plausibleFeebackEnum.OUI ? "600" : "none"}
-                          sx={{ cursor: "pointer" }}
-                        >
-                          👍 Oui
-                        </Text>
-                        <Text
-                          onClick={() => setPlausibleFeedback(plausibleFeebackEnum.NON)}
-                          fontWeight={plausibleFeedback === plausibleFeebackEnum.NON ? "600" : "none"}
-                          sx={{ cursor: "pointer" }}
-                        >
-                          👎 Non
-                        </Text>
-                      </Center>
-                    </Flex>
-                    <Button
-                      variant="unstyled"
-                      type={"submit"}
-                      loading={submitLoading}
-                      disabled={submitLoading}
-                      bg={"grey.750"}
-                      borderRadius="10px"
-                      color="#FFFFFF"
-                      w="269px"
-                      h="44px"
-                      fontWeight="700"
-                      display="block"
-                      mx={["auto", "0", "0", "0"]}
-                      mt="2rem"
-                      _hover=""
-                      textAlign="center"
-                    >
-                      Envoyer ma demande
-                    </Button>
-                    {status.error && (
-                      <Text color="#cd201f" textAlign="center" mt={8}>
-                        {status.error}
-                      </Text>
-                    )}
-                  </Form>
-                )
-              }}
-            </Formik>
-          </Box>
-        )}
-      </FormLayoutComponent>
-    </>
+                  )}
+                </Form>
+              )
+            }}
+          </Formik>
+        </Box>
+      )}
+    </FormLayoutComponent>
   )
 }
