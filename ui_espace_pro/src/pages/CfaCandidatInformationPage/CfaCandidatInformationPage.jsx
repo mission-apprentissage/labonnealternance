b9d164fdef1bd55880cd52ea7689723733781428
David Boureau <bdavidxyz@gmail.com>
2023-04-03 15:17:20 +0200

    refactor: loading part

--- ui_espace_pro/src/pages/CfaCandidatInformationPage/CfaCandidatInformationPage.jsx
+++ ui_espace_pro/src/pages/CfaCandidatInformationPage/CfaCandidatInformationPage.jsx
@@ -63,70 +63,66 @@ export const CfaCandidatInformationPage = () => {
       }
     >
       {loading && <span>Chargement des données...</span>}
-      {data && (
-        <>
-          {data.user && (
-            <Box mt={10}>
-              <Text as="span" color="bluefrance" textStyle="h6">
-                Voici les coordonnées du candidat
+      {data?.user && (
+        <Box mt={10}>
+          <Text as="span" color="bluefrance" textStyle="h6">
+            Voici les coordonnées du candidat
+          </Text>
+          <Box mt={6} pb={8} borderBottom="solid 1px #D0C9C4">
+            <Text>
+              Nom :{" "}
+              <Text as="span">
+                <b>{data.user.lastname}</b>
               </Text>
-              <Box mt={6} pb={8} borderBottom="solid 1px #D0C9C4">
-                <Text>
-                  Nom :{" "}
-                  <Text as="span">
-                    <b>{data.user.lastname}</b>
-                  </Text>
-                </Text>
-                <Text>
-                  Prénom :{" "}
-                  <Text as="span">
-                    <b>{data.user.firstname}</b>
-                  </Text>
-                </Text>
-                <Text mt={4}>
-                  Numéro de téléphone :{" "}
-                  <Text as="span">
-                    <b>{data.user.phone.match(/.{1,2}/g).join(".")}</b>
-                  </Text>
-                </Text>
-                <Text>
-                  Email :{" "}
-                  <Text as="span">
-                    <b>{data.user.email}</b>
-                  </Text>
-                </Text>
-              </Box>
-              <Box mt={8} pb={8} borderBottom="solid 1px #D0C9C4">
-                Il ou elle souhaite aborder avec vous le(s) sujet(s) suivant :
-                <UnorderedList>
-                  <ListItem fontSize="16px" my="2" lineHeight="24px" fontWeight="700">
-                    Modalités d'inscription
-                  </ListItem>
-                  <ListItem fontSize="16px" mb="2" lineHeight="24px" fontWeight="700">
-                    Portes ouvertes
-                  </ListItem>
-                  <ListItem fontSize="16px" mb="2" lineHeight="24px" fontWeight="700">
-                    Autre sujet :
-                  </ListItem>
-                </UnorderedList>
-                <Text as="p" bg="#F6F6F6" color="#2A2A2A" fontSize="16px" lineHeight="24px" fontWeight="700" px="4" py="2">
-                  {data.appointment.motivations}
-                </Text>
-                <Text as="p" mt="2">
-                  à propos de la formation : <strong>{data.etablissement.intitule_long}</strong>
-                </Text>
-                <Text as="p" mt="1">
-                  dispensée par : <strong>{data.etablissement.etablissement_formateur_entreprise_raison_sociale}</strong>
-                </Text>
-              </Box>
-              {currentState === "initial" ? <CfaCandidatInformationForm formik={formik} setCurrentState={setCurrentState} /> : <></>}
-              {currentState === "sending" ? <div>Envoi en cours...</div> : <></>}
-              {currentState === "answered" ? <CfaCandidatInformationAnswered msg={formik.values.message} /> : <></>}
-              {currentState === "other" ? <CfaCandidatInformationOther /> : <></>}
-              {currentState === "unreachable" ? <CfaCandidatInformationUnreachable /> : <></>}
-            </Box>
-          )}
-        </>
+            </Text>
+            <Text>
+              Prénom :{" "}
+              <Text as="span">
+                <b>{data.user.firstname}</b>
+              </Text>
+            </Text>
+            <Text mt={4}>
+              Numéro de téléphone :{" "}
+              <Text as="span">
+                <b>{data.user.phone.match(/.{1,2}/g).join(".")}</b>
+              </Text>
+            </Text>
+            <Text>
+              Email :{" "}
+              <Text as="span">
+                <b>{data.user.email}</b>
+              </Text>
+            </Text>
+          </Box>
+          <Box mt={8} pb={8} borderBottom="solid 1px #D0C9C4">
+            Il ou elle souhaite aborder avec vous le(s) sujet(s) suivant :
+            <UnorderedList>
+              <ListItem fontSize="16px" my="2" lineHeight="24px" fontWeight="700">
+                Modalités d'inscription
+              </ListItem>
+              <ListItem fontSize="16px" mb="2" lineHeight="24px" fontWeight="700">
+                Portes ouvertes
+              </ListItem>
+              <ListItem fontSize="16px" mb="2" lineHeight="24px" fontWeight="700">
+                Autre sujet :
+              </ListItem>
+            </UnorderedList>
+            <Text as="p" bg="#F6F6F6" color="#2A2A2A" fontSize="16px" lineHeight="24px" fontWeight="700" px="4" py="2">
+              {data.appointment.motivations}
+            </Text>
+            <Text as="p" mt="2">
+              à propos de la formation : <strong>{data.etablissement.intitule_long}</strong>
+            </Text>
+            <Text as="p" mt="1">
+              dispensée par : <strong>{data.etablissement.etablissement_formateur_entreprise_raison_sociale}</strong>
+            </Text>
+          </Box>
+          {currentState === "initial" ? <CfaCandidatInformationForm formik={formik} setCurrentState={setCurrentState} /> : <></>}
+          {currentState === "sending" ? <div>Envoi en cours...</div> : <></>}
+          {currentState === "answered" ? <CfaCandidatInformationAnswered msg={formik.values.message} /> : <></>}
+          {currentState === "other" ? <CfaCandidatInformationOther /> : <></>}
+          {currentState === "unreachable" ? <CfaCandidatInformationUnreachable /> : <></>}
+        </Box>
       )}
     </FormLayoutComponent>
   )
