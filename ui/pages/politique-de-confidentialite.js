dbd04cce3b071e9d6acb6f6c6b2d777f3e1d31a2
Kevin Barnoin <k.barnoin@gmail.com>
2023-05-02 16:41:35 +0200

    feat: update page & revert chakraUI css (#380)

--- ui/pages/politique-de-confidentialite.js
+++ ui/pages/politique-de-confidentialite.js
@@ -1,14 +1,12 @@
-import { NextSeo } from "next-seo"
+import { Box, Container, Divider, SimpleGrid, Text } from "@chakra-ui/react"
 import React from "react"
+import { NextSeo } from "next-seo"
+import { NotionRenderer } from "react-notion-x"
+
 import Breadcrumb from "../components/breadcrumb"
 import Navigation from "../components/navigation"
 import ScrollToTop from "../components/ScrollToTop"
-import { NotionRenderer } from "react-notion-x"
-
 import Footer from "../components/footer"
-
-import { Box, Container, Divider, Grid, GridItem, Text } from "@chakra-ui/react"
-
 import { fetchNotionPage } from "../services/fetchNotionPage"
 
 export async function getStaticProps() {
@@ -28,14 +26,12 @@ const PolitiqueDeConfidentialite = ({ recordMap }) => {
         title="Politique de confidentialité | La bonne alternance | Trouvez votre alternance"
         description="Politique de confidentialité, traitement des données à caractère personnel sur le site de La bonne alternance."
       />
-
       <ScrollToTop />
       <Navigation />
       <Breadcrumb forPage="politique-de-confidentialite" label="Politique de confidentialité" />
-
       <Container p={12} my={0} mb={[0, 12]} variant="pageContainer">
-        <Grid templateColumns="repeat(12, 1fr)">
-          <GridItem px={4} colSpan={[12, 12, 12, 5]}>
+        <SimpleGrid columns={[1, 1, 1, 2]}>
+          <Box>
             <Box as="h1">
               <Text as="span" display="block" mb={1} variant="editorialContentH1" color="#2a2a2a">
                 Politique
@@ -45,20 +41,9 @@ const PolitiqueDeConfidentialite = ({ recordMap }) => {
               </Text>
             </Box>
             <Divider variant="pageTitleDivider" my={12} />
-          </GridItem>
-          <GridItem px={4} colSpan={[12, 12, 12, 7]}>
-            <Box>
-              <NotionRenderer
-                recordMap={recordMap}
-                fullPage={false}
-                darkMode={false}
-                disableHeader={true}
-                rootDomain={process.env.NEXT_PUBLIC_BASE_URL}
-                bodyClassName="notion-body"
-              />
-            </Box>
-          </GridItem>
-        </Grid>
+          </Box>
+          <NotionRenderer recordMap={recordMap} fullPage={false} darkMode={false} disableHeader={true} rootDomain={process.env.NEXT_PUBLIC_BASE_URL} className="disable-chakra" />
+        </SimpleGrid>
       </Container>
       <Footer />
     </Box>
