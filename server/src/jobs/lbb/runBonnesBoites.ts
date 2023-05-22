372082b58d1b7658bf4cf7446bcc049cdf96900d
Alan Le Ruyet <alan.leruyet@free.fr>
2023-05-22 16:37:29 +0200

    fix: useless and fix bad reference (#399)

--- server/src/jobs/lbb/runBonnesBoites.ts
+++
@@ -1,6 +0,0 @@
-import { runScript } from "../scriptWrapper.js"
-import updateBonnesBoites from "./updateBonnesBoites.js"
-
-runScript(async () => {
-  await updateBonnesBoites({ UseAlgoFile: true, ClearMongo: true, BuildIndex: true, UseSave: true })
-})
--- server/src/jobs/lbb/runOpco.ts
+++
@@ -1,6 +0,0 @@
-import { runScript } from "../scriptWrapper.js"
-import updateOpcoCompanies from "./updateOpcoCompanies.js"
-
-runScript(async () => {
-  await updateOpcoCompanies({ ClearMongo: false })
-})
