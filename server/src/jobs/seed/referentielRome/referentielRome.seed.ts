f857b0bb001bf52d8d3741d32f7cffbedeea8f8e
Kevin Barnoin <k.barnoin@gmail.com>
2023-06-18 15:11:20 +0200

    Feat: referentiel rome (#434)
    * feat: add packages
    
    * feat: seed job & file

---
+++ server/src/jobs/seed/referentielRome/referentielRome.seed.ts
@@ -0,0 +1,16 @@
+import { runScript } from "../../scriptWrapper.js"
+import * as xml2j from "xml2js"
+import { readFileSync } from "fs"
+import iconv from "iconv-lite"
+import __dirname from "../../../common/dirname.js"
+
+runScript(async () => {
+  const parser = new xml2j.Parser()
+
+  const filePath = `${__dirname(import.meta.url)}/unix_fiche_emploi_metier_v450_iso8859-15.xml`
+
+  const xmlBuffer = readFileSync(filePath)
+  const xml = iconv.decode(xmlBuffer, "iso8859-15")
+
+  const data = await parser.parseStringPromise(xml)
+})
