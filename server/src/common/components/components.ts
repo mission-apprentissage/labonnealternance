3c91c7d18f0da916788d6edfec74f51f5f47aec0
Alan Le Ruyet <alan.leruyet@free.fr>
2023-06-20 09:04:38 +0200

    fix: reference userRecruteurs in components (#448)

--- server/src/common/components/components.ts
+++ server/src/common/components/components.ts
@@ -3,17 +3,14 @@ import createMailer from "../mailer.js"
 import { connectToMongo } from "../mongodb.js"
 import createEtablissements from "./etablissement.js"
 import createEtablissementRecruteur from "./etablissementRecruteur.js"
-import createUserRecruteur from "./usersRecruteur.js"
 
 export default async function (options = {}) {
   const etablissements = await createEtablissements()
-  const usersRecruteur = await createUserRecruteur()
   const etablissementsRecruteur = await createEtablissementRecruteur()
 
   return {
     db: options.db || (await connectToMongo()).db,
     mailer: options.mailer || createMailer(),
-    usersRecruteur,
     etablissements,
     etablissementsRecruteur,
   }
