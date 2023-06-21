71975f26f828e74f13d335e6174e0f614d25f35a
Kevin Barnoin <k.barnoin@gmail.com>
2023-06-21 22:13:10 +0200

    feat: test

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,3 +11,4 @@ process.on("uncaughtException", (e) => logger.error(e, "An unexpected error occu
   const http = await server(components)
   http.listen(5000, () => logger.info(`Server ready and listening on port ${5000}`))
 })()
+//
