ad557f41742e5956748ba0a4014b366b669cf045
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-06-21 20:20:55 +0200

    feat: jsdoc updated

--- server/src/services/company.service.ts
+++ server/src/services/company.service.ts
@@ -1,5 +1,4 @@
-import { IBonneBoite } from "../common/model/schema/bonneboite/bonneboite.types.js";
-import _ from "lodash-es"
+import { IBonneBoite } from "../common/model/schema/bonneboite/bonneboite.types.js"
 import { BonnesBoites } from "../common/model/index.js"
 import { sentryCaptureException } from "../common/utils/sentryUtils.js"
 
@@ -9,9 +8,9 @@ import { sentryCaptureException } from "../common/utils/sentryUtils.js"
  * @param {string} siret
  * @param {string} email
  * @param {string} phone
- * @returns {Promise<IBonneBoite | string>}
+ * @returns {Promise<IBonneBoite | string>}
  */
-export const updateContactInfo = async ({ siret, email, phone }: { siret: string; email: string; phone: string }): Promise<IBonneBoite | string> => {
+export const updateContactInfo = async ({ siret, email, phone }: { siret: string; email: string; phone: string }): Promise<IBonneBoite | string> => {
   try {
     const bonneBoite = await BonnesBoites.findOne({ siret })
 
