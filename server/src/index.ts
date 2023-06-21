98c10e9b1e822654e5950640ad2545a4dc5f18ed
Kevin Barnoin <k.barnoin@gmail.com>
2023-06-21 22:21:41 +0200

    feat: add bug

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import "dotenv/config"
 import server from "./http/server.js"
-import { logger } from "./common/logger.js"
 import createComponents from "./common/components/components.js"
 
 process.on("unhandledRejection", (e) => logger.error(e, "An unexpected error occurred"))
@@ -11,4 +10,3 @@ process.on("uncaughtException", (e) => logger.error(e, "An unexpected error occu
   const http = await server(components)
   http.listen(5000, () => logger.info(`Server ready and listening on port ${5000}`))
 })()
-//
