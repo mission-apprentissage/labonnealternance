bc68e899a75d90e4e65e1721dd6564404cb37e3f
Alan Le Ruyet <alan.leruyet@free.fr>
2023-06-22 18:22:51 +0200

    feat: liste des éléments pour naviguer par swipe / bouton next

--- ui/components/ItemDetail/ItemDetailServices/getCurrentList.js
+++ ui/components/ItemDetail/ItemDetailServices/getCurrentList.js
@@ -6,15 +6,22 @@ export default function getCurrentList({ store, activeFilter, extendedSearch }) 
   let picked = pick(store, ["trainings", "jobs"])
   let trainingsArray = amongst(activeFilter, ["all", "trainings"]) ? get(picked, "trainings", []) : []
 
+  let partnerList = []
   let jobList = []
   let companyList = []
+
+  if (amongst(activeFilter, ["all", "duo"])) {
+    partnerList = jobs.matchas.length ? jobs.matchas.filter((job) => job.company?.mandataire) : []
+  }
+
   if (amongst(activeFilter, ["all", "jobs"])) {
-    if (extendedSearch) jobList = mergeOpportunities({ jobs: get(picked, "jobs") })
-    else {
+    if (extendedSearch) {
+      jobList = mergeOpportunities({ jobs: get(picked, "jobs") })
+    } else {
       jobList = mergeJobs(get(picked, "jobs"))
       companyList = mergeOpportunities({ jobs: get(picked, "jobs"), onlyLbbLbaCompanies: "onlyLbbLba" })
     }
   }
-  let fullList = concat([], trainingsArray, jobList, companyList)
+  let fullList = concat([], trainingsArray, jobList, companyList, partnerList)
   return fullList.filter((el) => !!el)
 }
