269f4a3276fb9e2effc37ccfa67e0965a22addf8
Kevin Barnoin <k.barnoin@gmail.com>
2023-06-26 12:46:39 +0200

    feat: add cleaning script

---
+++ server/src/jobs/lba_recruteur/user/misc/removeUsedFalseStatus.ts
@@ -0,0 +1,22 @@
+// @ts-nocheck
+import { UserRecruteur } from "../../../../common/model/index.js"
+import { asyncForEach } from "../../../../common/utils/asyncUtils.js"
+import { getUserValidationState } from "../../../../services/userRecruteur.service.js"
+import { runScript } from "../../../scriptWrapper.js"
+
+runScript(async () => {
+  const users = await UserRecruteur.find({ "status.status": null, type: "ENTREPRISE" })
+
+  let stat = { updated: 0, total: users.length }
+
+  await asyncForEach(users, async (user) => {
+    const lastState = user.status.pop()
+
+    if (!lastState.status) {
+      stat.updated++
+      user.save()
+    }
+  })
+
+  return { stat }
+})
