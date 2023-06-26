88ee9d225a638867a9c12a6fbf58db0d89192f24
Alan Le Ruyet <alan.leruyet@free.fr>
2023-06-26 13:55:15 +0200

    feat: mise à jour de l'api inserjeune (#453)

--- ui/services/fetchInserJeuneStats.js
+++ ui/services/fetchInserJeuneStats.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 import _ from "lodash"
-import { baseUrl } from "../config/config"
+import { baseUrl, inserJeuneApiUrl } from "../config/config"
 import { logError } from "../utils/tools"
 
 export default async function fetchInserJeuneStats(training, _baseUrl = baseUrl, _axios = axios, _window = window, _logError = logError) {
   let res = null
 
-  if (!training) return res
-
-  const ijApi = `https://trajectoires-pro.apprentissage.beta.gouv.fr/api/inserjeunes/regionales/${training.place.zipCode}/certifications/${training.cfd}`
-  const response = await _axios.get(ijApi)
+  if (!training) {
+    return res
+  }
+  const response = await _axios.get(`${inserJeuneApiUrl}/api/inserjeunes/regionales/${training.place.zipCode}/certifications/${training.cfd}`)
 
   const isAxiosError = !!_.get(response, "data.error")
 
