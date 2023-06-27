8208697261d2bf82848b86f0acfa6b8df58b6f3f
Kevin Barnoin <k.barnoin@gmail.com>
2023-06-27 10:36:01 +0200

    feat: merge jobs route  (#467)
    * feat: update global path - get path & security
    
    * fix: unrecognized space character
    
    * feat: get company
    
    * fix: unused
    
    * feat: fix typing in calling function
    
    * feat: get matcha  by id ok
    
    * feat: pe job by id ok
    
    * feat: ok in jobcontroller
    
    * feat: romes obligatoires et nettoyage
    
    * fix: add missing import
    
    * fix: removed bugs
    
    ---------
    
    Co-authored-by: Alan Le Ruyet <alan.leruyet@free.fr>

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -32,9 +32,6 @@ import formationRegionV1 from "./routes/formationRegionV1.controller.js"
 import formationV1 from "./routes/formationV1.controller.js"
 import formulaireRoute from "./routes/formulaire.controller.js"
 import jobEtFormationV1 from "./routes/jobEtFormationV1.controller.js"
-import jobV1 from "./routes/jobV1.controller.js"
-import metiersDAvenir from "./routes/metiersDAvenir.controller.js"
-import metiers from "./routes/metiers.controller.js"
 import optoutRoute from "./routes/optout.controller.js"
 import partnersRoute from "./routes/partners.controller.js"
 import rome from "./controllers/metiers/rome.controller.js"
@@ -172,6 +169,7 @@ export default async (components) => {
    * rate limiter sur les routes routes
    */
   app.use("/api/v1/metiers", limiter20PerSecond)
+  app.use("/api/v1/jobs", limiter5PerSecond)
   //app.use("/api/romelabels", limiter10PerSecond)
 
   RegisterRoutes(app)
@@ -183,7 +181,6 @@ export default async (components) => {
   app.use("/api/v1/formations", limiter7PerSecond, formationV1())
   app.use("/api/romelabels", limiter10PerSecond, rome())
   app.use("/api/v1/formationsParRegion", limiter5PerSecond, formationRegionV1())
-  app.use("/api/v1/jobs", limiter5PerSecond, jobV1())
   app.use("/api/v1/jobsEtFormations", limiter5PerSecond, jobEtFormationV1())
   app.use("/api/updateLBB", limiter1Per20Second, updateLBB())
   app.use("/api/mail", limiter1Per20Second, sendMail(components))
