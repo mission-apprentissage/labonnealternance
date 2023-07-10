aa37da620f7f85f30c1061b376d3278434c139c9
Kevin Barnoin <k.barnoin@gmail.com>
2023-07-10 18:04:08 +0200

    feat: disable mapbox for every test

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,8 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands";
+import "./commands"
 
 beforeEach(() => {
-  //
-});
+  cy.intercept("https://api.mapbox.com/**", (req) => {
+    req.reply({}) // Empty response
+  })
+})
