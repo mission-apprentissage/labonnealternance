3a726c8aae87654281ab6d463ccffbada225fa13
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2023-07-13 18:22:49 +0200

    Fix: Homogénéisation objets emails transactionnels

--- server/src/services/application.service.ts
+++ server/src/services/application.service.ts
@@ -543,7 +543,7 @@ export const sendNotificationToApplicant = async ({
     case "entretien": {
       mailer.sendEmail({
         to: application.applicant_email,
-        subject: `Réponse positive à votre candidature chez ${application.company_name}`,
+        subject: `Réponse positive de ${application.company_name}`,
         template: getEmailTemplate("mail-candidat-entretien"),
         data: { ...application.toObject(), ...images, email, phone, comment },
       })
@@ -552,7 +552,7 @@ export const sendNotificationToApplicant = async ({
     case "ne_sais_pas": {
       mailer.sendEmail({
         to: application.applicant_email,
-        subject: `Réponse à votre candidature chez ${application.company_name}`,
+        subject: `Réponse de ${application.company_name}`,
         template: getEmailTemplate("mail-candidat-nsp"),
         data: { ...application.toObject(), ...images, email, phone, comment },
       })
@@ -561,7 +561,7 @@ export const sendNotificationToApplicant = async ({
     case "refus": {
       mailer.sendEmail({
         to: application.applicant_email,
-        subject: `Réponse à votre candidature chez ${application.company_name}`,
+        subject: `Réponse négative de ${application.company_name}`,
         template: getEmailTemplate("mail-candidat-refus"),
         data: { ...application.toObject(), ...images, comment },
       })
@@ -634,7 +634,7 @@ export const updateApplicationStatus = async ({ payload }: { payload: any }): Pr
 const notifyHardbounceToApplicant = async ({ application }: { application: IApplication & Document<any, any, IApplication> }): Promise<void> => {
   mailer.sendEmail({
     to: application.applicant_email,
-    subject: `${application.company_name} n'a pas reçu votre candidature sur La bonne alternance`,
+    subject: `Votre candidature n'a pas pu être envoyée à ${application.company_name}`,
     template: getEmailTemplate("mail-candidat-hardbounce"),
     data: { ...application.toObject(), ...images },
   })
@@ -648,7 +648,7 @@ const notifyHardbounceToApplicant = async ({ application }: { application: IAppl
 const warnMatchaTeamAboutBouncedEmail = async ({ application }: { application: IApplication & Document<any, any, IApplication> }): Promise<void> => {
   mailer.sendEmail({
     to: config.transactionalEmail,
-    subject: `${application.company_name} n'a pas reçu votre candidature sur La bonne alternance`,
+    subject: `Votre candidature n'a pas pu être envoyée à ${application.company_name}`,
     template: getEmailTemplate("mail-matcha-hardbounce"),
     data: { ...application.toObject(), ...images },
   })
