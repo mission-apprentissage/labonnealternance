3b410771c6a8c2720ce02b1fc9f4068b3ba026fd
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-07-26 10:54:18 +0200

    feat: schema type created

---
+++ server/src/common/model/schema/referentielOnisep/referentielOnisep.types.ts
@@ -0,0 +1,10 @@
+interface IReferentielOnisep {
+  id_formation_catalogue: string
+  source_ideo2: string
+  id_formation_ideo2: string
+  id_action_ideo2: string
+  id_enseignement_ideo2: string
+  cle_ministere_educatif: string
+}
+
+export { IReferentielOnisep }
