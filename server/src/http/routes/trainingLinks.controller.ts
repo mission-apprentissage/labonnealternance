b18e98fda586a80fd29773a3755eef92224b4573
Kevin Barnoin <k.barnoin@gmail.com>
2023-08-16 17:51:32 +0200

    feat: update awaited api items

--- server/src/http/routes/trainingLinks.controller.ts
+++ server/src/http/routes/trainingLinks.controller.ts
@@ -17,10 +17,14 @@ export default () => {
             id: Joi.string().required(),
             cle_ministere_educatif: Joi.string().allow(""),
             mef: Joi.string().allow(""),
-            cfd: Joi.string().allow(""),
-            rncp: Joi.string().allow(""),
+            cfd: Joi.string().when("rncp", { not: Joi.exist(), then: Joi.string().required() }),
+            rncp: Joi.string().when("cfd", { not: Joi.exist(), then: Joi.string().required() }),
             code_postal: Joi.string().allow(""),
             uai: Joi.string().allow(""),
+            uai_formateur: Joi.string().when("uai_lieu_formation.uai_formateur_responsable", { not: Joi.exist(), then: Joi.string().required() }),
+            uai_formateur_responsable: Joi.string().when("uai_formateur.uai_lieu_formation", { not: Joi.exist(), then: Joi.string().required() }),
+            uai_lieu_formation: Joi.string().when("uai_formateur.uai_formateur_responsable", { not: Joi.exist(), then: Joi.string().required() }),
+            code_insee: Joi.string().allow(""),
           })
         )
         .max(100)
