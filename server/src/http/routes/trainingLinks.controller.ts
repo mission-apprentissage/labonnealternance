6cc2c30c40c806a22a8ffef4da72d2ec45e33216
Kevin Barnoin <k.barnoin@gmail.com>
2023-08-17 14:36:33 +0200

    feat: update input validator

--- server/src/http/routes/trainingLinks.controller.ts
+++ server/src/http/routes/trainingLinks.controller.ts
@@ -17,22 +17,22 @@ export default () => {
             id: Joi.string().required(),
             cle_ministere_educatif: Joi.string().allow(""),
             mef: Joi.string().allow(""),
-            cfd: Joi.string().when("rncp", { not: Joi.exist(), then: Joi.string().required() }),
-            rncp: Joi.string().when("cfd", { not: Joi.exist(), then: Joi.string().required() }),
+            cfd: Joi.string(),
+            rncp: Joi.when("cfd", { not: Joi.exist(), then: Joi.string().required() }),
             code_postal: Joi.string().allow(""),
             uai: Joi.string().allow(""),
-            uai_formateur: Joi.string().when("uai_lieu_formation.uai_formateur_responsable", { not: Joi.exist(), then: Joi.string().required() }),
-            uai_formateur_responsable: Joi.string().when("uai_formateur.uai_lieu_formation", { not: Joi.exist(), then: Joi.string().required() }),
-            uai_lieu_formation: Joi.string().when("uai_formateur.uai_formateur_responsable", { not: Joi.exist(), then: Joi.string().required() }),
+            uai_lieu_formation: Joi.string().allow(""),
+            uai_formateur: Joi.string().allow(""),
+            uai_formateur_responsable: Joi.string().allow(""),
             code_insee: Joi.string().allow(""),
-          })
+          }).or("uai_lieu_formation", "uai_formateur", "uai_formateur_responsable")
         )
         .max(100)
         .messages({
           "array.base": "body must be an Array",
           "array.max": "maximum 100 trainings",
         })
-        .validateAsync(params)
+        .validateAsync(params, { abortEarly: false })
 
       const results = await getTrainingLinks(params)
 
