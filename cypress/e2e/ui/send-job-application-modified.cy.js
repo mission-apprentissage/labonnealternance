a4e864ece5eac2bcf48166b304652b7888b083c4
Alan Le Ruyet <alan.leruyet@free.fr>
2023-08-17 17:44:55 +0200

    feat: test ok

--- cypress/e2e/ui/send-job-application-modified.cy.js
+++ cypress/e2e/ui/send-job-application-modified.cy.js
@@ -6,6 +6,7 @@ describe("send-job-application", () => {
     })
 
     cy.intercept("GET", Cypress.env('host')+"/api/v1/jobs?*").as("submitJobCall");
+    cy.intercept("POST", Cypress.env('host')+"/api/application").as("submitApplication");
 
     cy.generateRandomEmail("test-auto-","@nexistepas.fr",10).then((randomEmail) => {
       cy.viewport(1254, 704);
@@ -32,9 +33,13 @@ describe("send-job-application", () => {
         cy.get("[data-testid='phone']").click();
         cy.get("[data-testid='phone']").type("0700000000");
         cy.get("[data-testid='message']").click();
-        cy.get("[data-testid='message']").type("Madame, Monsieur,\nEtant actuellement à la recherche d’un emploi, je me permets de vous proposer ma candidature au poste de (emploi).\n\nEn effet, mon profil correspond à la description recherchée sur l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat possède peu d’expérience professionnelle) Ma formation en (préciser la formation) m'a permis d'acquérir de nombreuses compétences parmi celles que vous recherchez. Je possède tous les atouts qui me permettront de réussir dans le rôle que vous voudrez bien me confier. Motivation, rigueur et écoute sont les maîtres mots de mon comportement professionnel.(Si le candidat possède une expérience significative dans le poste à pourvoir). Mon expérience en tant que (emploi) m’a permis d’acquérir toutes les connaissances nécessaires à la bonne exécution des tâches du poste à pourvoir. Régulièrement confronté aux aléas du métier, je suis capable de répondre aux imprévus en toute autonomie. Intégrer votre entreprise représente pour moi un réel enjeu d’avenir dans lequel mon travail et mon honnêteté pourront s’exprimer pleinement.\n\nRestant à votre disposition pour toute information complémentaire, je suis disponible pour vous rencontrer lors d’un entretien à votre convenance\n\nVeuillez agréer, Madame, Monsieur, l’expression de mes sincères salutations.");
+        cy.get("[data-testid='message']").type("Madame, Monsieur,\nEtant actuellement à la recherche d’un emploi, ...");
         cy.get("[data-testid='fileDropzone']").selectFile("cypress/fixtures/CV - John Doe.docx", { action: "drag-drop" });
         cy.get("[data-testid='candidature-not-sent']").click();
+        
+        cy.wait("@submitApplication").then((applicationResultData) => {
+          cy.get("[data-testid='CandidatureSpontaneeWorked']");          
+        });
       });
       
       
