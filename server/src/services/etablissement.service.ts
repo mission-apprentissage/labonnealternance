65de1962f74bed3ca70b4497c1dafd1f72b9496b
Rémy Auricoste <remy.auricoste@gmail.com>
2023-09-01 11:24:38 +0200

    fix: api siren: fix fallback status 404 et 422

--- server/src/services/etablissement.service.ts
+++ server/src/services/etablissement.service.ts
@@ -246,7 +246,7 @@ export const getEtablissementFromGouv = async (siret: string): Promise<IAPIEtabl
     })
     return data
   } catch (error) {
-    if (error?.response?.status === "404" || error?.response?.status === "422") {
+    if (error?.response?.status === 404 || error?.response?.status === 422) {
       return null
     }
     sentryCaptureException(error)
