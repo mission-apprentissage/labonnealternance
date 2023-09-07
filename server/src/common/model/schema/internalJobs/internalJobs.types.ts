f6031f0ee2bf7a0be3f1d658710374bd70e99c51
Antoine Bigard <bigard.antoine@gmail.com>
2023-09-07 17:25:57 +0200

    chore: make server run with errors

--- server/src/common/model/schema/internalJobs/internalJobs.types.ts
+++ server/src/common/model/schema/internalJobs/internalJobs.types.ts
@@ -4,8 +4,8 @@ interface IInternalJobs {
   type: "simple" | "cron" | "cron_task"
   status: "pending" | "will_start" | "running" | "finished" | "blocked" | "errored"
   sync: boolean
-  payload: any
-  output: any
+  // payload: any
+  // output: any
   cron_string?: string
   scheduled_for: Date
   started_at?: Date
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -12,7 +12,7 @@ import __dirname from "../common/dirname"
 import { logger } from "../common/logger"
 import config from "../config"
 // eslint-disable-next-line import/no-unresolved, node/no-unpublished-import
-import { RegisterRoutes } from "../generated/routes"
+// import { RegisterRoutes } from "../generated/routes"
 import { initBrevoWebhooks } from "../services/brevo.service"
 
 import rome from "./controllers/metiers/rome.controller"
@@ -56,8 +56,8 @@ import "../auth/passport-strategy"
  * LBA-Candidat Swagger file
  */
 const dirname = __dirname(import.meta.url)
-const deprecatedSwaggerDocument = JSON.parse(readFileSync(path.resolve(dirname, "../assets/api-docs/swagger.json")))
-const swaggerDocument = JSON.parse(readFileSync(path.resolve(dirname, "../generated/swagger.json")))
+const deprecatedSwaggerDocument = JSON.parse(readFileSync(path.resolve(dirname, "../src/assets/api-docs/swagger.json")))
+// const swaggerDocument = JSON.parse(readFileSync(path.resolve(dirname, "../generated/swagger.json")))
 
 /**
  * LBA-Recruteur Swagger configuration
@@ -167,7 +167,7 @@ export default async (components) => {
     res.sendFile(path.resolve(dirname, "../assets/api-docs/swagger.json"))
   })
 
-  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  // app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   app.use("/api/v1/lba-docs", swaggerUi.serve, swaggerUi.setup(deprecatedSwaggerDocument, swaggerUIOptions))
   app.use("/api/v1/lbar-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecification, swaggerUIOptions))
 
@@ -178,7 +178,7 @@ export default async (components) => {
   app.use("/api/v1/jobs", limiter5PerSecond)
   //app.use("/api/romelabels", limiter10PerSecond)
 
-  RegisterRoutes(app)
+  // RegisterRoutes(app)
 
   /**
    * LBACandidat
