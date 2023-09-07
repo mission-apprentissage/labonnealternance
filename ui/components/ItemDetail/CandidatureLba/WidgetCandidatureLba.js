9b5b45ef288cd14efd44b8c1ea8f3ea9b2781e2e
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-07 16:57:05 +0200

    Update ui/components/ItemDetail/CandidatureLba/WidgetCandidatureLba.js
    Co-authored-by: Rémy Auricoste <remy.auricoste@gmail.com>

--- ui/components/ItemDetail/CandidatureLba/WidgetCandidatureLba.js
+++ ui/components/ItemDetail/CandidatureLba/WidgetCandidatureLba.js
@@ -34,7 +34,7 @@ const WidgetCandidatureLba = ({ item, caller, fakeLocalStorage }) => {
     // HACK HERE : reapply setApplied to currentUniqId to re-detect
     // if user already applied each time the user swap to another item.
     if (item) {
-      let currentUniqId = actualLocalStorage.getItem(uniqId())
+      let currentUniqId = actualLocalStorage.getItem(uniqId)
       if (currentUniqId) {
         setApplied(currentUniqId)
       } else {
