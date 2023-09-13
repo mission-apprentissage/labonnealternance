58e574cb5fca4f3b9b811434435e2691b203118c
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-13 18:13:12 +0200

    fix: removing unused

--- server/src/common/components/components.ts
+++ server/src/common/components/components.ts
@@ -1,13 +1,9 @@
 // @ts-nocheck
 import { connectToMongo } from "../mongodb.js"
-import createEtablissements from "./etablissement.js"
 
 export const components = async (options = {}) => {
-  const etablissements = await createEtablissements()
-
   return {
     db: options.db || (await connectToMongo()).db,
-    etablissements,
   }
 }
 
