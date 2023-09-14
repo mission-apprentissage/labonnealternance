eaaea4fbe04662e49554c85f7dd69177ce19f3d4
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-14 12:06:19 +0200

    feat: better bottom margin

--- ui/components/ItemDetail/LocationDetail.js
+++ ui/components/ItemDetail/LocationDetail.js
@@ -79,7 +79,7 @@ const LocationDetail = ({ item, isCfa }) => {
         </Box>
       )}
 
-      <Box pb="0px" mt={6} position="relative" background="white" padding={["1px 12px 50px 12px", "1px 24px 50px 24px", "1px 12px 24px 12px"]} mx={["0", "30px"]}>
+      <Box pb="0px" mt={6} mb={4} position="relative" background="white" padding={["1px 12px 50px 12px", "1px 24px 50px 24px", "1px 12px 24px 12px"]} mx={["0", "30px"]}>
         <Text as="h2" variant="itemDetailH2" mt={2}>
           {getTitle(item)}
         </Text>
