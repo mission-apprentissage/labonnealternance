6802add2d4c641ed16b77d6833d644f5919af9ac
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-09-14 16:38:18 +0200

    chore: fix setup-local-env

--- .bin/scripts/setup-local-env.sh
+++ .bin/scripts/setup-local-env.sh
@@ -6,6 +6,11 @@ ANSIBLE_CONFIG="${ROOT_DIR}/.infra/ansible/ansible.cfg" ansible all \
   --limit "local" \
   -m template \
   -a "src=\"${ROOT_DIR}/.infra/.env_server\" dest=\"${ROOT_DIR}/server/.env\"" \
+  --extra-vars "@${ROOT_DIR}/.infra/vault/vault.yml" \
+  --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh"
+ANSIBLE_CONFIG="${ROOT_DIR}/.infra/ansible/ansible.cfg" ansible all \
+  --limit "local" \
+  -m template \
   -a "src=\"${ROOT_DIR}/.infra/.env_ui\" dest=\"${ROOT_DIR}/ui/.env\"" \
   --extra-vars "@${ROOT_DIR}/.infra/vault/vault.yml" \
   --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh"
@@ -14,6 +19,8 @@ echo "NEXT_PUBLIC_ENV=local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_VERSION=0.0.0-local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_API_PORT=5001" >> "${ROOT_DIR}/ui/.env"
 
-echo "REACT_APP_ENV=local" >> "${ROOT_DIR}/ui_espace_pro/.env"
+echo "REACT_APP_ENV=local" > "${ROOT_DIR}/ui_espace_pro/.env"
 echo "REACT_APP_VERSION=0.0.0-local" >> "${ROOT_DIR}/ui_espace_pro/.env"
 echo "REACT_APP_API_PORT=5001" >> "${ROOT_DIR}/ui_espace_pro/.env"
+echo "PORT=3001" >> "${ROOT_DIR}/ui_espace_pro/.env"
+echo "SKIP_PREFLIGHT_CHECK=true" >> "${ROOT_DIR}/ui_espace_pro/.env"
