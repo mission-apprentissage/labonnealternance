205e286e38825754cec58509921f8aefa48db687
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-15 15:26:00 +0200

    feat: better jsdoc for tsoa

--- server/src/http/controllers/formations/formations.controller.ts
+++ server/src/http/controllers/formations/formations.controller.ts
@@ -9,17 +9,26 @@ import { trackApiCall } from "../../../common/utils/sendTrackingEvent.js"
 @Route("/api/v1/formations")
 export class FormationsController extends Controller {
   /**
-   * Get formations matching the query parameters
-   * @param {string} romes optional: some rome codes separated by commas (either romes or romeDomain must be present)
-   * @param {string} romeDomain optional: a rome domain (either romes or romeDomain must be present)
-   * @param {string} referer the referer provided in the HTTP query headers
-   * @param {string} caller the consumer id.
-   * @param {string} latitude optional: search center latitude. Without latitude, the search will target whole France
-   * @param {string} longitude optional: search center longitude. Without longitude, the search will target whole France
-   * @param {number} radius the search radius
-   * @param {string} diploma optional: targeted diploma
-   * @param {string[]} options optional: options
-   * @param {string} useMock optional: wether to return mocked values or not
+   * Get formations matching job and location parameters
+   * @param {string} romes (optional) a list of comma separated ROME codes matching the requested jobs. Max: 20.<br><br><strong>You must use either 'rome' or 'romeDomain'</strong>
+   * <br><br><italic>Example : F1603,I1308</italic>
+   * @param {string} romeDomain (optional) a ROME domain (1 letter and 2 digits) or great ROME domain (1 letter)<br><br><strong>You must use either 'rome' or 'romeDomain'</strong>
+   * <br><br><italic>Example : F or I13</italic>
+   * @param {string} caller Your company or product name and your contact email address.
+   * <br><br><italic>Example : contact@domain company_name</italic>
+   * @param {string} latitude (optional) search center latitude. Without latitude, the search will target whole France<br>
+   * with 'latitude' you also must fill 'insee' and 'longitude'
+   * <br><br><italic>Example : 48.845</italic>
+   * @param {string} longitude (optional) search center longitude. Without longitude, the search will target whole France
+   * with 'longitude' you also must fill 'insee' and 'latitude'
+   * <br><br><italic>Example : 2.3752</italic>
+   * @param {number} radius the search radius in kilometers. Default value is 30
+   * <br><br><italic>Example : 30</italic>
+   * @param {string} diploma (optional) the targeted diploma level. Must contain at least the number of an allowed value<br><br>
+   * Allowed values : 3 (CAP...), 4 (BAC...), 5 (BTS, DEUST...), 6 (Licence...), 7 (Master, titre ingénieur...)
+   * <br><br><italic>Example : 3</italic>
+   * @param {string[]} options (optional) options
+   * @param {string} useMock (optional) wether to return mocked values or not
    * @returns {Promise<IApiError | { results: ILbaItem[] }>} response
    */
   @Response<"Wrong parameters">(400)
@@ -31,14 +40,14 @@ export class FormationsController extends Controller {
     @Request() request: express.Request,
     @Query() romes?: string,
     @Query() romeDomain?: string,
-    @Header() @Hidden() referer?: string,
     @Query() caller?: string,
     @Query() latitude?: string,
     @Query() longitude?: string,
     @Query() radius?: string,
     @Query() diploma?: string,
     @Query() @Hidden() options?: string,
-    @Query() @Hidden() useMock?: string
+    @Query() @Hidden() useMock?: string,
+    @Header() @Hidden() referer?: string
   ): Promise<IApiError | { results: ILbaItem[] }> {
     const result = await getFormationsQuery({ romes, longitude, latitude, radius, diploma, romeDomain, caller, options, referer, useMock })
 
