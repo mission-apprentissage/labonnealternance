61ab5e754664697f91b0b0e557cccff841c1a8e4
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-22 11:29:21 +0200

    feat: routes done

--- shared/routes/application.routes.ts
+++ shared/routes/application.routes.ts
@@ -7,32 +7,59 @@ export const zApplicationRoutes = {
     "/api/V1/application": {
       queryParams: ZApplicationUI,
       response: {
-        "2xx": {
-          result: "ok",
-          message: "messages sent",
-        },
+        "200": z
+          .object({
+            result: z.literal("ok"),
+            message: z.literal("messages sent"),
+          })
+          .strict(),
       },
     },
     "/api/application": {
       queryParams: ZApplicationUI,
       response: {
-        "2xx": {
-          result: "ok",
-          message: "messages sent",
-        },
+        "200": z
+          .object({
+            result: z.literal("ok"),
+            message: z.literal("messages sent"),
+          })
+          .strict(),
       },
     },
     "/api/application/intentionComment": {
+      queryParams: z.object({
+        id: z.string(),
+        iv: z.string(),
+        comment: z.string(),
+      }),
       response: {
-        queryParams: z.object({
-          id: z.string(),
-          iv: z.string(),
-          comment: z.string(),
-        }),
-        "2xx": {
-          result: "ok",
-          message: "comment registered",
-        },
+        "200": z
+          .object({
+            result: z.literal("ok"),
+            message: z.literal("comment registered"),
+          })
+          .strict(),
+      },
+    },
+    "/api/application/webhook": {
+      queryParams: z.object({
+        event: z.string(),
+        id: z.string(),
+        date: z.string(),
+        ts: z.number(),
+        "message-id": z.string(),
+        email: z.string(),
+        ts_event: z.number(),
+        subject: z.string(),
+        sending_ip: z.string(),
+        ts_epoch: z.number(),
+      }),
+      response: {
+        "200": z
+          .object({
+            result: z.literal("ok"),
+          })
+          .strict(),
       },
     },
   },
