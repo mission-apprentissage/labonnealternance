77e6dc0c9aa318a402b88ba5d918263177dac1de
Kevin Barnoin <k.barnoin@gmail.com>
2023-09-22 17:25:27 +0200

    feat: add admin etablissements routes

--- shared/routes/etablissement.routes.ts
+++ shared/routes/etablissement.routes.ts
@@ -6,6 +6,34 @@ import { ZEtablissement } from "../models/etablissement.model"
 
 export const zEtablissementRoutes = {
   get: {
+    "/api/admin/etablissements/": {
+      queryString: z.object({ query: z.string(), limit: z.number(), page: z.number() }).strict(),
+      response: {
+        "2xx": z
+          .object({
+            etablissements: z.array(ZEtablissement),
+            pagination: z.object({
+              page: z.number(),
+              resultats_par_page: z.number(),
+              nombre_de_page: z.number(),
+              total: z.number(),
+            }),
+          })
+          .strict(),
+      },
+    },
+    "/api/admin/etablissements/siret-formateur/:siret": {
+      params: z.object({ siret: z.string() }),
+      response: {
+        "2xx": ZEtablissement,
+      },
+    },
+    "/api/admin/etablissements/:id": {
+      params: z.object({ id: z.string() }),
+      response: {
+        "2xx": ZEtablissement,
+      },
+    },
     "/api/etablissements/:id": {
       params: z.object({ id: zObjectId }).strict(),
       response: {
@@ -14,6 +42,12 @@ export const zEtablissementRoutes = {
     },
   },
   post: {
+    "/api/admin/etablissements/": {
+      body: ZEtablissement,
+      response: {
+        "2xx": ZEtablissement,
+      },
+    },
     "/api/etablissements/:id/premium/affelnet/accept": {
       params: z.object({ id: z.string() }).strict(),
       response: {
@@ -47,6 +81,12 @@ export const zEtablissementRoutes = {
     },
   },
   patch: {
+    "/api/admin/etablissements/:id": {
+      params: z.object({ id: z.string() }).strict(),
+      response: {
+        "2xx": ZEtablissement,
+      },
+    },
     "/api/etablissements/:id/appointments/:appointmentId": {
       body: z.object({ has_been_read: z.string() }).strict(),
       params: z.object({ id: zObjectId, appointmentId: zObjectId }).strict(),
@@ -55,6 +95,4 @@ export const zEtablissementRoutes = {
       },
     },
   },
-  put: {},
-  delete: {},
 }
