d1575f0d19c1f62117e9aa435d8fede1a6175a86
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-09-25 16:03:01 +0200

    chore: get /api/partners/parcoursup/formations route defined

---
+++ shared/routes/partners.routes.ts
@@ -0,0 +1,17 @@
+import { z } from "../helpers/zodWithOpenApi"
+import {IEligibleTrainingsForAppointmentSchema} from "shared/models/elligibleTraining.model"
+import { IRoutesDef } from "./common.routes"
+
+export const zCampaignWebhookRoutes = {
+  get: {
+    "/api/partners/parcoursup/formations": {
+      response: {
+        "200": z.array(z.object({parcoursup_id: Pick<IEligibleTrainingsForAppointmentSchema, 'parcoursup_id'>}))
+      },
+    },
+  },
+  post: {},
+  put: {},
+  delete: {},
+  patch: {},
+} satisfies IRoutesDef
