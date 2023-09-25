4701471eaf7582f8e553034a4c896d4a1df4e5cb
Antoine Bigard <bigard.antoine@gmail.com>
2023-09-25 16:47:28 +0200

    chore: wip

--- shared/routes/recruiters.routes.ts
+++ shared/routes/recruiters.routes.ts
@@ -48,15 +48,15 @@ export const zRecruiterRoutes = {
           .object({
             establishment_enseigne: z.string().nullish(),
             establishment_state: z.string(), // F pour fermé ou A pour actif
-            establishment_siret: z.string(),
-            establishment_raison_sociale: z.string(),
+            establishment_siret: z.string().nullish(),
+            establishment_raison_sociale: z.string().nullish(),
             address_detail: z.any(),
-            address: z.string(),
+            address: z.string().nullish(),
             contacts: z.array(z.any()), // conserve la coherence avec l'UI
-            naf_code: z.string(),
-            naf_label: z.string(),
-            establishment_size: z.string(),
-            establishment_creation_date: z.string(),
+            naf_code: z.string().nullish(),
+            naf_label: z.string().nullish(),
+            establishment_size: z.string().nullish(),
+            establishment_creation_date: z.date().nullish(),
             geo_coordinates: z.string(),
           })
           .strict(),
