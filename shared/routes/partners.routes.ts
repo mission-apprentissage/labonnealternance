4e307a3b4fd9abf4b2175a8a0bf5de152d6748a1
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-09-25 16:11:58 +0200

    style: lint:fix

--- shared/routes/partners.routes.ts
+++ shared/routes/partners.routes.ts
@@ -1,12 +1,14 @@
+import { IEligibleTrainingsForAppointmentSchema } from "shared/models/elligibleTraining.model"
+
 import { z } from "../helpers/zodWithOpenApi"
-import {IEligibleTrainingsForAppointmentSchema} from "shared/models/elligibleTraining.model"
+
 import { IRoutesDef } from "./common.routes"
 
 export const zCampaignWebhookRoutes = {
   get: {
     "/api/partners/parcoursup/formations": {
       response: {
-        "200": z.array(z.object({parcoursup_id: Pick<IEligibleTrainingsForAppointmentSchema, 'parcoursup_id'>}))
+        "200": z.array(z.object({ parcoursup_id: Pick<IEligibleTrainingsForAppointmentSchema, "parcoursup_id"> })),
       },
     },
   },
