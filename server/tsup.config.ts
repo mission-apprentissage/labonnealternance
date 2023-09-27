70478b4a278cb22da7c6684d8ddc03aefbd1fd73
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-09-27 08:15:02 +0200

    fix: watch mode shared

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -3,14 +3,16 @@ import { defineConfig } from "tsup"
 
 export default defineConfig((options) => {
   const isDev = options.env?.NODE_ENV !== "production"
+  const isWatched = options.env?.TSUP_WATCH === "true"
 
   const entry: Record<string, string> = {
     index: isDev ? "src/dev.ts" : "src/main.ts",
   }
   return {
     entry,
-    watch: isDev && options.watch ? ["./src", "../shared"] : false,
-    onSuccess: isDev && options.watch ? "yarn cli start --withProcessor" : "",
+    watch: isWatched ? ["./src", "../shared"] : false,
+    onSuccess: isWatched ? "yarn cli start --withProcessor" : "",
+    ignoreWatch: ["../shared/node_modules/**"],
     // In watch mode doesn't exit cleanly as it causes EADDRINUSE error
     killSignal: "SIGKILL",
     target: "es2022",
