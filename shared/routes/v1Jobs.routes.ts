eb01e3e6bb626df5076874d523599a0bf6ba829a
Alan Le Ruyet <alan.leruyet@free.fr>
2023-09-27 09:39:10 +0200

    fix: strip on headers in jobs.route

--- shared/routes/v1Jobs.routes.ts
+++ shared/routes/v1Jobs.routes.ts
@@ -103,7 +103,7 @@ export const zV1JobsRoutes = {
         .object({
           referer: z.string().optional(),
         })
-        .strict(),
+        .strip(),
       response: {
         "200": z
           .object({
@@ -165,7 +165,7 @@ export const zV1JobsRoutes = {
         .object({
           referer: z.string().optional(),
         })
-        .strict(),
+        .strip(),
       response: {
         "200": z
           .object({
@@ -196,7 +196,7 @@ export const zV1JobsRoutes = {
         .object({
           referer: z.string().optional(),
         })
-        .strict(),
+        .strip(),
       response: {
         "200": z
           .object({
@@ -227,7 +227,7 @@ export const zV1JobsRoutes = {
         .object({
           referer: z.string().optional(),
         })
-        .strict(),
+        .strip(),
       response: {
         "200": z
           .object({
