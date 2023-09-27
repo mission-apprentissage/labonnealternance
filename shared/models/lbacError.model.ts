234144d8ea6d11da1f9981a10929c8ac752b91ba
Rémy Auricoste <remy.auricoste@gmail.com>
2023-09-27 08:56:35 +0200

    fix: apiError type sur zV1JobsEtFormationsRoutes

--- shared/models/lbacError.model.ts
+++ shared/models/lbacError.model.ts
@@ -5,6 +5,16 @@ export const ZLbacError = z.object({
   error_messages: z.array(z.string()).nullish(),
 })
 
+export const ZApiError = z
+  .object({
+    result: z.string().optional(),
+    error: z.string(),
+    message: z.any().optional(),
+    status: z.number().optional(),
+    statusText: z.string().optional(),
+  })
+  .strict()
+
 export const ZLbarError = z.object({
   error: z.boolean(),
   message: z.string(),
