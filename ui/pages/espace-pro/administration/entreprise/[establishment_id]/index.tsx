44f6c2e078b9c9ddab30073ab5f859d4034b0b59
Rémy Auricoste <remy.auricoste@gmail.com>
2023-09-29 10:50:12 +0200

    fix: cfa: creation d offre pour une entreprise

--- ui/pages/espace-pro/administration/entreprise/[establishment_id]/index.tsx
+++ ui/pages/espace-pro/administration/entreprise/[establishment_id]/index.tsx
@@ -2,14 +2,10 @@ import { Layout } from "../../../../../components/espace_pro"
 import ListeOffres from "../../../../../components/espace_pro/ListeOffres"
 import withAuth from "../../../../../components/espace_pro/withAuth"
 
-function EntrepriseListOffre() {
-  return <ListeOffres />
-}
-
 function EntrepriseListOffrePage() {
   return (
     <Layout footer={false}>
-      <EntrepriseListOffre />
+      <ListeOffres />
     </Layout>
   )
 }
