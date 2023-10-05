ec14d518fb8843cc6e6e699bd3e9096a30350208
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-10-05 14:04:36 +0200

    fix: swagger path param format

--- shared/helpers/openapi/generateOpenapi.test.ts
+++ shared/helpers/openapi/generateOpenapi.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest"
 import { generateOpenApiSchema } from "./generateOpenapi"
 
 describe("generateOpenApiSchema", () => {
-  it.skip("should generate proper schema", async () => {
+  it("should generate proper schema", async () => {
     const s = generateOpenApiSchema("V1.0", "Production", "https://labonnealternance.apprentissage.beta.gouv.fr")
     expect(s).toMatchSnapshot()
   })
