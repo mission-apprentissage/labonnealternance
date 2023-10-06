f7ea2f66d5b74f536a398909b4bdd1afb9c000ac
Kevin Barnoin <k.barnoin@gmail.com>
2023-10-06 11:37:37 +0200

    feat: adapt script

--- server/src/jobs/lba_recruteur/formulaire/misc/recoverGeocoordinates.ts
+++ server/src/jobs/lba_recruteur/formulaire/misc/recoverGeocoordinates.ts
@@ -4,12 +4,21 @@ import { asyncForEach, sleep } from "../../../../common/utils/asyncUtils"
 import { getGeoCoordinates } from "../../../../services/etablissement.service"
 
 export const recoverMissingGeocoordinates = async () => {
-  const recruiters = await Recruiter.find({ geo_coordinates: null })
+  const recruiters = await Recruiter.find({ geo_coordinates: "NOT FOUND" })
 
   await asyncForEach(recruiters, async (recruiter) => {
     await sleep(1000)
-    const geocoord = await getGeoCoordinates(`${recruiter.address_detail.l4} ${recruiter.address_detail.l6}`)
-    logger.info(`${recruiter.establishment_siret} - geocoord: ${geocoord} - adresse: ${recruiter.address_detail.l4} ${recruiter.address_detail.l6} `)
+    let geocoord
+    if (recruiter.address_detail.l4) {
+      // if address data is in API address V2
+      geocoord = await getGeoCoordinates(`${recruiter.address_detail.l4} ${recruiter.address_detail.l6}`)
+    } else {
+      // else API address V3
+      geocoord = await getGeoCoordinates(`${recruiter.address_detail.acheminement_postal.l4} ${recruiter.address_detail.acheminement_postal.l6}`)
+    }
+    logger.info(
+      `${recruiter.establishment_siret} - geocoord: ${geocoord} - adresse: ${recruiter.address_detail.acheminement_postal.l4} ${recruiter.address_detail.acheminement_postal.l6} `
+    )
     recruiter.geo_coordinates = geocoord
     await recruiter.save()
   })
