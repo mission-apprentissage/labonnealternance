82a37b25426e08e8df919160fd9f8f51777dcb86
Kevin Barnoin <k.barnoin@gmail.com>
2023-10-06 12:14:06 +0200

    feat: update script

--- server/src/jobs/lba_recruteur/formulaire/misc/recoverGeocoordinates.ts
+++ server/src/jobs/lba_recruteur/formulaire/misc/recoverGeocoordinates.ts
@@ -1,13 +1,39 @@
+import { ENTREPRISE } from "shared/constants/recruteur"
+
 import { logger } from "../../../../common/logger"
-import { Recruiter } from "../../../../common/model"
+import { Recruiter, UserRecruteur } from "../../../../common/model"
 import { asyncForEach, sleep } from "../../../../common/utils/asyncUtils"
 import { getGeoCoordinates } from "../../../../services/etablissement.service"
 
-export const recoverMissingGeocoordinates = async () => {
+const recoverMissingGeocoordinatesUserRecruteur = async () => {
+  const users = await UserRecruteur.find({ geo_coordinates: "NOT FOUND", type: ENTREPRISE })
+
+  await asyncForEach(users, async (user) => {
+    if (!user.address_detail) return
+    await sleep(500)
+
+    let geocoord
+    if ("l4" in user.address_detail) {
+      // if address data is in API address V2
+      geocoord = await getGeoCoordinates(`${user.address_detail.l4} ${user.address_detail.l6}`)
+      logger.info(`${user.establishment_siret} - geocoord: ${geocoord} - adresse: ${user.address_detail.l4} ${user.address_detail.l6} `)
+    } else {
+      // else API address V3
+      geocoord = await getGeoCoordinates(`${user.address_detail?.acheminement_postal?.l4} ${user.address_detail?.acheminement_postal?.l6}`)
+      logger.info(`${user.establishment_siret} - geocoord: ${geocoord} - adresse: ${user.address_detail?.acheminement_postal?.l4} ${user.address_detail?.acheminement_postal?.l6} `)
+    }
+    user.geo_coordinates = geocoord
+    await user.save()
+  })
+}
+
+const recoverMissingGeocoordinatesRecruiters = async () => {
   const recruiters = await Recruiter.find({ geo_coordinates: "NOT FOUND" })
 
   await asyncForEach(recruiters, async (recruiter) => {
-    await sleep(1000)
+    if (!recruiter.address_detail) return
+    await sleep(500)
+
     let geocoord
     if (recruiter.address_detail.l4) {
       // if address data is in API address V2
@@ -22,4 +48,9 @@ export const recoverMissingGeocoordinates = async () => {
     recruiter.geo_coordinates = geocoord
     await recruiter.save()
   })
+}
+
+export const recoverMissingGeocoordinates = async () => {
+  await recoverMissingGeocoordinatesRecruiters()
+  await recoverMissingGeocoordinatesUserRecruteur()
 }
