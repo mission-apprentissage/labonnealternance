309c0dd38b93d9c2f0cbf6ff03def3d013b8acd7
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-10-19 09:49:36 +0200

    fix: extract user data

--- server/src/http/sentry.ts
+++ server/src/http/sentry.ts
@@ -29,9 +29,45 @@ export async function closeSentry(): Promise<void> {
   await Sentry.close(2_000)
 }
 
+function extractUserData(request: FastifyRequest) {
+  const user = request.user
+
+  if (!user) {
+    return {
+      segment: "anonymous",
+    }
+  }
+
+  if (user.type === "ICredential") {
+    return {
+      segment: "api-key",
+      id: user.value._id.toString(),
+      email: user.value.email,
+    }
+  }
+
+  if (user.type === "IAccessToken") {
+    const identity = user.value.identity
+    return {
+      segment: "access-token",
+      id: "_id" in identity ? identity._id.toString() : identity.email,
+      email: identity.email,
+      type: identity.type,
+    }
+  }
+
+  return {
+    segment: "session",
+    id: user.value._id.toString(),
+    email: user.value.email,
+    type: user.value.type,
+  }
+}
+
 export function initSentryFastify(app: Server) {
   const options: any = {
     setErrorHandler: false,
+    extractUserData: extractUserData,
     extractRequestData: (request: FastifyRequest) => {
       return {
         headers: request.headers,
