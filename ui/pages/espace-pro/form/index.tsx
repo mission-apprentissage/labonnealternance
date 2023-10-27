3f35a17fa5f68947c0440f42b4fe6a052a49ee86
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2023-10-27 11:08:40 +0200

    Update index.tsx

--- ui/pages/espace-pro/form/index.tsx
+++ ui/pages/espace-pro/form/index.tsx
@@ -294,6 +294,7 @@ export default function FormCreatePage() {
                     <span style={{ color: "#B34000", paddingRight: "5px" }}>*</span>champs obligatoires
                   </Text>
                   <Button
+                    data-trackingid="prendre-rdv-cfa"
                     variant="unstyled"
                     type={"submit"}
                     disabled={submitLoading}
