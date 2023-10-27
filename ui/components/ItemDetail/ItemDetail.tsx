7c0fac0811af8f86dc2fd541a1161c93629acee2
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2023-10-27 11:28:53 +0200

    Update ItemDetail.tsx

--- ui/components/ItemDetail/ItemDetail.tsx
+++ ui/components/ItemDetail/ItemDetail.tsx
@@ -139,7 +139,7 @@ const ItemDetail = ({ selectedItem, handleClose, handleSelectItem }) => {
 
           {buttonJePostuleShouldBeDisplayed(kind, selectedItem) && (
             <Box my={4}>
-              <Link data-trackingid="postuler-offre-partenaire" variant="postuler" href={selectedItem.url} target="poleemploi" onClick={postuleSurPoleEmploi}>
+              <Link data-tracking-id="postuler-offre-partenaire" variant="postuler" href={selectedItem.url} target="poleemploi" onClick={postuleSurPoleEmploi}>
                 Je postule sur Pôle emploi
               </Link>
             </Box>
