6d7e6b43d5c5a5739f1d40ba10928df7e02228be
Antoine Bigard <bigard.antoine@gmail.com>
2023-10-30 17:22:21 +0100

    chore: wip [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", { name: "hotfix", channel: "hotfix", prerelease: "hotfix" }],
+  branches: ["main", { name: "develop", channel: "develop", prerelease: "beta" }, { name: "hotfix", channel: "hotfix", prerelease: "hotfix" }],
   repositoryUrl: "https://github.com/mission-apprentissage/labonnealternance.git",
   plugins: [
     "@semantic-release/commit-analyzer",
