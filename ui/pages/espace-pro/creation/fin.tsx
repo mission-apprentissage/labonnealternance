4ac53dc007d55238d833b1f68150ea58d6cb204f
Rémy Auricoste <remy.auricoste@gmail.com>
2023-11-02 17:57:43 +0100

    fix: securisation du flow de dépot rapide d'offre (#763)
    * fix: securisation de getFormulaire
    
    * fix: securisation du depot simplifié
    
    * fix: onRequests
    
    * fix: securisation du lien de renvoi de mail
    
    * fix: renommage de la route en /login/:userId/resend-confirmation-email
    
    * fix: affichage backoffice
    
    * fix: remplacement du retry par un enabled
    
    * fix: suppression Promise.reject
    
    * feat: update seed with fiche metier
    
    ---------
    
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>

--- ui/pages/espace-pro/creation/fin.tsx
+++ ui/pages/espace-pro/creation/fin.tsx
@@ -4,13 +4,11 @@ import { useRouter } from "next/router"
 import { useContext, useState } from "react"
 import { useQuery, useQueryClient } from "react-query"
 
-import { apiGet } from "@/utils/api.utils"
-
 import { AuthentificationLayout, LoadingEmptySpace } from "../../../components/espace_pro"
 import { WidgetContext } from "../../../context/contextWidget"
 import { InfoCircle } from "../../../theme/components/icons"
 import { MailCloud } from "../../../theme/components/logos"
-import { sendValidationLink } from "../../../utils/api"
+import { getUserStatus, sendValidationLink } from "../../../utils/api"
 
 function parseQueryString(value: string | string[]): string {
   return Array.isArray(value) ? value[0] : value
@@ -44,8 +42,8 @@ export default function DepotRapideFin() {
    * KBA 20230130 : retry set to false to avoid waiting for failure if user is from dashboard (userId is not passed)
    * - To be changed with userID in URL params
    */
-  const { isFetched } = useQuery("userdetail", () => apiGet("/user/status/:userId", { params: { userId } }), {
-    retry: userId ? true : false,
+  const { isFetched } = useQuery("userdetail", () => getUserStatus(userId), {
+    enabled: Boolean(userId),
     onSettled: (data) => {
       if (data?.status_current === "ERROR") {
         setUserIsInError(true)
@@ -73,8 +71,8 @@ export default function DepotRapideFin() {
     return <LoadingEmptySpace />
   }
 
-  const resendMail = (email) => {
-    sendValidationLink({ email })
+  const resendMail = () => {
+    sendValidationLink(userId)
       .then(() => {
         toast({
           title: "Email envoyé.",
@@ -139,10 +137,10 @@ export default function DepotRapideFin() {
             </Text>
           </Box>
         </Flex>
-        {userIsInError && (
+        {!userIsInError && (
           <Stack direction="row" align="center" spacing={4} mt={4} ml={6}>
             <Text mr={10}>Vous n’avez pas reçu le mail ? </Text>
-            <Button variant="popover" textDecoration="underline" onClick={() => resendMail(email)} isDisabled={disableLink}>
+            <Button variant="popover" textDecoration="underline" onClick={resendMail} isDisabled={disableLink}>
               Renvoyer le mail
             </Button>
           </Stack>
@@ -164,10 +162,10 @@ export default function DepotRapideFin() {
               Afin de finaliser la diffusion de votre besoin auprès des jeunes, merci de confirmer votre adresse mail en cliquant sur le lien que nous venons de vous transmettre à
               l’adresse suivante: <span style={{ fontWeight: "700" }}>{email}</span>.
             </Text>
-            {userIsInError && (
+            {!userIsInError && (
               <Stack direction="row" align="center" spacing={4} mt={4}>
                 <Text mr={10}>Vous n’avez pas reçu le mail ? </Text>
-                <Button variant="popover" textDecoration="underline" onClick={() => resendMail(email)} isDisabled={disableLink}>
+                <Button variant="popover" textDecoration="underline" onClick={resendMail} isDisabled={disableLink}>
                   Renvoyer le mail
                 </Button>
               </Stack>
