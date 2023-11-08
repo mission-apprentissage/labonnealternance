81e4ebf4e097786735b54cc9ce1ec4b186a6fb59
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2023-11-08 18:06:21 +0100

    LBAC-1719 : MAJ lien inserjeunes

--- ui/components/ItemDetail/StatsInserJeunes.tsx
+++ ui/components/ItemDetail/StatsInserJeunes.tsx
@@ -53,7 +53,7 @@ const StatsInserJeunes = ({ stats }) => {
           </Box>
         </Flex>
         <Text mt={3} color="grey.425" fontSize="12px">
-          *Données issues du dispositif InserJeunes promotion {stats.millesime.replace("_", "/")}
+          *Données issues du <Link href="https://documentation.exposition.inserjeunes.beta.gouv.fr/" textDecoration="underline" isExternal>dispositif InserJeunes promotion {stats.millesime.replace("_", "/")}</Link>
         </Text>
       </Box>
     )
