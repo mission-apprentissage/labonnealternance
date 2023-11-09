fbebb91eb2564f2c00f683cd70c3a88b45729622
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-09 14:31:47 +0100

    feat: remplacement openapi par describe

--- shared/models/anonymizedApplications.model.ts
+++ shared/models/anonymizedApplications.model.ts
@@ -8,27 +8,19 @@ export const ZAnonymizedApplication = z
     _id: zObjectId,
     company_recruitment_intention: z.string().nullable().describe("L'intention de la société vis à vis du candidat"),
     company_feedback_date: z.date().nullable().describe("Date d'intention/avis donnée"),
-    company_siret: extensions.siret.openapi({
-      description: "Le siret de l'entreprise. Fourni par La bonne alternance. ",
-      example: "00004993900000",
-    }),
-    company_naf: z.string().openapi({
-      description: "La valeur associée au code NAF de l'entreprise. Fournie par La bonne alternance. ",
-      example: "Boulangerie et boulangerie-pâtisserie",
-    }),
-    job_origin: z.string().nullable().openapi({
-      description: "Le type de société selon la nomenclature La bonne alternance. Fourni par La bonne alternance.",
-      example: "lba|lbb|matcha",
-    }),
-    job_id: z.string().nullable().openapi({
-      description:
-        "L'identifiant de l'offre La bonne alternance Recruteur pour laquelle la candidature est envoyée. Seulement si le type de la société (company_type) est \"matcha\" . La valeur est fournie par La bonne alternance. ",
-      example: "...59c24c059b...",
-    }),
+    company_siret: extensions.siret.describe('Le siret de l\'entreprise. Fourni par La bonne alternance. Example: "00004993900000"'),
+    company_naf: z.string().describe('La valeur associée au code NAF de l\'entreprise. Fournie par La bonne alternance. Example: "Boulangerie et boulangerie-pâtisserie"'),
+    job_origin: z.string().nullable().describe('Le type de société selon la nomenclature La bonne alternance. Fourni par La bonne alternance. Example: "lba|lbb|matcha"'),
+    job_id: z
+      .string()
+      .nullable()
+      .describe(
+        'L\'identifiant de l\'offre La bonne alternance Recruteur pour laquelle la candidature est envoyée. Seulement si le type de la société (company_type) est "matcha" . La valeur est fournie par La bonne alternance. Example: "...59c24c059b..."'
+      ),
     caller: z.string().nullable().describe("L'identification de la source d'émission de la candidature (pour widget et api)"),
     created_at: z.date().nullable().describe("La date création de la demande"),
   })
   .strict()
-  .openapi("Anonymized Application")
+  .describe("Anonymized Application")
 
 export type IAnonymizedApplication = z.output<typeof ZAnonymizedApplication>
