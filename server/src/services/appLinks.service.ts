39c620aedb731f43deba50f82beed1dbf6900ffd
Rémy Auricoste <remy.auricoste@gmail.com>
2023-11-15 17:08:28 +0100

    fix: typing

--- server/src/services/appLinks.service.ts
+++ server/src/services/appLinks.service.ts
@@ -2,19 +2,18 @@ import { IUserRecruteur } from "shared/models"
 import { zRoutes } from "shared/routes"
 
 import config from "@/config"
-import { generateAccessToken } from "@/security/accessTokenService"
+import { IScope, generateAccessToken } from "@/security/accessTokenService"
 
 export function createAuthMagicLinkToken(user: IUserRecruteur) {
-  return generateAccessToken(user, [
-    {
-      schema: zRoutes.post["/login/verification"],
-      options: {
-        params: undefined,
-        querystring: undefined,
-      },
-      resources: {},
+  const endpoint: IScope<(typeof zRoutes.post)["/login/verification"]> = {
+    schema: zRoutes.post["/login/verification"],
+    options: {
+      params: undefined,
+      querystring: undefined,
     },
-  ])
+    resources: {},
+  }
+  return generateAccessToken(user, [endpoint])
 }
 
 export function createAuthMagicLink(user: IUserRecruteur) {
@@ -24,64 +23,49 @@ export function createAuthMagicLink(user: IUserRecruteur) {
 }
 
 export function createValidationMagicLink(user: IUserRecruteur) {
-  const token = generateAccessToken(
-    user,
-    [
-      {
-        schema: zRoutes.post["/etablissement/validation"],
-        options: {
-          params: undefined,
-          querystring: undefined,
-        },
-        resources: {
-          user: [user._id.toString()],
-        },
-      },
-    ],
-    {
-      expiresIn: "30d",
-    }
-  )
+  const endpoint: IScope<(typeof zRoutes.post)["/etablissement/validation"]> = {
+    schema: zRoutes.post["/etablissement/validation"],
+    options: {
+      params: undefined,
+      querystring: undefined,
+    },
+    resources: {
+      user: [user._id.toString()],
+    },
+  }
+  const token = generateAccessToken(user, [endpoint], {
+    expiresIn: "30d",
+  })
   return `${config.publicUrl}/espace-pro/authentification/validation/${user._id}?token=${encodeURIComponent(token)}`
 }
 
 export function createOptoutValidateMagicLink(email: string, siret: string) {
-  const token = generateAccessToken(
-    { type: "cfa", email, siret },
-    [
-      {
-        schema: zRoutes.get["/optout/validate"],
-        options: {
-          params: undefined,
-          querystring: undefined,
-        },
-        resources: {},
-      },
-    ],
-    {
-      expiresIn: "45d",
-    }
-  )
+  const endpoint: IScope<(typeof zRoutes.get)["/optout/validate"]> = {
+    schema: zRoutes.get["/optout/validate"],
+    options: {
+      params: undefined,
+      querystring: undefined,
+    },
+    resources: {},
+  }
+  const token = generateAccessToken({ type: "cfa", email, siret }, [endpoint], {
+    expiresIn: "45d",
+  })
   return `${config.publicUrl}/espace-pro/authentification/optout/verification?token=${encodeURIComponent(token)}`
 }
 
 export function createCfaUnsubscribeToken(email: string, siret: string) {
-  return generateAccessToken(
-    { type: "cfa", email, siret },
-    [
-      {
-        schema: zRoutes.post["/etablissement/:establishment_siret/proposition/unsubscribe"],
-        options: {
-          params: {
-            establishment_siret: siret,
-          },
-          querystring: undefined,
-        },
-        resources: {},
+  const endpoint: IScope<(typeof zRoutes.post)["/etablissement/:establishment_siret/proposition/unsubscribe"]> = {
+    schema: zRoutes.post["/etablissement/:establishment_siret/proposition/unsubscribe"],
+    options: {
+      params: {
+        establishment_siret: siret,
       },
-    ],
-    {
-      expiresIn: "30d",
-    }
-  )
+      querystring: undefined,
+    },
+    resources: {},
+  }
+  return generateAccessToken({ type: "cfa", email, siret }, [endpoint], {
+    expiresIn: "30d",
+  })
 }
