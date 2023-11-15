64c8ce90dab6ca628866fcfae37760c30778cb50
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-15 13:01:26 +0100

    feat: LBAC-1711 sécurisation routes (#803)
    * feat: securisation route
    
    route /etablissement/:establishment_siret/proposition/unsubscribe
    
    * feat: commentaires transformés en ticket
    
    * feat: refactoring des access token pour embarquer l'uri complète
    
    * fix: remove fonction d'example
    
    * fix: refactoring pour autoriser des options indéfinie dans le token
    
    * fix: test
    
    * fix: import in api.utils.test.ts
    
    * fix: ajout de tests unitaires
    
    * fix: tests
    
    * fix: passage automatique du paramètre token
    
    * feat: ajout token à unsubscribe
    
    * feat: transmission du token
    
    * feat: transmission correcte du token
    
    * fix: pas d'accessToken en querystring pour les POST
    
    * fix: passage du token dans le header à la place de la querystring
    
    * fix: tests
    
    * fix: ajout correct de l'authorization header
    
    * feat: redondant
    
    * feat: essai
    
    * feat: cheat
    
    * feat: passage du bearer auth au niveau apiPost
    
    ---------
    
    Co-authored-by: Rémy Auricoste <remy.auricoste@gmail.com>
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>

--- ui/utils/api.ts
+++ ui/utils/api.ts
@@ -108,7 +108,13 @@ export const getRomeDetail = (rome: string) => API.get(`/rome/detail/${rome}`)
 export const getRelatedEtablissementsFromRome = ({ rome, latitude, longitude }: { rome: string; latitude: number; longitude: number }) =>
   API.get(`/etablissement/cfas-proches?rome=${rome}&latitude=${latitude}&longitude=${longitude}`)
 
-export const etablissementUnsubscribeDemandeDelegation = (establishmentSiret) => API.post(`/etablissement/${establishmentSiret}/proposition/unsubscribe`)
+export const etablissementUnsubscribeDemandeDelegation = (establishment_siret: any, token: string) =>
+  apiPost("/etablissement/:establishment_siret/proposition/unsubscribe", {
+    params: { establishment_siret },
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  })
 
 /**
  * Administration OPCO
