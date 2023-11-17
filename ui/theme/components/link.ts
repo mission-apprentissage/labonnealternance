cf704494d7d6285b01b836b6d2623bf94d32afe9
Kevin Barnoin <k.barnoin@gmail.com>
2023-11-17 18:33:00 +0100

    feat: Accessibility update (#824)
    * feat: logo redirect to homepage
    
    * feat: link component instead of button
    
    * feat: restore button & select outline
    
    * fix: logo layout
    
    * fix: focus color on menu items
    
    * feat: fix focus visible

--- ui/theme/components/link.ts
+++ ui/theme/components/link.ts
@@ -1,15 +1,5 @@
 const Link = {
-  baseStyle: {
-    _focus: {
-      // boxShadow: '0 0 0 3px #000091',
-      // outlineColor: 'bluefrance.500',
-      boxShadow: "none",
-    },
-  },
   variants: {
-    unstyled: {
-      _focus: { boxShadow: "none", outlineColor: "none" },
-    },
     classic: {
       textDecoration: "underline",
       fontWeight: "700",
@@ -20,20 +10,17 @@ const Link = {
       my: 3,
       bg: "white",
       _hover: { bg: "#eceae3", textDecoration: "none" },
-      _focus: { boxShadow: "none", outlineColor: "none" },
       display: "block",
     },
     editorialContentLink: {
       color: "info",
       textDecoration: "underline",
       _hover: { color: "bluefrance.500" },
-      _focus: { boxShadow: "none", outlineColor: "none" },
     },
     homeEditorialLink: {
       color: "#161616",
       textDecoration: "underline",
       _hover: { color: "#2A2A2A" },
-      _focus: { boxShadow: "none", outlineColor: "none" },
     },
     postuler: {
       background: "bluefrance.500",
@@ -60,7 +47,6 @@ const Link = {
         textDecoration: "underline",
         textUnderlineOffset: "5px",
       },
-      _focus: { boxShadow: "0 0 0 3px rgba(66, 153, 225, 0.6)" },
     },
   },
 }
