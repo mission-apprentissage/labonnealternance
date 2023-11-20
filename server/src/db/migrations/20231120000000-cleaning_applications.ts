86e52b2c281127c6251ff9b002fced677b423e77
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-20 18:18:44 +0100

    feat: new migrate

---
+++ server/src/db/migrations/20231120000000-cleaning_applications.ts
@@ -0,0 +1,25 @@
+import { Db } from "mongodb"
+
+import { logger } from "@/common/logger"
+
+export const up = async (db: Db) => {
+  await db.collection("applications").updateMany(
+    { company_naf: null },
+    { $set: { company_naf: "" } },
+    {
+      // @ts-expect-error bypassDocumentValidation is not properly set in @types/mongodb
+      bypassDocumentValidation: true,
+    }
+  )
+
+  await db.collection("applications").updateMany(
+    { job_title: null },
+    { $set: { job_title: "" } },
+    {
+      // @ts-expect-error bypassDocumentValidation is not properly set in @types/mongodb
+      bypassDocumentValidation: true,
+    }
+  )
+
+  logger.info("20231120000000-cleaning_applications")
+}
