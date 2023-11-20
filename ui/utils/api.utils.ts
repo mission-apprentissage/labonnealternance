4c52e770af3c995de54b08afe852b0ce04af6870
Kevin Barnoin <k.barnoin@gmail.com>
2023-11-20 12:06:12 +0100

    Update ui/utils/api.utils.ts
    Co-authored-by: Rémy Auricoste <remy.auricoste@gmail.com>

--- ui/utils/api.utils.ts
+++ ui/utils/api.utils.ts
@@ -79,13 +79,6 @@ async function getHeaders(options: IRequestOptions) {
 
 const removeAtEnd = (url: string, removed: string): string => (url.endsWith(removed) ? url.slice(0, -removed.length) : url)
 
-const getAccessToken = () => {
-  if (typeof window !== "undefined") {
-    const token = new URLSearchParams(window.location.search).get("token")
-    return token
-  }
-}
-
 export function generateUrl(path: string, options: WithQueryStringAndPathParam = {}): string {
   const params = "params" in options ? options.params : {}
   const querystring = "querystring" in options ? options.querystring : {}
