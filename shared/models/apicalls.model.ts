99716bd2cec6ba523a8eec8691ffc41c34470014
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-22 18:24:50 +0100

    feat: oups

---
+++ shared/models/apicalls.model.ts
@@ -0,0 +1,16 @@
+import { z } from "../helpers/zodWithOpenApi"
+
+import { zObjectId } from "./common"
+
+export const ZApiCall = z
+  .object({
+    _id: zObjectId,
+    caller: z.string(),
+    api_path: z.string(),
+    response: z.string(),
+    result_count: z.number(),
+    job_count: z.number(),
+    training_count: z.number(),
+    created_at: z.date(),
+  })
+  .strict()
