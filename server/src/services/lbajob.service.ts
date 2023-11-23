df4d5fe397829591cf4ed904b9afe74cc5fb30df
Kevin Barnoin <k.barnoin@gmail.com>
2023-11-23 14:28:07 +0100

    feat:  no description if length is below 50 chars

--- server/src/services/lbajob.service.ts
+++ server/src/services/lbajob.service.ts
@@ -357,7 +357,7 @@ function transformLbaJob({
       diplomaLevel: offre.job_level_label || null,
       job: {
         id: offre._id.toString(),
-        description: offre.job_description || null,
+        description: offre.job_description && offre.job_description.length > 50 ? offre.job_description : null,
         employeurDescription: offre.job_employer_description || null,
         creationDate: offre.job_creation_date ? new Date(offre.job_creation_date) : null,
         contractType: offre.job_type ? offre.job_type.join(", ") : null,
