cb95f0cc5149e2ce059c0748c045d64c42b2c8ed
Rémy Auricoste <remy.auricoste@gmail.com>
2023-11-23 09:31:18 +0100

    fix: utilisation de la regex dans mongo (#847)

--- shared/models/unsubscribeOF.model.ts
+++ shared/models/unsubscribeOF.model.ts
@@ -1,8 +1,11 @@
 import { extensions } from "../helpers/zodHelpers/zodPrimitives"
 import { z } from "../helpers/zodWithOpenApi"
 
+import { zObjectId } from "./common"
+
 export const ZUnsubscribeOF = z
   .object({
+    _id: zObjectId,
     catalogue_id: z.string().describe("Id de l'organisme dans le catalogue"),
     establishment_siret: extensions.siret.describe("Le Siret de l'organisme de formation"),
     unsubscribe_date: z.coerce.date().describe("Date de désinscription"),
