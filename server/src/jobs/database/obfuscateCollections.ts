8f33a941e1b87be961859074d52db4d6f0eabd54
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-27 15:40:13 +0100

    feat: balle à kevin pour recruiters et userRecruteurs

--- server/src/jobs/database/obfuscateCollections.ts
+++ server/src/jobs/database/obfuscateCollections.ts
@@ -1,26 +1,15 @@
-// import { captureException } from "@sentry/node"
 import { Model } from "mongoose"
 
-// import { logger } from "@/common/logger"
 import { logger } from "@/common/logger"
 import {
   Application,
-  // Appointment,
-  // AppointmentDetailed,
-  // Credential,
-  // EligibleTrainingsForAppointment,
-  // Etablissement,
-  // FormationCatalogue,
-  // Job,
-  // LbaCompany,
-  // LbaCompanyLegacy,
-  // Optout,
+  EligibleTrainingsForAppointment,
+  Etablissement,
+  FormationCatalogue,
+  LbaCompany,
   // Recruiter,
-  // ReferentielOpco,
-  // UnsubscribeOF,
-  // UnsubscribedLbaCompany,
+  UnsubscribedLbaCompany,
   // UserRecruteur,
-  // eligibleTrainingsForAppointmentHistory,
   ApiCalls,
   AnonymizedApplication,
   Appointment,
@@ -30,13 +19,7 @@ import { Pagination } from "@/common/model/schema/_shared/mongoose-paginate"
 async function reduceModel<T>(model: Model<T> | Pagination<T>, limit = 20000) {
   logger.info(`reducing collection ${model.collection.name} to ${limit} latest documents`)
   try {
-    const aggregationPipeline = [
-      { $match: {} },
-      { $sort: { created_at: -1 } },
-      { $limit: limit },
-      { $group: { _id: null, minDate: { $min: "$created_at" } } },
-      { $project: { _id: 0, minDate: 1 } },
-    ]
+    const aggregationPipeline = [{ $match: {} }, { $sort: { created_at: -1 } }, { $skip: limit }, { $limit: 1 }, { $project: { _id: 0, minDate: "$created_at" } }]
 
     const result = await model.aggregate(aggregationPipeline)
 
@@ -59,6 +42,7 @@ const obfuscateApplications = async () => {
       applicant_first_name: "prenom",
       applicant_attachment_name: "titre_cv.pdf",
       applicant_message_to_company: "Cher recruteur, embauchez moi ...",
+      company_feedback: "Cher candidat ...",
       company_email: "faux_email@faux-domaine-compagnie.com",
     }
   )
@@ -76,6 +60,86 @@ const obfuscateAppointments = async () => {
   )
 }
 
+const obfuscateLbaCompanies = async () => {
+  logger.info(`obfuscating lbacompanies`)
+  await LbaCompany.updateMany(
+    {},
+    {
+      email: "faux_email@faux-domaine-compagnie.com",
+      phone: "0601010106",
+    }
+  )
+
+  await UnsubscribedLbaCompany.updateMany(
+    {},
+    {
+      email: "faux_email@faux-domaine-compagnie.com",
+      phone: "0601010106",
+    }
+  )
+}
+
+const obfuscateElligibleTrainingsForAppointment = async () => {
+  logger.info(`obfuscating elligible trainings for appointments`)
+  await EligibleTrainingsForAppointment.updateMany(
+    {},
+    {
+      lieu_formation_email: "faux_email@faux-domaine-compagnie.com",
+    }
+  )
+}
+
+const obfuscateEtablissements = async () => {
+  logger.info(`obfuscating etablissements`)
+  await Etablissement.updateMany(
+    {},
+    {
+      gestionnaire_email: "faux_email@faux-domaine-compagnie.com",
+    }
+  )
+}
+
+const obfuscateFormations = async () => {
+  logger.info(`obfuscating formations`)
+  await FormationCatalogue.updateMany(
+    {},
+    {
+      email: "faux_email@faux-domaine-compagnie.com",
+      etablissement_gestionnaire_courriel: "faux_email@faux-domaine-compagnie.com",
+      etablissement_formateur_courriel: "faux_email@faux-domaine-compagnie.com",
+      num_tel: "0601010106",
+    }
+  )
+}
+
+// // TODO Kevin
+// const obfuscateRecruiter = async () => {
+//   logger.info(`obfuscating recruiters`)
+//   await Recruiter.updateMany(
+//     {},
+//     {
+//       //email: "faux_email@faux-domaine-compagnie.com", TODO should be unique
+//       phone: "0601010106",
+//       last_name: "nom_famille",
+//       first_name: "prenom",
+//     }
+//   )
+// }
+
+// // TODO Kevin
+// const obfuscateUserRecruiter = async () => {
+//   logger.info(`obfuscating recruiters`)
+//   await UserRecruteur.updateMany(
+//     {},
+//     {
+//       //email: "faux_email@faux-domaine-compagnie.com", TODO should be unique
+//       phone: "0601010106",
+//       last_name: "nom_famille",
+//       first_name: "prenom",
+//     }
+//   )
+// }
+
 export async function obfuscateCollections(): Promise<void> {
   await reduceModel(ApiCalls)
   await reduceModel(Application, 50000)
@@ -84,4 +148,10 @@ export async function obfuscateCollections(): Promise<void> {
 
   await obfuscateApplications()
   await obfuscateAppointments()
+  await obfuscateLbaCompanies()
+  await obfuscateElligibleTrainingsForAppointment()
+  await obfuscateEtablissements()
+  await obfuscateFormations()
+  // await obfuscateRecruiter()
+  // await obfuscateRecruiter()
 }
