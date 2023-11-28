bccc8d516ab6ec5ccb754d33df18c82f1bf13ee4
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-11-28 17:22:18 +0100

    feat: rework

--- server/src/jobs/rdv/anonymizeUsers.ts
+++ server/src/jobs/rdv/anonymizeUsers.ts
@@ -1,27 +1,48 @@
-import { logger } from "../../common/logger"
-import { User } from "../../common/model/index"
+import { oleoduc, writeData } from "oleoduc"
 
-export const anonimizeUsers = async () => {
-  logger.info("job #anonimizeUsers start")
-  const archiveThreshold = new Date()
-  archiveThreshold.setFullYear(archiveThreshold.getFullYear() - 1)
+import { logger } from "@/common/logger"
+import { IUser } from "@/common/model/schema/user/user.types"
 
-  const result = await User.updateMany(
-    { last_action_date: { $lte: archiveThreshold } },
-    {
-      $set: {
-        username: null,
-        password: null,
-        firstname: null,
-        lastname: null,
-        phone: null,
-        email: null,
-        role: null,
-        is_anonymized: true,
+import { AnonymizedUser, User } from "../../common/model/index"
+import dayjs from "../../services/dayjs.service"
+
+/**
+ * Anonymize users older than 2 years.
+ */
+export const anonymizeUsers = async () => {
+  logger.info("Cron #anonymizeUsers started.")
+
+  const stats = {
+    AncientUsersCount: 0,
+    NewUsersCount: 0,
+  }
+
+  stats.AncientUsersCount = await User.countDocuments()
+
+  const anonymizeUsersOlderThanDate = dayjs().subtract(2, "years").format("YYYY-MM-DD")
+
+  await oleoduc(
+    User.find({ created_at: { $lte: anonymizeUsersOlderThanDate } })
+      .lean()
+      .cursor(),
+    writeData(
+      async (user: IUser) => {
+        await AnonymizedUser.create({
+          userId: user._id,
+          type: user.type,
+          role: user.role,
+          last_action_date: user.last_action_date,
+        })
       },
-    }
+      { parallel: 100 }
+    )
   )
-  logger.info("job #anonimizeUsers done")
 
-  return result
+  await User.deleteMany({ created_at: { $lte: anonymizeUsersOlderThanDate } })
+
+  stats.AncientUsersCount = await User.countDocuments()
+
+  logger.info("Cron #anonymizeUsers done.")
+
+  return stats
 }
