564811edcff88e8316a0847084e54c84ec900e4c
Kevin Barnoin <k.barnoin@gmail.com>
2023-11-28 10:12:44 +0100

    refactor: remove username

--- server/tests/integration/common/users.test.ts
+++ server/tests/integration/common/users.test.ts
@@ -8,36 +8,38 @@ import { User } from "../../../src/common/model/index"
 import { ROLES } from "../../../src/services/constant.service"
 import { createUser } from "../../../src/services/user.service"
 
+const userTest = { lastname: "lastname", firstname: "firstname" }
+
 describe("users", () => {
   useMongo()
 
   it("Permet de créer un utilisateur", async () => {
-    const created = await createUser("user", {})
-    assert.strictEqual(created.username, "user")
+    const created = await createUser(userTest)
+    assert.strictEqual(created.firstname, "user")
 
-    const found = await User.findOne({ username: "user" })
-    assert.strictEqual(found?.username, "user")
+    const found = await User.findOne({ firstname: "firstname" })
+    assert.strictEqual(found?.firstname, "firstname")
   })
 
   it("Permet de créer un utilisateur avec le role d'administrateur", async () => {
-    const user = await createUser("userAdmin", { role: ROLES.administrator })
-    const found = await User.findOne({ username: "userAdmin" })
+    const user = await createUser({ role: ROLES.administrator, ...userTest })
+    const found = await User.findOne({ firstname: "firstname", role: ROLES.administrator })
 
     assert.strictEqual(user.role, ROLES.administrator)
     assert.strictEqual(found?.role, ROLES.administrator)
   })
 
   it("Permet de créer un utilisateur avec le role de candidat", async () => {
-    const user = await createUser("userCandidat", { role: ROLES.candidat })
-    const found = await User.findOne({ username: "userCandidat" })
+    const user = await createUser({ role: ROLES.candidat, ...userTest })
+    const found = await User.findOne({ firstname: "firstname", role: ROLES.candidat })
 
     assert.strictEqual(user.role, ROLES.candidat)
     assert.strictEqual(found?.role, ROLES.candidat)
   })
 
   it("Permet de créer un utilisateur avec le role de cfa", async () => {
-    const user = await createUser("userCfa", { role: ROLES.cfa })
-    const found = await User.findOne({ username: "userCfa" })
+    const user = await createUser({ role: ROLES.cfa, ...userTest })
+    const found = await User.findOne({ firstname: "firstname", role: ROLES.cfa })
 
     assert.strictEqual(user.role, ROLES.cfa)
     assert.strictEqual(found?.role, ROLES.cfa)
