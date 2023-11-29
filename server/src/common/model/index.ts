3ed61c30400b551d86977fa6dcb7ec4c1fe66c66
Alan Le Ruyet <alan.leruyet@free.fr>
2023-11-29 18:35:40 +0100

    feat: cleaning

--- server/src/common/model/index.ts
+++ server/src/common/model/index.ts
@@ -23,7 +23,6 @@ import InternalJobs from "./schema/internalJobs/internalJobs.schema"
 import Job from "./schema/jobs/jobs.schema"
 import LbaCompany from "./schema/lbaCompany/lbaCompany.schema"
 import LbaCompanyLegacy from "./schema/lbaCompanylegacy/lbaCompanyLegacy.schema"
-import LbaCompanyNonDiffusible from "./schema/lbaCompanyNonDiffusible/lbaCompanyNonDiffusible.schema"
 import Opco from "./schema/opco/opco.schema"
 import Optout from "./schema/optout/optout.schema"
 import Recruiter from "./schema/recruiter/recruiter.schema"
@@ -91,7 +90,6 @@ export {
   Job,
   LbaCompany,
   LbaCompanyLegacy,
-  LbaCompanyNonDiffusible,
   Opco,
   Optout,
   Recruiter,
--- server/src/common/model/schema/lbaCompanyNonDiffusible/lbaCompanyNonDiffusible.schema.ts
+++
@@ -1,6 +0,0 @@
-import { ILbaCompany } from "shared"
-
-import { model } from "../../../mongodb"
-import { lbaCompanySchema } from "../lbaCompany/lbaCompany.schema.js"
-
-export default model<ILbaCompany>("bonnesboitesnondiffusibles", lbaCompanySchema)
