23bedb38b76ee8b6281e30e159fd9e2d584041c0
Kevin Barnoin <k.barnoin@gmail.com>
2023-12-05 16:27:30 +0100

    fix: typo

--- server/src/jobs/verifications/controlApplications.ts
+++ server/src/jobs/verifications/controlApplications.ts
@@ -5,8 +5,8 @@ import { notifyToSlack } from "../../common/utils/slackUtils"
 
 export const controlApplications = async () => {
   const timestamp = dayjs().subtract(1, "hour").toDate()
-  const countAppointments = await Application.countDocuments({ created_at: { $gte: timestamp } })
-  if (countAppointments === 0) {
+  const countApplications = await Application.countDocuments({ created_at: { $gte: timestamp } })
+  if (countApplications === 0) {
     await notifyToSlack({ subject: "Verification des candidatures", message: "Aucune candidature prise dans la dernière heure" })
   }
 }
