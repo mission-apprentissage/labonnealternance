ab43dcded6426a736b3b0fec7a9642ff83c2090e
Alan Le Ruyet <alan.leruyet@free.fr>
2023-12-11 11:24:33 +0100

    fix: import

--- server/src/common/model/schema/unsubscribedLbaCompany/unsubscribedLbaCompany.schema.ts
+++ server/src/common/model/schema/unsubscribedLbaCompany/unsubscribedLbaCompany.schema.ts
@@ -1,7 +1,7 @@
 import { IUnsubscribedLbaCompany } from "shared"
 
 import { model, Schema } from "../../../mongodb.js"
-import lbaCompanySchema from "../lbaCompany/lbaCompany.schema.js"
+import { lbaCompanySchema } from "../lbaCompany/lbaCompany.schema.js"
 
 const { siret, raison_sociale, enseigne, naf_code, naf_label, rome_codes, insee_city_code, zip_code, city, company_size, created_at, last_update_at } = lbaCompanySchema.obj
 const unsubscribedLbaCompanySchema = new Schema<IUnsubscribedLbaCompany>(
