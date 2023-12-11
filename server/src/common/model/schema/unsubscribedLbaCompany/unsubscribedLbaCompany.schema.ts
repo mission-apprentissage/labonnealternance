d09e1873ecb9a7f411b50ed84797ec683fcceaeb
Alan Le Ruyet <alan.leruyet@free.fr>
2023-12-11 11:21:38 +0100

    feat: définition du modèle

--- server/src/common/model/schema/unsubscribedLbaCompany/unsubscribedLbaCompany.schema.ts
+++ server/src/common/model/schema/unsubscribedLbaCompany/unsubscribedLbaCompany.schema.ts
@@ -1,11 +1,23 @@
 import { IUnsubscribedLbaCompany } from "shared"
 
 import { model, Schema } from "../../../mongodb.js"
-import { lbaCompanySchema } from "../lbaCompany/lbaCompany.schema.js"
+import lbaCompanySchema from "../lbaCompany/lbaCompany.schema.js"
 
+const { siret, raison_sociale, enseigne, naf_code, naf_label, rome_codes, insee_city_code, zip_code, city, company_size, created_at, last_update_at } = lbaCompanySchema.obj
 const unsubscribedLbaCompanySchema = new Schema<IUnsubscribedLbaCompany>(
   {
-    ...lbaCompanySchema.obj,
+    siret,
+    raison_sociale,
+    enseigne,
+    naf_code,
+    naf_label,
+    rome_codes,
+    insee_city_code,
+    zip_code,
+    city,
+    company_size,
+    created_at,
+    last_update_at,
     unsubscribe_date: {
       type: Date,
       default: Date.now,
