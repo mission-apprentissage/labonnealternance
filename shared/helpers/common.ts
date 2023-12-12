97fb907d33e19cb5392c98f968c3438eb3e6e44f
Alan Le Ruyet <alan.leruyet@free.fr>
2023-12-12 16:15:52 +0100

    feat: ajout {

--- shared/helpers/common.ts
+++ shared/helpers/common.ts
@@ -2,7 +2,7 @@ export const getSirenFromSiret = (siret: string) => siret.substring(0, 9)
 
 export const cleanEmail = (email: string) => {
   let cleanedEmail = email.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-  cleanedEmail = cleanedEmail.replace(new RegExp(`[’£'^!&=/*?}\\s]`, "gi"), "")
+  cleanedEmail = cleanedEmail.replace(new RegExp(`[’£'^!&=/*?{}\\s]`, "gi"), "")
   cleanedEmail = cleanedEmail.replace(new RegExp("œ", "gi"), "o")
   return cleanedEmail
 }
