1b0183e1437f230f568dba423f76c727e1ea4ef2
Kevin Barnoin <k.barnoin@gmail.com>
2023-12-15 11:39:54 +0100

    refactor: service

--- server/src/services/login.service.ts
+++ server/src/services/login.service.ts
@@ -1,14 +1,23 @@
+import Boom from "boom"
 import { IUserRecruteur } from "shared"
 import { ETAT_UTILISATEUR } from "shared/constants/recruteur"
 
 import { getUserStatus } from "./userRecruteur.service"
 
-export const controlUserState = (status: IUserRecruteur["status"]): { error: boolean; reason: string } | undefined => {
+export const controlUserState = (status: IUserRecruteur["status"]): { error: boolean; reason: string } => {
   const currentState = getUserStatus(status)
-  if (!currentState || [ETAT_UTILISATEUR.ATTENTE, ETAT_UTILISATEUR.ERROR].includes(currentState)) {
-    return { error: true, reason: "VALIDATION" }
-  }
-  if (currentState === ETAT_UTILISATEUR.DESACTIVE) {
-    return { error: true, reason: "DISABLED" }
+  switch (currentState) {
+    case ETAT_UTILISATEUR.ATTENTE:
+    case ETAT_UTILISATEUR.ERROR:
+      return { error: true, reason: "VALIDATION" }
+
+    case ETAT_UTILISATEUR.DESACTIVE:
+      return { error: true, reason: "DISABLED" }
+
+    case ETAT_UTILISATEUR.VALIDE:
+      return { error: false, reason: "" }
+
+    default:
+      throw Boom.badRequest("L'état utilisateur est inconnu")
   }
 }
