d191ee22eeee245347140a05eecffe996ce5397a
Rémy Auricoste <remy.auricoste@gmail.com>
2023-12-21 16:37:41 +0100

    fix: smtpClient: ajout du basic auth

--- cypress/api/smtpClient.ts
+++ cypress/api/smtpClient.ts
@@ -15,14 +15,27 @@ type MailDetail = MailSummary & { Text: string }
 
 export const smtpClient = {
   getMail(to: string, includedSubject: string, limit = 20) {
+    const baseUrl = Cypress.env("smtp")
+    const login = Cypress.env("SMTP_LOGIN")
+    const password = Cypress.env("SMTP_PASSWORD")
+    const Authorization = login && password ? `Basic ${btoa(`${login}:${password}`)}` : undefined
+    const headers = Authorization ? { Authorization } : undefined
     return cy
-      .request("GET", `${Cypress.env("smtp")}/api/v1/messages?limit=${limit}`)
+      .request({
+        method: "GET",
+        url: `${baseUrl}/api/v1/messages?limit=${limit}`,
+        headers,
+      })
       .then((messagesResponse: Cypress.Response<MessagesResponse>) => {
         const messageOpt = messagesResponse.body.messages.find((message) => message.To[0].Address === to.toLowerCase() && message.Subject.includes(includedSubject))
         if (!messageOpt) {
           throw new Error(`could not find mail sent to ${to} with subject=${includedSubject}`)
         }
-        return cy.request("GET", `${Cypress.env("smtp")}/api/v1/message/${messageOpt.ID}`)
+        return cy.request({
+          method: "GET",
+          url: `${baseUrl}/api/v1/message/${messageOpt.ID}`,
+          headers,
+        })
       })
       .then((detailResponse: Cypress.Response<MailDetail>) => {
         return detailResponse.body.Text
