0330133966c4d60b65f7ff3c1895765c5bf296ef
Kevin Barnoin <k.barnoin@gmail.com>
2023-12-22 14:51:59 +0100

    feat: anonymize userrecruteurs & recruiters

--- server/src/jobs/anonymization/anonymizeIndividual.ts
+++ server/src/jobs/anonymization/anonymizeIndividual.ts
@@ -1,7 +1,72 @@
 import { ObjectId } from "mongodb"
+import { CFA, ENTREPRISE } from "shared/constants/recruteur"
 
 import { logger } from "../../common/logger"
-import { AnonymizedUser, Application, User } from "../../common/model/index"
+import { AnonymizedUser, Application, Recruiter, User, UserRecruteur } from "../../common/model/index"
+
+const anonimizeUserRecruteur = (_id: string) =>
+  UserRecruteur.aggregate([
+    {
+      $match: { _id },
+    },
+    {
+      $project: {
+        opco: 1,
+        idcc: 1,
+        establishment_raison_sociale: 1,
+        establishment_enseigne: 1,
+        establishment_siret: 1,
+        address_detail: 1,
+        address: 1,
+        geo_coordinates: 1,
+        scope: 1,
+        is_email_checked: 1,
+        type: 1,
+        establishment_id: 1,
+        last_connection: 1,
+        origin: 1,
+        status: 1,
+        is_qualiopi: 1,
+      },
+    },
+    {
+      $merge: "anonymizeduserrecruteurs",
+    },
+  ])
+const anonimizeRecruiter = (query: object) =>
+  Recruiter.aggregate([
+    {
+      $match: query,
+    },
+    {
+      $project: {
+        establishment_id: 1,
+        establishment_raison_sociale: 1,
+        establishment_enseigne: 1,
+        establishment_siret: 1,
+        address_detail: 1,
+        address: 1,
+        geo_coordinates: 1,
+        is_delegated: 1,
+        cfa_delegated_siret: 1,
+        jobs: 1,
+        origin: 1,
+        opco: 1,
+        idcc: 1,
+        status: 1,
+        naf_code: 1,
+        naf_label: 1,
+        establishment_size: 1,
+        establishment_creation_date: 1,
+      },
+    },
+    {
+      $merge: "anonymizedrecruiteurs",
+    },
+  ])
+
+const deleteRecruiter = (query) => Recruiter.deleteMany(query)
+const deleteUserRecruteur = (query) => UserRecruteur.deleteMany(query)
 
 const anonymizeApplication = async (_id: string) => {
   await Application.aggregate([
@@ -48,6 +113,30 @@ const anonymizeUser = async (_id: string) => {
   }
 }
 
+const anonymizeUserRecruterAndRecruiter = async (_id: string) => {
+  const user = await UserRecruteur.findById(_id).lean()
+
+  if (!user) {
+    throw new Error("Anonymize userRecruter not found")
+  }
+
+  switch (user.type) {
+    case ENTREPRISE:
+      await Promise.all([anonimizeUserRecruteur(user._id.toString()), anonimizeRecruiter({ establishment_id: user.establishment_id })])
+      await Promise.all([deleteUserRecruteur({ _id: user._id }), deleteRecruiter({ establishment_id: user.establishment_id })])
+
+      break
+    case CFA:
+      await Promise.all([anonimizeUserRecruteur(user._id.toString()), anonimizeRecruiter({ cfa_delegated_siret: user.establishment_siret })])
+      await Promise.all([deleteUserRecruteur({ _id: user._id }), deleteRecruiter({ cfa_delegated_siret: user.establishment_siret })])
+
+      break
+
+    default:
+      throw new Error(`Anonymize ${user.type} is not permitted. script must be updated manually to delete this type of user.`)
+  }
+}
+
 export async function anonymizeIndividual(payload: { collection: string; id: string }): Promise<void> {
   const { collection, id } = payload
 
@@ -61,6 +150,7 @@ export async function anonymizeIndividual(payload: { collection: string; id: str
       break
     }
     case "userrecruteurs": {
+      await anonymizeUserRecruterAndRecruiter(id)
       break
     }
     default:
