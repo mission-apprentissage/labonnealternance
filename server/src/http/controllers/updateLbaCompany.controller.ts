bbbe3e321287f71f6274627b049b1fbed20abf2e
Kevin Barnoin <k.barnoin@gmail.com>
2023-12-29 10:52:34 +0100

    fix: update lba companies (#959)
    * fix: update lba companies
    
    * fix: yarn lock dedupe

--- server/src/http/controllers/updateLbaCompany.controller.ts
+++ server/src/http/controllers/updateLbaCompany.controller.ts
@@ -1,8 +1,7 @@
-import Joi from "joi"
+import Boom from "boom"
 import { zRoutes } from "shared/index"
 
 import config from "../../config"
-import { REGEX } from "../../services/constant.service"
 import { updateContactInfo } from "../../services/lbacompany.service"
 import { Server } from "../server"
 
@@ -19,24 +18,14 @@ export default function (server: Server) {
       },
     },
     async (req, res) => {
-      const { email, phone, siret } = await Joi.object({
-        secret: Joi.string().required(),
-        email: Joi.string().allow("").email(),
-        phone: Joi.string().pattern(REGEX["TELEPHONE"]).allow(""),
-        siret: Joi.string().pattern(REGEX["SIRET"]).required(),
-      }).validateAsync(req.query)
+      const { email, phone, siret } = req.query
 
       if (req.query.secret !== config.secretUpdateRomesMetiers) {
-        return res.status(401).send("unauthorized")
-      } else {
-        const result = await updateContactInfo({ email, phone, siret })
-
-        if (result === "not_found") {
-          return res.status(404).send(result)
-        }
-
-        return res.status(200).send(result)
+        throw Boom.unauthorized()
       }
+
+      await updateContactInfo({ email, phone, siret })
+      return res.status(200).send("OK")
     }
   )
 }
