988845e5d3a0f8c9f7cc4b769a769d4ece099194
Kevin Barnoin <k.barnoin@gmail.com>
2024-01-02 14:21:06 +0100

    fix: rncp search (#958)
    * fix: rncp search
    
    * fix: keep rncp in search url
    
    * fix: rome params + storage issue
    
    * fix: widgetparameters value

--- ui/utils/getSearchParameters.ts
+++ ui/utils/getSearchParameters.ts
@@ -3,15 +3,31 @@ export const getSearchQueryParameters = (searchParameters) => {
     return ""
   }
 
-  const result = `job_name=${searchParameters?.job.label ? encodeURIComponent(searchParameters?.job.label) : ""}&romes=${searchParameters?.job.romes.toString()}${
-    searchParameters.diploma ? "&diploma=" + searchParameters.diploma : ""
-  }&radius=${searchParameters.radius || 30}${
-    searchParameters?.location?.value
-      ? `&lat=${searchParameters.location.value.coordinates[1]}&lon=${searchParameters.location.value.coordinates[0]}&zipcode=${searchParameters.location.zipcode || ""}&insee=${
-          searchParameters.location.insee || ""
-        }&address=${searchParameters.location.label ? encodeURIComponent(searchParameters.location.label) : ""}`
-      : ""
-  }${searchParameters?.opcoFilter ? `&opco=${searchParameters.opcoFilter}` : ""}${searchParameters?.opcoUrlFilter ? `&opcoUrl=${searchParameters.opcoUrlFilter}` : ""}`
+  const {
+    job: { label: jobLabel, romes, rncp },
+    diploma,
+    radius = 30,
+    location,
+    opcoFilter,
+    opcoUrlFilter,
+  } = searchParameters
+
+  const jobNameParam = jobLabel ? `job_name=${encodeURIComponent(jobLabel)}` : ""
+  const romesParam = romes !== "" ? `&romes=${romes.toString()}` : ""
+  const rncpParam = romes === "" ? `&rncp=${rncp}` : ""
+  const diplomaParam = diploma ? `&diploma=${diploma}` : ""
+  const radiusParam = `&radius=${radius}`
+
+  let locationParams = ""
+  if (location?.value) {
+    const { coordinates, zipcode = "", insee = "", label: locationLabel } = location.value
+    locationParams = `&lat=${coordinates[1]}&lon=${coordinates[0]}&zipcode=${zipcode}&insee=${insee}&address=${locationLabel ? encodeURIComponent(locationLabel) : ""}`
+  }
+
+  const opcoFilterParam = opcoFilter ? `&opco=${opcoFilter}` : ""
+  const opcoUrlFilterParam = opcoUrlFilter ? `&opcoUrl=${opcoUrlFilter}` : ""
+
+  const result = `${jobNameParam}${romesParam}${rncpParam}${diplomaParam}${radiusParam}${locationParams}${opcoFilterParam}${opcoUrlFilterParam}`
 
   return result
 }
