6cac1b904ac6c659f445bf6437161afb6d829037
Alan Le Ruyet <alan.leruyet@free.fr>
2024-01-11 15:40:24 +0100

    feat: nettoyage du réindexeur

--- server/src/jobs/lba_recruteur/indexes/generateIndexes.ts
+++ server/src/jobs/lba_recruteur/indexes/generateIndexes.ts
@@ -1,44 +1,24 @@
 import { captureException } from "@sentry/node"
 
 import { logger } from "../../../common/logger"
-import { DiplomesMetiers, DomainesMetiers, FormationCatalogue, LbaCompany, Recruiter } from "../../../common/model/index"
+import { Recruiter } from "../../../common/model/index"
 import { rebuildIndex } from "../../../common/utils/esUtils"
 
 export const generateIndexes = async (payload: { index_list?: string; recreate?: boolean }) => {
-  const indexList = payload?.index_list ?? "recruiters,formationcatalogues,lbacompanies,diplomesmetiers,domainesmetiers"
+  const indexList = payload?.index_list ?? "recruiters"
   const recreate = payload?.recreate ?? false
   const list = indexList.split(",")
 
   const results = await Promise.allSettled(
     list.map(async (index) => {
       switch (index) {
-        case "domainesmetiers": {
-          await DomainesMetiers.syncIndexes()
-          await rebuildIndex(DomainesMetiers, { skipNotFound: true, recreate })
-          break
-        }
-        case "diplomesmetiers": {
-          await DiplomesMetiers.syncIndexes()
-          await rebuildIndex(DiplomesMetiers, { skipNotFound: true, recreate })
-          break
-        }
-        case "lbacompanies": {
-          await LbaCompany.syncIndexes()
-          await rebuildIndex(LbaCompany, { skipNotFound: true, recreate })
-          break
-        }
-        case "formationcatalogues": {
-          await FormationCatalogue.syncIndexes()
-          await rebuildIndex(FormationCatalogue, { skipNotFound: true, recreate })
-          break
-        }
         case "recruiters": {
           await Recruiter.syncIndexes()
           await rebuildIndex(Recruiter, { skipNotFound: true, recreate })
           break
         }
         default: {
-          logger.error(`Attention l'index "${index}" ne fait pas partie de la liste elligible : recruiters,formationcatalogues,lbacompanies,diplomesmetiers,domainesmetiers`)
+          logger.error(`Attention l'index "${index}" ne fait pas partie de la liste elligible : recruiters`)
           break
         }
       }
