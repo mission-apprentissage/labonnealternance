7a0f4918d9db73fd7f452de6fccd3e67b70c4c6f
Alan Le Ruyet <alan.leruyet@free.fr>
2024-01-24 19:03:49 +0100

    feat: c'est long ...

--- ui/components/Ressources/ressourcesRecruteur.tsx
+++ ui/components/Ressources/ressourcesRecruteur.tsx
@@ -4,7 +4,19 @@ import React from "react"
 const RessourcesRecruteur = () => {
   return (
     <>
-      <Box>Ressources recruteur</Box>
+      <Box>
+        La bonne alternance vous propose un ensemble d’outils et de liens pour faciliter vos démarches liées à l’alternance. Liens utiles Estimez le coût d’un alternant pour votre
+        entreprise avec le simulateur de l’URSSAF. En savoir plus En janvier dernier, le Gouvernement a annoncé son nouveau plan d’aide aux entreprises, allant jusqu’à 6000 euros
+        pour toutes les embauches d'alternants en 2023 et 2024. Ces aides sont valables jusqu'en 2027. En savoir plus Retrouvez les informations légales de votre entreprise sur
+        l’annuaire des entreprises, à partir de votre SIRET. En savoir plus Chaque entreprise est rattachée à un Opérateur de compétences (OPCO). Il s’agit de votre interlocuteur
+        de proximité pour vos démarches liées à l’alternance (financement des contrats, formation, ...). Votre OPCO peut vous aider à affiner vos besoins de recrutement. Aussi,
+        sachez qu’en déposant une offre d’emploi en alternance sur le site de votre OPCO, celle-ci sera rediffusée sur les sites consultés par les jeunes. Vous ne connaissez pas
+        votre OPCO ? Retrouvez votre OPCO sur France compétences Quelle est la différence entre un contrat en apprentissage et un contrat de professionnalisation ? Le portail de
+        l’alternance vous explique les différences. En savoir plus Le Précis de l’apprentissage vous présente des repères sur l’apprentissage. Il est issu des travaux de la DGEFP
+        et d’une consultation des acteurs institutionnels de l’apprentissage. Il répond à l’objectif d’harmoniser les pratiques des acteurs de l’apprentissage et vise à donner des
+        repères juridiques et des clefs de compréhension autour de bases documentaires et méthodologiques communes. Diffusez simplement et gratuitement vos offres en alternance
+        Accéder à la page d’accueil Vous avez une question sur le fonctionnement de notre plateforme ? Consulter la FAQ
+      </Box>
     </>
   )
 }
