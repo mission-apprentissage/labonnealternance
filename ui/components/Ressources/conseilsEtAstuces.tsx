ce095307aac3f36e5a21afa339bd9ac7f836b53f
Alan Le Ruyet <alan.leruyet@free.fr>
2024-01-26 11:02:33 +0100

    feat: accordion behaviour ok

--- ui/components/Ressources/conseilsEtAstuces.tsx
+++ ui/components/Ressources/conseilsEtAstuces.tsx
@@ -3,7 +3,7 @@ import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPane
 
 const ConseilsEtAstuces = () => {
   return (
-    <Accordion defaultIndex={[0]} allowMultiple>
+    <Accordion allowMultiple={false} allowToggle defaultIndex={[-1]}>
       <AccordionItem borderTop="1px solid" borderTopColor="#DDD">
         <AccordionButton>
           <Box as="span" flex="1" textAlign="left">
@@ -65,19 +65,23 @@ const ConseilsEtAstuces = () => {
                 </Text>{" "}
                 Des conseillers vous aideront dans vos recherches d'entreprise. Trouver ma mission locale
               </ListItem>
+              https://www.unml.info/le-reseau/annuaire/
               <ListItem mb={4}>
                 <Text as="span" fontWeight={700}>
                   L'ANAF (Association Nationale des Apprentis de France)
                 </Text>{" "}
                 peut vous aider à tout moment de votre parcours et pour toute question que vous vous posez ! En savoir plus{" "}
               </ListItem>
+              https://www.anaf.fr/apprentissage/
               <ListItem mb={4}>
                 <Text as="span" fontWeight={700}>
                   Un mentor (ancien alternant ou adulte actif inséré dans la vie professionnelle)
                 </Text>{" "}
                 peut vous épauler tout au long de votre parcours et suivant vos besoins ! En savoir plus
               </ListItem>
+              https://www.mentorat-apprentissage.fr/
               <ListItem mb={4}>Profitez d'un accompagnement proche de chez vous pour votre parcours et vos démarches avec 1 jeune 1 solution</ListItem>
+              https://www.1jeune1solution.gouv.fr/accompagnement%20En%20savoir%20plus
             </UnorderedList>
           </Box>
         </AccordionPanel>
@@ -103,11 +107,13 @@ const ConseilsEtAstuces = () => {
                 {" "}
                 Combien de personnes ont trouvé un emploi dans les 6 mois après avoir obtenu le diplôme que vous souhaitez préparer ? La réponse ici ! En savoir plus
               </ListItem>
+              https://www.inserjeunes.education.gouv.fr/diffusion/accueil
               <ListItem mb={4}>
                 {" "}
                 Certains établissements de formation obtiennent des labels d'excellence, c'est par exemple le cas des CMQ (Campus des Métiers et Qualifications d'excellence). En
                 savoir plus
               </ListItem>
+              https://www.education.gouv.fr/les-campus-des-metiers-et-des-qualifications-5075
             </UnorderedList>
           </Box>
         </AccordionPanel>
@@ -125,14 +131,16 @@ const ConseilsEtAstuces = () => {
             <UnorderedList>
               <ListItem mb={4}>
                 Tout au long de l'année, il existe des salons de recrutement (physiques ou virtuels) spécialisés pour l'alternance : renseignez-vous ! Voir les salons l’Étudiant
-                Voir les salons Studyrama
+                https://www.letudiant.fr/etudes/salons.html Voir les salons Studyrama https://www.studyrama.com/salons
               </ListItem>
               <ListItem mb={4}>
                 {" "}
                 France Travail recense de nombreux évènements (conférences, salons, job dating, conférences en ligne…) pour vous aider dans vos recherches de contrat. En savoir
                 plus
               </ListItem>
+              https://mesevenementsemploi.pole-emploi.fr/mes-evenements-emploi/evenements
               <ListItem mb={4}>Le secteur public (mairies, ministères, départements, régions, ...) recrute aussi des alternants, pensez-y ! En savoir plus</ListItem>
+              https://www.pass.fonction-publique.gouv.fr/
               <ListItem mb={4}>
                 {" "}
                 Plus d’un employeur sur deux recrute sans déposer d'offre d'emploi : optimisez vos chances en adressant aussi des candidatures spontanées !
@@ -157,7 +165,9 @@ const ConseilsEtAstuces = () => {
                 Besoin d'aide pour construire un CV à partir de vos expériences ? Inscrivez-vous gratuitement sur Diagoriente et laissez-vous guider dans la construction d'un CV
                 pertinent, mettant en lumière vos compétences. En savoir plus
               </ListItem>
+              https://web-app.diagoriente.beta.gouv.fr/#/inscription?utm_source=lba&utm_campaign=lba-dec2021
               <ListItem mb={4}> Besoin d'aide pour concevoir un beau CV ? Vous pouvez le faire gratuitement sur CVdesignr. En savoir plus</ListItem>
+              https://cvdesignr.com/fr
               <ListItem mb={4}>
                 {" "}
                 Motivation, Dynamisme et Présentation soignée : 3 qualités recherchées par les employeurs de jeunes candidats. Mettez-les en avant dans votre candidature !
@@ -172,6 +182,7 @@ const ConseilsEtAstuces = () => {
                 Les employeurs qui embauchent des alternants reçoivent des aides, jusqu’à 6000€ selon le dernier décret : c'est un bon argument pour convaincre une entreprise qui
                 ne connaît pas l'alternance de vous embaucher !
               </ListItem>
+              https://entreprendre.service-public.fr/vosdroits/F23556
             </UnorderedList>
           </Box>
         </AccordionPanel>
@@ -191,11 +202,13 @@ const ConseilsEtAstuces = () => {
                 Se former en alternance ne vous coûte rien ! En plus de percevoir une rémunération, votre employeur prend en charge le financement de votre formation.
               </ListItem>
               <ListItem mb={4}> Quel sera votre salaire en alternance ? Faites une simulation sur le portail de l’alternance. En savoir plus </ListItem>
+              https://www.alternance.emploi.gouv.fr/simulateur-alternant/etape-1
               <ListItem mb={4}>
                 Le Précis de l’apprentissage vous présente des repères sur l’apprentissage. Il est issu des travaux de la DGEFP et d’une consultation des acteurs institutionnels de
                 l’apprentissage. Il répond à l’objectif d’harmoniser les pratiques des acteurs de l’apprentissage et vise à donner des repères juridiques et des clefs de
                 compréhension autour de bases documentaires et méthodologiques communes.
               </ListItem>
+              https://travail-emploi.gouv.fr/IMG/pdf/precis-apprentissage.pdf
             </UnorderedList>
           </Box>
         </AccordionPanel>
@@ -212,17 +225,20 @@ const ConseilsEtAstuces = () => {
           <Box>
             <UnorderedList>
               <ListItem mb={4}>Vous avez besoin de passer le permis ? Bénéficiez d'au moins 500€ d'aide dès maintenant. En savoir plus</ListItem>
+              https://mes-aides.pole-emploi.fr/transport-et-mobilite/financer-mon-permis
               <ListItem mb={4}>
                 {" "}
                 Certaines communes mettent à disposition des logements pour l'hébergement des alternants. Renseignez-vous en contactant la mairie de votre lieu de résidence.
               </ListItem>
               <ListItem mb={4}> Vous devez quitter le domicile familial pour vos études ? Action Logement vous aide à vous installer. En savoir plus</ListItem>
+              https://www.actionlogement.fr/moment-de-vie/trouver-un-logement
               <ListItem mb={4}>
                 {" "}
                 La plupart des régions aident au financement des abonnements en transport en commun pour les alternants : renseignez-vous auprès de votre mairie et de votre
                 opérateur de transport !
               </ListItem>
               <ListItem mb={4}> De nombreuses aides existent pour les jeunes : pour les connaître, faites une simulation sur 1 jeune 1 solution ! En savoir plus</ListItem>
+              https://mes-aides.1jeune1solution.beta.gouv.fr/?utm_source=labonnealternance
             </UnorderedList>
           </Box>
         </AccordionPanel>
