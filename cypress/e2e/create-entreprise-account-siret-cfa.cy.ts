f99896dc4c1765b57221124be137b5cb8f2f94df
Rémy Auricoste <remy.auricoste@gmail.com>
2024-01-29 09:58:31 +0100

    fix: cypress: tests de creation d entreprise avec siret cfa (#1001)

---
+++ cypress/e2e/create-entreprise-account-siret-cfa.cy.ts
@@ -0,0 +1,18 @@
+import { FlowCreationCfa } from "../pages/FlowCreationCfa"
+import { FlowCreationEntreprise } from "../pages/FlowCreationEntreprise"
+
+describe("create-entreprise-account-siret-cfa", () => {
+  it("test create-entreprise-account-siret-cfa", () => {
+    cy.viewport(1271, 721)
+
+    const cfaSiret = Cypress.env("CFA_AUTOVALIDE_SIRET")
+
+    FlowCreationEntreprise.siretPage.goTo()
+    FlowCreationEntreprise.siretPage.fillSiret(cfaSiret)
+    FlowCreationEntreprise.siretPage.submit()
+    cy.contains("veuillez utiliser ce lien").click()
+
+    FlowCreationCfa.siretPage.verify()
+  })
+})
+//# recorderSourceMap=BCBDBEBFBGBHA
