297077ad869fd808abc8951f1a7d9122ac9c7c2a
Alan Le Ruyet <alan.leruyet@free.fr>
2024-02-07 18:10:56 +0100

    fix: ctrl existance (#1059)

--- ui/components/SearchForTrainingsAndJobs/components/rechercheCDDCDI.tsx
+++ ui/components/SearchForTrainingsAndJobs/components/rechercheCDDCDI.tsx
@@ -6,7 +6,7 @@ import { DisplayContext } from "@/context/DisplayContextProvider"
 
 const buildPeUrl = (formValues) => {
   let peUrl = "https://candidat.pole-emploi.fr/offres/recherche?typeContrat=CDI,CDD&offresPartenaires=true&tri=0"
-  if (formValues.job) {
+  if (formValues?.job) {
     peUrl += `&motsCles=${formValues.job.romes}`
   }
   if (formValues?.location?.insee) {
@@ -19,20 +19,22 @@ const buildPeUrl = (formValues) => {
 const RechercheCDICDD = () => {
   const { formValues } = React.useContext(DisplayContext)
   return (
-    <Box textAlign="center" px="72px" py="24px" margin="auto" m={["0.5rem 0", "18px 25px", "0.5rem 0", "0.5rem 25px"]} backgroundColor="#f5f5fe">
-      <Flex>
-        <Box textAlign="left">
-          <Text mb={4}>Les opportunités sur notre site se limitent à des contrats en alternance mais ne vous arrêtez pas là. </Text>
-          <Text mb={4} fontWeight={700}>
-            Certaines entreprises proposent d’autres types de contrats, soumettez-leur l'idée de vous recruter en alternance.
-          </Text>
-          <Link ml="2px" isExternal variant="basicUnderlined" href={buildPeUrl(formValues)}>
-            Voir les offres France Travail <ExternalLinkIcon mb="3px" ml="2px" />
-          </Link>
-        </Box>
-        <Image display={{ base: "none", md: "block" }} src="/images/recherche_cdd_cdi.svg" aria-hidden="true" alt="" />
-      </Flex>
-    </Box>
+    (formValues?.job || formValues?.location) && (
+      <Box textAlign="center" px="72px" py="24px" margin="auto" m={["0.5rem 0", "18px 25px", "0.5rem 0", "0.5rem 25px"]} backgroundColor="#f5f5fe">
+        <Flex>
+          <Box textAlign="left">
+            <Text mb={4}>Les opportunités sur notre site se limitent à des contrats en alternance mais ne vous arrêtez pas là. </Text>
+            <Text mb={4} fontWeight={700}>
+              Certaines entreprises proposent d’autres types de contrats, soumettez-leur l'idée de vous recruter en alternance.
+            </Text>
+            <Link ml="2px" isExternal variant="basicUnderlined" href={buildPeUrl(formValues)}>
+              Voir les offres France Travail <ExternalLinkIcon mb="3px" ml="2px" />
+            </Link>
+          </Box>
+          <Image display={{ base: "none", md: "block" }} src="/images/recherche_cdd_cdi.svg" aria-hidden="true" alt="" />
+        </Flex>
+      </Box>
+    )
   )
 }
 
