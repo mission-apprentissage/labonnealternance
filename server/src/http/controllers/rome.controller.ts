15650a3b65706684c2deb0672aa7ea86f8691e55
Alan Le Ruyet <alan.leruyet@free.fr>
2024-02-19 07:50:09 +0100

    chore: LBAC-1678 suppression de la route rome en doublon (#1053)
    * feat: fusion /romelabels dans /rome
    
    * chore: suppression commentaire obsolète
    
    * feat: suppression routes obsolètes

--- server/src/http/controllers/rome.controller.ts
+++ server/src/http/controllers/rome.controller.ts
@@ -6,22 +6,10 @@ import { getRomeDetailsFromDB } from "@/services/rome.service"
 import { getRomesAndLabelsFromTitleQuery } from "../../services/metiers.service"
 import { Server } from "../server"
 
-// TODO: Remove duplicated routes
 /**
  * API romes
  */
 export default function (server: Server) {
-  server.get(
-    "/romelabels",
-    {
-      schema: zRoutes.get["/rome"],
-    },
-    async (req, res) => {
-      const result = await getRomesAndLabelsFromTitleQuery(req.query)
-      return res.status(200).send(result)
-    }
-  )
-
   server.get(
     "/rome",
     {
@@ -30,21 +18,6 @@ export default function (server: Server) {
     async (req, res) => {
       const result = await getRomesAndLabelsFromTitleQuery(req.query)
       return res.status(200).send(result)
-    }
-  )
-
-  server.get(
-    "/romelabels/detail/:rome",
-    {
-      schema: zRoutes.get["/rome/detail/:rome"],
-    },
-    async (req, res) => {
-      const { rome } = req.params
-      const romeData = await getRomeDetailsFromDB(rome)
-      if (!romeData) {
-        throw Boom.notFound(`rome ${rome} not found`)
-      }
-      return res.status(200).send(romeData.fiche_metier)
     }
   )
 
