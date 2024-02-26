c9732fd88e4b84e2c3e09c2a5020b10b207881b4
Alan Le Ruyet <alan.leruyet@free.fr>
2024-02-26 10:41:38 +0100

    feat: migration script

---
+++ server/src/db/migrations/202402260000000-migrate-blacklist-origin.ts
@@ -0,0 +1,18 @@
+import { Db } from "mongodb"
+
+export const up = async (db: Db) => {
+  await db.collection("emailblacklists").updateMany(
+    { blacklisting_origin: "rdv-transactional" },
+    { $set: { blacklisting_origin: "prise_de_rdv" } },
+    {
+      bypassDocumentValidation: true,
+    }
+  )
+  await db.collection("emailblacklists").updateMany(
+    { blacklisting_origin: "brevo" },
+    { $set: { blacklisting_origin: "brevo-spam" } },
+    {
+      bypassDocumentValidation: true,
+    }
+  )
+}
