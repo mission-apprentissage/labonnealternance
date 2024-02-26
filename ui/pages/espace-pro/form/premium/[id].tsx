a9853b1ae62ba3c37989413db6f42955d50feb63
Kevin Barnoin <k.barnoin@gmail.com>
2024-02-26 12:09:24 +0100

    fix: front premium

--- ui/pages/espace-pro/form/premium/[id].tsx
+++ ui/pages/espace-pro/form/premium/[id].tsx
@@ -149,7 +149,7 @@ export default function PremiumForm() {
               </Stack>
               <Text mt={6}>
                 Je prends acte du fait que la{" "}
-                <a style={{ textDecoration: "underline", cursor: "pointer" }} href="https://mission-apprentissage.gitbook.io/general/" target="_blank" rel="noreferrer">
+                <a style={{ textDecoration: "underline", cursor: "pointer" }} href="https://beta.gouv.fr/incubateurs/mission-apprentissage.html" target="_blank" rel="noreferrer">
                   Mission interministérielle pour l’apprentissage
                 </a>{" "}
                 pourra prendre toutes les mesures utiles pour mesurer le fait que cet engagement soit tenu (dont enquêtes en ligne ou orales auprès des candidats et des CFA).
@@ -172,45 +172,13 @@ export default function PremiumForm() {
                 </Text>
               </Flex>
               <Box>
-                <Text mt="-7px">Le service sera activé sur toutes les formations référencées dans Parcoursup de l’organisme suivant :</Text>
-                <Stack dir="column" bg="#F9F8F6" px={10} py={6} mt={3} spacing={4}>
-                  <Text>
-                    Raison sociale :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.raison_sociale}
-                    </Text>
+                <Text mt="-7px">
+                  Le service sera activé sur toutes les formations éligibles à être affichées sur Parcoursup, liées à votre SIRET{" "}
+                  <Text as="span" fontWeight="700">
+                    {etablissement.gestionnaire_siret}
                   </Text>
-                  <Text>
-                    SIRET Gestionnaire :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.gestionnaire_siret}
-                    </Text>
-                  </Text>
-                  <Text>
-                    SIRET Formateur :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.formateur_siret}
-                    </Text>
-                  </Text>
-                  <Text>
-                    Adresse :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.formateur_address}
-                    </Text>
-                  </Text>
-                  <Text>
-                    Code postal :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.formateur_zip_code}
-                    </Text>
-                  </Text>
-                  <Text>
-                    Ville :{" "}
-                    <Text as="span" fontWeight="700">
-                      {etablissement.formateur_city}
-                    </Text>
-                  </Text>
-                </Stack>
+                  .
+                </Text>
               </Box>
             </>
           )}
