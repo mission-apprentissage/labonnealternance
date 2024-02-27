7a11fa9f81b91e6c500aa112580db71c19b45b44
Alan Le Ruyet <alan.leruyet@free.fr>
2024-02-27 11:45:03 +0100

    feat: unused

--- server/src/db/migrations/202402260000000-migrate-blacklist-origin.ts
+++ server/src/db/migrations/202402260000000-migrate-blacklist-origin.ts
@@ -1,25 +1,7 @@
 import { Db } from "mongodb"
 
 export const up = async (db: Db) => {
-  await db.collection("emailblacklists").updateMany(
-    { blacklisting_origin: "lbb" },
-    { $set: { blacklisting_origin: "lba" } },
-    {
-      bypassDocumentValidation: true,
-    }
-  )
-  await db.collection("emailblacklists").updateMany(
-    { blacklisting_origin: "rdv-transactional" },
-    { $set: { blacklisting_origin: "prise_de_rdv" } },
-    {
-      bypassDocumentValidation: true,
-    }
-  )
-  await db.collection("emailblacklists").updateMany(
-    { blacklisting_origin: { $in: ["brevo", "sendinblue"] } },
-    { $set: { blacklisting_origin: "brevo_spam" } },
-    {
-      bypassDocumentValidation: true,
-    }
-  )
+  await db.collection("emailblacklists").updateMany({ blacklisting_origin: "lbb" }, { $set: { blacklisting_origin: "lba" } })
+  await db.collection("emailblacklists").updateMany({ blacklisting_origin: "rdv-transactional" }, { $set: { blacklisting_origin: "prise_de_rdv" } })
+  await db.collection("emailblacklists").updateMany({ blacklisting_origin: { $in: ["brevo", "sendinblue"] } }, { $set: { blacklisting_origin: "brevo_spam" } })
 }
