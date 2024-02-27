4d85589b9d9e972e7257923022e245043d9f848c
Alan Le Ruyet <alan.leruyet@free.fr>
2024-02-27 17:55:20 +0100

    feat: suppression champ is_anonymized de users

---
+++ server/src/db/migrations/202402270000000-remove-user-is-anonymized.ts
@@ -0,0 +1,18 @@
+import { Db } from "mongodb"
+
+import { logger } from "@/common/logger"
+
+export const up = async (db: Db) => {
+  await db.collection("users").updateMany(
+    {},
+    {
+      $unset: { is_anonymized: "" },
+    },
+    {
+      // @ts-expect-error bypassDocumentValidation is not properly set in @types/mongodb
+      bypassDocumentValidation: true,
+    }
+  )
+
+  logger.info("20240227000000-remove_is_anonymized_from_users")
+}
