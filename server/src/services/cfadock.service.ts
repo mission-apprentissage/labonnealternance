41d5cd18740674247a07311ea160716c67a01a2c
Rémy Auricoste <remy.auricoste@gmail.com>
2024-03-09 17:45:09 +0200

    fix: lbac 2341: sentry 4792: typage opco number

--- server/src/services/cfadock.service.ts
+++ server/src/services/cfadock.service.ts
@@ -1,18 +1,35 @@
-import axios, { AxiosResponse } from "axios"
+import axios from "axios"
+import { z } from "zod"
 
 const cfaDockEndpoint = "https://www.cfadock.fr/api/opcos"
 
 export const CFADOCK_FILTER_LIMIT = 100
 
+const ZResponseItem = z.object({
+  filters: z.object({ siret: z.string() }),
+  opcoName: z.string().nullish(),
+  searchStatus: z.string(),
+  url: z.string().nullish(),
+  idcc: z.number().nullish(),
+})
+export type ICfaDockOpcoItem = z.output<typeof ZResponseItem>
+
+const ZResponseArray = z.array(ZResponseItem)
+
 /**
  * @description Interroge CFADock pour récupérer les opcos associés aux sirens passés en paramètre
- * @param {Set<string>} un ensemble de sirens
- * @returns {Promise<AxiosResponse<any, any>>}
  */
-export const fetchOpcosFromCFADock = async (sirenSet: Set<string>): Promise<AxiosResponse<any, any>> => {
-  return axios.post(
+export const fetchOpcosFromCFADock = async (sirenSet: Set<string>) => {
+  const response = await axios.post(
     cfaDockEndpoint,
     Array.from(sirenSet, (siret) => ({ siret })),
     { headers: { accept: "text/plain" } }
   )
+  const { data } = response
+  if (data?.found) {
+    const parsed = ZResponseArray.parse(data.found)
+    return parsed
+  } else {
+    return []
+  }
 }
