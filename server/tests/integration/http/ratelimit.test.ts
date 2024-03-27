c6b5f3c081c3a11a633a9e08626ff88f30809d32
Kevin Barnoin <k.barnoin@gmail.com>
2024-03-27 15:19:31 +0100

    chore: remove express & rate-limiter utils + fix test

--- server/tests/integration/http/ratelimit.test.ts
+++ server/tests/integration/http/ratelimit.test.ts
@@ -1,18 +1,13 @@
 import assert from "assert"
 
-import { describe, it, beforeAll } from "vitest"
+import { describe, it } from "vitest"
 
-import { enableRateLimiter } from "@/http/utils/rateLimiters"
 import { useMongo } from "@tests/utils/mongo.utils"
 import { useServer } from "@tests/utils/server.utils"
 
 describe("ratelimit", () => {
   useMongo()
   const httpClient = useServer()
-
-  beforeAll(() => {
-    enableRateLimiter()
-  })
 
   it("rate-limit, exemple avec /api/version : 6 requêtes consécutives : les 5 premières sont acceptées, mais pas la 6ème", async () => {
     const response1 = await httpClient().inject({ method: "GET", path: "/api/version", remoteAddress: "101.188.67.134" })
