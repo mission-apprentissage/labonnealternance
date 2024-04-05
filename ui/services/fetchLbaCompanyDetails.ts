f0918c057263324ee52f6753f204f4a7294cff79
Alan Le Ruyet <alan.leruyet@free.fr>
2024-04-05 17:24:19 +0200

    feat: loadItem ok pour jobs et companies

--- ui/services/fetchLbaCompanyDetails.ts
+++ ui/services/fetchLbaCompanyDetails.ts
@@ -1,13 +1,16 @@
-import axios from "axios"
+import { ILbaItemLbaCompany } from "@/../shared"
 
-import { companyApi } from "@/components/SearchForTrainingsAndJobs/services/utils"
+import { apiGet } from "@/utils/api.utils"
 
-export default async function fetchLbaCompanyDetails(company) {
-  const res = null
-  if (!company) {
-    return res
+const fetchLbaCompanyDetails = async (company): Promise<ILbaItemLbaCompany> => {
+  const response = await apiGet("/v1/jobs/company/:siret", { params: { siret: encodeURIComponent(company.id) } })
+
+  if (response?.lbaCompanies?.length) {
+    response.lbaCompanies[0].detailsLoaded = true
+    return response.lbaCompanies[0]
+  } else {
+    throw new Error("unexpected_error")
   }
-  const lbaCompanyApi = `${companyApi}/${encodeURIComponent(company.id)}`
-  const response = await axios.get(lbaCompanyApi)
-  return response.data.lbaCompanies[0]
 }
+
+export default fetchLbaCompanyDetails
