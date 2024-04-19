d2e57e3a996da9afeb612d5200093cab5c7b03cd
Alan Le Ruyet <alan.leruyet@free.fr>
2024-04-19 13:02:00 +0200

    feat: redondant car géré par apiGet

--- ui/services/fetchLbaCompanyDetails.ts
+++ ui/services/fetchLbaCompanyDetails.ts
@@ -3,7 +3,7 @@ import { ILbaItemLbaCompany } from "@/../shared"
 import { apiGet } from "@/utils/api.utils"
 
 const fetchLbaCompanyDetails = async (company): Promise<ILbaItemLbaCompany> => {
-  const response = await apiGet("/v1/jobs/company/:siret", { params: { siret: encodeURIComponent(company.id) } })
+  const response = await apiGet("/v1/jobs/company/:siret", { params: { siret: company.id } })
 
   if (response?.lbaCompanies?.length) {
     response.lbaCompanies[0].detailsLoaded = true
