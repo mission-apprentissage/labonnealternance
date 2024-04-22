41b67caf1e33410b29901bbf5e0c781632af37ed
Rémy Auricoste <remy.auricoste@gmail.com>
2024-04-22 11:15:52 +0200

    fix: typage ui client sur tableau

--- ui/services/fetchLbaJobDetails.ts
+++ ui/services/fetchLbaJobDetails.ts
@@ -1,13 +1,17 @@
-import { ILbaItemLbaJob } from "@/../shared"
+import { ILbaItemLbaJob, zRoutes } from "shared"
+import { z } from "zod"
 
 import { apiGet } from "@/utils/api.utils"
 
+const zodSchema = zRoutes.get["/v1/jobs/matcha/:id"].response["200"]
+
 const fetchLbaJobDetails = async (job): Promise<ILbaItemLbaJob> => {
   const response = await apiGet("/v1/jobs/matcha/:id", { params: { id: job.id }, querystring: {} })
-
-  if (response?.matchas?.length) {
-    response.matchas[0].detailsLoaded = true
-    return response.matchas[0]
+  const typedResponse = response as z.output<typeof zodSchema>
+  const [firstMatcha] = typedResponse?.matchas ?? []
+  if (firstMatcha) {
+    firstMatcha.detailsLoaded = true
+    return firstMatcha
   } else {
     throw new Error("unexpected_error")
   }
