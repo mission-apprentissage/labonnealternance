9635d7abb5c647093495ad113e520d1342664bdc
Alan Le Ruyet <alan.leruyet@free.fr>
2024-04-23 11:35:27 +0200

    feat: nouvelles fonctions d'accès rome V4

--- server/src/common/apis/FranceTravail.ts
+++ server/src/common/apis/FranceTravail.ts
@@ -5,7 +5,7 @@ import FormData from "form-data"
 
 import config from "@/config"
 import { FTResponse } from "@/services/ftjob.service.types"
-import { IAppelattionDetailsFromAPI, IFTAPIToken, IRomeDetailsFromAPI } from "@/services/rome.service.types"
+import { IAppelattionDetailsFromAPI, IFTAPIToken, IRomeDetailsFromAPI, IRomeV4DetailsFromAPI, IRomeV4Short } from "@/services/rome.service.types"
 
 import dayjs from "../../services/dayjs.service"
 import { sentryCaptureException } from "../utils/sentryUtils"
@@ -16,6 +16,7 @@ const FT_IO_API_ROME_V1_BASE_URL = "https://api.pole-emploi.io/partenaire/rome/v
 const FT_IO_API_OFFRES_BASE_URL = "https://api.pole-emploi.io/partenaire/offresdemploi/v2"
 const FT_AUTH_BASE_URL = "https://entreprise.pole-emploi.fr/connexion/oauth2"
 const FT_PORTAIL_BASE_URL = "https://portail-partenaire.pole-emploi.fr/partenaire"
+const FT_IO_API_ROME_V4_BASE_URL = "https://api.francetravail.io/partenaire/rome-fiches-metiers"
 
 // paramètres exclurant les offres LBA des résultats de l'api PE
 const FT_LBA_PARTENAIRE = "LABONNEALTERNANCE"
@@ -23,14 +24,21 @@ const FT_PARTENAIRE_MODE = "EXCLU"
 
 const axiosClient = getApiClient({})
 
-const ROME_ACESS = querystring.stringify({
+const ROME_ACCESS = querystring.stringify({
   grant_type: "client_credentials",
   client_id: config.esdClientId,
   client_secret: config.esdClientSecret,
   scope: `application_${config.esdClientId} api_romev1 nomenclatureRome`,
 })
 
-const OFFRES_ACESS = querystring.stringify({
+const ROME_V4_ACCESS = querystring.stringify({
+  grant_type: "client_credentials",
+  client_id: config.esdClientId,
+  client_secret: config.esdClientSecret,
+  scope: `application_${config.esdClientId} api_rome-fiches-metiersv1 nomenclatureRome`,
+})
+
+const OFFRES_ACCESS = querystring.stringify({
   grant_type: "client_credentials",
   client_id: config.esdClientId,
   client_secret: config.esdClientSecret,
@@ -43,7 +51,7 @@ let tokenOffreFT: IFTAPIToken = {
   token_type: "",
   expires_in: 0,
 }
-let tokenRomeFT: IFTAPIToken = {
+let tokenRomeV4FT: IFTAPIToken = {
   access_token: "",
   scope: "",
   token_type: "",
@@ -52,7 +60,7 @@ let tokenRomeFT: IFTAPIToken = {
 
 const isTokenValid = (token: IFTAPIToken): any => token?.expire?.isAfter(dayjs())
 
-const getFtAccessToken = async (access: "OFFRE" | "ROME", token): Promise<IFTAPIToken> => {
+const getFtAccessToken = async (access: "OFFRE" | "ROME" | "ROMEV4", token): Promise<IFTAPIToken> => {
   const isValid = isTokenValid(token)
 
   if (isValid) {
@@ -60,7 +68,9 @@ const getFtAccessToken = async (access: "OFFRE" | "ROME", token): Promise<IFTAPI
   }
 
   try {
-    const response = await axiosClient.post(`${FT_AUTH_BASE_URL}/access_token?realm=%2Fpartenaire`, access === "OFFRE" ? OFFRES_ACESS : ROME_ACESS, {
+    const tokenParams = access === "OFFRE" ? OFFRES_ACCESS : access === "ROME" ? ROME_ACCESS : ROME_V4_ACCESS
+
+    const response = await axiosClient.post(`${FT_AUTH_BASE_URL}/access_token?realm=%2Fpartenaire`, tokenParams, {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       timeout: 3000,
     })
@@ -163,12 +173,12 @@ export const getFtReferentiels = async (referentiel: string) => {
  * @returns
  */
 export const getRomeDetailsFromAPI = async (romeCode: string): Promise<IRomeDetailsFromAPI | null | undefined> => {
-  tokenRomeFT = await getFtAccessToken("ROME", tokenRomeFT)
+  tokenOffreFT = await getFtAccessToken("ROME", tokenOffreFT)
 
   try {
     const { data } = await axiosClient.get<IRomeDetailsFromAPI>(`${FT_IO_API_ROME_V1_BASE_URL}/metier/${romeCode}`, {
       headers: {
-        Authorization: `Bearer ${tokenRomeFT.access_token}`,
+        Authorization: `Bearer ${tokenOffreFT.access_token}`,
       },
     })
 
@@ -180,12 +190,12 @@ export const getRomeDetailsFromAPI = async (romeCode: string): Promise<IRomeDeta
 }
 
 export const getAppellationDetailsFromAPI = async (appellationCode: string): Promise<IAppelattionDetailsFromAPI | null | undefined> => {
-  tokenRomeFT = await getFtAccessToken("ROME", tokenRomeFT)
+  tokenOffreFT = await getFtAccessToken("ROME", tokenOffreFT)
 
   try {
     const { data } = await axiosClient.get<IAppelattionDetailsFromAPI>(`${FT_IO_API_ROME_V1_BASE_URL}/appellation/${appellationCode}`, {
       headers: {
-        Authorization: `Bearer ${tokenRomeFT.access_token}`,
+        Authorization: `Bearer ${tokenOffreFT.access_token}`,
       },
     })
 
@@ -195,6 +205,39 @@ export const getAppellationDetailsFromAPI = async (appellationCode: string): Pro
     if (error.response.status === 404) {
       return null
     }
+  }
+}
+
+export const getRomeV4DetailsFromFT = async (romeCode: string): Promise<IRomeV4DetailsFromAPI | null | undefined> => {
+  tokenRomeV4FT = await getFtAccessToken("ROMEV4", tokenRomeV4FT)
+
+  try {
+    const { data } = await axiosClient.get<IRomeV4DetailsFromAPI>(`${FT_IO_API_ROME_V4_BASE_URL}/v1/fiches-rome/fiche-metier/${romeCode}`, {
+      headers: {
+        Authorization: `Bearer ${tokenRomeV4FT.access_token}`,
+      },
+    })
+
+    return data
+  } catch (error: any) {
+    sentryCaptureException(error, { extra: { responseData: error.response?.data } })
+    return null
+  }
+}
+
+export const getRomeV4ListFromFT = async (): Promise<IRomeV4Short[] | null | undefined> => {
+  tokenRomeV4FT = await getFtAccessToken("ROMEV4", tokenRomeV4FT)
+  try {
+    const { data } = await axiosClient.get<IRomeV4Short[]>(`${FT_IO_API_ROME_V4_BASE_URL}/v1/fiches-rome/fiche-metier`, {
+      headers: {
+        Authorization: `Bearer ${tokenRomeV4FT.access_token}`,
+      },
+    })
+
+    return data
+  } catch (error: any) {
+    sentryCaptureException(error, { extra: { responseData: error.response?.data } })
+    return null
   }
 }
 
