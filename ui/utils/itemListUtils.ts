fd1ef2300595d9ccd3d5c625b65109825e49ad6e
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2024-04-23 17:06:59 +0200

    fix: lba-2131: renommer pe en ft

--- ui/utils/itemListUtils.ts
+++ ui/utils/itemListUtils.ts
@@ -2,7 +2,7 @@ import { LBA_ITEM_TYPE_OLD } from "shared/constants/lbaitem"
 
 // retourne les offres issues du dépôt d'offre simplifié (ex Matcha) triées par ordre croissant de distance au centre de recherche
 
-// suivi des offres pe triées par ordre croissant de distance au centre de recherche.
+// suivi des offres France travail triées par ordre croissant de distance au centre de recherche.
 export const mergeJobs = ({ jobs, activeFilters }) => {
   let mergedArray = []
 
@@ -18,7 +18,7 @@ export const mergeJobs = ({ jobs, activeFilters }) => {
   return mergedArray
 }
 
-// fusionne les résultats lbb et lba et les trie par ordre croissant de distance, optionnellement intègre aussi les offres PE et matchas
+// fusionne les résultats lbb et lba et les trie par ordre croissant de distance, optionnellement intègre aussi les offres France travail et LBA
 export const mergeOpportunities = ({ jobs, onlyLbbLbaCompanies = undefined, activeFilters }) => {
   let mergedArray = []
   if (jobs) {
@@ -67,7 +67,7 @@ const sortMergedSources = (mergedArray) => {
   return mergedArray
 }
 
-// détermine si l'offre pe est liée au département avec une géoloc non précisée
+// détermine si l'offre France travail est liée au département avec une géoloc non précisée
 export const isDepartmentJob = (job) => {
   let isDepartmentJob = false
   if (!job.place.distance && (!job.place.zipCode || job.place.zipCode.substring(0, 2) === job.place.city.substring(0, 2))) {
