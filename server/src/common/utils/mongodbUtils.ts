f623c4cb79184d80c6ffcda84fed09ab5ac3f5c7
Kevin Barnoin <k.barnoin@gmail.com>
2024-04-27 19:40:29 +0200

    feat: add mongodb utils

---
+++ server/src/common/utils/mongodbUtils.ts
@@ -0,0 +1,177 @@
+import { captureException } from "@sentry/node"
+import { Collection, CollectionInfo, MongoClient, MongoServerError } from "mongodb"
+import { CollectionName, IModelDescriptor } from "shared/models/common"
+import { IDocumentMap, modelDescriptors } from "shared/models/models"
+import { zodToMongoSchema } from "zod-mongodb-schema"
+
+import { logger } from "../logger"
+
+import { sleep } from "./asyncUtils"
+
+let mongodbClient: MongoClient | null = null
+
+export const ensureInitialization = () => {
+  if (!mongodbClient) {
+    throw new Error("Database connection does not exist. Please call connectToMongodb before.")
+  }
+  return mongodbClient
+}
+
+/**
+ * @param  {string} uri
+ * @returns client
+ */
+export const connectToMongodb = async (uri: string) => {
+  const client = new MongoClient(uri)
+
+  client.on("connectionPoolReady", () => {
+    logger.info("MongoDB reconnected")
+    mongodbClient = client
+  })
+
+  client.on("connectionPoolClosed", () => {
+    logger.warn("MongoDB closed")
+    mongodbClient = null
+  })
+
+  await client.connect()
+  mongodbClient = client
+  logger.info("Connected to MongoDB")
+
+  return client
+}
+
+export const getMongodbClient = () => mongodbClient
+
+export const closeMongodbConnection = async () => {
+  logger.warn("Closing MongoDB")
+  // Let 100ms for possible callback cleanup to register tasks in mongodb queue
+  await sleep(200)
+  return mongodbClient?.close()
+}
+
+export const getDatabase = () => {
+  return ensureInitialization().db()
+}
+
+export const getDbCollection = <K extends CollectionName>(name: K): Collection<IDocumentMap[K]> => {
+  return ensureInitialization().db().collection(name)
+}
+
+export const getCollectionList = () => {
+  return ensureInitialization().db().listCollections().toArray()
+}
+
+export const getDbCollectionIndexes = async (name: CollectionName) => {
+  return await ensureInitialization().db().collection(name).indexes()
+}
+
+/**
+ * Création d'une collection si elle n'existe pas
+ * @param {string} collectionName
+ */
+const createCollectionIfDoesNotExist = async (collectionName: CollectionName) => {
+  const db = getDatabase()
+  const collectionsInDb = await db.listCollections().toArray()
+  const collectionExistsInDb = collectionsInDb.map(({ name }) => name).includes(collectionName)
+
+  if (!collectionExistsInDb) {
+    try {
+      await db.createCollection(collectionName)
+    } catch (err) {
+      if ((err as MongoServerError).codeName !== "NamespaceExists") {
+        throw err
+      }
+    }
+  }
+}
+
+/**
+ * Vérification de l'existence d'une collection à partir de la liste des collections
+ * @param {*} collectionsInDb
+ * @param {*} collectionName
+ * @returns
+ */
+export const collectionExistInDb = (collectionsInDb: CollectionInfo[], collectionName: string) => collectionsInDb.map(({ name }: { name: string }) => name).includes(collectionName)
+
+/**
+ * Config de la validation
+ * @param {*} modelDescriptors
+ */
+export const configureDbSchemaValidation = async (modelDescriptors: IModelDescriptor[]) => {
+  const db = getDatabase()
+  ensureInitialization()
+  await Promise.all(
+    modelDescriptors.map(async ({ collectionName, zod }) => {
+      await createCollectionIfDoesNotExist(collectionName)
+
+      const convertedSchema = zodToMongoSchema(zod)
+
+      try {
+        await db.command({
+          collMod: collectionName,
+          validationLevel: "strict",
+          validationAction: "error",
+          validator: {
+            $jsonSchema: {
+              title: `${collectionName} validation schema`,
+              ...convertedSchema,
+            },
+          },
+          ...(collectionName === "deca" ? { changeStreamPreAndPostImages: { enabled: true } } : {}),
+        })
+      } catch (error) {
+        captureException(error)
+        logger.error(error)
+      }
+    })
+  )
+}
+
+/**
+ * Clear de toutes les collections
+ * @returns
+ */
+export const clearAllCollections = async () => {
+  const collections = await getDatabase().collections()
+  return Promise.all(collections.map((c) => c.deleteMany({})))
+}
+
+/**
+ * Clear d'une collection
+ * @param {string} name
+ * @returns
+ */
+export async function clearCollection(name: string) {
+  ensureInitialization()
+  await getDatabase().collection(name).deleteMany({})
+}
+
+export const createIndexes = async () => {
+  for (const descriptor of modelDescriptors) {
+    if (!descriptor.indexes) {
+      return
+    }
+    logger.info(`Create indexes for collection ${descriptor.collectionName}`)
+    await Promise.all(
+      descriptor.indexes.map(async ([index, options]) => {
+        try {
+          await getDbCollection(descriptor.collectionName).createIndex(index, options)
+        } catch (err) {
+          captureException(err)
+          logger.error(`Error creating indexes for ${descriptor.collectionName}: ${err}`)
+        }
+      })
+    )
+  }
+}
+
+export const dropIndexes = async () => {
+  const collections = (await getCollectionList()).map((collection) => collection.name)
+  for (const descriptor of modelDescriptors) {
+    logger.info(`Drop indexes for collection ${descriptor.collectionName}`)
+    if (collections.includes(descriptor.collectionName)) {
+      await getDbCollection(descriptor.collectionName).dropIndexes()
+    }
+  }
+}
