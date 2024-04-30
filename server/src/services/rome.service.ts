d7c250543612c8f381bc31474fa6906c137e0cea
Alan Le Ruyet <alan.leruyet@free.fr>
2024-04-30 19:57:26 +0200

    feat: select 0 sur le _id

--- server/src/services/rome.service.ts
+++ server/src/services/rome.service.ts
@@ -1,4 +1,4 @@
 import { ReferentielRome } from "../common/model"
 
-export const getRomeDetailsFromDB = async (romeCode: string) => ReferentielRome.findOne({ "rome.code_rome": romeCode }).lean()
+export const getRomeDetailsFromDB = async (romeCode: string) => ReferentielRome.findOne({ "rome.code_rome": romeCode }).select({ _id: 0 }).lean()
 export const getFicheMetierFromDB = async ({ query }) => ReferentielRome.findOne(query).lean()
