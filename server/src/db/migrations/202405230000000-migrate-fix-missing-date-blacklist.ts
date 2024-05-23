3d7151b7a93bcb5902cfe87d955fbf97f618f330
Alan Le Ruyet <alan.leruyet@free.fr>
2024-05-23 15:32:36 +0200

    feat: nettoyage db

---
+++ server/src/db/migrations/202405230000000-migrate-fix-missing-date-blacklist.ts
@@ -0,0 +1,24 @@
+import { Db } from "mongodb"
+
+import { logger } from "@/common/logger"
+
+export const up = async (db: Db) => {
+  // suppression des entrées ne respectant pas la validation email zod
+  const emailRegexp = /^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
+  await db.collection("emailblacklists").deleteMany({ email: { $not: emailRegexp } })
+
+  // restauration du champ created_at pour les entrées qui en sont dépourvues
+  const DATE = dayjs().toDate()
+  await db.collection("emailblacklists").updateMany(
+    { created_at: { $exists: false } },
+    {
+      $set: { created_at: DATE },
+    },
+    {
+      // @ts-expect-error bypassDocumentValidation is not properly set in @types/mongodb
+      bypassDocumentValidation: true,
+    }
+  )
+
+  logger.info("20240523000000-correction-modeles-lba-2222")
+}
