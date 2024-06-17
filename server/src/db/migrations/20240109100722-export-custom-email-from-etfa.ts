227ee89b82ca7be3ac83c0121ca09a55343030b4
Alan Le Ruyet <alan.leruyet@free.fr>
2024-06-17 10:04:14 +0200

    fix: missing toArray

--- server/src/db/migrations/20240109100722-export-custom-email-from-etfa.ts
+++ server/src/db/migrations/20240109100722-export-custom-email-from-etfa.ts
@@ -3,16 +3,18 @@ import { asyncForEach } from "../../common/utils/asyncUtils"
 import * as eligibleTrainingsForAppointmentService from "../../services/eligibleTrainingsForAppointment.service"
 
 export const up = async () => {
-  const emailCustom = await eligibleTrainingsForAppointmentService.find(
-    { is_lieu_formation_email_customized: true },
-    {
-      projection: {
-        lieu_formation_email: 1,
-        cle_ministere_educatif: 1,
-        _id: 0,
-      },
-    }
-  )
+  const emailCustom = await eligibleTrainingsForAppointmentService
+    .find(
+      { is_lieu_formation_email_customized: true },
+      {
+        projection: {
+          lieu_formation_email: 1,
+          cle_ministere_educatif: 1,
+          _id: 0,
+        },
+      }
+    )
+    .toArray()
 
   await asyncForEach(emailCustom, async (custom) => {
     await CustomEmailETFA.create({ email: custom.lieu_formation_email, ...custom })
