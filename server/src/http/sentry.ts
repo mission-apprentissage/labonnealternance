1f92d3051357c3a97245b6845aea56c760432357
Kevin Barnoin <k.barnoin@gmail.com>
2024-06-18 16:09:11 +0200

    fix: sentry

--- server/src/http/sentry.ts
+++ server/src/http/sentry.ts
@@ -9,8 +9,6 @@ import { ExtraErrorData } from "@sentry/integrations"
 import * as Sentry from "@sentry/node"
 import { FastifyRequest } from "fastify"
 
-import { mongooseInstance } from "@/common/mongodb"
-import { startSentryPerfRecording } from "@/common/utils/sentryUtils"
 import config from "@/config"
 
 import { Server } from "./server"
@@ -85,53 +83,4 @@ export function initSentryFastify(app: Server) {
   }
 
   app.register(fastifySentryPlugin, options)
-  registerMongoosePlugin()
-}
-
-function registerMongoosePlugin() {
-  // Tracing.Integrations.Mongo doesn't track properly db operations
-  // see https://github.com/getsentry/sentry-javascript/issues/4078
-  mongooseInstance.plugin((s) => {
-    // List of all events can be found here https://mongoosejs.com/docs/5.x/docs/middleware.html
-    const ops = [
-      "aggregate",
-      "count",
-      "countDocuments",
-      "deleteOne",
-      "deleteMany",
-      "estimatedDocumentCount",
-      "find",
-      "findOne",
-      "findOneAndDelete",
-      "findOneAndRemove",
-      "findOneAndReplace",
-      "findOneAndUpdate",
-      "insertMany",
-      "remove",
-      "replaceOne",
-      "save",
-      "update",
-      "updateOne",
-      "updateMany",
-      "validate",
-    ]
-
-    // For each event we will start perf recording on pre event and stop it on post event
-    ops.forEach((op) => {
-      // We need to store callback returned from startSentryPerfRecording to be called in post event
-      // Because queries can run in parallel we need to execute the proper callback on post event
-      // Finally we need a WeakMap to be sure we release memory in case of any failure.
-      const queryToCbMap = new WeakMap()
-      s.pre(op, function () {
-        // this reference the current query according to mongoose plugin API
-        queryToCbMap.set(this, startSentryPerfRecording("mongoose", op))
-      })
-      s.post(op, function () {
-        // Execute the associated callback if found
-        queryToCbMap.get(this)?.()
-        // Make sure to remove the query from the map
-        queryToCbMap.delete(this)
-      })
-    })
-  })
 }
