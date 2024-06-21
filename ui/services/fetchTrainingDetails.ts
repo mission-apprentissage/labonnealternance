8230292737e03b2dbf9373d3b4e035bc919c9a68
Alan Le Ruyet <alan.leruyet@free.fr>
2024-06-21 10:30:02 +0200

    fix: mixmatch

--- ui/services/fetchTrainingDetails.ts
+++ ui/services/fetchTrainingDetails.ts
@@ -1,11 +1,11 @@
-import { zRoutes } from "shared"
+import { ILbaItemFormation, zRoutes } from "shared"
 import { z } from "zod"
 
 import { apiGet } from "@/utils/api.utils"
 
 const zodSchema = zRoutes.get["/v1/formations/formation/:id"].response["200"]
 
-export const fetchTrainingDetails = async (training): Promise<Jsonify<ILbaItemFormation2> & { detailsLoaded: true }> => {
+export const fetchTrainingDetails = async (training): Promise<ILbaItemFormation> => {
   const response = await apiGet("/v1/formations/formation/:id", { params: { id: training.id }, querystring: {} })
 
   const typedResponse = response as z.output<typeof zodSchema>
