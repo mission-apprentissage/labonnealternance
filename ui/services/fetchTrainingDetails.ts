8b3df37d9fea38abc650bf770ba8ebf69a971c9b
Alan Le Ruyet <alan.leruyet@free.fr>
2024-06-21 10:10:31 +0200

    fix: reverted v2 plug to v1

--- ui/services/fetchTrainingDetails.ts
+++ ui/services/fetchTrainingDetails.ts
@@ -4,6 +4,14 @@ import { Jsonify } from "type-fest"
 import { apiGet } from "@/utils/api.utils"
 
 export const fetchTrainingDetails = async (training): Promise<Jsonify<ILbaItemFormation2> & { detailsLoaded: true }> => {
-  const response = await apiGet("/formations/formation/:id", { params: { id: training.id }, querystring: {} }, undefined, "V2")
-  return { ...response, detailsLoaded: true }
+  const response = await apiGet("/v1/formations/formation/:id", { params: { id: training.id }, querystring: {} })
+
+  const typedResponse = response as z.output<typeof zodSchema>
+  const [firstTraining] = typedResponse?.results ?? []
+  if (firstTraining) {
+    firstTraining.detailsLoaded = true
+    return firstTraining
+  } else {
+    throw new Error("unexpected_error")
+  }
 }
