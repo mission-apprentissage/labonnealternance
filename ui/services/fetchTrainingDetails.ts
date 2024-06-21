e2090b83b87a720096aa278e60191ad12136010d
Alan Le Ruyet <alan.leruyet@free.fr>
2024-06-21 10:41:05 +0200

    fix: LBA-2300 affichage du détail des formations HS (#1309)

--- ui/services/fetchTrainingDetails.ts
+++ ui/services/fetchTrainingDetails.ts
@@ -1,9 +1,19 @@
-import { ILbaItemFormation2 } from "shared"
-import { Jsonify } from "type-fest"
+import { ILbaItemFormation, zRoutes } from "shared"
+import { z } from "zod"
 
 import { apiGet } from "@/utils/api.utils"
 
-export const fetchTrainingDetails = async (training): Promise<Jsonify<ILbaItemFormation2> & { detailsLoaded: true }> => {
-  const response = await apiGet("/formations/formation/:id", { params: { id: training.id }, querystring: {} }, undefined, "V2")
-  return { ...response, detailsLoaded: true }
+const zodSchema = zRoutes.get["/v1/formations/formation/:id"].response["200"]
+
+export const fetchTrainingDetails = async (training): Promise<ILbaItemFormation> => {
+  const response = await apiGet("/v1/formations/formation/:id", { params: { id: training.id }, querystring: {} })
+
+  const typedResponse = response as z.output<typeof zodSchema>
+  const [firstTraining] = typedResponse?.results ?? []
+  if (firstTraining) {
+    firstTraining.detailsLoaded = true
+    return firstTraining
+  } else {
+    throw new Error("unexpected_error")
+  }
 }
