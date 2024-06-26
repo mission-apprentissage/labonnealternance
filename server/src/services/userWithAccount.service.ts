1801d51c109da67ad72a2ba61bb296b900944941
Kevin Barnoin <k.barnoin@gmail.com>
2024-06-26 16:02:26 +0200

    fix: missing status

--- server/src/services/userWithAccount.service.ts
+++ server/src/services/userWithAccount.service.ts
@@ -33,6 +33,13 @@ export const createUser2IfNotExist = async (
         granted_by: grantedBy,
       })
     }
+    status.push({
+      date: new Date(),
+      reason: "creation de l'utilisateur",
+      status: UserEventType.ACTIF,
+      validation_type: VALIDATION_UTILISATEUR.MANUAL,
+      granted_by: grantedBy,
+    })
     const now = new Date()
     const userFields: IUserWithAccount = {
       _id: id,
