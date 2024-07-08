fd8d64ab30643d8c13a4ed8f448c7572e602ce47
Rémy Auricoste <remy.auricoste@gmail.com>
2024-07-08 16:05:31 +0200

    feat: lbac 2221: creation de la collection rolemanagement360 (#1343)
    * feat: lbac 2221: creation de la collection rolemanagement360
    
    * fix: ajout last status
    
    * Update server/src/commands.ts
    
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>
    
    ---------
    
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>

---
+++ server/src/jobs/lba_recruteur/createRoleManagement360.ts
@@ -0,0 +1,80 @@
+import { AccessEntityType, ZRoleManagement } from "shared/models"
+import cfaModel, { zCFA } from "shared/models/cfa.model"
+import entrepriseModel, { ZEntreprise } from "shared/models/entreprise.model"
+import userWithAccountModel, { ZUserWithAccount } from "shared/models/userWithAccount.model"
+
+import { logger } from "@/common/logger"
+import { getDatabase, getDbCollection } from "@/common/utils/mongodbUtils"
+import { notifyToSlack } from "@/common/utils/slackUtils"
+
+export const createRoleManagement360 = async () => {
+  logger.info("Creating roleManagement360 collection...")
+  const destCollectionName = "rolemanagement360"
+  await getDbCollection("rolemanagements")
+    .aggregate([
+      { $match: { authorized_type: { $in: [AccessEntityType.CFA, AccessEntityType.ENTREPRISE] } } },
+      {
+        $addFields: {
+          authorized_object_id: { $convert: { input: "$authorized_id", to: "objectId" } },
+        },
+      },
+      {
+        $lookup: {
+          from: userWithAccountModel.collectionName,
+          foreignField: "_id",
+          localField: "user_id",
+          as: "user",
+        },
+      },
+      { $unwind: "$user" },
+      {
+        $lookup: {
+          from: entrepriseModel.collectionName,
+          foreignField: "_id",
+          localField: "authorized_object_id",
+          as: "entrepriseArray",
+        },
+      },
+      {
+        $lookup: {
+          from: cfaModel.collectionName,
+          foreignField: "_id",
+          localField: "authorized_object_id",
+          as: "cfaArray",
+        },
+      },
+      {
+        $addFields: {
+          entreprise: { $first: "$entrepriseArray" },
+          cfa: { $first: "$cfaArray" },
+        },
+      },
+      {
+        $unset: ["entrepriseArray", "cfaArray"],
+      },
+      {
+        $project: Object.fromEntries([
+          ...Object.keys(ZRoleManagement.shape).map((field) => [`role_${field}`, `$${field}`]),
+          ...Object.keys(ZUserWithAccount.shape).map((field) => [`user_${field}`, `$user.${field}`]),
+          ...Object.keys(ZEntreprise.shape).map((field) => [`entreprise_${field}`, `$entreprise.${field}`]),
+          ...Object.keys(zCFA.shape).map((field) => [`cfa_${field}`, `$cfa.${field}`]),
+        ]),
+      },
+      {
+        $addFields: {
+          user_last_status: { $last: "$user_status.status" },
+          role_last_status: { $last: "$role_status.status" },
+          entreprise_last_status: { $last: "$entreprise_status.status" },
+        },
+      },
+      { $out: destCollectionName },
+    ])
+    .toArray()
+  const destCount = await getDatabase().collection(destCollectionName).countDocuments({})
+  await notifyToSlack({
+    subject: `Génération de la collection ${destCollectionName}`,
+    message: `${destCollectionName} created with ${destCount} documents.`,
+    error: destCount <= 0,
+  })
+  logger.info("roleManagement360 collection created")
+}
