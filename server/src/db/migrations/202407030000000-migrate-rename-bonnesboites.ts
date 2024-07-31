e43920cbdfc31f6735d54f01da162afd593101dd
Kevin Barnoin <k.barnoin@gmail.com>
2024-07-31 14:16:43 +0200

    fix: training links (#1396)
    * fix: france entière si pas de rome
    
    * fix: homepage si pas de rome
    
    * fix: formation query
    
    * fix: query
    
    * feat: add extra ban search
    
    * fix: migration

--- server/src/db/migrations/202407030000000-migrate-rename-bonnesboites.ts
+++ server/src/db/migrations/202407030000000-migrate-rename-bonnesboites.ts
@@ -5,9 +5,19 @@ import { logger } from "@/common/logger"
 export const up = async (db: Db) => {
   logger.info("202407030000000-migrate-rename-bonnesboites started")
 
-  await db.collection("bonnesboites").rename("recruteurslba")
-  await db.collection("unsubscribedbonnesboites").rename("unsubscribedrecruteurslba")
-  await db.collection("bonnesboiteslegacies").rename("recruteurslbalegacies")
+  const renameCollectionIfNecessary = async (db, oldName, newName) => {
+    const collections = await db.listCollections({ name: newName }).toArray()
+    if (collections.length === 0) {
+      await db.collection(oldName).rename(newName)
+      console.log(`Renamed collection ${oldName} to ${newName}`)
+    } else {
+      console.log(`Collection ${newName} already exists, skipping rename of ${oldName}`)
+    }
+  }
+
+  await renameCollectionIfNecessary(db, "bonnesboites", "recruteurslba")
+  await renameCollectionIfNecessary(db, "unsubscribedbonnesboites", "unsubscribedrecruteurslba")
+  await renameCollectionIfNecessary(db, "bonnesboiteslegacies", "recruteurslbalegacies")
 
   logger.info("202407030000000-migrate-rename-bonnesboites ended")
 }
