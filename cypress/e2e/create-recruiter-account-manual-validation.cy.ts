e3a8aecb0160d8d198ceecd3ea47788f41f2d0b5
Rémy Auricoste <remy.auricoste@gmail.com>
2024-08-01 17:51:39 +0200

    feat: lbac 2040: ajout test cypress activation et desactivation recruteur

--- cypress/e2e/create-recruiter-account-manual-validation.cy.ts
+++ cypress/e2e/create-recruiter-account-manual-validation.cy.ts
@@ -2,8 +2,11 @@ import { slowCypressDown } from "cypress-slow-down"
 
 import { loginClient } from "../api/loginClient"
 import { smtpClient } from "../api/smtpClient"
+import { FlowAdminPage } from "../pages/FlowAdminPage"
 import { FlowCreationEntreprise } from "../pages/FlowCreationEntreprise"
 import { JobPage } from "../pages/JobPage"
+import { LoginBar } from "../pages/LoginBar"
+import { LoginPage } from "../pages/LoginPage"
 import { generateRandomString } from "../utils/generateRandomString"
 
 slowCypressDown(200)
@@ -62,6 +65,33 @@ describe("create-recruiter-account-manual-validation", () => {
 
         // vérifie que le recruteur ne peut pas se connecter
         loginClient.expectLoginFail(email, "VALIDATION")
+
+        loginClient.loginAsAdmin()
+        FlowAdminPage.navigation.goToAccountValidation()
+        FlowAdminPage.gestionDesRecruteurs.selectTab("En attente de vérification")
+        FlowAdminPage.gestionDesRecruteurs.fillSearchInput(email)
+        FlowAdminPage.gestionDesRecruteurs.selectByEmail(email)
+        FlowAdminPage.pageDunRecruteur.activer()
+        LoginBar.disconnect()
+
+        LoginPage.goTo()
+        LoginPage.submitEmail(email)
+        LoginPage.clickLinkInEmail(email)
+        LoginBar.disconnect()
+
+        loginClient.loginAsAdmin()
+        FlowAdminPage.navigation.goToAccountValidation()
+        FlowAdminPage.gestionDesRecruteurs.selectTab("Actifs")
+        FlowAdminPage.gestionDesRecruteurs.fillSearchInput(email)
+        FlowAdminPage.gestionDesRecruteurs.selectByEmail(email)
+        FlowAdminPage.pageDunRecruteur.desactiver("Injoignable")
+        LoginBar.disconnect()
+
+        LoginPage.goTo()
+        LoginPage.submitEmail(email)
+        cy.contains("Le compte utilisateur est en attente de validation")
+
+        smtpClient.getMail(email, "Votre compte a été désactivé sur La bonne alternance")
       })
     })
   })
