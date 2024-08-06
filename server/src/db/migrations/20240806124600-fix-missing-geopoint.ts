64553004e7195ff9616b6f261f1f4af04ca8630e
Kevin Barnoin <k.barnoin@gmail.com>
2024-08-06 15:41:03 +0200

    fix: query

--- server/src/db/migrations/20240806124600-fix-missing-geopoint.ts
+++ server/src/db/migrations/20240806124600-fix-missing-geopoint.ts
@@ -2,7 +2,7 @@ import { getDbCollection } from "../../common/utils/mongodbUtils"
 
 export const up = async () => {
   await getDbCollection("recruiters").updateMany({ geo_coordinates: "NOT FOUND" }, { $set: { geo_coordinates: null } })
-  for await (const { _id, geo_coordinates } of getDbCollection("recruiters").find({ geopoint: { $exist: false }, geo_coordinates: { $ne: null } })) {
+  for await (const { _id, geo_coordinates } of getDbCollection("recruiters").find({ geopoint: { $exists: false }, geo_coordinates: { $ne: null } })) {
     const [latitude, longitude] = geo_coordinates!.split(",").map((coord) => parseFloat(coord))
     await getDbCollection("recruiters").updateOne({ _id }, { $set: { geopoint: { type: "Point", coordinates: [longitude, latitude] } } })
   }
