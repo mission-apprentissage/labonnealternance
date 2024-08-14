9ab7f99f46f8ad83f46d2b6e8898ddc498d8831e
Alan Le Ruyet <alan.leruyet@free.fr>
2024-08-14 17:52:33 +0200

    feat: restore

---
+++ ui/components/ItemDetail/CandidatureLba/services/useLocalStorage.ts
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function useLocalStorage(key, initialValue, stubbedLocalStorage) {
+  const actualLocalStorage = stubbedLocalStorage || window.localStorage || {}
+
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = actualLocalStorage.getItem(key)
+    return item ? item : initialValue
+  })
+  const setValue = (value) => {
+    setStoredValue(value)
+    actualLocalStorage.setItem(key, value)
+  }
+
+  return [storedValue, setValue]
+}
