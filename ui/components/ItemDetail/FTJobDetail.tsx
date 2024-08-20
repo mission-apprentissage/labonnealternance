5040c4cf8951b4acef411e0510c5129ff5374c3d
Alan Le Ruyet <alan.leruyet@free.fr>
2024-08-20 14:01:19 +0200

    feat: LBA-2271 refonte des blocs infos entreprises (#1424)
    * feat: mise en composant
    
    * feat: ajout icône loupe
    
    * feat: training detail ok
    
    * feat: lbaJob ok
    
    * feat: ftjob ok
    
    * feat: mb
    
    * feat: bloc swap
    
    * feat: lisibilité + exclusion
    
    * feat: aération top lbajob
    
    * feat: aération recruteurs lba
    
    * feat: aération formation
    
    * feat: aération ftjob
    
    * feat: affichage conditionnel du secteur d'activité
    
    * feat: harmonisation marge

--- ui/components/ItemDetail/FTJobDetail.tsx
+++ ui/components/ItemDetail/FTJobDetail.tsx
@@ -1,4 +1,5 @@
-import { Box, ListItem, Text, UnorderedList } from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Box, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react"
 import mdIt from "markdown-it"
 import React, { useEffect } from "react"
 import ReactHtmlParser from "react-html-parser"
@@ -8,6 +9,8 @@ import { DisplayContext } from "../../context/DisplayContextProvider"
 import { SendPlausibleEvent } from "../../utils/plausible"
 import { formatDate } from "../../utils/strutils"
 
+import ItemGoogleSearchLink from "./ItemDetailServices/ItemGoogleSearchLink"
+import ItemLocalisation from "./ItemDetailServices/ItemLocalisation"
 import { ReportJobLink } from "./ReportJobLink"
 
 const md = mdIt().disable(["link", "image"])
@@ -32,60 +35,97 @@ const FTJobDetail = ({ job }) => {
   const creationDate = formatDate(job?.job?.creationDate)
 
   return (
-    <Box pb="0px" mt={6} position="relative" background="white" padding="16px 24px" maxWidth="970px" mx={["0", "30px", "30px", "auto"]}>
-      <Text as="h2" variant="itemDetailH2" mt={2} mb={4}>
-        Description de l&apos;offre
-      </Text>
-      <Box p={4} mb={6} borderRadius="8px" background="#f6f6f6">
-        <Box mb={2}>
-          <strong>Publiée le : </strong> {creationDate}
-        </Box>
-        <Box mb={2}>
-          <strong>Nature du contrat : </strong> Alternance
-        </Box>
-        <Box mb={2}>
-          <strong>Durée :</strong> {contractDuration}
-        </Box>
-        <Box>
-          <strong>Rythme :</strong> {contractRythm}
-        </Box>
-      </Box>
-      {description && (
-        <Box mt={8}>
-          <Box whiteSpace="pre-wrap" pl={4}>
-            {ReactHtmlParser(md.render(description))}
+    <>
+      <Box pb="0px" mt={6} position="relative" background="white" padding="16px 24px" maxWidth="970px" mx={["0", "30px", "30px", "auto"]}>
+        <Text as="h2" variant="itemDetailH2" mt={2} mb={4}>
+          Description de l&apos;offre
+        </Text>
+        <Box p={4} mb={6} borderRadius="8px" background="#f6f6f6">
+          <Box mb={2}>
+            <strong>Publiée le : </strong> {creationDate}
+          </Box>
+          <Box mb={2}>
+            <strong>Nature du contrat : </strong> Alternance
+          </Box>
+          <Box mb={2}>
+            <strong>Durée :</strong> {contractDuration}
+          </Box>
+          <Box>
+            <strong>Rythme :</strong> {contractRythm}
           </Box>
         </Box>
-      )}
-      <Box marginTop="10px">
-        <ReportJobLink
-          width="490px"
-          itemId={job?.job?.id}
-          type={LBA_ITEM_TYPE.OFFRES_EMPLOI_PARTENAIRES}
-          linkLabelNotReported="Signaler l’offre"
-          linkLabelReported="Offre signalée"
-          tooltip={
-            <Box>
-              <Text fontSize="16px" lineHeight="24px" fontWeight="700" marginBottom="8px" color="#161616">
-                Cette offre vous semble inappropriée ? Voici les raisons pour lesquelles vous pouvez nous signaler une offre :
-              </Text>
-              <UnorderedList
-                style={{
-                  color: "#383838",
-                  fontSize: "16px",
-                  lineHeight: "24px",
-                }}
-              >
-                <ListItem>Offre offensante ou discriminatoire</ListItem>
-                <ListItem>Offre inexacte ou expirée</ListItem>
-                <ListItem>Fausse offre provenant d’un centre de formation</ListItem>
-                <ListItem>Tentative d'escroquerie</ListItem>
-              </UnorderedList>
+        {description && (
+          <Box mt={8}>
+            <Box whiteSpace="pre-wrap" pl={4}>
+              {ReactHtmlParser(md.render(description))}
             </Box>
-          }
-        />
+          </Box>
+        )}
+        <Box marginTop="10px">
+          <ReportJobLink
+            width="490px"
+            itemId={job?.job?.id}
+            type={LBA_ITEM_TYPE.OFFRES_EMPLOI_PARTENAIRES}
+            linkLabelNotReported="Signaler l’entreprise"
+            linkLabelReported="Entreprise signalée"
+            tooltip={
+              <Box>
+                <Text fontSize="16px" lineHeight="24px" fontWeight="700" marginBottom="8px" color="#161616">
+                  Cette offre vous semble inappropriée ? Voici les raisons pour lesquelles vous pouvez nous signaler une offre :
+                </Text>
+                <UnorderedList
+                  style={{
+                    color: "#383838",
+                    fontSize: "16px",
+                    lineHeight: "24px",
+                  }}
+                >
+                  <ListItem>Offre offensante ou discriminatoire</ListItem>
+                  <ListItem>Offre inexacte ou expirée</ListItem>
+                  <ListItem>Fausse offre provenant d’un centre de formation</ListItem>
+                  <ListItem>Tentative d'escroquerie</ListItem>
+                </UnorderedList>
+              </Box>
+            }
+          />
+        </Box>
       </Box>
-    </Box>
+      <Box pb="0px" mt={6} position="relative" background="white" padding="16px 24px" maxWidth="970px" mx={["0", "30px", "30px", "auto"]}>
+        <Text as="h2" variant="itemDetailH2" mt={2}>
+          Quelques informations sur l'entreprise
+        </Text>
+        <Text my={3}>
+          Renseignez-vous sur l’entreprise, ses activités et ses valeurs pour préparer votre candidature. Vous pouvez rechercher leur site internet et leur présence sur les réseaux
+          sociaux.
+        </Text>
+
+        <ItemLocalisation item={job} />
+
+        {job?.nafs?.length && (
+          <Text mt={1}>
+            <Text as="span" fontWeight={700}>
+              Secteur d'activité :{" "}
+            </Text>
+            <Text as="span">{job?.nafs[0]?.label}</Text>
+          </Text>
+        )}
+
+        {job?.contact?.phone && (
+          <Text mt={1}>
+            <Text as="span" fontWeight={700}>
+              Téléphone :{" "}
+            </Text>
+            <Text as="span">
+              <Link ml="2px" isExternal variant="basicUnderlined" href={`tel:${job.contact.phone}`} aria-label="Appeler la société au téléphone">
+                {job.contact.phone} <ExternalLinkIcon mb="3px" ml="2px" />
+              </Link>
+            </Text>
+          </Text>
+        )}
+
+        {job?.company?.name && <ItemGoogleSearchLink item={job} />}
+      </Box>
+    </>
   )
 }
 
