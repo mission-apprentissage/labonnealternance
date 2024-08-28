e2e783ef02aa15536e5cd59b9e65f672eff7e15a
Alan Le Ruyet <alan.leruyet@free.fr>
2024-08-28 09:14:42 +0200

    fix: boom seems to not expose named exports (#1466)

--- server/src/services/jobs/jobOpportunity/jobOpportunity.service.test.ts
+++ server/src/services/jobs/jobOpportunity/jobOpportunity.service.test.ts
@@ -1,6 +1,6 @@
 import { generateFtJobFixture } from "@tests/fixtures/ftJobs.fixture"
 import { useMongo } from "@tests/utils/mongo.test.utils"
-import { internal } from "boom"
+import Boom from "boom"
 import nock from "nock"
 import { NIVEAUX_POUR_LBA, NIVEAUX_POUR_OFFRES_PE, RECRUITER_STATUS } from "shared/constants"
 import { generateCfaFixture } from "shared/fixtures/cfa.fixture"
@@ -380,7 +380,7 @@ describe("findJobsOpportunities", () => {
           },
           new JobOpportunityRequestContext({ path: "/api/route" }, "api-alternance")
         )
-      ).rejects.toThrowError(internal("Erreur lors de la récupération des informations de certification"))
+      ).rejects.toThrowError(Boom.internal("Erreur lors de la récupération des informations de certification"))
     })
 
     it("should throw bad request when rncp code is not found", async () => {
@@ -400,7 +400,7 @@ describe("findJobsOpportunities", () => {
           },
           new JobOpportunityRequestContext({ path: "/api/route" }, "api-alternance")
         )
-      ).rejects.toThrowError(internal("Cannot find an active Certification for the given RNCP"))
+      ).rejects.toThrowError(Boom.internal("Cannot find an active Certification for the given RNCP"))
 
       expect(scopeApiAlternance.isDone()).toBeTruthy()
     })
@@ -422,7 +422,7 @@ describe("findJobsOpportunities", () => {
           },
           new JobOpportunityRequestContext({ path: "/api/route" }, "api-alternance")
         )
-      ).rejects.toThrowError(internal("Cannot find an active Certification for the given RNCP"))
+      ).rejects.toThrowError(Boom.internal("Cannot find an active Certification for the given RNCP"))
 
       expect(scopeApiAlternance.isDone()).toBeTruthy()
     })
