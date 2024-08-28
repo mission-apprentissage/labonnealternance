823733034c0d34b34b00325d9d36bc8a7a85a405
Kevin Barnoin <k.barnoin@gmail.com>
2024-08-28 13:54:41 +0200

    feat: lbac-2423: job processor (#1463)
    * feat: add job-processor package & update command & jobs
    
    * feat: start job processor
    
    * feat: clean-up
    
    * feat: move migration to migrations folder
    
    * feat: clean up + update jobs
    
    * feat: update doc & artillery folder name
    
    * feat: group api provider into folders
    
    * feat: dedupe

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { connectToMongodb } from "@/common/utils/mongodbUtils"
 import { startCLI } from "./commands"
 import { logger } from "./common/logger"
 import config from "./config"
+import { setupJobProcessor } from "./jobs/jobs"
 
 process.on("unhandledRejection", (err) => logger.error(err, "unhandledRejection"))
 process.on("uncaughtException", (err) => logger.error(err, "uncaughtException"))
@@ -10,6 +11,7 @@ process.on("uncaughtException", (err) => logger.error(err, "uncaughtException"))
 try {
   logger.warn("starting application")
   await connectToMongodb(config.mongodb.uri)
+  await setupJobProcessor()
   await startCLI()
 } catch (err) {
   logger.error(err, "startup error")
