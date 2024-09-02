923a7eb1b2eaec8d2e244fa16ed13105dad9d4a1
Alan Le Ruyet <alan.leruyet@free.fr>
2024-09-02 22:06:54 +0200

    feat: ajustements query

--- server/src/jobs/offrePartenaire/importFromComputedToJobsPartners.ts
+++ server/src/jobs/offrePartenaire/importFromComputedToJobsPartners.ts
@@ -7,7 +7,7 @@ import { IJobsPartnersOfferPrivate } from "shared/models/jobsPartners.model"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 export const importFromComputedToJobsPartners = async () => {
-  const cursor = await getDbCollection("computed_jobs_partners").find({ validated: true }).project({ _id: -1, created_at: -1, updated_at: -1 }).stream()
+  const cursor = await getDbCollection("computed_jobs_partners").find({ validated: true }).project({ _id: 0, validated: 0 }).stream()
 
   const transform = new Transform({
     objectMode: true,
@@ -17,7 +17,7 @@ export const importFromComputedToJobsPartners = async () => {
           { partner_job_id: computedJobPartner.partner_job_id },
           {
             $set: { ...computedJobPartner },
-            $setOnInsert: { _id: new ObjectId(), created_at: new Date() },
+            $setOnInsert: { _id: new ObjectId() },
           },
           { upsert: true }
         )
@@ -29,14 +29,4 @@ export const importFromComputedToJobsPartners = async () => {
   })
 
   await pipeline(cursor, transform)
-
-  // tests :
-
-  // créations de plusieurs éléments existants dans jobs partners
-  // création de plusieurs éléments dans computed jobs partners . certains avec validated true, d'autres false
-  // certains éléments validated de computed sont déjà présents dans jobs partners
-
-  // les éléments non validated ne doivent pas se retrouver dans jobs partners
-  // les éléments validated et absents initialement de jobs partners doivent se rerouver dans jobs partners
-  // les éléments validated et déjà dans jobs partners doivent toujours y être avec les data modifiées à jour
 }
