229af085a71bb18a6a7cd4f5fd73701649609126
Kevin Barnoin <k.barnoin@gmail.com>
2024-09-04 15:27:14 +0200

    fix: add invalid test case

--- shared/validators/phoneValidator.test.ts
+++ shared/validators/phoneValidator.test.ts
@@ -14,7 +14,9 @@ const phoneExample = paysUE
   .filter((x) => x !== null) as string[]
 
 describe("validatePhone", () => {
-  it("should convert a french number and validate it", () => expect(validatePhone("0132568790")).toBe(true))
+  it("should success converting a french number and validate it", () => expect(validatePhone("0132568790")).toBe(true))
+  it("should throw error as it's a premium rate phone number", () => expect(validatePhone("0813458765")).toBe(false))
+  it("should throw error as it's malformed", () => expect(validatePhone("0013458765")).toBe(false))
   describe("validate international format", () => {
     it.each(phoneExample)("should validate %s phone number ", (siret) => {
       expect(validatePhone(siret)).toBe(true)
