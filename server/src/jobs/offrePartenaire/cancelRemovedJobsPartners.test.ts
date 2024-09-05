2c8064c0b28dd06cf4e43435c2a27120ee39734e
Alan Le Ruyet <alan.leruyet@free.fr>
2024-09-05 08:51:07 +0200

    feat: reste à faire utilisation double clé partner_label partner_job_id

--- server/src/jobs/offrePartenaire/cancelRemovedJobsPartners.test.ts
+++ server/src/jobs/offrePartenaire/cancelRemovedJobsPartners.test.ts
@@ -15,48 +15,44 @@ describe("Canceling jobs_partners that have been removed from computed_jobs_part
     // création de plusieurs éléments dans computed jobs partners . certains avec validated true, d'autres false
     // certains éléments validated de computed sont déjà présents dans jobs partners
     // certains éléments dans jobs partners ne sont pas dans computed
-    await createJobPartner({ partner_job_id: "existing_1", offer_status: JOB_STATUS.ACTIVE })
-    await createJobPartner({ partner_job_id: "existing_2", offer_status: JOB_STATUS.ACTIVE })
-    await createJobPartner({ partner_job_id: "existing_3", offer_status: JOB_STATUS.ACTIVE })
-    await createJobPartner({ partner_job_id: "existing_4", offer_status: JOB_STATUS.ACTIVE })
-    await createJobPartner({ partner_job_id: "existing_5", offer_status: JOB_STATUS.ACTIVE })
-    await createComputedJobPartner({ partner_job_id: "computed_1", validated: true })
-    await createComputedJobPartner({ partner_job_id: "computed_2", validated: false })
-    await createComputedJobPartner({ partner_job_id: "existing_3", validated: true })
-    await createComputedJobPartner({ partner_job_id: "existing_4", validated: true })
-    await createComputedJobPartner({ partner_job_id: "existing_5", validated: false })
+    await createJobPartner({ partner_job_id: "existing_1", partner_label: "ft", offer_status: JOB_STATUS.ACTIVE })
+    await createJobPartner({ partner_job_id: "existing_2", partner_label: "ft", offer_status: JOB_STATUS.ACTIVE })
+    await createJobPartner({ partner_job_id: "existing_3", partner_label: "hw", offer_status: JOB_STATUS.ACTIVE })
+    await createJobPartner({ partner_job_id: "existing_4", partner_label: "hw", offer_status: JOB_STATUS.ACTIVE })
+    await createJobPartner({ partner_job_id: "existing_5", partner_label: "hw", offer_status: JOB_STATUS.ACTIVE })
+    await createComputedJobPartner({ partner_job_id: "existing_1", partner_label: "notft", validated: true })
+    await createComputedJobPartner({ partner_job_id: "computed_1", partner_label: "ft", validated: true })
+    await createComputedJobPartner({ partner_job_id: "computed_2", partner_label: "ft", validated: false })
+    await createComputedJobPartner({ partner_job_id: "existing_3", partner_label: "hw", validated: true })
+    await createComputedJobPartner({ partner_job_id: "existing_4", partner_label: "hw", validated: true })
+    await createComputedJobPartner({ partner_job_id: "existing_5", partner_label: "hw", validated: false })
 
     return async () => {
-      await getDbCollection("computed_jobs_partners").deleteMany({})
-      await getDbCollection("jobs_partners").deleteMany({})
+      // await getDbCollection("computed_jobs_partners").deleteMany({})
+      // await getDbCollection("jobs_partners").deleteMany({})
     }
   })
 
-  it("L'annulation dans jobs_partners fonctionne comme attendue : \n- les éléments devant être ann\n- les éléments validated et absents initialement de jobs partners doivent se rerouver dans jobs partners\n- les éléments validated et déjà dans jobs partners doivent toujours y être avec les data modifiées à jour", async () => {
+  it("L'annulation dans jobs_partners fonctionne comme attendue : \n- les éléments de jobs_partners qui ne sont plus dans computed doivent être taggés Annulé\n- les éléments de jobs_partners qui sont également dans computed sont toujours présents\n-  aucun éléments de jobs_partners n'a été retiré de la collection", async () => {
     await cancelRemovedJobsPartners()
 
-    // // les éléments non validated ne doivent pas se retrouver dans jobs partners
+    // les éléments de jobs_partners qui ne sont plus dans computed doivent être taggés Annulé
     const countCanceledJobsPartners = await getDbCollection("jobs_partners").countDocuments({
       partner_job_id: { $in: ["existing_1", "existing_2"] },
       offer_status: JOB_STATUS.ANNULEE,
     })
     expect.soft(countCanceledJobsPartners).toEqual(2)
 
-    const collectionA = await getDbCollection("jobs_partners").find().toArray()
-    console.log(collectionA.map((doc) => doc.partner_job_id))
-    const collectionB = await getDbCollection("computed_jobs_partners").find().toArray()
-    console.log(collectionB.map((doc) => doc.partner_job_id))
+    // les éléments de jobs_partners qui sont également dans computed sont toujours présents
+    const countRemainingJobsPartners = await getDbCollection("jobs_partners").countDocuments({
+      partner_job_id: { $in: ["existing_3", "existing_4", "existing_5"] },
+    })
+    expect.soft(countRemainingJobsPartners).toEqual(3)
 
-    // // les éléments validated et absents initialement de jobs partners doivent se rerouver dans jobs partners
-    // const countNewValidatedInJobsPartners = await getDbCollection("jobs_partners").countDocuments({ partner_job_id: { $in: ["computed_1"] } })
-    // expect.soft(countNewValidatedInJobsPartners).toEqual(1)
+    // aucun éléments de jobs_partners n'a été retiré de la collection
+    const countJobsPartners = await getDbCollection("jobs_partners").countDocuments({})
+    expect.soft(countJobsPartners).toEqual(5)
 
-    // // les éléments qui existaient avant l'import sont toujours là
-    // const countExistingStillHere = await getDbCollection("jobs_partners").countDocuments({ partner_job_id: { $in: ["existing_1", "existing_2", "existing_3"] } })
-    // expect.soft(countExistingStillHere).toEqual(3)
-
-    // // les éléments validated et déjà dans jobs partners doivent toujours y être avec les data modifiées à jour
-    // const existing_3 = await getDbCollection("jobs_partners").findOne({ partner_job_id: "existing_3" })
-    // expect.soft(existing_3?.offer_description === newDesc)
+    //gérer les cas de partner_label et job_partner_id différents pour vérifier la compréhension de l'unicité
   })
 })
