fde24383d254e68e90095ba8be7b483e1bd98290
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-09-10 14:53:38 +0200

    feat: utilisation fixtures jobOpporunity

--- server/tests/utils/jobsPartners.test.utils.ts
+++ server/tests/utils/jobsPartners.test.utils.ts
@@ -1,17 +1,16 @@
+import { generateJobsPartnersOfferPrivate } from "shared/fixtures/jobPartners.fixture"
 import { IJobsPartnersOfferPrivate, ZJobsPartnersOfferPrivate } from "shared/models/jobsPartners.model"
 import { IComputedJobsPartners } from "shared/models/jobsPartnersComputed.model "
 
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
-import { getFixture, saveDbEntity } from "./user.test.utils"
+import { saveDbEntity } from "./user.test.utils"
 
 export async function createComputedJobPartner(data: Partial<IComputedJobsPartners>) {
-  // note: passage par ZJobsPartnersOfferPrivate car getFixture sur ZComputedJobsPartners retourne des undefined sur beaucoup de champs
   const cjp = {
-    ...getFixture().fromSchema(ZJobsPartnersOfferPrivate),
     errors: [],
     validated: true,
-    ...data,
+    ...generateJobsPartnersOfferPrivate(data),
   }
   await getDbCollection("computed_jobs_partners").insertOne(cjp)
   return cjp
@@ -19,8 +18,7 @@ export async function createComputedJobPartner(data: Partial<IComputedJobsPartne
 
 export async function createJobPartner(data: Partial<IJobsPartnersOfferPrivate>) {
   const jp = {
-    ...getFixture().fromSchema(ZJobsPartnersOfferPrivate),
-    ...data,
+    ...generateJobsPartnersOfferPrivate(data),
   }
   await getDbCollection("jobs_partners").insertOne(jp)
   return jp
