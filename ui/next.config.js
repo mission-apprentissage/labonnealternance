ea9ed4ea45b16899782abe161ab4fd44ffe35364
Rémy Auricoste <remy.auricoste@gmail.com>
2024-09-18 15:16:27 +0200

    fix: hideSourceMaps: false

--- ui/next.config.js
+++ ui/next.config.js
@@ -156,7 +156,7 @@ module.exports = withSentryConfig(nextConfig, {
   // tunnelRoute: "/monitoring",
 
   // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+  hideSourceMaps: false,
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
