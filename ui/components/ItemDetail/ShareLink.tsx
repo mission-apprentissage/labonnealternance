9044d8a29a0b58489c26b6482746502fa6e0008a
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-09-26 18:40:09 +0200

    feat: ui candidat ok  with tracking

--- ui/components/ItemDetail/ShareLink.tsx
+++ ui/components/ItemDetail/ShareLink.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
+import { Button, Image, Text } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { ILbaItemFormation, ILbaItemFtJob, ILbaItemLbaCompany, ILbaItemLbaJob } from "shared"
+import { oldItemTypeToNewItemType } from "shared/constants/lbaitem"
 
 const ShareLink = ({ item }: { item: ILbaItemFormation | ILbaItemFtJob | ILbaItemLbaCompany | ILbaItemLbaJob }) => {
   const [copied, setCopied] = useState(false)
@@ -17,41 +18,40 @@ const ShareLink = ({ item }: { item: ILbaItemFormation | ILbaItemFtJob | ILbaIte
   }
 
   return (
-    <Box>
-      <Button
-        sx={copied ? {} : { borderBottom: "1px solid #000091" }}
-        mx={2}
-        mb={4}
-        px={0}
-        pb={0}
-        height={7}
-        borderRadius={0}
-        _hover={{ bg: "none" }}
-        _focus={{ bg: "none" }}
-        background="none"
-        border="none"
-        display="flex"
-        alignItems="center"
-        fontWeight={400}
-        onClick={copyLink}
-      >
-        {copied ? (
-          <>
-            <Image mr={2} src="/images/icons/share_copied_icon.svg" aria-hidden={true} alt="" />
-            <Text fontSize={14} color="bluefrance.500">
-              Lien copié !
-            </Text>
-          </>
-        ) : (
-          <>
-            <Image mr={2} src="/images/icons/share_icon.svg" aria-hidden={true} alt="" />
-            <Text fontSize={14} color="bluefrance.500">
-              Partager
-            </Text>
-          </>
-        )}
-      </Button>
-    </Box>
+    <Button
+      sx={copied ? {} : { borderBottom: "1px solid #000091" }}
+      mx={2}
+      mb={4}
+      px={0}
+      pb={0}
+      height={7}
+      borderRadius={0}
+      _hover={{ bg: "none" }}
+      _focus={{ bg: "none" }}
+      background="none"
+      border="none"
+      display="flex"
+      alignItems="center"
+      fontWeight={400}
+      onClick={copyLink}
+      data-tracking-id={`partager-${oldItemTypeToNewItemType(item.ideaType)}`}
+    >
+      {copied ? (
+        <>
+          <Image mr={2} src="/images/icons/share_copied_icon.svg" aria-hidden={true} alt="" />
+          <Text fontSize={14} color="bluefrance.500">
+            Lien copié !
+          </Text>
+        </>
+      ) : (
+        <>
+          <Image mr={2} src="/images/icons/share_icon.svg" aria-hidden={true} alt="" />
+          <Text fontSize={14} color="bluefrance.500">
+            Partager
+          </Text>
+        </>
+      )}
+    </Button>
   )
 }
 
