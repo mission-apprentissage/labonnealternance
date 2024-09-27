3c40fb610a0a35b2f7261eacc9ef9eff3de88107
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2024-09-27 15:11:56 +0200

    Update InfoBanner.tsx

--- ui/components/InfoBanner/InfoBanner.tsx
+++ ui/components/InfoBanner/InfoBanner.tsx
@@ -39,7 +39,7 @@ const envBannerText = (
       Vous êtes ici sur un site de test !
     </Text>{" "}
     Vos candidatures ne seront pas transmises aux recruteurs ni aux centres de formation. Pour accéder au site La bonne alternance, veuillez cliquer sur ce lien :{" "}
-    <Link href="https://labonnealternance.apprentissage.beta.gouv.fr" textDecoration="underline">
+    <Link href="https://labonnealternance.apprentissage.beta.gouv.fr" textDecoration="underline" isExternal aria-label="Accéder au formulaire - nouvelle fenêtre">
       La bonne alternance - Trouvez votre alternance
     </Link>
   </Text>
