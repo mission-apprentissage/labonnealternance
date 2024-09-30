c1173732a0bcf4d981507c19477f4b4449969412
Rémy Auricoste <remy.auricoste@gmail.com>
2024-09-30 10:19:29 +0200

    fix: lbac 2112: mise a jour de job processor + ajout de /processor/healthcheck (#1527)
    * fix: lbac 2112: mise a jour de job processor + ajout de /processor/healthcheck
    
    * fix: integration dans /healthcheck
    
    * fix: update job-processor
    
    * fix: tests
    
    * fix: test status code

--- server/src/http/controllers/core.controller.ts
+++ server/src/http/controllers/core.controller.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs"
+import { getProcessorHealthcheck } from "job-processor"
 import { zRoutes } from "shared"
 
 import config from "@/config"
@@ -5,25 +7,42 @@ import config from "@/config"
 import { ensureInitialization, getMongodbClientState } from "../../common/utils/mongodbUtils"
 import { Server } from "../server"
 
+const computeProcessorHealthCheck = async () => {
+  const health = await getProcessorHealthcheck()
+  const { workers } = health
+  const startedAtOpt = workers.at(0)?.task?.started_at
+  const error: boolean = !workers.length || Boolean(startedAtOpt && dayjs(startedAtOpt).isBefore(dayjs().subtract(6, "hour")))
+  return {
+    ...health,
+    error,
+  }
+}
+
 const getHealthCheck = async () => {
   ensureInitialization()
   const dbState = await getMongodbClientState()
+  const mongo = dbState === "connected"
+  const processorHealth = await computeProcessorHealthCheck()
+  const error = !mongo || processorHealth.error
+
   return {
     name: "La bonne alternance",
     version: config.version,
     env: config.env,
     commitHash: config.commitHash,
-    mongo: dbState === "connected",
+    mongo,
+    processor: processorHealth,
+    error,
   }
 }
 
 export const coreRoutes = (app: Server) => {
-  app.get("/", { schema: zRoutes.get["/"] }, async (request, response) => {
+  app.get("/", { schema: zRoutes.get["/"] }, async (_request, response) => {
     const result = await getHealthCheck()
-    response.status(result.mongo ? 200 : 500).send(result)
+    response.status(result.error ? 500 : 200).send(result)
   })
-  app.get("/healthcheck", { schema: zRoutes.get["/healthcheck"] }, async (request, response) => {
+  app.get("/healthcheck", { schema: zRoutes.get["/healthcheck"] }, async (_request, response) => {
     const result = await getHealthCheck()
-    response.status(result.mongo ? 200 : 500).send(result)
+    response.status(result.error ? 500 : 200).send(result)
   })
 }
--- server/src/http/healthcheck.test.ts
+++ server/src/http/healthcheck.test.ts
@@ -1,6 +1,9 @@
 import { useMongo } from "@tests/utils/mongo.test.utils"
 import { useServer } from "@tests/utils/server.test.utils"
+import { addJob } from "job-processor"
 import { describe, expect, it } from "vitest"
+
+import { setupJobProcessor } from "@/jobs/jobs"
 
 import config from "../config"
 
@@ -8,10 +11,13 @@ describe("healthcheckRoutes", () => {
   useMongo()
   const httpClient = useServer()
   it("Vérifie que le server fonctionne", async () => {
+    await setupJobProcessor()
+    await addJob({ name: "db:validate", queued: false, payload: {} })
     const response = await httpClient().inject({ method: "GET", path: "/api" })
 
-    expect(response.statusCode).toBe(200)
-    expect(JSON.parse(response.body)).toEqual({
+    expect.soft(response.statusCode).toEqual(200)
+    const json = JSON.parse(response.body)
+    expect.soft(json).toMatchObject({
       commitHash: "hash-test",
       name: "La bonne alternance",
       version: config.version,
