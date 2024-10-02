ded65ddd463b267580f88d7882b461986e270245
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-10-02 18:33:36 +0200

    feat: setting tracking cookie

--- ui/common/utils/trackingCookieUtils.ts
+++ ui/common/utils/trackingCookieUtils.ts
@@ -1,3 +1,5 @@
+import { NextRouter } from "next/router"
+
 const COOKIE_REMOVE_TIME = -36 * 60 * 60 * 1000
 export const MTM_CONSENT_COOKIE_DURATION = 30 * 365 * 24 * 60 * 60 * 1000
 
@@ -7,9 +9,9 @@ export const CONSENT_COOKIE_NAME = "mtm_consent"
 export const getCookie = (cookieName) => {
   const cookiePattern = new RegExp("(^|;)[ ]*" + cookieName + "=([^;]*)"),
     cookieMatch = cookiePattern.exec(document.cookie)
-  return cookieMatch ? window.decodeURIComponent(cookieMatch[2]) : 0
+  return cookieMatch ? window.decodeURIComponent(cookieMatch[2]) : null
 }
-export const setCookie = (cookieName, value, msToExpire) => {
+export const setCookie = (cookieName: string, value: string, msToExpire?: number) => {
   const expiryDate = new Date()
   expiryDate.setTime(new Date().getTime() + msToExpire)
   document.cookie = cookieName + "=" + window.encodeURIComponent(value) + (msToExpire ? ";expires=" + expiryDate.toString() : "") + ";path=/;domain=;SameSite=Lax"
@@ -18,9 +20,10 @@ export const removeCookie = (cookieName) => {
   setCookie(cookieName, "", COOKIE_REMOVE_TIME)
 }
 export const setIsTrackingEnabled = () => {
-  const consentRemovedCookie = getCookie(CONSENT_REMOVED_COOKIE_NAME)
-  if (consentRemovedCookie) {
+  if (isConsentRemoved()) {
     optUserOut()
+  } else {
+    setReferer()
   }
 }
 
@@ -32,4 +35,24 @@ export const optUserOut = () => {
 export const forgetOptUserOut = () => {
   // @ts-expect-error
   window?._paq?.push(["forgetUserOptOut"])
+}
+
+const isConsentRemoved = () => {
+  return getCookie(CONSENT_REMOVED_COOKIE_NAME) ? true : false
+}
+
+export const setReferer = () => {
+  if (document.referrer) {
+    setCookie("referer", document.referrer)
+  }
+}
+export const setTrackingCookies = (router: NextRouter) => {
+  if (!isConsentRemoved() && !getCookie("utm_campaign")) {
+    const { query } = router
+    const mtm_campaign = query?.mtm_campaign as string
+    const utm_campaign = mtm_campaign || (query?.utm_campaign as string)
+    if (utm_campaign) {
+      setCookie("utm_campaign", utm_campaign)
+    }
+  }
 }
