c003b3a1ef64861700ebd28865f9d77c1a16a894
Kevin Barnoin <k.barnoin@gmail.com>
2024-10-08 17:58:40 +0200

    feat: sentry v8

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,4 +4,11 @@ config({ path: ".env" })
 config({ path: ".env.local", override: true })
 
 // Dynamic import to start server after env are loaded
-import("./main")
+import("./common/sentry/sentry.js")
+  .then(async ({ initSentry }) => {
+    initSentry()
+  })
+  .then(async () => {
+    // Dynamic import to start server after env are loaded
+    return import("./main.js")
+  })
