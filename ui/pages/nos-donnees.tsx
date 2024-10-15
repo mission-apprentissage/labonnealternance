8e73370f42860f57beea4f941db5c5e3647c7dae
Samir BENFARES <sbenfares@gmail.com>
2024-10-15 17:20:31 +0200

    feat: update ui

--- ui/pages/nos-donnees.tsx
+++ ui/pages/nos-donnees.tsx
@@ -1,5 +1,5 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { Box, Container, Divider, Grid, GridItem, Link, Text } from "@chakra-ui/react"
+import { Box, Code, Container, Divider, Grid, GridItem, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react"
 import { NextSeo } from "next-seo"
 import React from "react"
 
@@ -28,30 +28,74 @@ const Developpeurs = () => (
           </GridItem>
           <GridItem px={4} colSpan={[12, 12, 12, 7]}>
             <Text as="p" mb={4}>
-              Afin de faciliter l’accès aux données de l'apprentissage pour les publics là où ils se trouvent (notamment sur votre site internet !), nous avons développé 4 API et
-              un Widget, disponibles en open source.
+              Les opportunités d'emplois et de formation exposées sur le site La bonne alternance sont accessibles à tous via notre API.
             </Text>
-
+            <Text as="p" mt={4} mb={4}>
+              Utilisée dans sa globalité cette API permet la mise en relation des candidats à l’alternance avec des entreprises accueillant des alternants d’une part et/ou avec des
+              organismes de formation en alternance d’autre part.
+            </Text>
+            <UnorderedList>
+              <ListItem>
+                Recherche d'opportunités d'emploi en alternance :{" "}
+                <Link
+                  variant="editorialContentLink"
+                  aria-label="Accès au site api.apprentissage - nouvelle fenêtre"
+                  href="https://api.apprentissage.beta.gouv.fr/fr/explorer/recherche-offre"
+                  isExternal
+                >
+                  https://api.apprentissage.beta.gouv.fr/fr/explorer/recherche-offre
+                  <ExternalLinkIcon mx="2px" />
+                </Link>
+              </ListItem>
+              <ListItem>Recherche d'opportunités de formation en alternance : à venir</ListItem>
+              <ListItem>Envoi de candidatures à une opportunités d'emploi : à venir</ListItem>
+              <ListItem>Envoi de prise de contact à une opportunité de formation : à venir</ListItem>
+            </UnorderedList>
+            <Text as="p" mt={4} mb={4}>
+              Pour une intégration rapide et simplifiée, les données présentées ci-dessus sont également disponibles sous forme de widget.
+            </Text>
+            <Text as="p" mt={4} mb={4}>
+              Disponible en marque blanche ce widget est proposé en plusieurs tailles. Par ailleurs, différents filtres peuvent être appliqués aux données qu’il restitue.
+            </Text>
+            <Text as="p" mb={4}>
+              Pour exploiter le widget,{" "}
+              <Link
+                variant="editorialContentLink"
+                aria-label="Exposez tout ou partie de l'offre de formation et d'emploi en alternance - nouvelle fenêtre"
+                href="https://api.gouv.fr/guides/widget-la-bonne-alternance"
+                isExternal
+              >
+                consultez cette documentation.
+              </Link>
+            </Text>
+            <Text as="p" mb={4}>
+              Pour tester le widget,{" "}
+              <Link
+                variant="editorialContentLink"
+                aria-label="Testez le widget - nouvelle fenêtre"
+                href="https://labonnealternance.apprentissage.beta.gouv.fr/test-widget"
+                isExternal
+              >
+                consultez cette page.
+              </Link>
+            </Text>
             <Divider mt={4} my={12} w="100%" />
             <Text as="p" mb={4}>
-              Testez la dernière version (v2) de nos API et accédez à leur documentation sur la plateforme API Apprentissage :
+              Par ailleurs, collectez et partagez les besoins en recrutement en alternance de vos entreprises, afin d’améliorer leur visibilité auprès des candidats à l’alternance
+              via cette API :{" "}
+              <Link variant="editorialContentLink" aria-label="Dépôt d'offre" href="https://api.apprentissage.beta.gouv.fr/fr/explorer/depot-offre" isExternal>
+                https://api.apprentissage.beta.gouv.fr/fr/explorer/depot-offre
+              </Link>
             </Text>
-            <Link
-              variant="editorialContentLink"
-              aria-label="Accès au site api.apprentissage - nouvelle fenêtre"
-              href="https://api.apprentissage.beta.gouv.fr/fr/explorer"
-              isExternal
-            >
-              https://api.apprentissage.beta.gouv.fr/fr/explorer <ExternalLinkIcon mx="2px" />
-            </Link>
-
-            <Divider mt={4} my={12} w="100%" />
             <Text as="p" mb={4}>
-              Testez le widget et l'ancienne version (v1) de nos API et accédez à leur documentation sur le site API.gouv :
+              Pour une intégration rapide et simplifiée, cette fonctionnalité est également disponible sous forme de widget.
             </Text>
-            <Link variant="editorialContentLink" aria-label="Accès au site api.gouv - nouvelle fenêtre" href="https://api.gouv.fr/les-api/api-la-bonne-alternance" isExternal>
-              https://api.gouv.fr/les-api/api-la-bonne-alternance <ExternalLinkIcon mx="2px" />
-            </Link>
+            <Text as="p" mb={4}>
+              Pour exploiter le widget, utilisez l’adresse suivante au sein d’une balise HTML en remplaçant "ORIGINE" par le nom de votre établissement.
+            </Text>
+            <Code>
+              {"<iframe loading=`lazy` src=`https://labonnealternance.apprentissage.beta.gouv.fr/espace-pro/widget/ORIGINE` width=`100%` height=`800` frameBorder=`0`></iframe>"}
+            </Code>
           </GridItem>
         </Grid>
       </Container>
