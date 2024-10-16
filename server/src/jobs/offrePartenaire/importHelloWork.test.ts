d21321705300b8394f07a26b736ea55aa00eb359
Rémy Auricoste <remy.auricoste@gmail.com>
2024-10-16 11:07:11 +0200

    fix: tests

--- server/src/jobs/offrePartenaire/importHelloWork.test.ts
+++ server/src/jobs/offrePartenaire/importHelloWork.test.ts
@@ -2,14 +2,25 @@ import fs from "node:fs"
 
 import { useMongo } from "@tests/utils/mongo.test.utils"
 import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it, vi } from "vitest"
 
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 import { importHelloWorkRaw, importHelloWorkToComputed } from "./importHelloWork"
 
+const now = new Date("2024-07-21T04:49:06.000+02:00")
+
 describe("importHelloWork", () => {
   useMongo()
+
+  beforeEach(() => {
+    vi.useFakeTimers()
+    vi.setSystemTime(now)
+
+    return () => {
+      vi.useRealTimers()
+    }
+  })
 
   it("should test the whole import", async () => {
     const fileStream = fs.createReadStream("server/src/jobs/offrePartenaire/importHelloWork.test.input.xml")
