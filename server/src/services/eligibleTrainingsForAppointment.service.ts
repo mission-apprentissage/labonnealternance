48b42dd67dc89eadad813c9046fac06f17856546
Kevin Barnoin <k.barnoin@gmail.com>
2024-10-17 09:33:01 +0200

    feat: lbac 2148: appointment route V2, test and model (#1581)
    * feat: appointment route, test and model
    
    * refactor: return 200 if error to avoid mistake and match v1
    
    * fix: update snapshot
    
    * feat: remove id rco

--- server/src/services/eligibleTrainingsForAppointment.service.ts
+++ server/src/services/eligibleTrainingsForAppointment.service.ts
@@ -2,6 +2,7 @@ import { badRequest, internal, notFound } from "@hapi/boom"
 import { Filter, ObjectId } from "mongodb"
 import { IEligibleTrainingsForAppointment, IFormationCatalogue } from "shared"
 import { IAppointmentRequestContextCreateResponseSchema } from "shared/routes/appointments.routes"
+import { IAppointmentContextAPI, IAppointmentResponseSchema } from "shared/routes/appointments.routes.v2"
 
 import { logger } from "@/common/logger"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
@@ -125,6 +126,52 @@ export const findElligibleTrainingForAppointment = async (req: any): Promise<IAp
     cfd: eligibleTrainingsForAppointment.training_code_formation_diplome,
     localite: eligibleTrainingsForAppointment.lieu_formation_city,
     id_rco_formation: eligibleTrainingsForAppointment.rco_formation_id,
+    cle_ministere_educatif: eligibleTrainingsForAppointment.cle_ministere_educatif,
+    form_url: `${config.publicUrl}/espace-pro/form?referrer=${referrerObj.name.toLowerCase()}&cleMinistereEducatif=${encodeURIComponent(
+      eligibleTrainingsForAppointment.cle_ministere_educatif
+    )}`,
+  }
+}
+
+export const findElligibleTrainingForAppointmentV2 = async (context: IAppointmentContextAPI): Promise<IAppointmentResponseSchema> => {
+  const { referrer } = context
+  const referrerObj = getReferrerByKeyName(referrer)
+  let eligibleTrainingsForAppointment: IEligibleTrainingsForAppointment | null
+
+  if ("cle_ministere_educatif" in context) {
+    eligibleTrainingsForAppointment = await findEligibleTrainingByMinisterialKey(context.cle_ministere_educatif)
+  } else if ("parcoursup_id" in context) {
+    eligibleTrainingsForAppointment = await findEligibleTrainingByParcoursupId(context.parcoursup_id)
+  } else if ("onisep_id" in context) {
+    eligibleTrainingsForAppointment = await findEligibleTrainingByActionFormation(context.onisep_id)
+  } else {
+    throw badRequest("Critère de recherche non conforme.")
+  }
+
+  if (!eligibleTrainingsForAppointment) {
+    throw notFound("Training not found")
+  }
+
+  if (!isOpenForAppointments(eligibleTrainingsForAppointment, referrerObj.name)) {
+    return {
+      error: "Appointment request not available",
+    }
+  }
+
+  const etablissement = await findEtablissement(eligibleTrainingsForAppointment.etablissement_formateur_siret)
+
+  if (!etablissement) {
+    throw internal("Training establishment not found")
+  }
+
+  return {
+    etablissement_formateur_entreprise_raison_sociale: etablissement.raison_sociale,
+    intitule_long: eligibleTrainingsForAppointment.training_intitule_long,
+    lieu_formation_adresse: eligibleTrainingsForAppointment.lieu_formation_street,
+    code_postal: eligibleTrainingsForAppointment.lieu_formation_zip_code,
+    etablissement_formateur_siret: etablissement.formateur_siret ?? null,
+    cfd: eligibleTrainingsForAppointment.training_code_formation_diplome,
+    localite: eligibleTrainingsForAppointment.lieu_formation_city,
     cle_ministere_educatif: eligibleTrainingsForAppointment.cle_ministere_educatif,
     form_url: `${config.publicUrl}/espace-pro/form?referrer=${referrerObj.name.toLowerCase()}&cleMinistereEducatif=${encodeURIComponent(
       eligibleTrainingsForAppointment.cle_ministere_educatif
