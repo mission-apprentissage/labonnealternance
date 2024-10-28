0310cb04aaec3ba2bb7768aa4bdb9cf410661107
Kevin Barnoin <k.barnoin@gmail.com>
2024-10-28 11:50:31 +0100

    feat: update sentry (#1370)
    * feat: update sentry
    
    * feat: update package
    
    * feat: update config (wip)
    
    * feat: sentry v8
    
    * fix: dedupe yarn.lock
    
    * fix: remove comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,5 +3,11 @@ import { config } from "dotenv"
 config({ path: ".env" })
 config({ path: ".env.local", override: true })
 
-// Dynamic import to start server after env are loaded
-import("./main")
+import("./common/sentry/sentry.js")
+  .then(async ({ initSentry }) => {
+    initSentry()
+  })
+  .then(async () => {
+    // Dynamic import to start server after env are loaded
+    return import("./main.js")
+  })
