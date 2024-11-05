b356e9a2b2d6adb54415344ac250a5d82a7d6fce
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-05 17:14:57 +0100

    feat: fonction utilitaire

--- shared/utils/stringUtils.ts
+++ shared/utils/stringUtils.ts
@@ -1,2 +1,11 @@
 export const removeAccents = (str: string) => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 export const removeRegexChars = (str: string) => str.replace(/[.*+?^${}()|[\]\\]/g, "")
+export const joinNonNullStrings = (values: (string | null)[]): string | null => {
+  const result = values
+    .filter((item) => item !== null && item.trim() !== "")
+    .map((item) => item!.trim() + " ")
+    .join("")
+    .trim()
+
+  return result || null
+}
