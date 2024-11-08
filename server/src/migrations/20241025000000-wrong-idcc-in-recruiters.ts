a5e725dc207536327092ccc8608caf975341279d
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-08 10:12:53 +0100

    feat: migrations idcc number nullable

--- server/src/migrations/20241025000000-wrong-idcc-in-recruiters.ts
+++ server/src/migrations/20241025000000-wrong-idcc-in-recruiters.ts
@@ -7,7 +7,7 @@ export const up = async (db: Db) => {
 
   await db.collection("recruiters").updateMany(
     {
-      $or: [{ idcc: { $regex: "^a0I0Y" } }, { idcc: { $regex: "^Opco multiple" } }],
+      $or: [{ idcc: { $regex: "^a0I0Y" } }, { idcc: { $regex: "^Opco multiple" } }, { idcc: { $exists: false } }],
     },
     {
       $set: {
@@ -18,6 +18,29 @@ export const up = async (db: Db) => {
       bypassDocumentValidation: true,
     }
   )
+  await db.collection("recruiters").updateMany({ idcc: { $type: "string" } }, [{ $set: { idcc: { $toInt: "$idcc" } } }], {
+    bypassDocumentValidation: true,
+  })
+
+  await db.collection("entreprises").updateMany(
+    { $or: [{ idcc: { $regex: "^a0I0Y" } }, { idcc: { $regex: "^Opco multiple" } }, { idcc: { $exists: false } }] },
+    {
+      $set: {
+        idcc: null,
+      },
+    },
+    {
+      bypassDocumentValidation: true,
+    }
+  )
+
+  await db.collection("entreprises").updateMany({ idcc: { $type: "string" } }, [{ $set: { idcc: { $toInt: "$idcc" } } }], {
+    bypassDocumentValidation: true,
+  })
+
+  await db.collection("opcos").updateMany({ idcc: { $type: "string" } }, [{ $set: { idcc: { $toInt: "$idcc" } } }], {
+    bypassDocumentValidation: true,
+  })
 
   logger.info("20241025000000-wrong-idcc-in-recruiters ended")
 }
