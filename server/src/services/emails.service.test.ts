832e4cb46334eeeda068f2ba6007594d2230db7c
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-12 16:03:38 +0100

    feat(lba-2126): améliorations autour des événement brevo de blacklisting (#1620)
    * feat: ajout d'autres events pour les emails à surveiller
    
    * feat: wip identification des reasons
    
    * feat: wip
    
    * feat: seems ok
    
    * fix: typage
    
    * feat: normalisation constantes
    
    * feat: wip tests
    
    * feat: wip tests
    
    * feat: wip tests
    
    * feat: début tests sur job blocked emails
    
    * feat: tests job blocked mail FIN
    
    * feat: cleaning
    
    * feat: simplification avec else if
    
    * feat: noms constantes
    
    * feat: else if
    
    * feat: test mock
    
    * feat: mock underlying function

---
+++ server/src/services/emails.service.test.ts
@@ -0,0 +1,272 @@
+import { useMongo } from "@tests/utils/mongo.test.utils"
+import { EApplicantRole } from "shared/constants/rdva"
+import { generateApplicationFixture } from "shared/fixtures/application.fixture"
+import { generateAppointmentFixture, generateEligibleTrainingEstablishmentFixture, generateEligibleTrainingFixture } from "shared/fixtures/appointment.fixture"
+import { generateUserFixture } from "shared/fixtures/user.fixture"
+import { generateUserWithAccountFixture } from "shared/fixtures/userWithAccount.fixture"
+import { beforeEach, describe, expect, it, vi } from "vitest"
+
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { BrevoBlockedReasons, saveBlacklistEmails } from "@/jobs/updateBrevoBlockedEmails/updateBrevoBlockedEmails"
+import { BlackListOrigins } from "@/services/application.service"
+
+import { BrevoEventStatus } from "./brevo.service"
+import { IBrevoWebhookEvent, processHardBounceWebhookEvent } from "./emails.service"
+
+async function cleanTest() {
+  await getDbCollection("emailblacklists").deleteMany({})
+  await getDbCollection("applications").deleteMany({})
+  await getDbCollection("recruteurslba").deleteMany({})
+  await getDbCollection("etablissements").deleteMany({})
+  await getDbCollection("users").deleteMany({})
+  await getDbCollection("userswithaccounts").deleteMany({})
+  await getDbCollection("eligible_trainings_for_appointments").deleteMany({})
+}
+
+describe("email blaklist events", () => {
+  useMongo()
+
+  beforeEach(async () => {
+    return async () => {
+      await cleanTest()
+    }
+  })
+
+  const blacklistedEmail = "blacklisted@email.com"
+
+  const fakeMessageId_1 = "<fake_id@domain.net>"
+
+  const baseWebHookPayload: IBrevoWebhookEvent = {
+    event: BrevoEventStatus.HARD_BOUNCE,
+    email: blacklistedEmail,
+    id: 1,
+    date: "2024-10-15 11:00:00",
+    "message-id": "<xxx@msgid.domain>",
+    reason: "",
+    tag: "",
+    subject: "",
+    sending_ip: "x.x.x.x",
+    ts_epoch: 1,
+    template_id: 1,
+  }
+
+  const baseBlockedAddress = [
+    {
+      email: blacklistedEmail,
+      reason: {
+        code: BrevoBlockedReasons.ADMIN_BLOCKED,
+      },
+    },
+  ]
+
+  it("Non 'blocked' event shoud throw an error", async () => {
+    baseWebHookPayload.event = BrevoEventStatus.DELIVRE
+    await expect.soft(processHardBounceWebhookEvent(baseWebHookPayload)).rejects.toThrow("Non hardbounce event received on hardbounce webhook route")
+  })
+
+  it("Unidentified hardbounce should register campaign origin", async () => {
+    baseWebHookPayload.event = BrevoEventStatus.HARD_BOUNCE
+    await processHardBounceWebhookEvent(baseWebHookPayload)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.CAMPAIGN} (${BrevoEventStatus.HARD_BOUNCE})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Unidentified blocked should register unknown origin", async () => {
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.UNKNOWN} (${BrevoEventStatus.BLOCKED})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Applicant blocked should register candidature_spontanee_candidat (blocked)", async () => {
+    await getDbCollection("applications").insertOne(generateApplicationFixture({ applicant_email: blacklistedEmail, to_applicant_message_id: fakeMessageId_1 }))
+    baseWebHookPayload.event = BrevoEventStatus.BLOCKED
+    baseWebHookPayload["message-id"] = fakeMessageId_1
+
+    await processHardBounceWebhookEvent(baseWebHookPayload)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.CANDIDATURE_SPONTANEE_CANDIDAT} (${BrevoEventStatus.BLOCKED})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Unsubscribed variation events should register correct origin with (unsubscribed) reason", async () => {
+    baseBlockedAddress[0].reason.code = BrevoBlockedReasons.UNSUBSCRIBED_VIA_API
+    await getDbCollection("applications").insertOne(generateApplicationFixture({ applicant_email: blacklistedEmail }))
+
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    let blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.CANDIDATURE_SPONTANEE_CANDIDAT} (${BrevoEventStatus.UNSUBSCRIBED})`,
+        email: blacklistedEmail,
+      })
+    )
+
+    await getDbCollection("emailblacklists").deleteMany({})
+    await getDbCollection("applications").insertOne(generateApplicationFixture({ company_email: blacklistedEmail }))
+    baseBlockedAddress[0].reason.code = BrevoBlockedReasons.UNSUBSCRIBED_VIA_EMAIL
+
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.CANDIDATURE_SPONTANEE_RECRUTEUR} (${BrevoEventStatus.UNSUBSCRIBED})`,
+        email: blacklistedEmail,
+      })
+    )
+
+    await getDbCollection("emailblacklists").deleteMany({})
+    await getDbCollection("applications").deleteMany({})
+    await getDbCollection("users").insertOne(generateUserFixture({ email: blacklistedEmail, role: EApplicantRole.CANDIDAT }))
+    baseBlockedAddress[0].reason.code = BrevoBlockedReasons.UNSUBSCRIBED_VIA_MA
+
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.PRDV_CANDIDAT} (${BrevoEventStatus.UNSUBSCRIBED})`,
+        email: blacklistedEmail,
+      })
+    )
+
+    await getDbCollection("emailblacklists").deleteMany({})
+    await getDbCollection("users").deleteMany({})
+    await getDbCollection("userswithaccounts").insertOne(generateUserWithAccountFixture({ email: blacklistedEmail }))
+    baseBlockedAddress[0].reason.code = BrevoBlockedReasons.HARD_BOUNCE
+
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.USER_WITH_ACCOUNT} (${BrevoEventStatus.HARD_BOUNCE})`,
+        email: blacklistedEmail,
+      })
+    )
+
+    await getDbCollection("emailblacklists").deleteMany({})
+    await getDbCollection("eligible_trainings_for_appointments").insertOne(generateEligibleTrainingFixture({ lieu_formation_email: blacklistedEmail }))
+    baseBlockedAddress[0].reason.code = BrevoBlockedReasons.SPAM
+
+    await saveBlacklistEmails(baseBlockedAddress)
+
+    blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.PRDV_CFA} (${BrevoEventStatus.SPAM})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Recruteur LBA with SPAM (plainte) should register candidature_spontanee_recruteur (spam)", async () => {
+    await getDbCollection("applications").insertOne(generateApplicationFixture({ company_email: blacklistedEmail, to_company_message_id: fakeMessageId_1 }))
+    baseWebHookPayload.event = BrevoEventStatus.SPAM
+    baseWebHookPayload["message-id"] = fakeMessageId_1
+
+    const mockedFn = vi.fn().mockReturnValue(null)
+
+    await processHardBounceWebhookEvent(baseWebHookPayload, mockedFn)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.CANDIDATURE_SPONTANEE_RECRUTEUR} (${BrevoEventStatus.SPAM})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Adresse CFA PRDV with Unsubscribe should register prise_de_rdv_CFA (unsubscribed)", async () => {
+    await getDbCollection("appointments").insertOne(
+      generateAppointmentFixture({
+        to_cfa_mails: [
+          {
+            campaign: "CANDIDAT_APPOINTMENT",
+            status: null,
+            message_id: fakeMessageId_1,
+            email_sent_at: new Date(),
+          },
+        ],
+      })
+    )
+    baseWebHookPayload.event = BrevoEventStatus.UNSUBSCRIBED
+    baseWebHookPayload["message-id"] = fakeMessageId_1
+
+    await processHardBounceWebhookEvent(baseWebHookPayload)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.PRDV_CFA} (${BrevoEventStatus.UNSUBSCRIBED})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Adresse Candidat PRDV with hardbounce should register prise_de_rdv_candidat (hardbounce)", async () => {
+    await getDbCollection("appointments").insertOne(
+      generateAppointmentFixture({
+        to_applicant_mails: [
+          {
+            campaign: "CANDIDAT_APPOINTMENT",
+            status: null,
+            message_id: fakeMessageId_1,
+            email_sent_at: new Date(),
+          },
+        ],
+      })
+    )
+    baseWebHookPayload.event = BrevoEventStatus.HARD_BOUNCE
+    baseWebHookPayload["message-id"] = fakeMessageId_1
+
+    await processHardBounceWebhookEvent(baseWebHookPayload)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.PRDV_CANDIDAT} (${BrevoEventStatus.HARD_BOUNCE})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+
+  it("Adresse campagne PRDV with SPAM should register prise_de_rdv_invitation (SPAM)", async () => {
+    await getDbCollection("etablissements").insertOne(
+      generateEligibleTrainingEstablishmentFixture({
+        to_CFA_invite_optout_last_message_id: fakeMessageId_1,
+      })
+    )
+    baseWebHookPayload.event = BrevoEventStatus.HARD_BOUNCE
+    baseWebHookPayload["message-id"] = fakeMessageId_1
+
+    await processHardBounceWebhookEvent(baseWebHookPayload)
+
+    const blEvent = await getDbCollection("emailblacklists").findOne({ email: blacklistedEmail })
+    expect.soft(blEvent).toEqual(
+      expect.objectContaining({
+        blacklisting_origin: `${BlackListOrigins.PRDV_INVITATION} (${BrevoEventStatus.HARD_BOUNCE})`,
+        email: blacklistedEmail,
+      })
+    )
+  })
+})
