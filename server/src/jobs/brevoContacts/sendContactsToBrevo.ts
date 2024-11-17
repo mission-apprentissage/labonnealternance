11964bdd818e3f7dc1c14b036d8e940a5218dec9
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-17 22:08:41 +0100

    feat: cleaner

--- server/src/jobs/brevoContacts/sendContactsToBrevo.ts
+++ server/src/jobs/brevoContacts/sendContactsToBrevo.ts
@@ -2,7 +2,7 @@ import { Transform } from "stream"
 import { pipeline } from "stream/promises"
 
 import dayjs from "dayjs"
-import { /*AccessEntityType, */ AccessStatus } from "shared/models"
+import { AccessEntityType, AccessStatus } from "shared/models"
 import { UserEventType } from "shared/models/userWithAccount.model"
 import SibApiV3Sdk from "sib-api-v3-sdk"
 
@@ -11,30 +11,33 @@ import { ensureInitialization } from "@/common/utils/mongodbUtils"
 import { notifyToSlack } from "@/common/utils/slackUtils"
 import config from "@/config"
 
-// type IBrevoContact = {
-//   user_origin: string
-//   user_first_name: string            PRENOM
-//   user_last_name: string             NOM
-//   user_email: string                 EMAIL
-//   role_createdAt: Date
-//   role_authorized_type: AccessEntityType.CFA | AccessEntityType.ENTREPRISE
-//   entreprise_enseigne?: string | null | undefined
-//   entreprise_raison_sociale?: string | null | undefined
-//   cfa_enseigne?: string | null | undefined
-//   cfa_raison_sociale?: string | null | undefined
-//   job_count?: string | null | undefined
-// }
+type IBrevoContact = {
+  user_origin: string
+  user_first_name: string
+  user_last_name: string
+  user_email: string
+  role_createdAt: Date
+  role_authorized_type: AccessEntityType.CFA | AccessEntityType.ENTREPRISE
+  entreprise_enseigne?: string | null | undefined
+  entreprise_raison_sociale?: string | null | undefined
+  cfa_enseigne?: string | null | undefined
+  cfa_raison_sociale?: string | null | undefined
+  job_count?: string | null | undefined
+}
 
-let batch = []
+const CONTACT_LIST_RECETTE = 5
+const CONTACT_LIST = 6
+
+let batch: IBrevoContact[] = []
 let contactCount = 0
 
 const chunkTransform = new Transform({
   objectMode: true,
   transform(contact, _, callback) {
-    batch.push(contact)
+    batch.push(contact as IBrevoContact)
 
     if (batch.length === 1000) {
-      postToBrevo(batch)
+      postToBrevo(batch as IBrevoContact[])
       callback()
       batch = []
     } else {
@@ -43,17 +46,14 @@ const chunkTransform = new Transform({
   },
   flush(callback) {
     if (batch && batch.length > 0) {
-      postToBrevo(batch)
+      postToBrevo(batch as IBrevoContact[])
     }
     callback()
   },
 })
 
-const postToBrevo = async (contacts) => {
+const postToBrevo = async (contacts: IBrevoContact[]) => {
   contactCount += contacts.length
-  console.log("last stage doc", contacts.length, contacts[0].user_email, contacts.at(-1).user_email)
-
-  //console.log("contacts : ", contacts.length, contacts.length ? contacts[0] : null)
 
   const defaultClient = SibApiV3Sdk.ApiClient.instance
   const apiKey = defaultClient.authentications["api-key"]
@@ -67,7 +67,7 @@ const postToBrevo = async (contacts) => {
   contacts.forEach((contact) => {
     requestContactImport.fileBody += `${contact.user_email};${contact.user_first_name};${contact.user_last_name};${contact.user_origin ?? ""};${contact.role_authorized_type ?? ""};${dayjs(contact.role_createdAt).format("YYYY-MM-DD")};${contact.entreprise_enseigne ?? ""};${contact.entreprise_raison_sociale ?? ""};${contact.cfa_enseigne ?? ""};${contact.cfa_raison_sociale ?? ""};0\n`
   })
-  requestContactImport.listIds = [5]
+  requestContactImport.listIds = [config.env === "production" ? CONTACT_LIST : CONTACT_LIST_RECETTE]
   requestContactImport.updateExistingContacts = true
   requestContactImport.emptyContactsAttributes = true
 
@@ -109,49 +109,15 @@ export const sendContactsToBrevo = async () => {
       .stream()
     await pipeline(cursor, chunkTransform)
 
-    console.log("All documents processed!")
-
     await notifyToSlack({
       subject: `Envoi des contacts vers Brevo`,
       message: `${contactCount} envoyés vers Brevo.`,
       error: contactCount === 0,
     })
   } catch (err) {
-    console.log("rpout ", err)
     await notifyToSlack({ subject: "Envoi des contacts vers Brevo", message: `ECHEC envoi des contacts vers Brevo`, error: true })
     throw err
   }
 
   logger.info("contacts sent to brevo")
 }
-
-// const defaultClient = SibApiV3Sdk.ApiClient.instance
-//   const apiKey = defaultClient.authentications["api-key"]
-//   apiKey.apiKey = config.smtp.brevoApiKey
-
-//   const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
-// let result = await apiInstance.getTransacBlockedContacts(opts)
-
-//   total = result.count
-
-//   if (!Number.isFinite(total)) {
-//     return
-//   }const SibApiV3Sdk = require('sib-api-v3-sdk');
-// let defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-// let apiKey = defaultClient.authentications['api-key'];
-// apiKey.apiKey = 'YOUR API KEY';
-
-// let apiInstance = new SibApiV3Sdk.AttributesApi();
-// apiInstance.getAttributes().then(function(data) {
-//   console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-// }, function(error) {
-//   console.error(error);
-// });
-
-//   while (offset < total) {
-//     await saveBlacklistEmails(result.contacts)
-
-//     offset += limit
-//     result = await apiInstance.getTransacBlockedContacts({ ...opts, offset })
-//   }
