e40067140a59b20f8e681a949af1596fe2e9cf80
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-22 11:27:42 +0100

    Update shared/utils/stringUtils.ts
    Co-authored-by: Rémy Auricoste <remy.auricoste@gmail.com>

--- shared/utils/stringUtils.ts
+++ shared/utils/stringUtils.ts
@@ -2,10 +2,7 @@ export const removeAccents = (str: string) => str.normalize("NFD").replace(/[\u0
 export const removeRegexChars = (str: string) => str.replace(/[.*+?^${}()|[\]\\]/g, "")
 export const joinNonNullStrings = (values: (string | null | undefined)[]): string | null => {
   const result = values
-    .filter((item) => item !== null && item !== undefined && item.trim() !== "")
-    .map((item) => item!.trim() + " ")
-    .join("")
-    .trim()
+  .flatMap(item => item && item.trim() ? [item.trim()] : []).join(" ")
 
   return result || null
 }
