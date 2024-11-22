94febc08641f9e91c48514c95d58321b0a74a61b
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-22 11:29:55 +0100

    feat: embryon

--- server/src/jobs/offrePartenaire/fillComputedJobsPartners.ts
+++ server/src/jobs/offrePartenaire/fillComputedJobsPartners.ts
@@ -1,3 +1,4 @@
+import { fillLocationInfosForPartners } from "./fillLocationInfosForPartners copy"
 import { fillOpcoInfosForPartners } from "./fillOpcoInfosForPartners"
 import { fillSiretInfosForPartners } from "./fillSiretInfosForPartners"
 import { validateComputedJobPartners } from "./validateComputedJobPartners"
@@ -5,5 +6,6 @@ import { validateComputedJobPartners } from "./validateComputedJobPartners"
 export const fillComputedJobsPartners = async () => {
   await fillOpcoInfosForPartners()
   await fillSiretInfosForPartners()
+  await fillLocationInfosForPartners()
   await validateComputedJobPartners()
 }
