43e239bfcff3c4ab97ea46710c3f3fa00912eb74
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-26 16:55:19 +0100

    fix: tests jobOpportunity

--- server/src/services/jobs/jobOpportunity/jobOpportunity.service.test.ts
+++ server/src/services/jobs/jobOpportunity/jobOpportunity.service.test.ts
@@ -22,6 +22,7 @@ import { apiEntrepriseEtablissementFixture } from "@/common/apis/apiEntreprise/a
 import { getRomeoPredictions, searchForFtJobs } from "@/common/apis/franceTravail/franceTravail.client"
 import { franceTravailRomeoFixture, generateFtJobFixture } from "@/common/apis/franceTravail/franceTravail.client.fixture"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { saveGeolocationInCache } from "@/services/cacheGeolocation.service"
 import { certificationFixtures } from "@/services/external/api-alternance/certification.fixture"
 
 import { FTJob } from "../../ftjob.service.types"
@@ -1824,21 +1825,18 @@ describe("createJobOffer", () => {
   })
 
   it('should get workplace location from given "workplace_address_*" fields', async () => {
-    nock("https://api-adresse.data.gouv.fr:443")
-      .get("/search")
-      .query({ q: "1T IMPASSE PASSOIR CLICHY", limit: "1" })
-      .reply(200, {
-        features: [
-          {
-            geometry: clichyFixture.centre,
-            properties: generateFeaturePropertyFixture({
-              city: clichyFixture.nom,
-              postcode: clichyFixture.codesPostaux[0],
-              name: "1T impasse Passoir",
-            }),
-          },
-        ],
-      })
+    await saveGeolocationInCache("1T IMPASSE PASSOIR CLICHY", [
+      {
+        type: "Feature",
+        geometry: clichyFixture.centre,
+        properties: generateFeaturePropertyFixture({
+          city: clichyFixture.nom,
+          postcode: clichyFixture.codesPostaux[0],
+          name: "1T impasse Passoir",
+          street: "impasse Passoir",
+        }),
+      },
+    ])
 
     const result = await createJobOffer(identity, {
       ...minimalData,
@@ -1916,22 +1914,17 @@ describe("updateJobOffer", () => {
 
     vi.mocked(getEtablissementFromGouvSafe).mockResolvedValue(apiEntrepriseEtablissementFixture.dinum)
 
-    nock("https://api-adresse.data.gouv.fr:443")
-      .get("/search")
-      .query({ q: "20 AVENUE DE SEGUR, 75007 PARIS", limit: "1" })
-      .reply(200, {
-        features: [
-          {
-            geometry: parisFixture.centre,
-            properties: generateFeaturePropertyFixture({
-              city: parisFixture.nom,
-              postcode: parisFixture.codesPostaux[0],
-              name: "20 AVENUE DE SEGUR",
-            }),
-          },
-        ],
-      })
-      .persist()
+    await saveGeolocationInCache("20 AVENUE DE SEGUR, 75007 PARIS", [
+      {
+        type: "Feature",
+        geometry: parisFixture.centre,
+        properties: generateFeaturePropertyFixture({
+          city: parisFixture.nom,
+          postcode: parisFixture.codesPostaux[0],
+          name: "20 AVENUE DE SEGUR",
+        }),
+      },
+    ])
 
     await getDbCollection("jobs_partners").insertOne(originalJob)
 
@@ -1985,21 +1978,30 @@ describe("updateJobOffer", () => {
   })
 
   it('should get workplace location from given "workplace_address_label" fields', async () => {
-    nock("https://api-adresse.data.gouv.fr:443")
-      .get("/search")
-      .query({ q: "1T IMPASSE PASSOIR CLICHY", limit: "1" })
-      .reply(200, {
-        features: [
-          {
-            geometry: clichyFixture.centre,
-            properties: generateFeaturePropertyFixture({
-              city: clichyFixture.nom,
-              postcode: clichyFixture.codesPostaux[0],
-              name: "1T impasse Passoir",
-            }),
-          },
-        ],
-      })
+    await saveGeolocationInCache("20 AVENUE DE SEGUR, 75007 PARIS", [
+      {
+        type: "Feature",
+        geometry: parisFixture.centre,
+        properties: generateFeaturePropertyFixture({
+          city: parisFixture.nom,
+          postcode: parisFixture.codesPostaux[0],
+          name: "20 AVENUE DE SEGUR",
+          street: "AVENUE DE SEGUR",
+        }),
+      },
+    ])
+    await saveGeolocationInCache("1T IMPASSE PASSOIR CLICHY", [
+      {
+        type: "Feature",
+        geometry: clichyFixture.centre,
+        properties: generateFeaturePropertyFixture({
+          city: clichyFixture.nom,
+          postcode: clichyFixture.codesPostaux[0],
+          name: "1T impasse Passoir",
+          street: "impasse Passoir",
+        }),
+      },
+    ])
 
     await updateJobOffer(_id, identity, {
       ...minimalData,
