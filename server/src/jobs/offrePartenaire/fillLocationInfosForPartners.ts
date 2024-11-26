b0fbf85907db8ab6292f6e894b0c108d451134af
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-26 15:02:46 +0100

    feat: si geoloc trouvée remplacement par tout afin d'être raccord

--- server/src/jobs/offrePartenaire/fillLocationInfosForPartners.ts
+++ server/src/jobs/offrePartenaire/fillLocationInfosForPartners.ts
@@ -24,9 +24,8 @@ export const fillLocationInfosForPartners = async () => {
     groupSize: 1,
     getData: async (documents) => {
       const [document] = documents
-      const { workplace_address_label, workplace_address_street_label, workplace_address_city, workplace_address_zipcode, workplace_geopoint } = document
+      const { workplace_address_label, workplace_geopoint } = document
 
-      //TODO voir avec le métier concernant les détermination d'élément d'adresse abusifs (ex: GRAND EST -> Rue Grand)
       const geolocation = workplace_address_label
         ? await getGeolocation(workplace_address_label!)
         : workplace_geopoint && (await getReverseGeolocationFromApiAdresse(workplace_geopoint.coordinates[0], workplace_geopoint.coordinates[1]))
@@ -41,11 +40,11 @@ export const fillLocationInfosForPartners = async () => {
         const found_address_label = joinNonNullStrings([found_street_label, found_zipcode, found_city])
 
         const result: Pick<IComputedJobsPartners, (typeof filledFields)[number]> = {
-          workplace_address_label: workplace_address_label ?? found_address_label ?? null,
-          workplace_address_street_label: workplace_address_street_label ?? found_street_label ?? null,
-          workplace_address_city: workplace_address_city ?? found_city ?? null,
-          workplace_address_zipcode: workplace_address_zipcode ?? found_zipcode ?? null,
-          workplace_geopoint: workplace_geopoint ?? found_geopoint ?? null,
+          workplace_address_label: found_address_label ?? null,
+          workplace_address_street_label: found_street_label ?? null,
+          workplace_address_city: found_city ?? null,
+          workplace_address_zipcode: found_zipcode ?? null,
+          workplace_geopoint: found_geopoint ?? null,
         }
 
         return [result]
