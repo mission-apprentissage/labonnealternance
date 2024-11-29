d42ac11ab6dacea7268b9ba7bd8b4916a34ad70d
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-11-29 17:14:14 +0100

    feat: nettoyage

--- .bin/scripts/setup-local-env.sh
+++ .bin/scripts/setup-local-env.sh
@@ -33,19 +33,7 @@ echo "NEXT_PUBLIC_ENV=local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_VERSION=0.0.0-local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_API_PORT=5001" >> "${ROOT_DIR}/ui/.env"
 
-# yarn
-
-# if [ "$OS_NAME" == "Linux" ]; then
-#     chmod 440 "${ROOT_DIR}/.infra/local/mongo_keyfile"
-#     chown 999:999 "${ROOT_DIR}/.infra/local/mongo_keyfile"
-# elif [ "$OS_NAME" == "Darwin" ]; then
-#     chmod 400 "${ROOT_DIR}/.infra/local/mongo_keyfile"
-# else
-#     echo "Système d'exploitation non pris en charge : $OS_NAME"
-# fi
-
 yarn
-# chmod 400 "${ROOT_DIR}/.infra/local/mongo_keyfile"
 yarn services:start
 yarn setup:mongodb
 yarn build:dev
