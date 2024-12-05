176d23fa2252197eba2b319dd6e431ede5209342
Kevin Barnoin <k.barnoin@gmail.com>
2024-12-05 17:05:05 +0100

    fix: model field escape

--- server/src/services/formulaire.service.ts
+++ server/src/services/formulaire.service.ts
@@ -104,6 +104,8 @@ const romeDetailAndCandidatureCountAggregateStage = [
           $project: {
             referentielrome: 0,
             "jobs.rome_detail._id": 0,
+            "jobs.rome_detail.couple_appellation_rome": 0,
+            "jobs.rome_detail.appellations_romes_sans_accent_computed": 0,
             "jobs.jobIdStr": 0,
             applications: 0,
           },
--- server/src/services/rome.service.ts
+++ server/src/services/rome.service.ts
@@ -6,6 +6,8 @@ export const getRomeDetailsFromDB = async (romeCode: string) =>
     {
       projection: {
         _id: 0,
+        couple_appellation_rome: 0,
+        appellations_romes_sans_accent_computed: 0,
       },
     }
   )
