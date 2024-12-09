dfcf59fafbfc69692a8cb5694b2a75d7dca5778b
Rémy Auricoste <remy.auricoste@gmail.com>
2024-12-09 15:59:24 +0100

    fix(lbac-2298): ajout de la verif code postal dans les duplicates

--- server/src/jobs/offrePartenaire/detectDuplicateJobPartners.test.ts
+++ server/src/jobs/offrePartenaire/detectDuplicateJobPartners.test.ts
@@ -1,6 +1,7 @@
 import { givenSomeComputedJobPartners } from "@tests/fixture/givenSomeComputedJobPartners"
 import { useMongo } from "@tests/utils/mongo.test.utils"
 import { saveRecruiter } from "@tests/utils/user.test.utils"
+import { ObjectId } from "bson"
 import { RECRUITER_STATUS } from "shared/constants"
 import { generateJobFixture, generateRecruiterFixture } from "shared/fixtures/recruiter.fixture"
 import { JOB_STATUS } from "shared/models"
@@ -10,7 +11,7 @@ import { beforeEach, describe, expect, it } from "vitest"
 import { getPairs } from "@/common/utils/array"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
-import { checkSimilarity, detectDuplicateJobPartners } from "./detectDuplicateJobPartners"
+import { checkSimilarity, detectDuplicateJobPartners, isCanonicalForDuplicate, OfferRef } from "./detectDuplicateJobPartners"
 
 const siret = "42476141900045"
 
@@ -32,11 +33,13 @@ describe("detectDuplicateJobPartners", () => {
         partner_label: JOBPARTNERS_LABEL.HELLOWORK,
         workplace_siret: siret,
         offer_title: offerTitle,
+        workplace_address_zipcode: "75007",
       },
       {
         partner_label: JOBPARTNERS_LABEL.RH_ALTERNANCE,
         workplace_siret: siret,
         offer_title: offerTitle,
+        workplace_address_zipcode: "75007",
       },
     ])
     // when
@@ -66,12 +69,17 @@ describe("detectDuplicateJobPartners", () => {
         partner_label: JOBPARTNERS_LABEL.RH_ALTERNANCE,
         workplace_siret: siret,
         offer_title: offerTitle,
+        workplace_address_zipcode: "75007",
       },
     ])
     const recruiter = await saveRecruiter(
       generateRecruiterFixture({
         establishment_siret: siret,
         status: RECRUITER_STATUS.ACTIF,
+        address_detail: {
+          ...generateRecruiterFixture().address_detail,
+          code_postal: "75007",
+        },
         jobs: [
           generateJobFixture({
             rome_appellation_label: offerTitle,
@@ -93,6 +101,63 @@ describe("detectDuplicateJobPartners", () => {
       },
     ])
   })
+  it("should not detect a duplicate with an exact match in the offer title and the siret but a different zip code (job_partner)", async () => {
+    // given
+    await givenSomeComputedJobPartners([
+      {
+        partner_label: JOBPARTNERS_LABEL.HELLOWORK,
+        workplace_siret: siret,
+        offer_title: offerTitle,
+        workplace_address_zipcode: "75002",
+      },
+      {
+        partner_label: JOBPARTNERS_LABEL.RH_ALTERNANCE,
+        workplace_siret: siret,
+        offer_title: offerTitle,
+        workplace_address_zipcode: "75007",
+      },
+    ])
+    // when
+    await detectDuplicateJobPartners()
+    // then
+    const jobs = await getDbCollection("computed_jobs_partners").find({}).toArray()
+    const [job, job2] = jobs
+    expect.soft(job.duplicates).toEqual([])
+    expect.soft(job2.duplicates).toEqual([])
+  })
+  it("should not detect a duplicate with an exact match in the offer title and the siret but a different zip code (recruiter)", async () => {
+    // given
+    await givenSomeComputedJobPartners([
+      {
+        partner_label: JOBPARTNERS_LABEL.RH_ALTERNANCE,
+        workplace_siret: siret,
+        offer_title: offerTitle,
+        workplace_address_zipcode: "75002",
+      },
+    ])
+    await saveRecruiter(
+      generateRecruiterFixture({
+        establishment_siret: siret,
+        status: RECRUITER_STATUS.ACTIF,
+        address_detail: {
+          ...generateRecruiterFixture().address_detail,
+          code_postal: "75007",
+        },
+        jobs: [
+          generateJobFixture({
+            rome_appellation_label: offerTitle,
+            job_status: JOB_STATUS.ACTIVE,
+          }),
+        ],
+      })
+    )
+    // when
+    await detectDuplicateJobPartners()
+    // then
+    const jobs = await getDbCollection("computed_jobs_partners").find({}).toArray()
+    const [job] = jobs
+    expect.soft(job.duplicates).toEqual([])
+  })
   describe("checkSimilarity", () => {
     it("should not detect when one string is empty", async () => {
       expect.soft(checkSimilarity(undefined, offerTitle)).toEqual(undefined)
@@ -106,29 +171,54 @@ describe("detectDuplicateJobPartners", () => {
       expect.soft(checkSimilarity(offerTitle, offerTitle)).toEqual("identical")
     })
 
-    const coiffeurGroup = ["H/F Coiffeur", "Coiffeur H/F", "Coiffeur", "Coiffeuse", "Coiffeur.se", "Coiffeur (H/F)"]
-    it.each(getPairs(coiffeurGroup))("should detect similar strings: %s === %s", (item1, item2) => {
-      expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
+    const groups = [
+      ["H/F Coiffeur", "Coiffeur H/F", "Coiffeur", "Coiffeuse", "Coiffeur.se", "Coiffeur (H/F)"],
+      ["Direction interministérielle du numérique", "Dir. interministérielle du numérique"],
+      ["MFR la pignerie", "Maison familiale rurale la pignerie"],
+      ["Chargé de mission en Ressources Humaines (RH) (H/F)", "Chargé de mission RH", "Chargé.e de mission RH"],
+      // ["Drysoft", "dyrsoft"]
+    ]
+    groups.forEach((group) => {
+      it.each(getPairs(group))("should detect similar strings: %s === %s", (item1, item2) => {
+        expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
+      })
     })
+  })
+  describe("isCanonicalForDuplicate", () => {
+    // cf https://www.math.univ-toulouse.fr/~msablik/Cours/MathDiscretes/Slide4-Relation.pdf
+    describe("doit être une relation d ordre", () => {
+      const offerRefs: OfferRef[] = [
+        { collectionName: "recruiters", _id: new ObjectId("60646425184afd00e017c1ab") },
+        { collectionName: "recruiters", _id: new ObjectId("642605b7c52247005267027e") },
+        { collectionName: "recruiters", _id: new ObjectId("682605b7c52247005267027e") },
+        { collectionName: "computed_jobs_partners", _id: new ObjectId("6525231132fe045f240e4bcd") },
+        { collectionName: "computed_jobs_partners", _id: new ObjectId("67347c0c9af58cb6e6ebbb65") },
+        { collectionName: "computed_jobs_partners", _id: new ObjectId("63347c0c9af58cb6e6ebbb65") },
+      ]
 
-    const directionGroup = ["Direction interministérielle du numérique", "Dir. interministérielle du numérique"]
-    it.each(getPairs(directionGroup))("should detect similar strings: %s === %s", (item1, item2) => {
-      expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
-    })
-
-    const sigleGroup = ["MFR la pignerie", "Maison familiale rurale la pignerie"]
-    it.each(getPairs(sigleGroup))("should detect similar strings: %s === %s", (item1, item2) => {
-      expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
-    })
-
-    const sensSimilaireGroup = ["Chargé de mission en Ressources Humaines (RH) (H/F)", "Chargé de mission RH", "Chargé.e de mission RH"]
-    it.each(getPairs(sensSimilaireGroup))("should detect similar strings: %s === %s", (item1, item2) => {
-      expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
-    })
-
-    const typoGroup = ["Drysoft", "dyrsoft"]
-    it.skip.each(getPairs(typoGroup))("should detect similar strings: %s === %s", (item1, item2) => {
-      expect.soft(checkSimilarity(item1, item2)).toEqual(expect.stringContaining("similar"))
+      it("la relation doit être reflexive (xRx)", () => {
+        offerRefs.forEach((offerRef) => expect(isCanonicalForDuplicate(offerRef, offerRef)).toBe(true))
+      })
+      it("la relation doit être antisymétrique (xRy and yRx => x = y)", () => {
+        getPairs(offerRefs)
+          .filter(([item, item2]) => isCanonicalForDuplicate(item, item2) && isCanonicalForDuplicate(item2, item))
+          .forEach(([item, item2]) => expect(item).toBe(item2))
+      })
+      it("la relation doit être transitive (xRy and yRz => xRz)", () => {
+        const validPairs = getPairs(offerRefs).flatMap(([item, item2]) => {
+          const results: [OfferRef, OfferRef][] = []
+          if (isCanonicalForDuplicate(item, item2)) {
+            results.push([item, item2])
+          }
+          if (isCanonicalForDuplicate(item2, item)) {
+            results.push([item2, item])
+          }
+          return results
+        })
+        getPairs(validPairs)
+          .filter(([[_item, item2], [item3, _item4]]) => item2 === item3)
+          .forEach(([[item, _item2], [_item3, item4]]) => expect(isCanonicalForDuplicate(item, item4)).toBe(true))
+      })
     })
   })
 })
