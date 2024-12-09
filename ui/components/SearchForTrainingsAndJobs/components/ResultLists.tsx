2693ec70129b1aa23d193ee1ed1cc21999571005
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-12-09 17:54:37 +0100

    feat: in job

--- ui/components/SearchForTrainingsAndJobs/components/ResultLists.tsx
+++ ui/components/SearchForTrainingsAndJobs/components/ResultLists.tsx
@@ -216,7 +216,7 @@ const ResultLists = ({
       return (
         <>
           {mergedOpportunities.map((opportunity, idx) => {
-            if ([LBA_ITEM_TYPE_OLD.MATCHA, LBA_ITEM_TYPE_OLD.PEJOB].includes(opportunity.ideaType)) {
+            if ([LBA_ITEM_TYPE_OLD.MATCHA, LBA_ITEM_TYPE_OLD.PEJOB, LBA_ITEM_TYPE_OLD.PARTNER_JOB].includes(opportunity.ideaType)) {
               return renderJob(isTestMode, idx, opportunity, handleSelectItem, searchForTrainingsOnNewCenter)
             } else {
               return renderLbb(isTestMode, idx, opportunity, handleSelectItem, searchForTrainingsOnNewCenter)
