b2979a37c6c483e9aea7baac5956c4f7eccddba8
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-12-11 11:21:16 +0100

    feat: icon website

--- ui/components/ItemDetail/ItemDetailServices/ItemWebsiteLink.tsx
+++ ui/components/ItemDetail/ItemDetailServices/ItemWebsiteLink.tsx
@@ -5,7 +5,7 @@ import { Flex, Image, Link, Text } from "@chakra-ui/react"
 export default function ItemWebsiteLink({ item }: { item: ILbaItemFormation2 | ILbaItemLbaCompany | ILbaItemLbaJob | ILbaItemFtJob | ILbaItemPartnerJob }) {
   return "url" in item.company && item?.company?.url ? (
     <Flex alignItems="center" mt={4}>
-      <Image mr={2} alt="" aria-hidden={true} src="/images/icons/magnifyingglass.svg" width="24px" height="24px" />
+      <Image mr={2} alt="" aria-hidden={true} src="/images/icons/world.svg" width="24px" height="24px" />
       <Text as="span">
         Le site internet de l'entreprise{" "}
         <Link ml="2px" isExternal variant="basicUnderlined" href={item?.company?.url} aria-label="Recherche de l'entreprise sur google.fr - nouvelle fenêtre">
