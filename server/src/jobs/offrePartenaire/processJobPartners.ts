6ebe2493bdf84611affbf638f65734d784adcc64
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-12-14 02:05:54 +0100

    feat: mise en place du job + cron

---
+++ server/src/jobs/offrePartenaire/processJobPartners.ts
@@ -0,0 +1,10 @@
+import { fillComputedJobsPartners } from "./fillComputedJobsPartners"
+import { importFromComputedToJobsPartners } from "./importFromComputedToJobsPartners"
+import { importRHAlternanceRaw, importRHAlternanceToComputed } from "./importRHAlternance"
+
+export const processJobPartners = async () => {
+  await importRHAlternanceRaw()
+  await importRHAlternanceToComputed()
+  await fillComputedJobsPartners()
+  await importFromComputedToJobsPartners()
+}
