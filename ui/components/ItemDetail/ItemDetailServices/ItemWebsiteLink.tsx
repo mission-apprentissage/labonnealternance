029812a6a026140f1107e8a1131fb96095635155
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2024-12-16 19:02:57 +0100

    feat: tests à jour

--- ui/components/ItemDetail/ItemDetailServices/ItemWebsiteLink.tsx
+++ ui/components/ItemDetail/ItemDetailServices/ItemWebsiteLink.tsx
@@ -7,7 +7,7 @@ export default function ItemWebsiteLink({ item }: { item: ILbaItemFormation2 | I
     <Flex alignItems="center" mt={4}>
       <Image mr={2} alt="" aria-hidden={true} src="/images/icons/world.svg" width="24px" height="24px" />
       <Text as="span">
-        Le site internet de l'entreprise{" "}
+        Plus d'info sur l'entreprise{" "}
         <Link ml="2px" isExternal variant="basicUnderlined" href={item?.company?.url} aria-label="Recherche de l'entreprise sur google.fr - nouvelle fenêtre">
           {item?.company?.url} <ExternalLinkIcon mb="3px" ml="2px" />
         </Link>
