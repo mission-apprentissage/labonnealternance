abf9f710dd42cdae5ea3054dfdfc0f33bc6a563f
Rémy Auricoste <remy.auricoste@gmail.com>
2025-01-07 11:43:48 +0100

    feat(lbac-2269): maj des logos des partenaires

--- ui/components/DepotOffre/InfosDiffusionOffre.tsx
+++ ui/components/DepotOffre/InfosDiffusionOffre.tsx
@@ -1,21 +1,35 @@
-import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react"
-
-import { J1S, Parcoursup } from "@/theme/components/logos"
+import { Box, Flex, Grid, Heading, Image, Text } from "@chakra-ui/react"
 
 export const InfosDiffusionOffre = () => {
   return (
     <Box border="1px solid #000091" p={5}>
-      <Heading fontSize="24px" mb={3}>
+      <Heading fontSize="24px" mb={6}>
         Profitez d'une visibilité accrue
       </Heading>
-      <Box ml={5}>
-        <Text>Une fois créée, votre offre d’emploi sera immédiatement mise en ligne sur les sites suivants : </Text>
-        <Stack direction={["column", "row"]} spacing={2} align="center" justify="center" mt={3}>
-          <Image src="/images/logo_LBA.svg" alt="" minWidth="150px" width="150px" />
-          <J1S w="100px" h="100px" />
-          <Parcoursup w="220px" h="100px" />
-        </Stack>
-      </Box>
+      <Text>Une fois créée, votre offre d’emploi sera immédiatement mise en ligne sur les sites suivants : </Text>
+      <Grid
+        templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(3, 1fr)"]}
+        gap={4}
+        mt={6}
+        sx={{
+          "& > *": {
+            border: "solid 1px #DDDDDD",
+            borderRadius: "3px",
+            padding: "11px",
+            height: "80px",
+            width: "100%",
+          },
+        }}
+      >
+        <Image src="/images/logosPartenaires/minimal/1j1s.svg" alt="" />
+        <Image src="/images/logosPartenaires/minimal/portail-alternance.svg" alt="" />
+        <Flex align="center">
+          <Image src="/images/logosPartenaires/minimal/affelnet.png" alt="" />
+        </Flex>
+        <Image src="/images/logosPartenaires/minimal/mon-master.svg" alt="" padding="22px !important" />
+        <Image src="/images/logosPartenaires/minimal/parcoursup.svg" alt="" />
+        <Image src="/images/logosPartenaires/minimal/france-travail.svg" alt="" padding="14px !important" />
+      </Grid>
     </Box>
   )
 }
