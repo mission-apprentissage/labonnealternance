dac404b92ce951133a74601b249cad7509e3c85f
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-08 19:49:05 +0100

    feat(front): add france travail button logic

---
+++ ui/components/ItemDetail/ItemDetailServices/getFranceTravailButton.tsx
@@ -0,0 +1,92 @@
+import { Box, Container, Flex, Heading, Link, Text, useDisclosure } from "@chakra-ui/react"
+import { ILbaItemFtJob } from "shared"
+
+import { focusWithin } from "../../../theme/theme-lba-tools"
+import { SendPlausibleEvent } from "../../../utils/plausible"
+import { ModalReadOnly } from "../../ModalReadOnly"
+
+export default function GetFranceTravailButton(offreFT: ILbaItemFtJob) {
+  const { isOpen: isModalOpen, onClose: onModalClose, onOpen: setModalOpen } = useDisclosure()
+  const urlPostulation = offreFT?.contact?.url || null
+  const contactPhone = offreFT?.contact?.phone || null
+  const contactName = offreFT?.contact?.name || null
+  const url = offreFT?.url || null
+  const id = offreFT?.job?.id || null
+  const companyName = offreFT.company.name || null
+
+  if (!urlPostulation && !contactPhone && !url) return null
+
+  if (contactPhone) {
+    return (
+      <>
+        <Box my={4}>
+          <Link data-tracking-id="postuler-offre-partenaire" {...focusWithin} variant="postuler" href={urlPostulation} target="_blank" onClick={setModalOpen}>
+            J'envoie ma candidature
+          </Link>
+        </Box>
+        <ModalReadOnly isOpen={isModalOpen} onClose={onModalClose}>
+          <Container size={{ base: "full", md: "container.md" }}>
+            <Heading as="h2" fontSize="xl" mb={4}>
+              Posutler à l'offre de {companyName}
+            </Heading>
+            <Text>L'entreprise préfère être contactée par téléphone. Pour proposer votre candidature, appeler directement le numéro suivant :</Text>
+            <Flex direction={"column"} alignItems="center" flexDirection={"column"} justifyContent={"center"} my={4}>
+              <Text fontWeight="bold" fontSize="lg" my={4}>
+                {contactName}
+              </Text>
+              <Box borderRadius={"md"} backgroundColor={"#ECECFE"} p={3}>
+                <Link href={`tel:${contactPhone}`}>
+                  <Text color="#000091" fontSize={"2xl"} fontWeight="bold">
+                    {contactPhone}
+                  </Text>
+                </Link>
+              </Box>
+            </Flex>
+          </Container>
+        </ModalReadOnly>
+      </>
+    )
+  }
+
+  if (urlPostulation) {
+    return (
+      <Box my={4}>
+        <Link
+          data-tracking-id="postuler-offre-partenaire"
+          {...focusWithin}
+          variant="postuler"
+          href={urlPostulation}
+          target="_blank"
+          onClick={() =>
+            SendPlausibleEvent("Clic Postuler - Fiche entreprise Offre FT", {
+              info_fiche: id,
+            })
+          }
+        >
+          Je postule sur le site du recruteur
+        </Link>
+      </Box>
+    )
+  }
+
+  if (url) {
+    return (
+      <Box my={4}>
+        <Link
+          data-tracking-id="postuler-offre-partenaire"
+          {...focusWithin}
+          variant="postuler"
+          href={url}
+          target="francetravail"
+          onClick={() =>
+            SendPlausibleEvent("Clic Postuler - Fiche entreprise Offre FT", {
+              info_fiche: id,
+            })
+          }
+        >
+          Je postule sur France Travail
+        </Link>
+      </Box>
+    )
+  }
+}
--- ui/components/ItemDetail/loadedItemDetail.tsx
+++ ui/components/ItemDetail/loadedItemDetail.tsx
@@ -1,16 +1,14 @@
 import { ILbaItemLbaCompany, ILbaItemLbaJob, ILbaItemPartnerJob } from "@/../shared"
-import { Box, Divider, Flex, Link, Text } from "@chakra-ui/react"
+import { Box, Divider, Flex, Text } from "@chakra-ui/react"
 import { useContext, useEffect, useState } from "react"
 import { LBA_ITEM_TYPE_OLD } from "shared/constants/lbaitem"
 
 import DemandeDeContact from "@/components/RDV/DemandeDeContact"
-import { focusWithin } from "@/theme/theme-lba-tools"
 
 import { DisplayContext } from "../../context/DisplayContextProvider"
 import { SearchResultContext } from "../../context/SearchResultContextProvider"
 import { isCfaEntreprise } from "../../services/cfaEntreprise"
 import { filterLayers } from "../../utils/mapTools"
-import { SendPlausibleEvent } from "../../utils/plausible"
 import InfoBanner from "../InfoBanner/InfoBanner"
 
 import AideApprentissage from "./AideApprentissage"
@@ -21,6 +19,7 @@ import FTJobDetail from "./FTJobDetail"
 import GoingToContactQuestion, { getGoingtoId } from "./GoingToContactQuestion"
 import getActualTitle from "./ItemDetailServices/getActualTitle"
 import { BuildSwipe, buttonJePostuleShouldBeDisplayed, buttonRdvShouldBeDisplayed, getNavigationButtons } from "./ItemDetailServices/getButtons"
+import GetFranceTravailButton from "./ItemDetailServices/getFranceTravailButton"
 import getJobPublishedTimeAndApplications from "./ItemDetailServices/getJobPublishedTimeAndApplications"
 import getTags from "./ItemDetailServices/getTags"
 import ItemDetailApplicationsStatus, { hasApplied } from "./ItemDetailServices/ItemDetailApplicationStatus"
@@ -62,14 +61,6 @@ const LoadedItemDetail = ({ handleClose, handleSelectItem }) => {
     let currentScroll = document.querySelector("#itemDetailColumn").scrollTop
     currentScroll += isCollapsedHeader ? 100 : -100
     setIsCollapsedHeader(currentScroll > maxScroll)
-  }
-
-  const postuleSurFranceTravail = () => {
-    if (selectedItem.ideaType === LBA_ITEM_TYPE_OLD.PEJOB) {
-      SendPlausibleEvent("Clic Postuler - Fiche entreprise Offre FT", {
-        info_fiche: selectedItem.job.id,
-      })
-    }
   }
 
   const stickyHeaderProperties = isCollapsedHeader
@@ -157,20 +148,7 @@ const LoadedItemDetail = ({ handleClose, handleSelectItem }) => {
 
               {selectedItem.ideaType === LBA_ITEM_TYPE_OLD.PARTNER_JOB && <PartnerJobPostuler job={selectedItem} />}
 
-              {selectedItem.ideaType === LBA_ITEM_TYPE_OLD.PEJOB && buttonJePostuleShouldBeDisplayed(selectedItem) && (
-                <Box my={4}>
-                  <Link
-                    data-tracking-id="postuler-offre-partenaire"
-                    {...focusWithin}
-                    variant="postuler"
-                    href={selectedItem.url}
-                    target="francetravail"
-                    onClick={postuleSurFranceTravail}
-                  >
-                    Je postule sur France Travail
-                  </Link>
-                </Box>
-              )}
+              {selectedItem.ideaType === LBA_ITEM_TYPE_OLD.PEJOB && GetFranceTravailButton(selectedItem)}
             </Box>
             <Box pt={{ base: 0, sm: 4 }}>
               <ShareLink item={selectedItem} />
