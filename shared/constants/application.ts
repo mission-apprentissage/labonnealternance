a8069f41cef59fa7f0253bc7eabdef66cb81823d
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2025-01-14 14:07:13 +0100

    correction typo réponse négative recruteur

--- shared/constants/application.ts
+++ shared/constants/application.ts
@@ -6,7 +6,7 @@ export enum ApplicationIntention {
 
 export enum ApplicationIntentionDefaultText {
   ENTRETIEN = "Bonjour, \r\n\r\nMerci pour l'intérêt que vous portez à notre établissement. \r\n\r\nVotre candidature a retenu toute notre attention et nous souhaitons échanger avec vous. \r\n\r\nPouvez-vous me recontacter au numéro de téléphone ou via l'email ci-dessous afin que nous puissions convenir d'un rendez-vous ?",
-  REFUS = "Bonjour, \r\n\r\nMerci pour l'intérêt que vous portez à notre établissement. Nous avons étudié votre candidature avec attention. Nous ne sommes malheureusement pas en mesure de donner lui une suite favorable. \r\n\r\nNous vous souhaitons bonne chance dans vos recherches. \r\n\r\nBonne continuation",
+  REFUS = "Bonjour, \r\n\r\nMerci pour l'intérêt que vous portez à notre établissement. Nous avons étudié votre candidature avec attention. Nous ne sommes malheureusement pas en mesure de lui donner une suite favorable. \r\n\r\nNous vous souhaitons bonne chance dans vos recherches. \r\n\r\nBonne continuation",
 }
 
 export enum RefusalReasons {
