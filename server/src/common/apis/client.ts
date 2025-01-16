939bf35c646cb31c65ee48932c2077e847b14869
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-16 17:19:28 +0100

    fix: typing

--- server/src/common/apis/client.ts
+++ server/src/common/apis/client.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios"
-import { MemoryStorage, buildMemoryStorage, setupCache } from "axios-cache-interceptor"
+import { AxiosCacheInstance, MemoryStorage, buildMemoryStorage, setupCache } from "axios-cache-interceptor"
 
 const CLEANUP_INTERVAL = 1000 * 60 * 10 // 10 minutes
 const MAX_ENTRIES = 200
@@ -14,7 +14,7 @@ const closeMemoryCache = () => {
   })
 }
 
-const getApiClient = (options: AxiosRequestConfig, { cache }: { cache: boolean } = { cache: true }): AxiosInstance => {
+const getApiClient = (options: AxiosRequestConfig, { cache }: { cache: boolean } = { cache: true }): AxiosCacheInstance | AxiosInstance => {
   const client: AxiosInstance = axios.create({
     timeout: 5000,
     ...options,
