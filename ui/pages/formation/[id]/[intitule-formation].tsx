8452774cce4d7911c1a47ebd7ef644fb8de4f73b
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-01-20 23:10:13 +0100

    feat: page formation

---
+++ ui/pages/formation/[id]/[intitule-formation].tsx
@@ -0,0 +1,89 @@
+import { Box } from "@chakra-ui/react"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/router"
+import { NextSeo } from "next-seo"
+import { useContext } from "react"
+
+import { ItemDetail } from "@/components"
+import { DisplayContext } from "@/context/DisplayContextProvider"
+import { ParameterContext } from "@/context/ParameterContextProvider"
+import { SearchResultContext } from "@/context/SearchResultContextProvider"
+import { currentSearch, setCurrentPage } from "@/utils/currentPage"
+import { closeMapPopups, flyToMarker, setSelectedMarker } from "@/utils/mapTools"
+import pushHistory from "@/utils/pushHistory"
+
+export default function DetailFormation() {
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const { /*extendedSearch, jobs,*/ selectedItem, setSelectedItem, setItemToScrollTo /*setJobsAndSelectedItem, setTrainingsAndSelectedItem, trainings*/ } =
+    useContext(SearchResultContext)
+  const { formValues } = useContext(DisplayContext)
+  const { displayMap } = useContext(ParameterContext)
+
+  // const intituleOffre = router.query["intitule-offre"]
+  // const type = router.query.type
+  // const id = router.query.id
+  const path = searchParams.get("path")
+
+  const unSelectItem = (doNotSaveToHistory) => {
+    setCurrentPage("")
+    setSelectedItem(null)
+    setSelectedMarker(null)
+    if (selectedItem) {
+      setItemToScrollTo(selectedItem)
+    }
+
+    if (!doNotSaveToHistory) {
+      pushHistory({ router, searchParameters: formValues, searchTimestamp: currentSearch, displayMap, path })
+    }
+  }
+
+  const handleClose = () => {
+    setCurrentPage("")
+    pushHistory({
+      router,
+      page: "list",
+      display: "list",
+      searchParameters: formValues,
+      searchTimestamp: currentSearch,
+      displayMap,
+      path,
+    })
+    unSelectItem("doNotSaveToHistory")
+  }
+
+  const handleSelectItem = (item) => {
+    console.log("HANDLE : ", item)
+    flyToMarker(item, 12)
+    closeMapPopups()
+    setSelectedItem(item)
+    setSelectedMarker(item)
+
+    setCurrentPage("fiche")
+
+    console.log("avant push")
+    pushHistory({
+      router,
+      item,
+      page: "fiche",
+      display: "list",
+      searchParameters: formValues,
+      searchTimestamp: currentSearch,
+      displayMap,
+      path,
+    })
+  }
+
+  return (
+    selectedItem && (
+      <Box className="choiceCol">
+        <NextSeo
+          title={`Tous les emplois et formations en alternance | La bonne alternance | Trouvez votre alternance`}
+          description={`Liste de métiers où trouver une formation ou un emploi en alternance`}
+        />
+        {<ItemDetail handleSelectItem={handleSelectItem} handleClose={handleClose} />}
+      </Box>
+    )
+  )
+}
