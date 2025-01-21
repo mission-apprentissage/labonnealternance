4b828b5b653c33eee018aebc2d4f576f5762b2b1
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-01-21 15:14:16 +0100

    feat: factorisation

---
+++ ui/services/itemDetailServices.ts
@@ -0,0 +1,62 @@
+import { currentSearch, setCurrentPage } from "@/utils/currentPage"
+import { closeMapPopups, flyToMarker, setSelectedMarker } from "@/utils/mapTools"
+import pushHistory from "@/utils/pushHistory"
+
+export const getCloseAndSelectFunctions = ({ router, searchParams, searchResultContext, displayContext, parameterContext }) => {
+  const { /*extendedSearch, jobs,*/ selectedItem, setSelectedItem, setItemToScrollTo /*setJobsAndSelectedItem, setTrainingsAndSelectedItem, trainings*/ } = searchResultContext
+  const { formValues } = displayContext
+  const { displayMap } = parameterContext
+  const path = searchParams.get("path")
+
+  const unSelectItem = (doNotSaveToHistory) => {
+    setCurrentPage("")
+    setSelectedItem(null)
+    setSelectedMarker(null)
+    if (selectedItem) {
+      setItemToScrollTo(selectedItem)
+    }
+
+    if (!doNotSaveToHistory) {
+      pushHistory({ router, searchParameters: formValues, searchTimestamp: currentSearch, displayMap, path })
+    }
+  }
+
+  const handleClose = () => {
+    setCurrentPage("")
+    pushHistory({
+      router,
+      page: "list",
+      display: "list",
+      searchParameters: formValues,
+      searchTimestamp: currentSearch,
+      displayMap,
+      path,
+    })
+    unSelectItem("doNotSaveToHistory")
+  }
+
+  const handleSelectItem = (item) => {
+    flyToMarker(item, 12)
+    closeMapPopups()
+    setSelectedItem(item)
+    setSelectedMarker(item)
+
+    setCurrentPage("fiche")
+
+    pushHistory({
+      router,
+      item,
+      page: "fiche",
+      display: "list",
+      searchParameters: formValues,
+      searchTimestamp: currentSearch,
+      displayMap,
+      path,
+    })
+  }
+
+  return {
+    handleClose,
+    handleSelectItem,
+  }
+}
