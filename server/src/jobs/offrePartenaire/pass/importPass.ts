bcb1d7a28a8c5f0ecf38435b99ec8c16c33a04a6
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-22 15:51:54 +0100

    feat: add test

--- server/src/jobs/offrePartenaire/pass/importPass.ts
+++ server/src/jobs/offrePartenaire/pass/importPass.ts
@@ -2,6 +2,7 @@ import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
 import rawPassModel from "shared/models/rawPass.model"
 
 import config from "../../../config"
+import { importFromStreamInXml } from "../importFromStreamInXml"
 import { importFromUrlInXml } from "../importFromUrlInXml"
 import { rawToComputedJobsPartners } from "../rawToComputedJobsPartners"
 
@@ -10,8 +11,13 @@ import { passJobToJobsPartners, ZPassJob } from "./passMapper"
 const rawCollectionName = rawPassModel.collectionName
 const offerXmlTag = "item"
 
-export const importPassRaw = async () =>
-  await importFromUrlInXml({ destinationCollection: rawCollectionName, url: config.passUrl, offerXmlTag, partnerLabel: JOBPARTNERS_LABEL.PASS })
+export const importPassRaw = async (sourceStream?: NodeJS.ReadableStream) => {
+  if (sourceStream) {
+    await importFromStreamInXml({ destinationCollection: rawCollectionName, offerXmlTag, stream: sourceStream, partnerLabel: JOBPARTNERS_LABEL.PASS })
+  } else {
+    await importFromUrlInXml({ destinationCollection: rawCollectionName, url: config.passUrl, offerXmlTag, partnerLabel: JOBPARTNERS_LABEL.PASS })
+  }
+}
 
 export const importPassToComputed = async () => {
   await rawToComputedJobsPartners({
