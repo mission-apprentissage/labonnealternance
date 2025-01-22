a2639f13ff7b56cf97a1c3bf419005660b27ff52
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-01-22 13:43:52 +0100

    fix: restore

--- ui/components/WidgetHeader/InitWidgetSearchParameters.ts
+++ ui/components/WidgetHeader/InitWidgetSearchParameters.ts
@@ -2,7 +2,6 @@ import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 
 import { ScopeContext } from "@/context/ScopeContext"
-import { SearchResultContext } from "@/context/SearchResultContextProvider"
 
 import { ParameterContext } from "../../context/ParameterContextProvider"
 import { fetchAddressFromCoordinates } from "../../services/baseAdresse"
@@ -10,30 +9,27 @@ import { logError } from "../../utils/tools"
 
 const InitWidgetSearchParameters = ({ handleSearchSubmit, handleItemLoad }) => {
   const { displayMap, widgetParameters, itemParameters, setWidgetParameters, setItemParameters } = useContext(ParameterContext)
-  const { jobs, trainings } = useContext(SearchResultContext)
   const router = useRouter()
   const scopeContext = useContext(ScopeContext)
 
   useEffect(() => {
     // initialisation par les query params
-    if (!jobs && !trainings) {
+    // @ts-expect-error: TODO
+    if (widgetParameters && widgetParameters.applyWidgetParameters && itemParameters && itemParameters.applyItemParameters) {
+      // launchWidget AND item
+      launchWidgetSearch({ selectItem: true })
+      setWidgetParameters({ ...widgetParameters, applyWidgetParameters: false }) // action one shot
+      setItemParameters({ ...itemParameters, applyItemParameters: false }) // action one shot
       // @ts-expect-error: TODO
-      if (widgetParameters && widgetParameters.applyWidgetParameters && itemParameters && itemParameters.applyItemParameters) {
-        // launchWidget AND item
-        launchWidgetSearch({ selectItem: true })
-        setWidgetParameters({ ...widgetParameters, applyWidgetParameters: false }) // action one shot
-        setItemParameters({ ...itemParameters, applyItemParameters: false }) // action one shot
-        // @ts-expect-error: TODO
-      } else if (widgetParameters && widgetParameters.applyWidgetParameters) {
-        // launchWidget only
-        launchWidgetSearch({ selectItem: false })
-        setWidgetParameters({ ...widgetParameters, applyWidgetParameters: false }) // action one shot
-        // @ts-expect-error: TODO
-      } else if (itemParameters && itemParameters.applyItemParameters) {
-        // launchItem only
-        launchItemFetch()
-        setItemParameters({ ...itemParameters, applyItemParameters: false }) // action one shot
-      }
+    } else if (widgetParameters && widgetParameters.applyWidgetParameters) {
+      // launchWidget only
+      launchWidgetSearch({ selectItem: false })
+      setWidgetParameters({ ...widgetParameters, applyWidgetParameters: false }) // action one shot
+      // @ts-expect-error: TODO
+    } else if (itemParameters && itemParameters.applyItemParameters) {
+      // launchItem only
+      launchItemFetch()
+      setItemParameters({ ...itemParameters, applyItemParameters: false }) // action one shot
     }
   })
 
