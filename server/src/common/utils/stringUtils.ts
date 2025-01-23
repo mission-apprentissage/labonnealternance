a3e7a1aa89ba39f7231cd55b597ce334ffb9207a
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-23 16:18:05 +0100

    fix: retour pass - html sanitizer config (#1791)
    * fix: html sanitizer config
    
    * fix: snapshot

--- server/src/common/utils/stringUtils.ts
+++ server/src/common/utils/stringUtils.ts
@@ -2,8 +2,7 @@ import sanitizeHtml from "sanitize-html"
 
 export const removeHtmlTagsFromString = (text: string | null | undefined, keepBr?: boolean) => {
   if (!text) return ""
-  const sanitizeOptions = keepBr ? { allowedTags: ["br"] } : {}
+  const sanitizeOptions = keepBr ? { allowedTags: ["br"], allowedAttributes: {} } : { allowedTags: [], allowedAttributes: {} }
   text = sanitizeHtml(text, sanitizeOptions)
-  text = text!.replaceAll(/\./g, "\u200B.\u200B")
   return text
 }
