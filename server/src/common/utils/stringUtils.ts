f5db8f859ab7b43e48de58b383af51f3241005d2
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-23 14:27:14 +0100

    fix: yarn lock + typing

--- server/src/common/utils/stringUtils.ts
+++ server/src/common/utils/stringUtils.ts
@@ -1,9 +1,9 @@
-import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from "sanitize-html"
 
 export const removeHtmlTagsFromString = (text: string | null | undefined, keepBr?: boolean) => {
   if (!text) return ""
-  const sanitizeOptions = keepBr ? { allowedTags: ['br'] } : {};
-  text = sanitizeHtml(text, sanitizeOptions);
-  text = text.replaceAll(/\./g, "\u200B.\u200B")
+  const sanitizeOptions = keepBr ? { allowedTags: ["br"] } : {}
+  text = sanitizeHtml(text, sanitizeOptions)
+  text = text!.replaceAll(/\./g, "\u200B.\u200B")
   return text
 }
