23b4a4611c254ecf4ef6d1f46a9aa4f1466fc26a
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-01-28 15:01:22 +0100

    feat: rollback sur pushHistory

--- ui/utils/pushHistory.ts
+++ ui/utils/pushHistory.ts
@@ -1,18 +1,20 @@
-import { paramCase } from "param-case"
-import { oldItemTypeToNewItemType } from "shared/constants/lbaitem"
+// import { paramCase } from "param-case"
+// import { oldItemTypeToNewItemType } from "shared/constants/lbaitem"
 
 import { getCampaignParameters } from "./campaignParameters"
+import { getItemQueryParameters } from "./getItemId"
 import { getSearchQueryParameters } from "./getSearchParameters"
 
-const buildQueryParams = ({ display, page, searchParameters, searchTimestamp, displayMap, path }) => {
+const buildQueryParams = ({ display, page, item, searchParameters, searchTimestamp, displayMap }) => {
   const queryParams = [
     display ? `display=${display}` : "",
     page ? `page=${page}` : "",
+    item ? getItemQueryParameters(item) : "",
     searchParameters ? getSearchQueryParameters(searchParameters) : "",
     searchTimestamp ? `s=${searchTimestamp}` : "",
     getCampaignParameters(),
     displayMap === true ? "displayMap=true" : "",
-    `path=${path ?? "/recherche"}`,
+    //`path=${path ?? "/recherche"}`,
   ]
 
   return queryParams.filter(Boolean).join("&")
@@ -20,6 +22,7 @@ const buildQueryParams = ({ display, page, searchParameters, searchTimestamp, di
 
 const pushHistory = ({
   router,
+  scopeContext,
   item = undefined,
   page = undefined,
   display = undefined,
@@ -27,29 +30,34 @@ const pushHistory = ({
   searchTimestamp = undefined,
   isReplace = false,
   displayMap = false,
-  path,
+  //path,
 }) => {
   const params = buildQueryParams({
     display,
     page,
+    item,
     searchParameters,
     searchTimestamp,
     displayMap,
-    path: path ?? router.pathname,
+    //path: path ?? router.pathname,
   })
 
   const navigationMethod = isReplace ? router.replace : router.push
-  if (page === "fiche") {
-    const title = paramCase(item?.title)
-    const link = `/${item?.ideaType === "formation" ? "" : "emploi/"}${oldItemTypeToNewItemType(item?.ideaType)}/${encodeURIComponent(item?.id)}/${title}`
-    navigationMethod(`${link}${params ? `?${params}` : ""}`, undefined, {
-      shallow: false,
-    })
-  } else {
-    navigationMethod(`${path ?? router.pathname}${params ? `?${params}` : ""}`, undefined, {
-      shallow: true,
-    })
-  }
+
+  navigationMethod(`${scopeContext.path}${params ? `?${params}` : ""}`, undefined, {
+    shallow: true,
+  })
+  // if (page === "fiche") {
+  //   const title = paramCase(item?.title)
+  //   const link = `/${item?.ideaType === "formation" ? "" : "emploi/"}${oldItemTypeToNewItemType(item?.ideaType)}/${encodeURIComponent(item?.id)}/${title}`
+  //   navigationMethod(`${link}${params ? `?${params}` : ""}`, undefined, {
+  //     shallow: false,
+  //   })
+  // } else {
+  //   navigationMethod(`${path ?? router.pathname}${params ? `?${params}` : ""}`, undefined, {
+  //     shallow: true,
+  //   })
+  // }
 }
 
 export default pushHistory
