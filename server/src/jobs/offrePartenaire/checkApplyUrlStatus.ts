1ebb6cc40f14ddfe0ced1d834a4b07a0ad7e1fb5
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-01-28 13:32:49 +0100

    chore: :rocket:

--- server/src/jobs/offrePartenaire/checkApplyUrlStatus.ts
+++ server/src/jobs/offrePartenaire/checkApplyUrlStatus.ts
@@ -1,10 +1,6 @@
-import { Transform, Writable } from "node:stream"
-import { pipeline } from "node:stream/promises"
-
 import { AnyBulkWriteOperation, FindCursor, type ObjectId } from "mongodb"
+import PQueue from "p-queue"
 import { IComputedJobsPartners, JOB_PARTNER_BUSINESS_ERROR } from "shared/models/jobsPartnersComputed.model"
-
-import { streamGroupByCount } from "@/common/utils/streamUtils"
 
 import { logger } from "../../common/logger"
 import { getDbCollection } from "../../common/utils/mongodbUtils"
@@ -17,10 +13,10 @@ import { getDbCollection } from "../../common/utils/mongodbUtils"
  * bulk write
  */
 
-const BATCH_SIZE = 50
+const BATCH_SIZE = 1000
 
 const checkUrlStatus = async (_id: ObjectId, url: string): Promise<AnyBulkWriteOperation<IComputedJobsPartners> | null> => {
-  const status = await fetch(url)
+  const status = await fetch(url, { keepalive: true })
     .then((res) => {
       return res.status
     })
@@ -43,9 +39,8 @@ const checkUrlStatus = async (_id: ObjectId, url: string): Promise<AnyBulkWriteO
   }
 }
 
-export const checkApplyUrlStatus = async () => {
+export async function checkApplyUrlStatus() {
   logger.info(`Validation des computed_job_partners`)
-  const counters = { total: 0, success: 0, error: 0 }
   const collection = getDbCollection("computed_jobs_partners")
   const count = await collection.countDocuments({
     business_error: null,
@@ -58,77 +53,40 @@ export const checkApplyUrlStatus = async () => {
     { projection: { _id: 1, apply_url: 1 } }
   )
 
-  await pipeline(
-    cursor,
-    new Transform({
-      objectMode: true,
-      highWaterMark: 50,
-      async transform(chunk: { _id: ObjectId; apply_url: string }, encoding, callback) {
-        counters.total++
-        const op = await checkUrlStatus(chunk._id, chunk.apply_url)
+  const counters = { total: 0, success: 0, error: 0, startTime: Date.now() }
 
-        if (op) {
-          counters.error++
-          callback(null, op)
-        } else {
-          counters.success++
-          callback()
-        }
-      },
-    }),
-    streamGroupByCount(1_000),
-    new Writable({
-      objectMode: true,
-      async write(chunk: Array<AnyBulkWriteOperation<IComputedJobsPartners> | null>, encoding, callback) {
-        if (counters.total % BATCH_SIZE === 0) {
-          logger.info(`Progression : ${counters.total}/${count} URLs traitées (Succès : ${counters.success}, Erreurs : ${counters.error})`)
-        }
+  const queue = new PQueue({ concurrency: BATCH_SIZE })
+  const bulk: Array<AnyBulkWriteOperation<IComputedJobsPartners>> = []
 
-        const ops = chunk.filter((op) => op != null)
+  for await (const doc of cursor) {
+    queue.add(async () => {
+      const op = await checkUrlStatus(doc._id, doc.apply_url)
+      counters.total++
+      if (op) {
+        counters.error++
+        bulk.push(op)
+      } else {
+        counters.success++
+      }
 
-        if (ops.length > 0) {
-          await collection.bulkWrite(ops)
-        }
-        callback()
-      },
+      if (bulk.length > BATCH_SIZE) {
+        // Prevent concurrency issues by copying the bulk array
+        const currentBulk = [...bulk]
+        bulk.length = 0
+
+        await collection.bulkWrite(currentBulk)
+        const elapsed = Date.now() - counters.startTime
+        const reqPerSec = Math.round((counters.total / elapsed) * 1000)
+        logger.info(`Progression : ${counters.total}/${count} URLs traitées (Succès : ${counters.success}, Erreurs : ${counters.error}, Req/s: ${reqPerSec})`)
+      }
     })
-  )
 
-  // await oleoduc(
-  //   collection.find({ business_error: null, apply_url: { $ne: null } }, { projection: { _id: 1, apply_url: 1 } }).stream(),
-  //   streamGroupByCount(BATCH_SIZE),
-  //   writeData(
-  //     async (batch: IJobApplyUrls[]) => {
-  //       // if http --> business_error URL_PROTOCOL
-  //       counters.total += batch.length
+    await queue.onEmpty()
+  }
 
-  //       // Traitement des URL dans le batch avec `Promise.all`
-  //       const bulk = []
-  //       await Promise.all(
-  //         batch.map(async (doc) => {
-  //           try {
-  //             const status = await checkUrlStatus(doc.apply_url)
-
-  //             if (status >= 200 && status < 400) {
-  //               counters.success++
-  //             } else {
-  //               counters.error++
-  //               // bulk.push[{}] bulk op
-  //               await collection.updateOne({ _id: doc._id }, { $set: { business_error: JOB_PARTNER_BUSINESS_ERROR.URL_UNAVAILABLE } })
-  //             }
-  //           } catch (error) {
-  //             counters.error++
-  //             await collection.updateOne({ _id: doc._id }, { $set: { business_error: JOB_PARTNER_BUSINESS_ERROR.URL_UNAVAILABLE } })
-  //           }
-  //         })
-  //       )
-  //       if (counters.total % BATCH_SIZE === 0) {
-  //         logger.info(`Progression : ${counters.total}/${count} URLs traitées (Succès : ${counters.success}, Erreurs : ${counters.error})`)
-  //       }
-  //     },
-  //     { parallel: 1 }
-  //   )
-  // )
+  if (bulk.length > 0) {
+    await collection.bulkWrite(bulk)
+  }
 
   logger.info(`Traitement terminé. Total: ${counters.total}, Succès: ${counters.success}, Erreurs: ${counters.error}`)
 }
