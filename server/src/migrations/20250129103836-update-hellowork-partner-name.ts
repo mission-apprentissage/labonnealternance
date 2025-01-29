38f9c652c372a5cdb0c7f04794d24327127960c0
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-29 11:43:17 +0100

    fix: migration boolean

--- server/src/migrations/20250129103836-update-hellowork-partner-name.ts
+++ server/src/migrations/20250129103836-update-hellowork-partner-name.ts
@@ -6,4 +6,4 @@ export const up = async () => {
 }
 
 // set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
-export const requireShutdown: boolean = true
+export const requireShutdown: boolean = false
