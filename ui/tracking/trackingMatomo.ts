77af99404cc309710400d1aec4a68205331f5bcc
Rémy Auricoste <remy.auricoste@gmail.com>
2025-01-29 10:58:11 +0100

    fix(lbac-2430): catch erreur de cookies (#1795)

--- ui/tracking/trackingMatomo.ts
+++ ui/tracking/trackingMatomo.ts
@@ -3,10 +3,14 @@ import { init } from "@socialgouv/matomo-next"
 import { publicConfig } from "@/config.public"
 
 export const initMatomo = () => {
-  init(publicConfig.matomo)
-  // @ts-expect-error
-  const paq = window._paq
-  paq.push(["setConsentGiven"])
-  paq.push(["rememberConsentGiven"])
-  paq.push(["HeatmapSessionRecording::enable"])
+  try {
+    init(publicConfig.matomo)
+    // @ts-expect-error
+    const paq = window._paq
+    paq.push(["setConsentGiven"])
+    paq.push(["rememberConsentGiven"])
+    paq.push(["HeatmapSessionRecording::enable"])
+  } catch (err) {
+    console.warn("error loading matomo", err)
+  }
 }
