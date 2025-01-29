52bd3359ba2be96eb7a8210ddd83d39101a5b027
Kevin Barnoin <k.barnoin@gmail.com>
2025-01-29 12:15:17 +0100

    feat: reorder cron

--- server/src/jobs/jobs.ts
+++ server/src/jobs/jobs.ts
@@ -50,7 +50,6 @@ import { syncEtablissementsAndFormations } from "./rdv/syncEtablissementsAndForm
 import { cancelOfferJob } from "./recruiters/cancelOfferJob"
 import { createApiUser } from "./recruiters/createApiUser"
 import { disableApiUser } from "./recruiters/disableApiUser"
-import { opcoReminderJob } from "./recruiters/opcoReminderJob"
 import { recruiterOfferExpirationReminderJob } from "./recruiters/recruiterOfferExpirationReminderJob"
 import { removeDuplicateRecruiters } from "./recruiters/removeDuplicatesRecruiters"
 import { resetApiKey } from "./recruiters/resetApiKey"
@@ -72,13 +71,37 @@ export async function setupJobProcessor() {
     crons: ["local", "preview", "pentest"].includes(config.env)
       ? {}
       : {
-          "Creation de la collection JOBS pour metabase": {
-            cron_string: "55 0 * * *",
-            handler: createJobsCollectionForMetabase,
+          "Génération du token France Travail pour la récupération des offres": {
+            cron_string: "*/5 * * * *",
+            handler: generateFranceTravailAccess,
+          },
+          "Scan et envoi des candidatures": {
+            cron_string: "*/10 * * * *",
+            handler: () => processApplications(),
+          },
+          "Mise à jour des adresses emails bloquées": {
+            cron_string: "5 0 * * *",
+            handler: () => updateBrevoBlockedEmails({}),
+          },
+          "Anonymisation des candidats & leurs candidatures de plus de deux (2) ans": {
+            cron_string: "10 0 * * *",
+            handler: anonymizeApplicantsAndApplications,
+          },
+          "Mise à jour des recruteurs en erreur": {
+            cron_string: "10 0 * * *",
+            handler: updateSiretInfosInError,
+          },
+          "Anonymisation des candidatures de plus de deux (2) ans": {
+            cron_string: "15 0 * * *",
+            handler: anonymizeApplications,
           },
           "Annulation des offres expirées": {
             cron_string: "15 0 * * *",
             handler: cancelOfferJob,
+          },
+          "Génération du sitemap pour les offres": {
+            cron_string: "20 0 * * *",
+            handler: generateSitemap,
           },
           // "Send offer reminder email at J+7": {
           //   cron_string: "20 0 * * *",
@@ -88,65 +111,25 @@ export async function setupJobProcessor() {
           //   cron_string: "25 0 * * *",
           //   handler: () => addJob({ name: "formulaire:relance", payload: { threshold: "1" } }),
           // },
-          "Envoi du rappel de validation des utilisateurs en attente aux OPCOs": {
-            cron_string: "30 0 * * 1,3,5",
-            handler: opcoReminderJob,
-          },
-          "Envoi des offres à France Travail": {
-            cron_string: "30 5 * * *",
-            handler: config.env === "production" ? () => exportJobsToFranceTravail() : () => Promise.resolve(0),
-          },
-          "Mise à jour des recruteurs en erreur": {
-            cron_string: "10 0 * * *",
-            handler: updateSiretInfosInError,
-          },
           "Active tous les établissements qui ont souscrits à l'opt-out": {
             cron_string: "50 0 * * *",
             handler: activateOptoutOnEtablissementAndUpdateReferrersOnETFA,
           },
-          "Invite les établissements (via email gestionnaire) à l'opt-out": {
-            cron_string: "0 9 * * *",
-            handler: inviteEtablissementToOptOut,
+          "Creation de la collection JOBS pour metabase": {
+            cron_string: "55 0 * * *",
+            handler: createJobsCollectionForMetabase,
           },
-          "Invite les établissements (via email gestionnaire) au premium (Parcoursup)": {
-            cron_string: "0 9 * * *",
-            handler: inviteEtablissementParcoursupToPremium,
+          "Anonymisation des user recruteurs de plus de deux (2) ans": {
+            cron_string: "0 1 * * *",
+            handler: anonimizeUsersWithAccounts,
           },
-          "(Relance) Invite les établissements (via email gestionnaire) au premium (Parcoursup)": {
-            cron_string: "30 9 * * *",
-            handler: () => inviteEtablissementParcoursupToPremiumFollowUp(),
+          "Anonimisation des utilisateurs RDVA de plus de deux (2) ans": {
+            cron_string: "5 1 * * *",
+            handler: anonymizeUsers,
           },
-          "Invite les établissements (via email gestionnaire) au premium (Affelnet)": {
-            cron_string: "15 9 * * *",
-            handler: inviteEtablissementAffelnetToPremium,
-          },
-          "(Relance) Invite les établissements (via email gestionnaire) au premium (Affelnet)": {
-            cron_string: "45 9 * * *",
-            handler: () => inviteEtablissementAffelnetToPremiumFollowUp(),
-          },
-          "Synchronise les formations eligibles à la prise de rendez-vous": {
-            cron_string: "45 2 * * *",
-            handler: syncEtablissementsAndFormations,
-          },
-          "Supprime les etablissements dupliqués à cause du parallélisme du job de synchronisation RDVA": {
-            cron_string: "30 3 * * *",
-            handler: removeDuplicateEtablissements,
-          },
-          "Synchronise les dates des etablissements eligible à la prise de rendez-vous": {
-            cron_string: "0 5 * * *",
-            handler: syncEtablissementDates,
-          },
-          "Supprime les dates d'invitation et de refus au premium (PARCOURSUP & AFFELNET) des etablissements": {
-            cron_string: "0 0 20 11 *",
-            handler: resetInvitationDates,
-          },
-          "Historisation des formations éligibles à la prise de rendez-vous": {
-            cron_string: "55 2 * * *",
-            handler: eligibleTrainingsForAppointmentsHistoryWithCatalogue,
-          },
-          "Mise à jour de la table de correspondance entre Id formation Onisep et Clé ME du catalogue RCO, utilisé pour diffuser la prise de RDV sur l’Onisep": {
-            cron_string: "45 23 * * 2",
-            handler: importReferentielOnisep,
+          "Anonymisation des appointments de plus de deux (2) ans": {
+            cron_string: "30 1 * * *",
+            handler: anonymizeAppointments,
           },
           "Import des formations depuis le Catalogue RCO": {
             cron_string: "15 2 * * *",
@@ -156,21 +139,69 @@ export async function setupJobProcessor() {
             cron_string: "30 2 * * *",
             handler: updateParcoursupAndAffelnetInfoOnFormationCatalogue,
           },
-          "Mise à jour des adresses emails bloquées": {
-            cron_string: "5 0 * * *",
-            handler: () => updateBrevoBlockedEmails({}),
+          "Historisation des formations éligibles à la prise de rendez-vous": {
+            cron_string: "55 2 * * *",
+            handler: eligibleTrainingsForAppointmentsHistoryWithCatalogue,
           },
-          "Géolocation de masse des sociétés issues de l'algo": {
-            cron_string: "0 5 * * 6",
-            handler: () => updateGeoLocations({}),
+          "Synchronise les formations eligibles à la prise de rendez-vous": {
+            cron_string: "45 2 * * *",
+            handler: syncEtablissementsAndFormations,
+          },
+          "Lancement du garbage collector": {
+            cron_string: "30 3 * * *",
+            handler: runGarbageCollector,
+          },
+          "Supprime les etablissements dupliqués à cause du parallélisme du job de synchronisation RDVA": {
+            cron_string: "30 3 * * *",
+            handler: removeDuplicateEtablissements,
+          },
+          "Traitement complet des jobs_partners": {
+            cron_string: "40 3 * * *",
+            handler: processJobPartners,
+          },
+          "Synchronise les dates des etablissements eligible à la prise de rendez-vous": {
+            cron_string: "0 5 * * *",
+            handler: syncEtablissementDates,
+          },
+          "Envoi des offres à France Travail": {
+            cron_string: "30 5 * * *",
+            handler: config.env === "production" ? () => exportJobsToFranceTravail() : () => Promise.resolve(0),
+          },
+          "Import complet des offres France Travail": {
+            cron_string: "0 6 * * *",
+            handler: importFranceTravailJobs,
           },
           "Détermination des opcos des sociétés issues de l'algo": {
             cron_string: "30 6 * * 6",
             handler: () => updateOpcoCompanies({}),
           },
-          "Mise à jour des sociétés issues de l'algo": {
-            cron_string: "0 5 * * 7",
-            handler: () => updateLbaCompanies({ useAlgoFile: true, clearMongo: true }),
+          "export des offres LBA sur S3": {
+            cron_string: "30 6 * * 1",
+            handler: config.env === "production" ? () => exportLbaJobsToS3() : () => Promise.resolve(0),
+          },
+          "Invite les établissements (via email gestionnaire) à l'opt-out": {
+            cron_string: "0 9 * * *",
+            handler: inviteEtablissementToOptOut,
+          },
+          "Invite les établissements (via email gestionnaire) au premium (Parcoursup)": {
+            cron_string: "0 9 * * *",
+            handler: inviteEtablissementParcoursupToPremium,
+          },
+          "Invite les établissements (via email gestionnaire) au premium (Affelnet)": {
+            cron_string: "15 9 * * *",
+            handler: inviteEtablissementAffelnetToPremium,
+          },
+          "(Relance) Invite les établissements (via email gestionnaire) au premium (Parcoursup)": {
+            cron_string: "30 9 * * *",
+            handler: () => inviteEtablissementParcoursupToPremiumFollowUp(),
+          },
+          "(Relance) Invite les établissements (via email gestionnaire) au premium (Affelnet)": {
+            cron_string: "45 9 * * *",
+            handler: () => inviteEtablissementAffelnetToPremiumFollowUp(),
+          },
+          "Creation de la collection rolemanagement360": {
+            cron_string: "00 10,13,17 * * *",
+            handler: createRoleManagement360,
           },
           "Contrôle quotidien des candidatures": {
             cron_string: "0 10-19/1 * * 1-5",
@@ -180,70 +211,25 @@ export async function setupJobProcessor() {
             cron_string: "0 11-19/2 * * 1-5",
             handler: config.env === "production" ? () => controlAppointments() : () => Promise.resolve(0),
           },
-          "Anonymisation des candidatures de plus de deux (2) ans": {
-            cron_string: "15 0 * * *",
-            handler: anonymizeApplications,
-          },
-          "Anonymisation des candidats & leurs candidatures de plus de deux (2) ans": {
-            cron_string: "10 0 * * *",
-            handler: anonymizeApplicantsAndApplications,
-          },
-          "Anonimisation des utilisateurs RDVA de plus de deux (2) ans": {
-            cron_string: "5 1 * * *",
-            handler: anonymizeUsers,
-          },
-          "Anonymisation des user recruteurs de plus de deux (2) ans": {
-            cron_string: "0 1 * * *",
-            handler: anonimizeUsersWithAccounts,
-          },
-          "Anonymisation des appointments de plus de deux (2) ans": {
-            cron_string: "30 1 * * *",
-            handler: anonymizeAppointments,
-          },
-          // KBA 20250225 : retrait du job pour monitoring avec 2 replicas.
-          // "Lancement du garbage collector": {
-          //   cron_string: "30 3 * * *",
-          //   handler: runGarbageCollector,
-          // },
-          "export des offres LBA sur S3": {
-            cron_string: "30 6 * * 1",
-            handler: config.env === "production" ? () => exportLbaJobsToS3() : () => Promise.resolve(0),
-          },
-          "Creation de la collection rolemanagement360": {
-            cron_string: "00 10,13,17 * * *",
-            handler: createRoleManagement360,
-          },
-          "Scan et envoi des candidatures": {
-            cron_string: "*/10 * * * *",
-            handler: () => processApplications(),
-          },
-          "Génération du token France Travail pour la récupération des offres": {
-            cron_string: "*/5 * * * *",
-            handler: generateFranceTravailAccess,
-          },
           "Mise à jour du référentiel commune": {
             cron_string: "0 15 * * SUN",
             handler: updateReferentielCommune,
+          },
+          "Emission des intentions des recruteurs": {
+            cron_string: "30 20 * * *",
+            handler: processRecruiterIntentions,
           },
           "Emission des contacts vers Brevo": {
             cron_string: "30 22 * * *",
             handler: sendContactsToBrevo,
           },
-          "Génération du sitemap pour les offres": {
-            cron_string: "20 0 * * *",
-            handler: generateSitemap,
+          "Mise à jour de la table de correspondance entre Id formation Onisep et Clé ME du catalogue RCO, utilisé pour diffuser la prise de RDV sur l’Onisep": {
+            cron_string: "45 23 * * 2",
+            handler: importReferentielOnisep,
           },
-          "Traitement complet des jobs_partners": {
-            cron_string: "40 3 * * *",
-            handler: processJobPartners,
-          },
-          "Import complet des offres France Travail": {
-            cron_string: "0 6 * * *",
-            handler: importFranceTravailJobs,
-          },
-          "Emission des intentions des recruteurs": {
-            cron_string: "30 20 * * *",
-            handler: processRecruiterIntentions,
+          "Supprime les dates d'invitation et de refus au premium (PARCOURSUP & AFFELNET) des etablissements": {
+            cron_string: "0 0 20 11 *",
+            handler: resetInvitationDates,
           },
         },
     jobs: {
