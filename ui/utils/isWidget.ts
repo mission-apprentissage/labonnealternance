faf5eaf7b5a0a062cd9ce1a1e4dee05f8d814174
Rémy Auricoste <remy.auricoste@gmail.com>
2025-01-30 15:19:46 +0100

    fix: amélioration isWidget

--- ui/utils/isWidget.ts
+++ ui/utils/isWidget.ts
@@ -1,6 +1,4 @@
 export const isWidget = () => {
   if (typeof window === "undefined") return false
-  const { pathname, search } = window.location
-  const searchParams = new URLSearchParams(search)
-  return pathname.includes("/widget/") || searchParams.get("utm_campaign") === "widget"
+  return window.parent !== window.self
 }
