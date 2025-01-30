48f0a5659fc8ed74620b7a46204fc8e413e27b06
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-01-30 11:39:11 +0100

    feat: tests

--- shared/utils/stringUtils.test.ts
+++ shared/utils/stringUtils.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest"
 
-import { joinNonNullStrings, removeAccents } from "./stringUtils.js"
+import { LBA_ITEM_TYPE } from "../constants/lbaitem.js"
+
+import { buildJobUrl, buildTrainingUrl, getDirectJobPath, joinNonNullStrings, removeAccents } from "./stringUtils.js"
 
 describe("stringUtils", () => {
   describe("removeAccents", () => {
@@ -47,6 +49,28 @@ describe("stringUtils", () => {
 
     it("should return a single word if only one non-null value is present", () => {
       expect(joinNonNullStrings([null, " Vitest ", null])).toBe("Vitest")
+    })
+  })
+
+  describe("buildJobUrl", () => {
+    it("should build a job URL", () => {
+      expect(buildJobUrl(LBA_ITEM_TYPE.RECRUTEURS_LBA, "123", "Job Title")).toBe("/emploi/recruteurs_lba/123/job-title")
+    })
+  })
+
+  describe("buildTrainingUrl", () => {
+    it("should build a training URL", () => {
+      expect(buildTrainingUrl("123#aaa", "Job Title")).toBe("/formation/123%23aaa/job-title")
+    })
+  })
+
+  describe("getDirectJobPath", () => {
+    it("should build a direct job path", () => {
+      expect(getDirectJobPath("123")).toBe("/emploi/offres_emploi_lba/123/offre")
+    })
+
+    it("should build a direct job path with title", () => {
+      expect(getDirectJobPath("123", "titre de l'offre")).toBe("/emploi/offres_emploi_lba/123/titre-de-l-offre")
     })
   })
 })
