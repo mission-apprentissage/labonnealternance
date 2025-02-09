fbadd424c580d8dcfbf45866efbfa30e1476e010
Kevin Barnoin <k.barnoin@gmail.com>
2025-02-09 14:23:10 +0100

    fix: export function contact page

--- ui/app/(home)/contact/page.tsx
+++ ui/app/(home)/contact/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Grid, GridItem, Text } from "@chakra-ui/react"
+import { Box, Container, Divider, Flex, Grid, GridItem, Text } from "@chakra-ui/react"
 import { Button } from "@codegouvfr/react-dsfr/Button"
 import { Metadata } from "next"
 
@@ -11,41 +11,43 @@ export const metadata: Metadata = {
   description: PAGES.static.contact.getMetadata().description,
 }
 
-const Contact = () => (
-  <Box>
-    <Box as="main">
-      <Breadcrumb pages={[PAGES.static.contact]} />
-      <Container p={12} my={0} mb={[0, 12]} variant="pageContainer">
-        <Grid templateColumns="repeat(12, 1fr)">
-          <GridItem px={4} colSpan={[12, 12, 12, 4]}>
-            <Text variant="editorialContentH1" as="h1">
-              Contact
-            </Text>
-            <Divider variant="pageTitleDivider" my={12} />
-          </GridItem>
-          <GridItem px={4} colSpan={[12, 12, 12, 8]}>
-            <Text variant="editorialContentH3" as="h3">
-              Nous contacter
-            </Text>
-            <Text as="p">Vous avez une question sur nos outils ? Consultez notre foire aux questions. </Text>
-            {/* @ts-expect-error: TODO */}
-            <Box align="center" my={12}>
-              <Button priority="secondary" aria-label="Accès à la Foire aux questions">
-                <DsfrLink href="/faq">Consulter la FAQ</DsfrLink>
-              </Button>
-            </Box>
-            <Text as="p" mb={4}>
-              Si jamais vous ne trouvez pas votre réponse dans notre FAQ, ou souhaitez nous partager votre avis ou une suggestion d’amélioration sur nos outils, contactez nous par
-              email à<br />
-              <DsfrLink aria-label="Envoi d'un email au service candidat de La bonne alternance" href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact">
-                labonnealternance@apprentissage.beta.gouv.fr
-              </DsfrLink>
-            </Text>
-          </GridItem>
-        </Grid>
-      </Container>
+export default function Contact() {
+  return (
+    <Box>
+      <Box as="main">
+        <Breadcrumb pages={[PAGES.static.contact]} />
+        <Container p={12} my={0} mb={[0, 12]} variant="pageContainer">
+          <Grid templateColumns="repeat(12, 1fr)">
+            <GridItem px={4} colSpan={[12, 12, 12, 4]}>
+              <Text variant="editorialContentH1" as="h1">
+                Contact
+              </Text>
+              <Divider variant="pageTitleDivider" my={12} />
+            </GridItem>
+            <GridItem px={4} colSpan={[12, 12, 12, 8]}>
+              <Text variant="editorialContentH3" as="h3">
+                Nous contacter
+              </Text>
+              <Text as="p">Vous avez une question sur nos outils ? Consultez notre foire aux questions. </Text>
+              <Flex justify="center" my={12}>
+                <Button priority="secondary" aria-label="Accès à la Foire aux questions">
+                  <DsfrLink href="/faq">Consulter la FAQ</DsfrLink>
+                </Button>
+              </Flex>
+              <Text as="p" mb={4}>
+                Si jamais vous ne trouvez pas votre réponse dans notre FAQ, ou souhaitez nous partager votre avis ou une suggestion d’amélioration sur nos outils, contactez nous
+                par email à<br />
+                <DsfrLink
+                  aria-label="Envoi d'un email au service candidat de La bonne alternance"
+                  href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact"
+                >
+                  labonnealternance@apprentissage.beta.gouv.fr
+                </DsfrLink>
+              </Text>
+            </GridItem>
+          </Grid>
+        </Container>
+      </Box>
     </Box>
-  </Box>
-)
-
-export default Contact
+  )
+}
