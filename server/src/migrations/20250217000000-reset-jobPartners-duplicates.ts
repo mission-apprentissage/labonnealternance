5257c93d1fd497233ec7dcd90f4ee44dba72cfd6
Rémy Auricoste <remy.auricoste@gmail.com>
2025-02-17 11:56:31 +0100

    fix: reset jobs_in_success

--- server/src/migrations/20250217000000-reset-jobPartners-duplicates.ts
+++ server/src/migrations/20250217000000-reset-jobPartners-duplicates.ts
@@ -2,7 +2,7 @@ import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 export const up = async () => {
   await getDbCollection("jobs_partners").updateMany({}, { $set: { duplicates: [] } })
-  await getDbCollection("computed_jobs_partners").updateMany({}, { $set: { duplicates: [] } })
+  await getDbCollection("computed_jobs_partners").updateMany({}, { $set: { duplicates: [], jobs_in_success: [] } })
 }
 
 // set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
