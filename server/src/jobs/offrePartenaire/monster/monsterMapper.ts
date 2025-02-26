0f336ed7895e42ee02d01ea047855bde5544ec19
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-02-26 18:08:44 +0100

    feat: monster mapper

--- server/src/jobs/offrePartenaire/monster/monsterMapper.ts
+++ server/src/jobs/offrePartenaire/monster/monsterMapper.ts
@@ -10,107 +10,28 @@ import { removeHtmlTagsFromString } from "@/common/utils/stringUtils"
 
 import { blankComputedJobPartner } from "../fillComputedJobsPartners"
 
-const ZMonsterJobLocation = z.object({
-  $: z.object({
-    label: z.string(),
-    lat: z.string().nullish(),
-    lng: z.string().nullish(),
-  }),
-  city: z.string().nullish(),
-  postalCode: z.string().nullish(),
-  department: z.object({ _: z.string(), $: z.object({ code: z.string() }) }).nullish(),
-  state: z.object({ _: z.string(), $: z.object({ code: z.string() }) }).nullish(),
-  country: z.object({ _: z.string(), $: z.object({ code: z.string() }) }),
-})
-type IMonsterJobLocation = z.output<typeof ZMonsterJobLocation>
-
-const CONTRAT_ALTERNANCE = "Alternance / Apprentissage"
-
 export const ZMonsterJob = z
   .object({
-    $: z.object({ id: z.string(), reference: z.string(), lang: z.string().nullish() }),
+    postingId: z.string(),
     title: z.string(),
-    description: z.string(),
-    link: z.string(),
-    publicationDate: z.string(),
-    lastModificationDate: z.string(),
-    position: z.string().nullish(),
-    industry: z.string().nullish(),
-    company: z.object({
-      $: z.object({ id: z.string(), anonymous: z.string() }),
-      description: z.string().nullish(),
-      name: z.string(),
-    }),
-    workplace: z.object({
-      locations: z.object({
-        location: z.union([ZMonsterJobLocation, z.array(ZMonsterJobLocation)]),
-      }),
-    }),
-    contract: z.object({
-      types: z.object({
-        type: z.union([
-          z.object({
-            _: z.string(),
-            $: z.object({ code: z.string() }),
-          }),
-          z.array(
-            z.object({
-              _: z.string(),
-              $: z.object({ code: z.string() }),
-            })
-          ),
-        ]),
-      }),
-      length: z
-        .object({
-          _: z.string(),
-          $: z.object({ value: z.string(), unit: z.string() }),
-        })
-        .nullish(),
-    }),
-    workSchedule: z
-      .object({
-        types: z.object({
-          type: z.object({
-            _: z.string(),
-            $: z.object({ code: z.string() }),
-          }),
-        }),
-      })
-      .nullish(),
-    benefits: z
-      .object({
-        salary: z
-          .object({
-            _: z.string(),
-            $: z.object({ lowEnd: z.string(), currency: z.string(), period: z.string() }),
-          })
-          .nullish(),
-        description: z.string().nullish(),
-      })
-      .nullish(),
-    profile: z
-      .object({
-        description: z.string().nullish(),
-        degrees: z
-          .object({
-            degree: z.union([
-              z.object({
-                _: z.string(),
-                $: z.object({ code: z.string() }),
-              }),
-              z.array(
-                z.object({
-                  _: z.string(),
-                  $: z.object({ code: z.string() }),
-                })
-              ),
-            ]),
-          })
-          .nullish(),
-        experienceLevels: z.object({ experienceLevel: z.array(z.object({ _: z.string(), $: z.object({ code: z.string() }) })) }).nullish(),
-      })
-      .nullable(),
+    JobActiveDate: z.string(),
+    CompanyName: z.string(),
+    siretNumber: z.string(),
+    JobPostalCode: z.string().nullable(),
+    JobCity: z.string(),
+    State: z.string(),
+    Country: z.string(),
+    Latitude: z.string(),
+    Longitude: z.string(),
+    Industry: z.string(),
+    JobCategory: z.string(),
+    JobOccupation: z.string(),
+    JobLevel: z.string(),
+    JobStatus: z.string(),
+    JobType: z.string(),
+    CompanyJobLogoURL: z.string(),
+    JobBody: z.string(),
+    guid: z.string(),
   })
   .passthrough()
 
@@ -123,69 +44,50 @@ const sanitizeHtml = (text: string) => {
 }
 
 export const monsterJobToJobsPartners = (job: IMonsterJob): IComputedJobsPartners => {
-  const { $, title, description, link, publicationDate, lastModificationDate, position, industry, company, contract, workSchedule, benefits, profile } = job
-  const workplaceLocation = job.workplace.locations.location instanceof Array ? job.workplace.locations.location[0] : job.workplace.locations.location
-  const workplace_geopoint = geolocToLatLon(workplaceLocation)
+  const { postingId, title, JobActiveDate, siretNumber, CompanyName, JobPostalCode, JobCity, Latitude, Longitude, JobBody, guid } = job
 
-  const urlParsing = extensions.url().safeParse(link)
-  const creationDate = new Date(publicationDate)
+  const workplace_geopoint: {
+    type: "Point"
+    coordinates: [number, number]
+  } = {
+    type: "Point",
+    coordinates: [parseFloat(Longitude), parseFloat(Latitude)],
+  }
 
-  const created_at = new Date()
+  const urlParsing = extensions.url().safeParse(guid)
 
-  let descriptionComputed = `${description}\r\n\r\n${industry ? `Secteur: ${industry}\r\n\r\n` : ""}
-  ${position ? `Poste: ${position}\r\n\r\n` : ""}
-  ${workSchedule ? `${workSchedule.types.type._}\r\n\r\n` : ""}
-  ${benefits ? `Avantages: ${benefits?.salary?._ ? benefits.salary._ : ""}\r\n\r\n` : ""}${benefits?.description ? `${benefits.description}\r\n\r\n` : ""}
-  ${profile?.description ? `Profil: ${profile.description}` : ""}`
+  let descriptionComputed = JobBody
 
   descriptionComputed = sanitizeHtml(descriptionComputed)
+
+  const created_at = new Date()
+  const publicationDate = new Date(JobActiveDate)
 
   const partnerJob: IComputedJobsPartners = {
     ...blankComputedJobPartner(),
     _id: new ObjectId(),
     created_at,
-    updated_at: lastModificationDate ? new Date(lastModificationDate) : created_at,
     partner_label: JOBPARTNERS_LABEL.MONSTER,
-    partner_job_id: $.id,
+    partner_job_id: postingId,
 
     offer_title: title,
     offer_description: descriptionComputed,
-    offer_rome_codes: undefined,
-    offer_creation: creationDate,
+    offer_creation: publicationDate,
 
     offer_expiration: dayjs
-      .tz(creationDate || created_at)
+      .tz(publicationDate || created_at)
       .add(2, "months")
       .toDate(),
 
-    workplace_name: company.name,
-    workplace_description: company.description && company.description.length >= 30 ? sanitizeHtml(company.description) : null,
-    workplace_address_zipcode: workplaceLocation?.postalCode || null,
-    workplace_address_city: workplaceLocation.city || null,
-    workplace_address_label: workplaceLocation.$?.label || null,
+    workplace_name: CompanyName,
+    workplace_siret: siretNumber,
+    workplace_address_zipcode: JobPostalCode || null,
+    workplace_address_city: JobCity,
+    workplace_address_label: [JobCity, JobPostalCode].join(" ").trim(),
     workplace_geopoint,
-    contract_type:
-      (!(contract.types.type instanceof Array) && contract.types.type?._ === CONTRAT_ALTERNANCE) ||
-      (contract.types.type instanceof Array && contract.types.type.find((type) => type._ === CONTRAT_ALTERNANCE))
-        ? [TRAINING_CONTRACT_TYPE.APPRENTISSAGE, TRAINING_CONTRACT_TYPE.PROFESSIONNALISATION]
-        : undefined,
-    contract_duration: contract?.length?.$?.value ? parseInt(contract.length.$.value) : null,
-    offer_desired_skills: [],
-    offer_access_conditions: profile?.experienceLevels?.experienceLevel?.length ? profile?.experienceLevels?.experienceLevel.map((l) => l._) : [],
-    offer_multicast: true,
-    offer_to_be_acquired_skills: [],
     apply_url: urlParsing.success ? urlParsing.data : null,
+    offer_multicast: true,
+    contract_type: [TRAINING_CONTRACT_TYPE.APPRENTISSAGE, TRAINING_CONTRACT_TYPE.PROFESSIONNALISATION],
   }
   return partnerJob
-}
-
-const geolocToLatLon = (location: IMonsterJobLocation): IComputedJobsPartners["workplace_geopoint"] => {
-  const { lat, lng } = location.$
-  if (!lat || !lng) return null
-  const latitude = parseFloat(lat)
-  const longitude = parseFloat(lng)
-  return {
-    type: "Point",
-    coordinates: [longitude, latitude],
-  }
 }
