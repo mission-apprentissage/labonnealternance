b6045eb588ea9e30e15947d0d5441b92a1f50329
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-02-27 17:49:56 +0100

    feat(lba-2418): import meteojob (#1862)

---
+++ server/src/jobs/offrePartenaire/meteojob/importMeteojob.test.ts
@@ -0,0 +1,41 @@
+import fs from "node:fs"
+
+import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
+import { beforeEach, describe, expect, it, vi } from "vitest"
+
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { useMongo } from "@tests/utils/mongo.test.utils"
+
+import { importMeteojobRaw, importMeteojobToComputed } from "./importMeteojob"
+
+const now = new Date("2024-07-21T04:49:06.000+02:00")
+
+describe("importMeteojob", () => {
+  useMongo()
+
+  beforeEach(() => {
+    vi.useFakeTimers()
+    vi.setSystemTime(now)
+
+    return async () => {
+      vi.useRealTimers()
+      await getDbCollection("computed_jobs_partners").deleteMany({})
+      await getDbCollection("raw_meteojob").deleteMany({})
+    }
+  })
+
+  it("should test the import of hellowork data into computed_job_partners", async () => {
+    const fileStream = fs.createReadStream("server/src/jobs/offrePartenaire/meteojob/importMeteojob.test.input.xml")
+    await importMeteojobRaw(fileStream)
+    expect.soft(await getDbCollection("raw_meteojob").countDocuments({})).toBe(3)
+
+    await importMeteojobToComputed()
+    const jobs = (
+      await getDbCollection("computed_jobs_partners")
+        .find({ partner_label: JOBPARTNERS_LABEL.METEOJOB }, { projection: { _id: 0, created_at: 0 } })
+        .toArray()
+    ).sort((a, b) => ((a.partner_job_id ?? "") < (b.partner_job_id ?? "") ? -1 : 1))
+    expect.soft(jobs.length).toBe(3)
+    expect.soft(jobs).toMatchSnapshot()
+  })
+})
