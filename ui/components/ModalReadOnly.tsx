871422700253b108a4d23c59c62fa45a474a3045
Rémy Auricoste <remy.auricoste@gmail.com>
2025-03-04 09:07:40 +0100

    Update ui/components/ModalReadOnly.tsx

--- ui/components/ModalReadOnly.tsx
+++ ui/components/ModalReadOnly.tsx
@@ -18,7 +18,6 @@ export const ModalReadOnly = ({
   const isMobile = () => window.innerWidth < 500
   return (
     <Modal closeOnOverlayClick={true} blockScrollOnMount={true} size={isMobile() ? "full" : "sm"} isOpen={isOpen} onClose={onClose}>
-      plop
       <ModalOverlay />
       <ModalContent overflowY="auto" margin="auto" maxHeight={["100%", "95%"]} maxWidth={["100%", "95%"]} width="fit-content" borderRadius={0} {...modalContentProps} pt={0}>
         {!hideCloseButton && <ModalReadOnlyCloseButton onClick={onClose} />}
