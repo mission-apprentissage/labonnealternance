c8f21a386b0d8cf899aebef7d170392a9e25e004
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-05 15:41:16 +0100

    feat: embryo

--- server/src/jobs/offrePartenaire/kelio/importKelio.ts
+++ server/src/jobs/offrePartenaire/kelio/importKelio.ts
@@ -1,5 +1,30 @@
-import { importFromUrlInXml } from "../importFromUrlInXml"
+import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
+import rawKelioModel from "shared/models/rawKelio.model"
 
-export const importKelio = async () => {
-  await importFromUrlInXml({ destinationCollection: "raw_kelio", url: "plop", offerXmlTag: "job", partnerLabel: "kelio" })
+import config from "../../../config"
+import { importFromStreamInXml } from "../importFromStreamInXml"
+import { importFromUrlInXml } from "../importFromUrlInXml"
+import { rawToComputedJobsPartners } from "../rawToComputedJobsPartners"
+
+import { kelioJobToJobsPartners, ZKelioJob } from "./kelioMapper"
+
+const rawCollectionName = rawKelioModel.collectionName
+const offerXmlTag = "job"
+
+export const importMonsterRaw = async (sourceStream?: NodeJS.ReadableStream) => {
+  if (sourceStream) {
+    await importFromStreamInXml({ destinationCollection: rawCollectionName, offerXmlTag, stream: sourceStream, partnerLabel: JOBPARTNERS_LABEL.MONSTER })
+  } else {
+    await importFromUrlInXml({ destinationCollection: rawCollectionName, url: config.monsterUrl, offerXmlTag, partnerLabel: JOBPARTNERS_LABEL.MONSTER })
+  }
+}
+
+export const importMonsterToComputed = async () => {
+  await rawToComputedJobsPartners({
+    collectionSource: rawCollectionName,
+    partnerLabel: JOBPARTNERS_LABEL.KELIO,
+    zodInput: ZKelioJob,
+    mapper: kelioJobToJobsPartners,
+    documentJobRoot: offerXmlTag,
+  })
 }
