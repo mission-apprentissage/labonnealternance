93f5fa62eb2fb99557aa15d32abbb0a14838ab26
Rémy Auricoste <remy.auricoste@gmail.com>
2025-03-06 15:42:23 +0100

    fix: animation

--- ui/components/ItemDetail/CandidatureLba/CandidatureTasksChecklist.tsx
+++ ui/components/ItemDetail/CandidatureLba/CandidatureTasksChecklist.tsx
@@ -1,6 +1,6 @@
 import { Box, Checkbox, Heading, Image, Text } from "@chakra-ui/react"
 import styled from "@emotion/styled"
-import { domAnimation, LazyMotion, m } from "framer-motion"
+import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion"
 import { useState } from "react"
 import { LBA_ITEM_TYPE_OLD } from "shared/constants/lbaitem"
 
@@ -44,33 +44,52 @@ export const CandidatureTasksChecklist = ({ kind }: { kind: LBA_ITEM_TYPE_OLD })
   const tasks = kind === LBA_ITEM_TYPE_OLD.LBA ? OffreSpontaneTasks : OffreTasks
   const [checkedTasks, setCheckedTasks] = useState<Record<string, boolean>>({})
 
+  const isFullyChecked = tasks.every((task) => Boolean(checkedTasks[task]))
+
   const toggleTask = (task: string) => {
     setCheckedTasks({ ...checkedTasks, [task]: !checkedTasks[task] })
   }
 
-  const isFullyChecked = tasks.every((task) => Boolean(checkedTasks[task]))
+  const delayBeforeStartingAnimation = 0.6
+  const fadeOutDuration = 0.3
+  const fadeInDuration = 0.4
 
   return (
     <TasksContainer>
       <LazyMotion features={domAnimation}>
         <Box width={363} minWidth={363} padding="124px 44px 44px 44px" height="100%">
-          {!isFullyChecked && (
-            <m.div initial={{ opacity: 1 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="animated-div">
-              <Image src="/images/head_with_bulb.svg" alt="" marginBottom={7} />
-              <Heading textAlign="center" color="#000091" fontSize="20px" lineHeight="28px" fontWeight={700} mb={6}>
-                Mettez toutes les chances de votre côté !
-              </Heading>
-              <Box className="checkbox-container">
-                {tasks.map((task) => (
-                  <Checkbox key={task} defaultChecked={false} marginTop={2} onChange={() => toggleTask(task)}>
-                    <Text>{task}</Text>
-                  </Checkbox>
-                ))}
-              </Box>
-            </m.div>
-          )}
+          <AnimatePresence>
+            {!isFullyChecked && (
+              <m.div
+                key="checkbox-list"
+                transition={{ duration: fadeOutDuration, delay: delayBeforeStartingAnimation }}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="animated-div"
+              >
+                <Image src="/images/head_with_bulb.svg" alt="" marginBottom={7} />
+                <Heading textAlign="center" color="#000091" fontSize="20px" lineHeight="28px" fontWeight={700} mb={6}>
+                  Mettez toutes les chances de votre côté !
+                </Heading>
+                <Box className="checkbox-container">
+                  {tasks.map((task) => (
+                    <Checkbox key={task} defaultChecked={false} marginTop={2} onChange={() => toggleTask(task)}>
+                      <Text>{task}</Text>
+                    </Checkbox>
+                  ))}
+                </Box>
+              </m.div>
+            )}
+          </AnimatePresence>
           {isFullyChecked && (
-            <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="animated-div">
+            <m.div
+              transition={{ delay: delayBeforeStartingAnimation + fadeOutDuration, duration: fadeInDuration }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="animated-div"
+            >
               <Image src="/images/launching_rocket.svg" alt="" marginBottom={7} />
               <Heading textAlign="center" color="#000091" fontSize="20px" lineHeight="28px" fontWeight={700} mb={6}>
                 Bravo !<br />
