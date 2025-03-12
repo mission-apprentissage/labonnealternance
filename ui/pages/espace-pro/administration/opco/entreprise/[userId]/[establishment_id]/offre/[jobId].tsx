6d55b06270988618318084a1a709b46857471e64
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-12 15:44:35 +0100

    feat: old page to remove

--- ui/pages/espace-pro/administration/opco/entreprise/[userId]/[establishment_id]/offre/[jobId].tsx
+++ ui/pages/espace-pro/administration/opco/entreprise/[userId]/[establishment_id]/offre/[jobId].tsx
@@ -1,5 +1,3 @@
-import { useRouter } from "next/router"
-
 import CreationOffre from "@/app/(espace-pro)/espace-pro/(connected)/entreprise/[establishment_id]/offre/creation/page"
 import { getAuthServerSideProps } from "@/common/SSR/getAuthServerSideProps"
 
@@ -7,15 +5,9 @@ import { Layout } from "../../../../../../../../components/espace_pro"
 import { authProvider, withAuth } from "../../../../../../../../components/espace_pro/withAuth"
 
 function OpcoEntrepriseCreationOffre() {
-  const router = useRouter()
-  const { establishment_id, userId, jobId } = router.query as { establishment_id: string; userId: string; jobId: string }
   return (
     <Layout footer={false}>
-      <CreationOffre
-        jobId={jobId === "creation" ? undefined : jobId}
-        establishment_id={establishment_id}
-        onSuccess={() => router.push(`/espace-pro/administration/opco/entreprise/${userId}/entreprise/${establishment_id}`)}
-      />
+      <CreationOffre />
     </Layout>
   )
 }
