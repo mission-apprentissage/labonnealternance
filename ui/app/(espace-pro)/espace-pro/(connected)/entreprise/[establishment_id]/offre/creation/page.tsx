4f03eb874bca362a98bd02c43c8ee534d4473e40
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-12 17:44:53 +0100

    feat: ajout styling sur formulaire

--- ui/app/(espace-pro)/espace-pro/(connected)/entreprise/[establishment_id]/offre/creation/page.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/entreprise/[establishment_id]/offre/creation/page.tsx
@@ -1,10 +1,10 @@
 import UpdateOffre from "@/app/(espace-pro)/espace-pro/(connected)/_components/UpdateOffre"
-import { DepotSimplifieLayout } from "@/components/espace_pro/common/components/DepotSimplifieLayout"
+import { DepotSimplifieStyling } from "@/components/espace_pro/common/components/DepotSimplifieLayout"
 
 export default async function CreationOffre() {
   return (
-    <DepotSimplifieLayout>
+    <DepotSimplifieStyling>
       <UpdateOffre />
-    </DepotSimplifieLayout>
+    </DepotSimplifieStyling>
   )
 }
