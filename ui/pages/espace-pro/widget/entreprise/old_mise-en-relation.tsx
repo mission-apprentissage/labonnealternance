050807076a38707ca655aad56d49935a61316354
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-13 11:32:47 +0100

    feat: cleaning

--- ui/pages/espace-pro/widget/entreprise/old_mise-en-relation.tsx
+++
@@ -1,7 +0,0 @@
-import CreationMiseEnRelationPage from "../../creation/old_mise-en-relation"
-
-export const WidgetEntrepriseMiseEnRelation = () => {
-  return <CreationMiseEnRelationPage isWidget={true} />
-}
-
-export default WidgetEntrepriseMiseEnRelation
