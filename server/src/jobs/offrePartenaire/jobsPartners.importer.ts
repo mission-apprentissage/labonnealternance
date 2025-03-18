8becd4a7ae19b7da1f5643f7382d79c2ab92cd87
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-18 11:06:26 +0100

    feat: ajout des importeurs au nouveau mécanisme

--- server/src/jobs/offrePartenaire/jobsPartners.importer.ts
+++ server/src/jobs/offrePartenaire/jobsPartners.importer.ts
@@ -2,7 +2,9 @@ import { CronDef } from "job-processor"
 
 import { processFranceTravail } from "@/jobs/offrePartenaire/france-travail/processFranceTravail"
 import { processHellowork } from "@/jobs/offrePartenaire/hellowork/processHellowork"
+import { processKelio } from "@/jobs/offrePartenaire/kelio/processKelio"
 import { processMeteojob } from "@/jobs/offrePartenaire/meteojob/processMeteojob"
+import { processMonster } from "@/jobs/offrePartenaire/monster/processMonster"
 import { processRhAlternance } from "@/jobs/offrePartenaire/rh-alternance/processRhAlternance"
 
 const timings = {
@@ -34,6 +36,20 @@ export const importers: Record<string, CronDef> = {
   "Import Meteojob": {
     cron_string: timings.import_source,
     handler: processMeteojob,
+    checkinMargin: 10,
+    maxRuntimeInMinutes: 30,
+    tag: "slave",
+  },
+  "Import Monster": {
+    cron_string: timings.import_source,
+    handler: processMonster,
+    checkinMargin: 10,
+    maxRuntimeInMinutes: 30,
+    tag: "slave",
+  },
+  "Import Kelio": {
+    cron_string: timings.import_source,
+    handler: processKelio,
     checkinMargin: 10,
     maxRuntimeInMinutes: 30,
     tag: "slave",
