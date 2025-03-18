9dd1a9de895b36c0337581c351ff129353b25bc5
Rémy Auricoste <remy.auricoste@gmail.com>
2025-03-18 17:57:43 +0100

    fix: espace opco

--- ui/app/(espace-pro)/espace-pro/(connected)/cfa/entreprise/[establishment_id]/creation-offre/page.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/cfa/entreprise/[establishment_id]/creation-offre/page.tsx
@@ -3,11 +3,17 @@
 import { useParams, useRouter } from "next/navigation"
 
 import UpsertOffre from "@/app/(espace-pro)/espace-pro/(connected)/_components/UpsertOffre"
+import { Breadcrumb } from "@/app/_components/Breadcrumb"
 import { PAGES } from "@/utils/routes.utils"
 
 export default function EntrepriseCreationOffrePage() {
   const router = useRouter()
   const { establishment_id } = useParams() as { establishment_id: string }
 
-  return <UpsertOffre establishment_id={establishment_id} onSuccess={() => router.push(PAGES.dynamic.backCfaPageEntreprise(establishment_id).getPath())} />
+  return (
+    <>
+      <Breadcrumb pages={[PAGES.static.backCfaHome, PAGES.dynamic.backCfaPageEntreprise(establishment_id), PAGES.dynamic.backCfaEntrepriseCreationOffre(establishment_id)]} />
+      <UpsertOffre establishment_id={establishment_id} onSuccess={() => router.push(PAGES.dynamic.backCfaPageEntreprise(establishment_id).getPath())} />
+    </>
+  )
 }
--- ui/app/(espace-pro)/espace-pro/(connected)/opco/entreprise/[establishment_id]/page.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/opco/entreprise/[establishment_id]/page.tsx
@@ -3,16 +3,16 @@
 import { useParams } from "next/navigation"
 
 import ListeOffres from "@/app/(espace-pro)/espace-pro/(connected)/_components/ListeOffres"
-import { Layout } from "@/components/espace_pro"
-
-//TODO doit contenir la liste des offres de la société administré par l'opco sélectionnée
+import { Breadcrumb } from "@/app/_components/Breadcrumb"
+import { PAGES } from "@/utils/routes.utils"
 
 export default function EntrepriseJobList() {
   const { establishment_id } = useParams() as { establishment_id: string }
 
   return (
-    <Layout footer={false}>
+    <>
+      <Breadcrumb pages={[PAGES.static.backOpcoHome, PAGES.dynamic.backOpcoEditionEntreprise({ establishment_id })]} />
       <ListeOffres hideModify={true} establishment_id={establishment_id} />
-    </Layout>
+    </>
   )
 }
