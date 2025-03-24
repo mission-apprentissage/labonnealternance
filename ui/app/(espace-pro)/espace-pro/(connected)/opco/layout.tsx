a6fd0c6d30aed1bce7f3fd6567d7fb25bb218fd1
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-24 14:26:46 +0100

    fix: width

--- ui/app/(espace-pro)/espace-pro/(connected)/opco/layout.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/opco/layout.tsx
@@ -1,12 +1,12 @@
-import { fr } from "@codegouvfr/react-dsfr"
 import { Container } from "@chakra-ui/react"
+import { fr } from "@codegouvfr/react-dsfr"
 import type { PropsWithChildren } from "react"
 
 import { DepotSimplifieStyling } from "@/components/espace_pro/common/components/DepotSimplifieLayout"
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <Container maxW="xl" sx={{ marginTop: fr.spacing("4v") }}>
+    <Container maxW="container.xl" sx={{ marginTop: fr.spacing("4v") }}>
       <DepotSimplifieStyling>{children}</DepotSimplifieStyling>
     </Container>
   )
