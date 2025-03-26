7ad824beeeba1399e1c4902e469d939a9fbae74f
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-26 12:08:35 +0100

    feat: suppression optouts car obsolète

--- ui/app/(espace-pro)/espace-pro/authentification/optout/verification/page.tsx
+++
@@ -1,5 +0,0 @@
-import OptOutValidation from "@/app/(espace-pro)/espace-pro/authentification/optout/verification/OptoutValidation"
-
-export default function OptOutVerification() {
-  return <OptOutValidation />
-}
