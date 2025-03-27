ecc71320e5a3aea4711bccf7dc77118848383242
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-27 17:12:22 +0100

    feat: réduction hauteur breadcrumb

--- ui/app/_components/Breadcrumb.tsx
+++ ui/app/_components/Breadcrumb.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@chakra-ui/react"
+import { fr } from "@codegouvfr/react-dsfr"
 import { Breadcrumb as DSFRBreadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb"
 
 import type { IPage } from "@/utils/routes.utils"
@@ -11,6 +12,7 @@ export function Breadcrumb({ pages }: { pages: IPage[] }) {
   return (
     <Container variant="responsiveContainer" display="flex">
       <DSFRBreadcrumb
+        style={{ marginBottom: fr.spacing("4v"), marginTop: fr.spacing("4v") }}
         currentPageLabel={currentPage?.title}
         homeLinkProps={{
           href: PAGES.static.home.getPath(),
