80afa991b42c5590a2f419bfb9c9f125c3470e63
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-28 09:02:13 +0100

    fix: divers ui (#1964)
    * feat: accessibilité non conforme
    
    * feat: espacement listes offres footer
    
    * feat: remove header from print

--- ui/app/_components/Footer.tsx
+++ ui/app/_components/Footer.tsx
@@ -7,7 +7,7 @@ import { DsfrHeaderProps } from "./Header"
 export function Footer() {
   return (
     <DsfrFooter
-      accessibility="fully compliant"
+      accessibility="non compliant"
       contentDescription="La bonne alternance simplifie les mises en relation  entre les trois d’acteurs candidats, recruteurs et centres de formation, afin de faciliter les entrées en  alternance."
       operatorLogo={{
         alt: "France relance",
