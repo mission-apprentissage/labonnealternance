ecf871a88440a8bee77ae3f2474cc54e88b7c2af
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-03-31 14:43:46 +0200

    fix: recherche form decoration (#1983)

--- ui/app/(home)/_components/HomeCircleImageDecoration.tsx
+++ ui/app/(home)/_components/HomeCircleImageDecoration.tsx
@@ -1,27 +1,22 @@
-import { Box } from "@chakra-ui/react"
 import NextImage from "next/image"
 
-const circleImgCssProperties = {
-  position: "absolute",
-  zIndex: 0,
-}
+import motif from "./home_form_decoration.svg"
 
 export const HomeCircleImageDecoration = () => (
-  <>
-    <Box sx={circleImgCssProperties} top="60px" left="50px">
-      <NextImage fetchPriority="low" src="/images/howtocircle1.png" alt="" width={70} height={70} />
-    </Box>
-    <Box sx={circleImgCssProperties} bottom="-28px" left="444px">
-      <NextImage fetchPriority="low" src="/images/howtocircle2.png" alt="" width={56} height={56} />
-    </Box>
-    <Box sx={circleImgCssProperties} top="182px" right="512px">
-      <NextImage fetchPriority="low" src="/images/howtocircle3.png" alt="" width={80} height={80} />
-    </Box>
-    <Box sx={circleImgCssProperties} top="12px" right="312px">
-      <NextImage fetchPriority="low" src="/images/howtocircle4.png" alt="" width={50} height={50} />
-    </Box>
-    <Box sx={circleImgCssProperties} bottom="112px" right="-12px">
-      <NextImage fetchPriority="low" src="/images/howtocircle5.png" alt="" width={89} height={77} />
-    </Box>
-  </>
+  <NextImage
+    fetchPriority="low"
+    src={motif.src}
+    alt=""
+    unoptimized
+    height={motif.height}
+    width={motif.width}
+    style={{
+      overflow: "visible",
+      height: "calc(100% - 10px)",
+      width: "100%",
+      top: "20px",
+      position: "absolute",
+      objectFit: "cover",
+    }}
+  />
 )
