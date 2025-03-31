b2f247d8d7cdd28e25bb3b4b1807f286c9226701
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-03-31 11:15:03 +0200

    fix: lba-2455 correction parser Kelio (#1980)

--- server/src/jobs/offrePartenaire/kelio/kelioMapper.ts
+++ server/src/jobs/offrePartenaire/kelio/kelioMapper.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongodb"
-import { TRAINING_CONTRACT_TYPE } from "shared/constants"
+import { NIVEAU_DIPLOME_LABEL, TRAINING_CONTRACT_TYPE, TRAINING_REMOTE_TYPE } from "shared/constants"
 import dayjs from "shared/helpers/dayjs"
 import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
 import { IComputedJobsPartners, JOB_PARTNER_BUSINESS_ERROR } from "shared/models/jobsPartnersComputed.model"
@@ -57,7 +57,7 @@ export const ZKelioJob = z
 export type IKelioJob = z.output<typeof ZKelioJob>
 
 export const kelioJobToJobsPartners = (job: IKelioJob): IComputedJobsPartners => {
-  const { id, name, html_description, html_profile, address, url, company, created_at } = job
+  const { id, name, html_description, html_profile, address, url, company, created_at, job_type, contract_duration, job_start_date, education_level } = job
 
   const workplace_geopoint: {
     type: "Point"
@@ -65,6 +65,56 @@ export const kelioJobToJobsPartners = (job: IKelioJob): IComputedJobsPartners =>
   } = {
     type: "Point",
     coordinates: [parseFloat(address.longitude), parseFloat(address.latitude)],
+  }
+
+  let business_error: string | null = null
+  if (isCompanyInBlockedCfaList(company.name)) {
+    business_error = JOB_PARTNER_BUSINESS_ERROR.CFA
+  }
+
+  let contract_type: ("Apprentissage" | "Professionnalisation")[] = []
+  if (job_type === "Alternating") {
+    contract_type = [TRAINING_CONTRACT_TYPE.APPRENTISSAGE, TRAINING_CONTRACT_TYPE.PROFESSIONNALISATION]
+  } else if (job_type.toUpperCase() === "Professional Contract".toUpperCase()) {
+    contract_type = [TRAINING_CONTRACT_TYPE.PROFESSIONNALISATION]
+  } else if (job_type === "Internship") {
+    business_error = JOB_PARTNER_BUSINESS_ERROR.STAGE
+  }
+
+  let contract_remote: TRAINING_REMOTE_TYPE | null = null
+  switch (job.remote_level) {
+    case "unauthorized":
+    case "fulltime":
+      contract_remote = TRAINING_REMOTE_TYPE.onsite
+      break
+    case "punctual":
+    case "partial":
+      contract_remote = TRAINING_REMOTE_TYPE.hybrid
+      break
+    default:
+      contract_remote = null
+  }
+
+  let offer_target_diploma: { european: "3" | "4" | "5" | "6" | "7"; label: string } | null = null
+  switch (education_level) {
+    case "CAP/BEP":
+      offer_target_diploma = { european: "3", label: NIVEAU_DIPLOME_LABEL["3"] }
+      break
+    case "BAC":
+      offer_target_diploma = { european: "4", label: NIVEAU_DIPLOME_LABEL["4"] }
+      break
+    case "BAC +2":
+      offer_target_diploma = { european: "5", label: NIVEAU_DIPLOME_LABEL["5"] }
+      break
+    case "BAC +3":
+      offer_target_diploma = { european: "6", label: NIVEAU_DIPLOME_LABEL["6"] }
+      break
+    case "BAC +5":
+    case ">BAC +5":
+      offer_target_diploma = { european: "7", label: NIVEAU_DIPLOME_LABEL["7"] }
+      break
+    default:
+      offer_target_diploma = null
   }
 
   const urlParsing = z.string().url().safeParse(url)
@@ -94,8 +144,12 @@ export const kelioJobToJobsPartners = (job: IKelioJob): IComputedJobsPartners =>
     workplace_geopoint,
     apply_url: urlParsing.success ? urlParsing.data : null,
     offer_multicast: true,
-    contract_type: [TRAINING_CONTRACT_TYPE.APPRENTISSAGE, TRAINING_CONTRACT_TYPE.PROFESSIONNALISATION],
-    business_error: isCompanyInBlockedCfaList(company.name) ? JOB_PARTNER_BUSINESS_ERROR.CFA : null,
+    contract_type,
+    contract_start: new Date(job_start_date!),
+    contract_remote,
+    offer_target_diploma,
+    contract_duration: contract_duration ? parseInt(contract_duration) : null,
+    business_error,
   }
   return partnerJob
 }
