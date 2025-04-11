8c6b8a40588a54ff67ec9b54e807e90cfe7ece27
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-04-11 16:58:24 +0200

    feat: meilleure présentation

--- ui/components/espace_pro/ConfirmationActivationUtilisateur.tsx
+++ ui/components/espace_pro/ConfirmationActivationUtilisateur.tsx
@@ -1,11 +1,11 @@
-import { Box, Heading, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react"
+import { Box, Heading, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react"
 import Button from "@codegouvfr/react-dsfr/Button"
 
 import ModalCloseButton from "@/app/_components/ModalCloseButton"
 import { useUserPermissionsActions } from "@/common/hooks/useUserPermissionsActions"
 
 const ConfirmationActivationUtilisateur = (props) => {
-  const { isOpen, onClose, establishment_raison_social, _id } = props
+  const { isOpen, onClose, establishment_raison_sociale, _id } = props
   const { activate } = useUserPermissionsActions(_id)
 
   const activateUser = async () => {
@@ -19,15 +19,13 @@ const ConfirmationActivationUtilisateur = (props) => {
       <ModalContent mt={["0", "3.75rem"]} h={["100%", "auto"]} mb={0} borderRadius={0}>
         <ModalCloseButton onClose={onClose} />
         <ModalHeader>
-          <Heading as="h2" fontSize="1.5rem">
-            <Text>Activation du compte</Text>
+          <Heading as="h2" mb={0} fontSize="1.5rem">
+            Activation du compte
           </Heading>
         </ModalHeader>
         <ModalBody pb={6}>
-          <Text>
-            Vous êtes sur le point d’activer le compte de l’entreprise {establishment_raison_social}. Une fois le compte validé, l’entreprise pourra accéder au service de dépot
-            d’offres et modifier ses informations.
-          </Text>
+          Vous êtes sur le point d’activer le compte de l’entreprise {establishment_raison_sociale}. Une fois le compte validé, l’entreprise pourra accéder au service de dépot
+          d’offres et modifier ses informations.
         </ModalBody>
 
         <ModalFooter>
