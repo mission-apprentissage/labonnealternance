276217d4cb838c7579051e2044a2e44b8ae178e2
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-04-23 00:50:51 +0200

    feat: layout commun pour page rdva

--- ui/app/(rdva)/rdva/page.tsx
+++ ui/app/(rdva)/rdva/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
-import { Box, Spinner, Text } from "@chakra-ui/react"
+import { Box, Container, Flex, Spinner, Text } from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 import { useSearchParams } from "next/navigation"
 
 import { ContactCfaSummary } from "@/components/espace_pro/Candidat/layout/ContactCfaSummary"
 import DemandeDeContact from "@/components/RDV/DemandeDeContact"
+import { IconeLogo } from "@/theme/components/icons"
 import { getPrdvContext } from "@/utils/api"
-
-import { FormLayoutComponent } from "../../../components/espace_pro/Candidat/layout/FormLayoutComponent"
 
 /**
  * Appointment form page.
@@ -25,18 +24,25 @@ export default function PriseDeRendezVous() {
   })
 
   return (
-    <FormLayoutComponent
-      headerText={
-        <>
-          Envoyer <br />
-          une demande de contact <br />
-          <Text textStyle="h2" as="span" color="grey.700">
-            au centre de formation
-          </Text>
-        </>
-      }
-      bg="white"
-    >
+    <Container maxWidth="82ch" mt={5}>
+      <Box bg="#F9F8F6">
+        <Flex alignItems="center" flexDirection={["column", "column", "row"]}>
+          <Box flex="1" ml={["0", "0", "6em"]}>
+            <Flex flexDirection={["column", "column", "row"]} mt={[7, 0, 0]}>
+              <Text fontSize="2rem" fontWeight="bold" lineHeight="2.5rem" color="info">
+                Envoyer <br />
+                une demande de contact <br />
+                <Text textStyle="h2" as="span" color="grey.700">
+                  au centre de formation
+                </Text>
+              </Text>
+            </Flex>
+          </Box>
+          <Box mr="2rem" mt={8}>
+            <IconeLogo w={["0px", "0px", "300px"]} h={["0px", "0px", "174px"]} />
+          </Box>
+        </Flex>
+      </Box>
       {isLoading && <Spinner display="block" mx="auto" size="xl" mt="10rem" />}
       {data && "error" in data && (
         <Box mt="5rem" textAlign="center">
@@ -56,6 +62,6 @@ export default function PriseDeRendezVous() {
           <DemandeDeContact context={data} referrer={referrer} showInModal={false} />
         </>
       )}
-    </FormLayoutComponent>
+    </Container>
   )
 }
