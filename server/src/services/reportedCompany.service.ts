7306e44970c08ff255cbc9d0592fdbab0a02d6d4
Rémy Auricoste <remy.auricoste@gmail.com>
2025-04-29 17:16:19 +0200

    fix(lbac-2536): reported companies : fix remplissage du partner label

--- server/src/services/reportedCompany.service.ts
+++ server/src/services/reportedCompany.service.ts
@@ -1,8 +1,11 @@
 import { ObjectId } from "mongodb"
+import { BusinessErrorCodes } from "shared/constants/errorCodes"
 import { LBA_ITEM_TYPE } from "shared/constants/lbaitem"
 import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
 
 import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { getSiretInfos } from "@/services/cacheInfosSiret.service"
+import { formatEntrepriseData } from "@/services/etablissement.service"
 import { getOffre } from "@/services/formulaire.service"
 
 export const reportCompany = async ({ reason, reasonDetails, itemId, type }: { reason: string; reasonDetails?: string; itemId: string; type: LBA_ITEM_TYPE }) => {
@@ -32,16 +35,29 @@ export const getReportAdditionalInfos = async (itemId: string, type: LBA_ITEM_TY
         siret: establishment_siret,
         companyName: establishment_raison_sociale || establishment_enseigne,
         jobTitle: offer_title_custom ?? rome_appellation_label,
+        partnerLabel: JOBPARTNERS_LABEL.OFFRES_EMPLOI_LBA,
       }
     }
     case LBA_ITEM_TYPE.RECRUTEURS_LBA: {
       const recruteur = await getDbCollection("jobs_partners").findOne({ workplace_siret: itemId, partner_label: JOBPARTNERS_LABEL.RECRUTEURS_LBA })
-      if (!recruteur) return null
-      const { workplace_siret, workplace_legal_name, workplace_name } = recruteur
-      return {
-        siret: workplace_siret,
-        companyName: workplace_legal_name || workplace_name,
+      if (recruteur) {
+        const { workplace_siret, workplace_legal_name, workplace_name } = recruteur
+        return {
+          siret: workplace_siret,
+          companyName: workplace_legal_name || workplace_name,
+          partnerLabel: JOBPARTNERS_LABEL.RECRUTEURS_LBA,
+        }
       }
+      const siretInfos = await getSiretInfos(itemId)
+      if (siretInfos && siretInfos !== BusinessErrorCodes.NON_DIFFUSIBLE) {
+        const { establishment_siret, establishment_raison_sociale, establishment_enseigne } = formatEntrepriseData(siretInfos.data)
+        return {
+          siret: establishment_siret,
+          partnerLabel: JOBPARTNERS_LABEL.RECRUTEURS_LBA,
+          companyName: establishment_enseigne ?? establishment_raison_sociale,
+        }
+      }
+      return null
     }
     case LBA_ITEM_TYPE.OFFRES_EMPLOI_PARTENAIRES: {
       const jobOpt = await getDbCollection("jobs_partners").findOne({ _id: new ObjectId(itemId) })
