7d0977568bcceb5bf5ebff6999e43d3328f996fc
Rémy Auricoste <remy.auricoste@gmail.com>
2025-05-05 17:46:07 +0200

    fix(lbac-2688): redirection des anciennes urls de détail des offres

--- server/src/jobs/simpleJobDefinitions.ts
+++ server/src/jobs/simpleJobDefinitions.ts
@@ -7,6 +7,7 @@ import { processMeteojob } from "@/jobs/offrePartenaire/meteojob/processMeteojob
 import { processPass } from "@/jobs/offrePartenaire/pass/processPass"
 import { processRecruteursLba } from "@/jobs/offrePartenaire/recruteur-lba/processRecruteursLba"
 import { processRhAlternance } from "@/jobs/offrePartenaire/rh-alternance/processRhAlternance"
+import { exportJobsToFranceTravail } from "@/jobs/partenaireExport/exportToFranceTravail"
 import { processScheduledRecruiterIntentions } from "@/services/application.service"
 import { generateSitemap } from "@/services/sitemap.service"
 
@@ -305,5 +306,9 @@ export const simpleJobDefinitions: SimpleJobDefinition[] = [
   {
     fct: classifyRomesForDomainesMetiersAnalyze,
     description: "Analyse les fichiers de sortie de classifyRomesForDomainesMetiers",
+  },
+  {
+    fct: exportJobsToFranceTravail,
+    description: "Envoie les offres LBA à France Travail",
   },
 ]
