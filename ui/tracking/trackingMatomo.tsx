a2ebf3fab24ed026c7512492e25e6f39e09df12e
Kevin Barnoin <k.barnoin@gmail.com>
2025-05-13 15:45:34 +0200

    fix: desactivation matomo sur widget (#2092)

--- ui/tracking/trackingMatomo.tsx
+++ ui/tracking/trackingMatomo.tsx
@@ -1,20 +1,23 @@
 import Script from "next/script"
 
 import { publicConfig } from "@/config.public"
+import { isWidget } from "@/utils/isWidget.utils"
 
 export const Matomo = () => {
-  return (
-    <Script id="matomo" strategy="afterInteractive">
-      {`
+  if (!isWidget()) {
+    return (
+      <Script id="matomo" strategy="afterInteractive">
+        {`
       var _mtm = window._mtm = window._mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
       (function() {
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.async=true; g.src='${publicConfig.matomo.url}/${publicConfig.matomo.jsTrackerFile}'; s.parentNode.insertBefore(g,s);
-      })();
-      `}
-    </Script>
-  )
+        })();
+        `}
+      </Script>
+    )
+  }
 }
 
 // keep it until heatmaps are up and running with the code above
