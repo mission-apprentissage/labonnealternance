265b1386bf77b598b67d13587a25b848f9aec65b
Rémy Auricoste <remy.auricoste@gmail.com>
2025-05-15 09:46:10 +0200

    fix(lbac-2622): ajout de logs + amelioration partner labels (#2042)
    * fix(lbac-2622): ajout de logs + amelioration partner labels
    
    * fix: changement des partenaires

--- server/src/jobs/offrePartenaire/processJobPartnersForApi.ts
+++ server/src/jobs/offrePartenaire/processJobPartnersForApi.ts
@@ -1,20 +1,22 @@
 import { ObjectId } from "mongodb"
+import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
 
+import { logger } from "@/common/logger"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 import { fillComputedJobsPartners } from "./fillComputedJobsPartners"
 import { importFromComputedToJobsPartners } from "./importFromComputedToJobsPartners"
-import { jobPartnersByFlux } from "./processJobPartners"
+
+const excludedJobPartnersFromApi = Object.values(JOBPARTNERS_LABEL)
 
 export const processJobPartnersForApi = async () => {
+  logger.info("début de processJobPartnersForApi")
   const processId: string = new ObjectId().toString()
-  await getDbCollection("computed_jobs_partners").updateMany(
-    { partner_label: { $nin: jobPartnersByFlux }, currently_processed_id: null },
-    { $set: { currently_processed_id: processId } }
-  )
+  await getDbCollection("computed_jobs_partners").updateMany({ partner_label: { $nin: excludedJobPartnersFromApi } }, { $set: { currently_processed_id: processId } })
   const filter = { currently_processed_id: processId }
   await fillComputedJobsPartners(filter)
   await importFromComputedToJobsPartners(filter)
   await getDbCollection("computed_jobs_partners").deleteMany({ $and: [filter, { validated: true }] })
   await getDbCollection("computed_jobs_partners").updateMany(filter, { $set: { currently_processed_id: null } })
+  logger.info("fin de processJobPartnersForApi")
 }
