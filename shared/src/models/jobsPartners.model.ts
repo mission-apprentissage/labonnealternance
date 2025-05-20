71c256d11627f761b6d3eb3501765c881fb6a141
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-05-20 09:29:33 +0200

    feat: ajouts autres partenaires

--- shared/src/models/jobsPartners.model.ts
+++ shared/src/models/jobsPartners.model.ts
@@ -28,6 +28,12 @@ export enum JOBPARTNERS_LABEL {
   KELIO = "Kelio",
   VERITONE = "Veritone",
   GRDF = "GRDF",
+  DAHER = "Daher",
+  LOREAL = "L'Oréal",
+  CREDIT_MUTUEL = "Crédit Mutuel",
+  BPCE = "BPCE",
+  DECATHLON = "Decathlon",
+  INSTITUT_PASTEUR = "Institut Pasteur",
 }
 
 export const jobPartnersExcludedFromFlux = [JOBPARTNERS_LABEL.OFFRES_EMPLOI_LBA, JOBPARTNERS_LABEL.RECRUTEURS_LBA]
