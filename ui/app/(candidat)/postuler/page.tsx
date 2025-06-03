2bb591eba1f84f1a6ba4c9b591b9c6eb2cf19427
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-06-03 10:02:40 +0200

    feat: suppression anciennes versions

--- ui/app/(candidat)/postuler/page.tsx
+++ ui/app/(candidat)/postuler/page.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query"
 import { useSearchParams } from "next/navigation"
 import { ILbaItemLbaCompanyJson, ILbaItemLbaJobJson, ILbaItemPartnerJobJson } from "shared"
 import { BusinessErrorCodes } from "shared/constants/errorCodes"
-import { LBA_ITEM_TYPE, LBA_ITEM_TYPE_OLD } from "shared/constants/lbaitem"
+import { LBA_ITEM_TYPE } from "shared/constants/lbaitem"
 
 import WidgetCandidatureLba from "@/components/ItemDetail/CandidatureLba/WidgetCandidatureLba"
 import { WidgetPostulerError } from "@/components/ItemDetail/CandidatureLba/WidgetPostulerError"
@@ -20,15 +20,12 @@ export default function WidgetPostuler() {
 
   const fetchPostulerItem = (parameters) => {
     switch (parameters.type) {
-      case LBA_ITEM_TYPE_OLD.MATCHA: // To remove when 1J1S switched to V2
       case LBA_ITEM_TYPE.OFFRES_EMPLOI_LBA: {
         return fetchLbaJobDetails({ id: parameters.itemId })
       }
-      case LBA_ITEM_TYPE_OLD.LBA: // To remove when 1J1S switched to V2
       case LBA_ITEM_TYPE.RECRUTEURS_LBA: {
         return fetchLbaCompanyDetails({ id: parameters.itemId })
       }
-      case LBA_ITEM_TYPE_OLD.PARTNER_JOB: // To remove when 1J1S switched to V2
       case LBA_ITEM_TYPE.OFFRES_EMPLOI_PARTENAIRES: {
         return fetchPartnerJobDetails({ id: parameters.itemId })
       }
