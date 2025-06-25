b870a6ff8c2bca10398d64d7ec45eb10b215bac4
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-06-25 15:14:05 +0200

    feat: traitement compteur candidature ok

--- server/src/services/partnerJob.service.ts
+++ server/src/services/partnerJob.service.ts
@@ -215,7 +215,7 @@ export const getPartnerJobs = async ({
     let applicationCountByJob: null | IApplicationCount[] = null
 
     if (force_partner_label === JOBPARTNERS_LABEL.OFFRES_EMPLOI_LBA) {
-      const ids = rawPartnerJobs.map(({ _id }) => _id.toString())
+      const ids = rawPartnerJobs.map(({ partner_job_id }) => partner_job_id)
       applicationCountByJob = await getApplicationByJobCount(ids)
     }
     let partnerJobs: ILbaItemPartnerJob[] = transformPartnerJobs({ partnerJobs: rawPartnerJobs, isMinimalData, applicationCountByJob })
