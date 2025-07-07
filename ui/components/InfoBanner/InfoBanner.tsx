e3e3f3403ec8ab2810750c4c2e637338004f0d99
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-07-07 17:32:51 +0200

    feat: boutons dsfr

--- ui/components/InfoBanner/InfoBanner.tsx
+++ ui/components/InfoBanner/InfoBanner.tsx
@@ -1,71 +1,55 @@
-import { WarningIcon, CheckCircleIcon, InfoIcon, CloseIcon } from "@chakra-ui/icons"
-import { Box, Button, Flex, Link, Text } from "@chakra-ui/react"
+import { WarningIcon, CheckCircleIcon, InfoIcon } from "@chakra-ui/icons"
+import Button from "@codegouvfr/react-dsfr/Button"
+import { Box, Link, Typography } from "@mui/material"
 import React, { useContext } from "react"
 
 import { publicConfig } from "@/config.public"
 import { DisplayContext } from "@/context/DisplayContextProvider"
 
 const blueBannerText = (
-  <Text>
-    <Text as="span" fontWeight={700}>
-      Souhaiteriez-vous accéder à toutes vos candidatures depuis votre espace connecté ?{" "}
-    </Text>
-    <Link textDecoration="underline" href="https://tally.so/r/3Xq19z" isExternal aria-label="Accéder au formulaire - nouvelle fenêtre">
+  <Typography>
+    <Typography sx={{ display: "inline", fontWeight: 700 }}>Souhaiteriez-vous accéder à toutes vos candidatures depuis votre espace connecté ? </Typography>
+    <Link underline="always" href="https://tally.so/r/3Xq19z" color="inherit" aria-label="Accéder au formulaire - nouvelle fenêtre" target="_blank" rel="noopener noreferrer">
       Donnez-nous votre avis en 3 clics.
     </Link>
-  </Text>
+  </Typography>
 )
 
 const greenBannerText = (
-  <Text>
-    <Text as="span" fontWeight={700}>
-      Problème résolu.
-    </Text>{" "}
-    Le service est à nouveau fonctionnel, merci d’avoir patienté.
-  </Text>
+  <Typography>
+    <Typography sx={{ display: "inline", fontWeight: 700 }}>Problème résolu.</Typography> Le service est à nouveau fonctionnel, merci d’avoir patienté.
+  </Typography>
 )
 
 const redBannerText = (
-  <Text>
-    <Text as="span" fontWeight={700}>
-      Service temporairement dégradé.
-    </Text>{" "}
-    Suite à un problème chez notre prestataire d'envoi d'emails, les envois de candidatures sont momentanément interrompus. Nous vous prions de nous excuser pour la gêne
-    occasionnée et vous invitons à revenir ultérieurement. {/*<Link textDecoration="underline">En savoir plus</Link>*/}
-  </Text>
+  <Typography>
+    <Typography sx={{ display: "inline", fontWeight: 700 }}>Service temporairement dégradé.</Typography> Suite à un problème chez notre prestataire d'envoi d'emails, les envois de
+    candidatures sont momentanément interrompus. Nous vous prions de nous excuser pour la gêne occasionnée et vous invitons à revenir ultérieurement.{" "}
+    {/*<Link textDecoration="underline">En savoir plus</Link>*/}
+  </Typography>
 )
 
 const envBannerText = (
-  <Text>
-    <Text as="span" fontWeight={700}>
-      Vous êtes ici sur un site de test !
-    </Text>{" "}
-    Vos candidatures ne seront pas transmises aux recruteurs ni aux centres de formation. Pour accéder au site La bonne alternance, veuillez cliquer sur ce lien :{" "}
-    <Link href="https://labonnealternance.apprentissage.beta.gouv.fr" textDecoration="underline">
+  <Typography>
+    <Typography sx={{ display: "inline", fontWeight: 700 }}>Vous êtes ici sur un site de test !</Typography> Vos candidatures ne seront pas transmises aux recruteurs ni aux centres
+    de formation. Pour accéder au site La bonne alternance, veuillez cliquer sur ce lien :{" "}
+    <Link href="https://labonnealternance.apprentissage.beta.gouv.fr" color="inherit" underline="always" target="_blank" rel="noopener noreferrer">
       La bonne alternance - Trouvez votre alternance
     </Link>
-  </Text>
+  </Typography>
 )
 
 const getCloseButton = (setBannerContext: any, isClosedStateName: string, color: string) => {
   return (
     <Button
-      fontWeight={700}
-      background="none"
-      alignItems="baseline"
-      height="1.5rem"
-      sx={{
-        _hover: {
-          background: "none",
-          textDecoration: "none",
-        },
-        _active: {
-          background: "none",
-        },
-      }}
+      style={{ color }}
+      type="button"
+      priority="tertiary no outline"
+      iconId="fr-icon-close-line"
+      iconPosition="right"
       onClick={() => setBannerContext(isClosedStateName, true)}
     >
-      <CloseIcon color={color} w={2} h={2} mt={2} ml={2} />
+      {" "}
     </Button>
   )
 }
@@ -97,39 +81,39 @@ const InfoBanner = ({
   return (
     <>
       {showEnvAlert && env !== "production" && (forceEnvBanner || !isEnvClosed) && (
-        <Box backgroundColor="#FFE9E6" p={2} mb={1}>
-          <Flex alignItems="center-start" maxWidth="1310px" margin="auto" color="#B34000">
+        <Box sx={{ backgroundColor: "#FFE9E6" }} p={2} mb={1}>
+          <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#B34000" }}>
             <WarningIcon mr={4} mt={1} />
             <Box flexGrow={1}>{envBannerText}</Box>
             {!forceEnvBanner && getCloseButton(setBannerContext, "isEnvClosed", "#B34000")}
-          </Flex>
+          </Box>
         </Box>
       )}
       {!isAlertClosed && showAlert && (
-        <Box backgroundColor="#FFE9E6" p={2} mb={1}>
-          <Flex alignItems="center-start" maxWidth="1310px" margin="auto" color="#B34000">
+        <Box sx={{ backgroundColor: "#FFE9E6" }} p={2} mb={1}>
+          <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#B34000" }}>
             <WarningIcon mr={4} mt={1} />
             <Box flexGrow={1}>{redBannerText}</Box>
             {getCloseButton(setBannerContext, "isAlertClosed", "#B34000")}
-          </Flex>
+          </Box>
         </Box>
       )}
       {!isOKClosed && showOK && (
-        <Box backgroundColor="#B8FEC9" p={2} mb={1}>
-          <Flex alignItems="center-start" maxWidth="1310px" margin="auto" color="#18753C">
+        <Box sx={{ backgroundColor: "#B8FEC9" }} p={2} mb={1}>
+          <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#18753C" }}>
             <CheckCircleIcon mr={4} mt={1} />
             <Box flexGrow={1}>{greenBannerText}</Box>
             {getCloseButton(setBannerContext, "isOKClosed", "#18753C")}
-          </Flex>
+          </Box>
         </Box>
       )}
       {!isInfoClosed && showInfo && (
-        <Box backgroundColor="#E8EDFF" p={2} mb={1}>
-          <Flex alignItems="center-start" maxWidth="1310px" margin="auto" color="#0063CB">
+        <Box sx={{ backgroundColor: "#E8EDFF" }} p={2} mb={1}>
+          <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#0063CB" }}>
             <InfoIcon mr={4} mt={1} />
             <Box flexGrow={1}>{blueBannerText}</Box>
             {getCloseButton(setBannerContext, "isInfoClosed", "#0063CB")}
-          </Flex>
+          </Box>
         </Box>
       )}
     </>
