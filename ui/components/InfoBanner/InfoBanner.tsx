b82c5c547a3519a41341e702eeaff2d4cd679bdf
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-07-08 15:48:48 +0200

    feat: utilisation icon remixicon

--- ui/components/InfoBanner/InfoBanner.tsx
+++ ui/components/InfoBanner/InfoBanner.tsx
@@ -1,4 +1,4 @@
-import { WarningIcon, CheckCircleIcon, InfoIcon } from "@chakra-ui/icons"
+import { fr } from "@codegouvfr/react-dsfr"
 import Button from "@codegouvfr/react-dsfr/Button"
 import { Box, Link, Typography } from "@mui/material"
 import React, { useContext } from "react"
@@ -8,7 +8,9 @@ import { DisplayContext } from "@/context/DisplayContextProvider"
 
 const blueBannerText = (
   <Typography>
-    <Typography sx={{ display: "inline", fontWeight: 700 }}>Souhaiteriez-vous accéder à toutes vos candidatures depuis votre espace connecté ? </Typography>
+    <Typography component="span" sx={{ fontWeight: 700 }}>
+      Souhaiteriez-vous accéder à toutes vos candidatures depuis votre espace connecté ?{" "}
+    </Typography>
     <Link underline="always" href="https://tally.so/r/3Xq19z" color="inherit" aria-label="Accéder au formulaire - nouvelle fenêtre" target="_blank" rel="noopener noreferrer">
       Donnez-nous votre avis en 3 clics.
     </Link>
@@ -17,22 +19,29 @@ const blueBannerText = (
 
 const greenBannerText = (
   <Typography>
-    <Typography sx={{ display: "inline", fontWeight: 700 }}>Problème résolu.</Typography> Le service est à nouveau fonctionnel, merci d’avoir patienté.
+    <Typography component="span" sx={{ fontWeight: 700 }}>
+      Problème résolu.
+    </Typography>{" "}
+    Le service est à nouveau fonctionnel, merci d’avoir patienté.
   </Typography>
 )
 
 const redBannerText = (
-  <Typography>
-    <Typography sx={{ display: "inline", fontWeight: 700 }}>Service temporairement dégradé.</Typography> Suite à un problème chez notre prestataire d'envoi d'emails, les envois de
-    candidatures sont momentanément interrompus. Nous vous prions de nous excuser pour la gêne occasionnée et vous invitons à revenir ultérieurement.{" "}
-    {/*<Link textDecoration="underline">En savoir plus</Link>*/}
+  <Typography component="span">
+    <Typography component="span" sx={{ fontWeight: 700 }}>
+      Service temporairement dégradé.
+    </Typography>{" "}
+    Suite à un problème chez notre prestataire d'envoi d'emails, les envois de candidatures sont momentanément interrompus. Nous vous prions de nous excuser pour la gêne
+    occasionnée et vous invitons à revenir ultérieurement. {/*<Link textDecoration="underline">En savoir plus</Link>*/}
   </Typography>
 )
 
 const envBannerText = (
   <Typography>
-    <Typography sx={{ display: "inline", fontWeight: 700 }}>Vous êtes ici sur un site de test !</Typography> Vos candidatures ne seront pas transmises aux recruteurs ni aux centres
-    de formation. Pour accéder au site La bonne alternance, veuillez cliquer sur ce lien :{" "}
+    <Typography component="span" sx={{ fontWeight: 700 }}>
+      Vous êtes ici sur un site de test !
+    </Typography>{" "}
+    Vos candidatures ne seront pas transmises aux recruteurs ni aux centres de formation. Pour accéder au site La bonne alternance, veuillez cliquer sur ce lien :{" "}
     <Link href="https://labonnealternance.apprentissage.beta.gouv.fr" color="inherit" underline="always" target="_blank" rel="noopener noreferrer">
       La bonne alternance - Trouvez votre alternance
     </Link>
@@ -83,8 +92,10 @@ const InfoBanner = ({
       {showEnvAlert && env !== "production" && (forceEnvBanner || !isEnvClosed) && (
         <Box sx={{ backgroundColor: "#FFE9E6" }} p={2} mb={1}>
           <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#B34000" }}>
-            <WarningIcon mr={4} mt={1} />
-            <Box flexGrow={1}>{envBannerText}</Box>
+            <Typography className={fr.cx("ri-error-warning-fill", "fr-icon--sm")} />
+            <Box sx={{ ml: fr.spacing("2w") }} flexGrow={1}>
+              {envBannerText}
+            </Box>
             {!forceEnvBanner && getCloseButton(setBannerContext, "isEnvClosed", "#B34000")}
           </Box>
         </Box>
@@ -92,8 +103,10 @@ const InfoBanner = ({
       {!isAlertClosed && showAlert && (
         <Box sx={{ backgroundColor: "#FFE9E6" }} p={2} mb={1}>
           <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#B34000" }}>
-            <WarningIcon mr={4} mt={1} />
-            <Box flexGrow={1}>{redBannerText}</Box>
+            <Typography className={fr.cx("ri-error-warning-fill", "fr-icon--sm")} />
+            <Box sx={{ ml: fr.spacing("2w") }} flexGrow={1}>
+              {redBannerText}
+            </Box>
             {getCloseButton(setBannerContext, "isAlertClosed", "#B34000")}
           </Box>
         </Box>
@@ -101,8 +114,10 @@ const InfoBanner = ({
       {!isOKClosed && showOK && (
         <Box sx={{ backgroundColor: "#B8FEC9" }} p={2} mb={1}>
           <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#18753C" }}>
-            <CheckCircleIcon mr={4} mt={1} />
-            <Box flexGrow={1}>{greenBannerText}</Box>
+            <Typography className={fr.cx("ri-checkbox-circle-fill", "fr-icon--sm")} />
+            <Box sx={{ ml: fr.spacing("2w") }} flexGrow={1}>
+              {greenBannerText}
+            </Box>
             {getCloseButton(setBannerContext, "isOKClosed", "#18753C")}
           </Box>
         </Box>
@@ -110,8 +125,10 @@ const InfoBanner = ({
       {!isInfoClosed && showInfo && (
         <Box sx={{ backgroundColor: "#E8EDFF" }} p={2} mb={1}>
           <Box style={{ display: "flex", alignItems: "center-start", maxWidth: "1310px", margin: "auto", color: "#0063CB" }}>
-            <InfoIcon mr={4} mt={1} />
-            <Box flexGrow={1}>{blueBannerText}</Box>
+            <Typography className={fr.cx("ri-information-fill", "fr-icon--sm")} />
+            <Box sx={{ ml: fr.spacing("2w") }} flexGrow={1}>
+              {blueBannerText}
+            </Box>
             {getCloseButton(setBannerContext, "isInfoClosed", "#0063CB")}
           </Box>
         </Box>
