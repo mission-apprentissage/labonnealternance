69d4d9efd176532e35051b8479d6cf727bd7ef5e
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-09 11:32:26 +0200

    fix: follow linkedIn

--- ui/app/(espace-pro)/_components/FollowLinkedIn.tsx
+++ ui/app/(espace-pro)/_components/FollowLinkedIn.tsx
@@ -1,24 +1,25 @@
 import { fr } from "@codegouvfr/react-dsfr"
+import Button from "@codegouvfr/react-dsfr/Button"
 import { Grid2 as Grid, Typography } from "@mui/material"
-
-import { DsfrLink } from "../../../components/dsfr/DsfrLink"
 
 export const FollowLinkedIn = () => {
   return (
-    <Grid container spacing={2} sx={{ alignItems: "center", justifyContent: "center", p: 1 }}>
-      <Grid size={{ xs: 12, md: 8 }}>
+    <Grid container spacing={fr.spacing("15w")} sx={{ alignItems: "center", justifyContent: "center" }}>
+      <Grid size={{ xs: 12, md: 9 }}>
         <Typography>La mission ministérielle pour l’apprentissage construit des services numériques qui facilitent les entrées en apprentissage.</Typography>
-        <Typography component={"h2"} variant="h2" sx={{ color: fr.colors.decisions.text.actionHigh.blueFrance.default }}>
+        <Typography sx={{ fontSize: "32px", fontWeight: "700", color: fr.colors.decisions.text.actionHigh.blueFrance.default }}>
           Rendez-vous sur LinkedIn pour suivre nos actualités
         </Typography>
       </Grid>
-      <Grid size={{ xs: 12, md: 4 }}>
-        <DsfrLink
-          href="https://www.linkedin.com/company/mission-apprentissage/posts/?feedView=all"
-          aria-label="Accès à la page Linkedin de la mission ministérielle pour l’apprentissage - nouvelle fenêtre"
+      <Grid size={{ xs: 12, md: 3 }}>
+        <Button
+          linkProps={{ href: "https://www.linkedin.com/company/mission-apprentissage/posts/?feedView=all", target: "_blank" }}
+          priority="primary"
+          iconId="ri-linkedin-box-fill"
+          iconPosition="right"
         >
           Voir notre page
-        </DsfrLink>
+        </Button>
       </Grid>
     </Grid>
   )
