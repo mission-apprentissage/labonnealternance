ad617a30c0a0502f67439116f28977e396b12d17
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-15 17:21:27 +0200

    feat: adapt job & source label

--- server/src/jobs/offrePartenaire/formatTextFieldsJobsPartners.ts
+++ server/src/jobs/offrePartenaire/formatTextFieldsJobsPartners.ts
@@ -1,3 +1,4 @@
+import { Filter } from "mongodb"
 import { COMPUTED_ERROR_SOURCE, IComputedJobsPartners } from "shared/models/jobsPartnersComputed.model"
 
 import { sanitizeTextField } from "@/common/utils/stringUtils"
@@ -8,22 +9,25 @@ import { fillFieldsForPartnersFactory } from "./fillFieldsForPartnersFactory"
 const fields = ["workplace_description", "workplace_name", "offer_description", "offer_title"] as const satisfies (keyof IComputedJobsPartners)[]
 
 export const formatTextFieldsJobsPartners = async ({ addedMatchFilter }: FillComputedJobsPartnersContext) => {
+  const filters: Filter<IComputedJobsPartners>[] = [{ workplace_description: { $ne: null } }, { workplace_name: { $ne: null } }, { offer_description: { $ne: null } }]
+  if (addedMatchFilter) filters.push(addedMatchFilter)
+
   return fillFieldsForPartnersFactory({
-    job: COMPUTED_ERROR_SOURCE.BLOCK_BAD_ROME,
+    job: COMPUTED_ERROR_SOURCE.SANITIZE_TEXT_FIELDS,
     sourceFields: ["offer_title"],
     filledFields: fields,
     groupSize: 500,
     addedMatchFilter,
-    replaceMatchFilter: [{ $or: { workplace_description: { $ne: null }, workplace_name: { $ne: null }, offer_description: { $ne: null } } }],
+    replaceMatchFilter: { $and: filters },
     getData: async (documents) => {
       return documents.map((document) => {
         const { _id, workplace_description, offer_description, offer_title, workplace_name } = document
         const result: Pick<IComputedJobsPartners, (typeof fields)[number] | "_id"> = {
           _id,
-          workplace_description: sanitizeTextField(workplace_description),
-          workplace_name: sanitizeTextField(workplace_name),
-          offer_description: sanitizeTextField(offer_description),
-          offer_title: sanitizeTextField(offer_title),
+          workplace_description: sanitizeTextField(workplace_description, true),
+          workplace_name: sanitizeTextField(workplace_name, true),
+          offer_description: sanitizeTextField(offer_description, true),
+          offer_title: sanitizeTextField(offer_title, true),
         }
         return result
       })
