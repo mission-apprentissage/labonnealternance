a6df8685d7ddf3e45084b5aa2eddaf515abc75e0
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-16 16:54:45 +0200

    fix(lbac-2732): apply general formatting policy for all offers (#2201)
    * fix: apply general formatting policy for all offers
    
    * fix: use correct fn
    
    * feat: add migration & missing field
    
    * feat: update following review
    
    * fix: format place as first step
    
    * feat: update sanitizeTextField global function & test
    
    * feat: enable html to be displayed in job offers
    
    * feat: adapt job & source label
    
    * fix: update migration
    
    * fix: migration script
    
    * fix: tests cases

---
+++ server/src/migrations/20250707103530-jobs-text-formating.ts
@@ -0,0 +1,26 @@
+import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
+
+import { asyncForEach } from "@/common/utils/asyncUtils"
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { sanitizeTextField } from "@/common/utils/stringUtils"
+
+export const up = async () => {
+  const jobs = await getDbCollection("jobs_partners")
+    .find(
+      { $or: [{ workplace_description: { $ne: null } }, { workplace_name: { $ne: null } }], partner_label: { $ne: JOBPARTNERS_LABEL.RECRUTEURS_LBA } },
+      { projection: { _id: 1, workplace_description: 1, workplace_name: 1, offer_description: 1, offer_title: 1 } }
+    )
+    .toArray()
+  asyncForEach(jobs, async (job) => {
+    const result = {
+      workplace_description: sanitizeTextField(job.workplace_description, true),
+      workplace_name: sanitizeTextField(job.workplace_name, true),
+      offer_description: sanitizeTextField(job.offer_description, true),
+      offer_title: sanitizeTextField(job.offer_title, true),
+    }
+    await getDbCollection("jobs_partners").updateOne({ _id: job._id }, { $set: result })
+  })
+}
+
+// set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
+export const requireShutdown: boolean = false
