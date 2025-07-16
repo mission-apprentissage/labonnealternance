48cae3a033f9a5da071bfdfa2cbbbbdc7ba7a1fc
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-16 16:55:31 +0200

    feat: use lba-lab to classify job offers

---
+++ server/src/common/apis/classification/classification.client.test.ts
@@ -0,0 +1,38 @@
+import nock from "nock"
+import { generateJobsPartnersFull } from "shared/fixtures/jobPartners.fixture"
+import { IClassificationLabResponse } from "shared/models/cacheClassification.model"
+import { describe, expect, it, vi } from "vitest"
+
+import { nockLabClassification } from "@/common/apis/classification/classification.client.fixture"
+import { getClassificationFromLab, TJobClassification } from "@/services/cacheClassification.service"
+import { useMongo } from "@tests/utils/mongo.test.utils"
+
+useMongo()
+
+vi.mock("@/common/utils/sentryUtils")
+
+describe("getLabClassification", () => {
+  const jobFixture = generateJobsPartnersFull({
+    workplace_name: "CFA",
+    workplace_description: "CFA",
+    offer_title: "Software Engineer",
+    offer_description: "Software Engineer",
+  })
+  const payload: TJobClassification = {
+    partner_job_id: jobFixture.partner_job_id,
+    partner_label: jobFixture.partner_label,
+    workplace_name: jobFixture.workplace_name!,
+    workplace_description: jobFixture.workplace_description!,
+    offer_title: jobFixture.offer_title,
+    offer_description: jobFixture.offer_description,
+  }
+  const apiPayload = [jobFixture.workplace_name, jobFixture.workplace_description, jobFixture.offer_title, jobFixture.offer_description].filter(Boolean).join("\n")
+  const apiResponse: IClassificationLabResponse = { label: "cfa", score: { cfa: 0.5, entreprise: 0.4, entreprise_cfa: 0.2 }, text: "Software Engineer" }
+
+  it("should request labonnealternance lab for a classification", async () => {
+    nockLabClassification(apiPayload, apiResponse)
+
+    expect(await getClassificationFromLab(payload)).toEqual(apiResponse.label)
+    expect(nock.isDone()).toBe(true)
+  })
+})
---
+++ server/src/services/cacheClassification.service.ts
@@ -0,0 +1,36 @@
+import { IClassificationJobsPartners } from "shared/models/cacheClassification.model"
+
+import { getLabClassification } from "@/common/apis/classification/classification.client"
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+
+export type TJobClassification = {
+  partner_label: string
+  partner_job_id: string
+  workplace_name: string
+  workplace_description: string
+  offer_title: string
+  offer_description: string
+}
+
+const getClassificationFromDB = async (job: TJobClassification): Promise<IClassificationJobsPartners | null> => {
+  const classification = await getDbCollection("cache_classification").findOne({ partner_label: job.partner_label, partner_job_id: job.partner_job_id })
+  return classification ?? null
+}
+
+export const getClassificationFromLab = async (job: TJobClassification): Promise<string | null> => {
+  const classificationString = [job.workplace_name, job.workplace_description, job.offer_title, job.offer_description].filter(Boolean).join("\n")
+  const classificationFromCache = await getClassificationFromDB(job)
+  if (classificationFromCache) return classificationFromCache.classification
+  const classificationFromLab = await getLabClassification(classificationString)
+  if (classificationFromLab) {
+    const payload: IClassificationJobsPartners = {
+      partner_label: job.partner_label,
+      partner_job_id: job.partner_job_id,
+      classification: classificationFromLab.label,
+      score: classificationFromLab.score,
+    }
+    await getDbCollection("cache_classification").insertOne(payload)
+    return classificationFromLab.label
+  }
+  return null
+}
