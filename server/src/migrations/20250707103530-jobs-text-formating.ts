694ea80e609960238dfa2dd093fbc6887f0518c4
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-17 10:20:54 +0200

    fix: use stream (#2222)

--- server/src/migrations/20250707103530-jobs-text-formating.ts
+++ server/src/migrations/20250707103530-jobs-text-formating.ts
@@ -1,25 +1,35 @@
+import { Transform } from "node:stream"
+import { pipeline } from "node:stream/promises"
+
 import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
 
-import { asyncForEach } from "@/common/utils/asyncUtils"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 import { sanitizeTextField } from "@/common/utils/stringUtils"
 
 export const up = async () => {
-  const jobs = await getDbCollection("jobs_partners")
+  const cursor = await getDbCollection("jobs_partners")
     .find(
       { $or: [{ workplace_description: { $ne: null } }, { workplace_name: { $ne: null } }], partner_label: { $ne: JOBPARTNERS_LABEL.RECRUTEURS_LBA } },
       { projection: { _id: 1, workplace_description: 1, workplace_name: 1, offer_description: 1, offer_title: 1 } }
     )
-    .toArray()
-  await asyncForEach(jobs, async (job) => {
-    const result = {
-      workplace_description: sanitizeTextField(job.workplace_description, true),
-      workplace_name: sanitizeTextField(job.workplace_name, true),
-      offer_description: sanitizeTextField(job.offer_description, true),
-      offer_title: sanitizeTextField(job.offer_title, true),
-    }
-    await getDbCollection("jobs_partners").updateOne({ _id: job._id }, { $set: result })
-  })
+    .stream()
+
+  await pipeline(
+    cursor,
+    new Transform({
+      objectMode: true,
+      async transform(job, _encoding, callback) {
+        const result = {
+          workplace_description: sanitizeTextField(job.workplace_description, true),
+          workplace_name: sanitizeTextField(job.workplace_name, true),
+          offer_description: sanitizeTextField(job.offer_description, true),
+          offer_title: sanitizeTextField(job.offer_title, true),
+        }
+        await getDbCollection("jobs_partners").updateOne({ _id: job._id }, { $set: result })
+        callback()
+      },
+    })
+  )
 }
 
 // set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
