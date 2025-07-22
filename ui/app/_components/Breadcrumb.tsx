4011bde6afde5b6ae5913e7983bf8824a4e39938
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-22 16:38:23 +0200

    feat(lbac-2900): DSFR landing pages (#2205)
    * feat: convert acces-recruteur
    
    * fix: container components
    
    * refactor: postez vos offres
    
    * refactor: hero recruteur
    
    * refactor: single hero component merge
    
    * feat: migrate algorecruteur
    
    * feat: temp linkedin
    
    * fix: follow linkedIn
    
    * feat: migrat FAQ
    
    * feat: migrate a-propos
    
    * feat: migrate ressource page
    
    * fix: margin
    
    * feat: migrate espace-developpeur
    
    * fix: follow linkedIn
    
    * fix: splt components
    
    * feat: migrate cfa langing
    
    * fix: file name
    
    * fix: retour recette
    
    * feat: migrate ressource components
    
    * fix: ressource mobile
    
    * fix: add padding bottom

--- ui/app/_components/Breadcrumb.tsx
+++ ui/app/_components/Breadcrumb.tsx
@@ -1,7 +1,7 @@
-import { Container } from "@chakra-ui/react"
 import { fr } from "@codegouvfr/react-dsfr"
 import { Breadcrumb as DSFRBreadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb"
 
+import DefaultContainer from "@/app/_components/Layout/DefaultContainer"
 import type { IPage } from "@/utils/routes.utils"
 import { PAGES } from "@/utils/routes.utils"
 
@@ -10,7 +10,7 @@ export function Breadcrumb({ pages }: { pages: IPage[] }) {
   const currentPage = rest.pop()
 
   return (
-    <Container variant="responsiveContainer" display="flex">
+    <DefaultContainer>
       <DSFRBreadcrumb
         style={{ marginBottom: fr.spacing("4v"), marginTop: fr.spacing("4v") }}
         currentPageLabel={currentPage?.title}
@@ -24,6 +24,6 @@ export function Breadcrumb({ pages }: { pages: IPage[] }) {
           },
         }))}
       />
-    </Container>
+    </DefaultContainer>
   )
 }
