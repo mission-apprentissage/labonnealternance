2612f794c437599a1f82c3d0cda173034bda6375
mathieu <mathieu.borrell@beta.gouv.fr>
2025-07-24 17:31:56 +0200

    feat: ajout d'un contrôle sur les commandes requises

---
+++ .bin/scripts/check-dependencies.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dependencies=(
+	"ansible"
+	"gpg"
+	"node"
+	"op"
+	"shred"
+	"sshpass"
+	"yq"
+)
+
+for command in "${dependencies[@]}"; do
+	if ! type -p "$command" > /dev/null; then
+		echo "$command missing !"
+		exit 1
+	fi
+done
