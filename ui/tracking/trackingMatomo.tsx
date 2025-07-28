0d9f0eaf328d1f62087d8cbbbbd562c20e9d09e3
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-07-28 15:49:29 +0200

    fix: missing eol

--- ui/tracking/trackingMatomo.tsx
+++ ui/tracking/trackingMatomo.tsx
@@ -12,8 +12,8 @@ export const Matomo = () => {
         {`
       var _mtm = window._mtm = window._mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-      _mtm.push(['setConsentGiven'])
-      _mtm.push(['rememberConsentGiven'])
+      _mtm.push(['setConsentGiven']);
+      _mtm.push(['rememberConsentGiven']);
       (function() {
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.async=true; g.src='${publicConfig.matomo.url}/${publicConfig.matomo.jsTrackerFile}'; s.parentNode.insertBefore(g,s);
