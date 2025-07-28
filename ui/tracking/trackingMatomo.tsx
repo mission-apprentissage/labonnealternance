26afa49eec1d2d4564bab4266af9fa38453bf059
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-07-28 14:43:23 +0200

    fix: rm comment

--- ui/tracking/trackingMatomo.tsx
+++ ui/tracking/trackingMatomo.tsx
@@ -23,17 +23,3 @@ export const Matomo = () => {
     )
   }
 }
-
-// keep it until heatmaps are up and running with the code above
-/*
-const initMatomo = () => {
-  if (!isWidget()) {
-    init(publicConfig.matomo)
-    // @ts-expect-error
-    const paq = window._paq
-    paq.push(["setConsentGiven"])
-    paq.push(["rememberConsentGiven"])
-    paq.push(["HeatmapSessionRecording::enable"])
-  }
-}
-*/
