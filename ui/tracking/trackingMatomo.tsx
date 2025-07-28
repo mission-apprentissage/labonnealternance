ecbd4db0105a4c265682e43c5c5cf12854c4812f
Kevin Barnoin <k.barnoin@gmail.com>
2025-07-28 14:43:58 +0200

    fix: matomo (#2249)
    * fix: try push consent
    
    * fix: remember consent given
    
    * fix: rm comment

--- ui/tracking/trackingMatomo.tsx
+++ ui/tracking/trackingMatomo.tsx
@@ -12,6 +12,8 @@ export const Matomo = () => {
         {`
       var _mtm = window._mtm = window._mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+      _mtm.push(['setConsentGiven'])
+      _mtm.push(['rememberConsentGiven'])
       (function() {
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.async=true; g.src='${publicConfig.matomo.url}/${publicConfig.matomo.jsTrackerFile}'; s.parentNode.insertBefore(g,s);
@@ -21,17 +23,3 @@ export const Matomo = () => {
     )
   }
 }
-
-// keep it until heatmaps are up and running with the code above
-/*
-const initMatomo = () => {
-  if (!isWidget()) {
-    init(publicConfig.matomo)
-    // @ts-expect-error
-    const paq = window._paq
-    paq.push(["setConsentGiven"])
-    paq.push(["rememberConsentGiven"])
-    paq.push(["HeatmapSessionRecording::enable"])
-  }
-}
-*/
