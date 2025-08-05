1e8f2abd2cd8896a17610d09e7cd36007cf062a5
Kevin Barnoin <k.barnoin@gmail.com>
2025-08-05 17:30:24 +0200

    fix: disable monster partner (#2261)

--- server/src/jobs/offrePartenaire/jobsPartners.importer.ts
+++ server/src/jobs/offrePartenaire/jobsPartners.importer.ts
@@ -11,7 +11,7 @@ import { processFranceTravail } from "@/jobs/offrePartenaire/france-travail/proc
 import { processHellowork } from "@/jobs/offrePartenaire/hellowork/processHellowork"
 import { processKelio } from "@/jobs/offrePartenaire/kelio/processKelio"
 import { processLaposte } from "@/jobs/offrePartenaire/laposte/processLaposte"
-import { processMonster } from "@/jobs/offrePartenaire/monster/processMonster"
+// import { processMonster } from "@/jobs/offrePartenaire/monster/processMonster"
 import { processPass } from "@/jobs/offrePartenaire/pass/processPass"
 import { processComputedAndImportToJobPartners } from "@/jobs/offrePartenaire/processJobPartners"
 import { processRhAlternance } from "@/jobs/offrePartenaire/rh-alternance/processRhAlternance"
@@ -50,13 +50,13 @@ export const importers: Record<string, CronDef> = {
     maxRuntimeInMinutes: 30,
     tag: "slave",
   },
-  "Import Monster": {
-    cron_string: timings.import_source,
-    handler: processMonster,
-    checkinMargin: 350,
-    maxRuntimeInMinutes: 30,
-    tag: "slave",
-  },
+  // "Import Monster": {
+  //   cron_string: timings.import_source,
+  //   handler: processMonster,
+  //   checkinMargin: 350,
+  //   maxRuntimeInMinutes: 30,
+  //   tag: "slave",
+  // },
   "Import Kelio": {
     cron_string: timings.import_source,
     handler: processKelio,
