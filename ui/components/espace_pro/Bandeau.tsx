dafbf2b11cbb44395a2ae4f1e997bd87fcc4ed15
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-08-23 00:04:56 +0200

    feat: bandeau

--- ui/components/espace_pro/Bandeau.tsx
+++ ui/components/espace_pro/Bandeau.tsx
@@ -1,8 +1,5 @@
-import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { Box, Center, Heading, Icon, Link, Stack, Text } from "@chakra-ui/react"
-import { assertUnreachable } from "shared"
-
-import { CircleCheck, OctogoneCross } from "../../theme/components/icons"
+import { Alert } from "@codegouvfr/react-dsfr/Alert"
+import { Link } from "@mui/material"
 
 export type BandeauProps = {
   header: React.ReactNode
@@ -12,38 +9,16 @@ export type BandeauProps = {
 }
 
 export const Bandeau = ({ header, description, lien = null, type }: BandeauProps) => {
-  const color = getBannerColor(type)
-
-  return (
-    <Stack direction="row" spacing={0} mb={3} maxH={["auto", "88px"]} className="bandeau">
-      <Box bg={color} w="40px" minW={["40px", "auto"]}>
-        <Center>
-          <Icon as={type === "success" ? CircleCheck : OctogoneCross} mt={3} w="22px" h="20px" />
-        </Center>
-      </Box>
-      <Box border="1px solid" borderColor={color} p={3} flexGrow="1">
-        <Heading fontSize="xl">{header}</Heading>
-        <Text lineHeight={9}>
-          {description}
-          {lien && (
-            <Link href={lien} textDecoration="underline" isExternal>
-              lien <ExternalLinkIcon mx="2px" />
-            </Link>
-          )}
-        </Text>
-      </Box>
-    </Stack>
+  const descriptionContent = (
+    <>
+      {description}
+      {lien && (
+        <Link ml={1} underline="always" href={lien} color="inherit" target="_blank" rel="noopener noreferrer">
+          {lien}
+        </Link>
+      )}
+    </>
   )
-}
 
-const getBannerColor = (type: "success" | "error") => {
-  switch (type) {
-    case "success":
-      return "#18753C"
-    case "error":
-      return "red.500"
-    default:
-      assertUnreachable(type)
-      return ""
-  }
+  return <Alert severity={type === "success" ? "success" : "error"} sx={{ mb: 4 }} title={header} description={descriptionContent} />
 }
