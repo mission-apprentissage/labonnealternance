8ddcb39dbaf41518bae7377172a6c1b4356d2629
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-09-02 18:08:31 +0200

    feat: migrate candidature success

--- ui/components/ItemDetail/CandidatureLba/CandidatureLbaWorked.tsx
+++ ui/components/ItemDetail/CandidatureLba/CandidatureLbaWorked.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Flex, Image, Text } from "@chakra-ui/react"
+import { Box, Container, Typography } from "@mui/material"
+import Image from "next/image"
 import React from "react"
 
 interface Props {
@@ -8,27 +9,27 @@ interface Props {
 
 const CandidatureLbaWorked = ({ email, company }: Props) => {
   return (
-    <Container data-testid="CandidatureSpontaneeWorked" mx={[6, 8]}>
-      <Flex direction="row" alignItems="center" my={4}>
-        <Image src="/images/paperplane2.svg" aria-hidden={true} alt="" />
-        <Box pl={4} ml={4}>
-          <Text data-testid="application-success" as="h2" fontSize="20px" fontWeight={700}>
+    <Container data-testid="CandidatureSpontaneeWorked" sx={{ mx: { xs: 6, sm: 8 } }}>
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", my: 4 }}>
+        <Image src="/images/paperplane2.svg" aria-hidden={true} alt="" width={48} height={48} />
+        <Box sx={{ pl: 4, ml: 4 }}>
+          <Typography data-testid="application-success" variant="h2" sx={{ fontSize: "20px", fontWeight: 700 }}>
             Votre candidature a bien été envoyée à{" "}
-            <Text as="span" fontSize="22px">
+            <Typography component="span" sx={{ fontSize: "22px" }}>
               {company}
-            </Text>
-          </Text>
+            </Typography>
+          </Typography>
         </Box>
-      </Flex>
-      <Text fontSize="18px">
+      </Box>
+      <Typography sx={{ fontSize: "18px" }}>
         Un e-mail de confirmation vous a été envoyé sur votre boite e-mail{" "}
-        <Text as="span" fontWeight={700} color="#f07f87">
+        <Typography component="span" sx={{ fontWeight: 700, color: "#f07f87" }}>
           {email}
-        </Text>
-      </Text>
-      <Text fontSize="18px" mt={4} mb={12}>
+        </Typography>
+      </Typography>
+      <Typography sx={{ fontSize: "18px", mt: 4, mb: 12 }}>
         Si vous n&apos;avez pas reçu d&apos;email de confirmation d&apos;ici 24 heures, soumettez à nouveau votre candidature
-      </Text>
+      </Typography>
     </Container>
   )
 }
