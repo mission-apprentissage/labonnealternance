ed0e25eccaa659aeafaf7f50cd20c714f2f88070
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-09-03 18:30:49 +0200

    feat: migrate recruteur job

--- ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaDetail.tsx
+++ ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaDetail.tsx
@@ -1,11 +1,10 @@
-import { AddIcon, MinusIcon } from "@chakra-ui/icons"
-import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Flex, Image, ListItem, /*Link,*/ Text, UnorderedList } from "@chakra-ui/react"
-import { SyntheticEvent, useContext, useEffect } from "react"
+import { Box, Flex, Image, ListItem, Text, UnorderedList } from "@chakra-ui/react"
+import Accordion from "@codegouvfr/react-dsfr/Accordion"
+import { useContext, useEffect } from "react"
 import { ILbaItemLbaCompanyJson, ILbaItemNaf } from "shared"
 import { LBA_ITEM_TYPE } from "shared/constants/lbaitem"
 
 import { DsfrLink } from "@/components/dsfr/DsfrLink"
-import { scrollToNestedElement } from "@/utils/tools"
 
 import { DisplayContext } from "../../../context/DisplayContextProvider"
 import { SendPlausibleEvent } from "../../../utils/plausible"
@@ -31,12 +30,6 @@ const RecruteurLbaDetail = ({ recruteurLba }: { recruteurLba: ILbaItemLbaCompany
 
   const { formValues } = useContext(DisplayContext)
 
-  const onClick = (e: SyntheticEvent) => {
-    setTimeout(() => {
-      scrollToNestedElement({ containerId: "itemDetailColumn", nestedElement: e.target as HTMLElement, yOffsett: 220 })
-    }, 200)
-  }
-
   return (
     <>
       <Box mt={6} mb={4} position="relative" background="white" pt={4} pb={6} px={6} maxWidth="970px" mx={["0", "30px", "30px", "auto"]}>
@@ -61,161 +54,122 @@ const RecruteurLbaDetail = ({ recruteurLba }: { recruteurLba: ILbaItemLbaCompany
         <Text mb={6} color="bluefrance.500" fontSize="22px" fontWeight={700} as="h2">
           Comment candidater ?
         </Text>
-        <Accordion allowMultiple={false} allowToggle defaultIndex={0}>
-          <AccordionItem borderBottom="1px solid #E5E5E5" onClick={onClick}>
-            {({ isExpanded }) => (
-              <>
-                <AccordionButton fontSize="1rem" fontWeight={700} color="#161616">
-                  <Box as="span" flex="1" textAlign="left">
-                    1. Renseignez-vous sur l’entreprise
-                  </Box>
-                  {isExpanded ? <MinusIcon fontSize="10px" /> : <AddIcon fontSize="10px" />}
-                </AccordionButton>
 
-                <AccordionPanel pb={4}>
-                  <Box background="#f6f6f6" mb={4} p={4}>
-                    <ItemLocalisation item={recruteurLba} />
-                    <Text mt={1}>
-                      <Text as="span" fontWeight={700}>
-                        Taille de l'entreprise :{" "}
-                      </Text>
-                      <Text as="span">{getCompanySize(recruteurLba)}</Text>
-                    </Text>
-                    <Text mt={1}>
-                      <Text as="span" fontWeight={700}>
-                        Secteur d'activité :{" "}
-                      </Text>
-                      <Text as="span">{(recruteurLba?.nafs as ILbaItemNaf[])?.[0].label}</Text>
-                    </Text>
-                    {recruteurLba?.contact?.phone && (
-                      <Text mt={1}>
-                        <Text as="span" fontWeight={700}>
-                          Téléphone :{" "}
-                        </Text>
-                        <Text as="span">
-                          <DsfrLink href={`tel:${recruteurLba.contact.phone}`} aria-label="Appeler la société au téléphone">
-                            {recruteurLba.contact.phone}
-                          </DsfrLink>
-                        </Text>
-                      </Text>
-                    )}
-                    <ItemGoogleSearchLink item={recruteurLba} />
-                  </Box>
-
-                  <Text>
-                    Avant de candidater, il est indispensable de prendre le temps de vous renseigner sur les activités de l’entreprise.{" "}
-                    <DsfrLink href={getCompanyGoogleSearchLink(recruteurLba)} aria-label="Recherche de l'entreprise sur google.fr - nouvelle fenêtre">
-                      Démarrez une recherche
-                    </DsfrLink>{" "}
-                    et visitez son site internet. Posez-vous les questions suivantes :
-                  </Text>
-                  <UnorderedList pl={3} mt={3}>
-                    <ListItem>Est-ce que les activités de l’entreprise correspondent au métier que je souhaite exercer, en lien avec ma formation ?</ListItem>
-                    <ListItem>Pourquoi cette entreprise plutôt que ses concurrents ?</ListItem>
-                    <ListItem>Quelles compétences souhaiteriez-vous développer en intégrant cette entreprise ?</ListItem>
-                    <ListItem>Parmi mes qualités, lesquelles pourraient être utiles à cette entreprise ?</ListItem>
-                  </UnorderedList>
-                </AccordionPanel>
-              </>
+        <Accordion label="1. Renseignez-vous sur l’entreprise" defaultExpanded>
+          <Box background="#f6f6f6" mb={4} p={4}>
+            <ItemLocalisation item={recruteurLba} />
+            <Text mt={1}>
+              <Text as="span" fontWeight={700}>
+                Taille de l'entreprise :{" "}
+              </Text>
+              <Text as="span">{getCompanySize(recruteurLba)}</Text>
+            </Text>
+            <Text mt={1}>
+              <Text as="span" fontWeight={700}>
+                Secteur d'activité :{" "}
+              </Text>
+              <Text as="span">{(recruteurLba?.nafs as ILbaItemNaf[])?.[0].label}</Text>
+            </Text>
+            {recruteurLba?.contact?.phone && (
+              <Text mt={1}>
+                <Text as="span" fontWeight={700}>
+                  Téléphone :{" "}
+                </Text>
+                <Text as="span">
+                  <DsfrLink href={`tel:${recruteurLba.contact.phone}`} aria-label="Appeler la société au téléphone">
+                    {recruteurLba.contact.phone}
+                  </DsfrLink>
+                </Text>
+              </Text>
             )}
-          </AccordionItem>
-          <AccordionItem borderBottom="1px solid #E5E5E5" onClick={onClick}>
-            {({ isExpanded }) => (
-              <>
-                <AccordionButton fontSize="1rem" fontWeight={700} color="#161616">
-                  <Box as="span" flex="1" textAlign="left">
-                    2. Préparez votre candidature spontanée
-                  </Box>
-                  {isExpanded ? <MinusIcon fontSize="10px" /> : <AddIcon fontSize="10px" />}
-                </AccordionButton>
+            <ItemGoogleSearchLink item={recruteurLba} />
+          </Box>
 
-                <AccordionPanel pb={4}>
-                  <Text>
-                    Après une recherche approfondie sur l'entreprise, personnalisez votre lettre de motivation en précisant tout d'abord pourquoi elle vous intéresse
-                    particulièrement : son domaine d'activité, ses valeurs, etc.
-                    <br />
-                    Mettez ensuite en avant vos qualités en lien avec le métier recherché. Puis terminez en exposant ce que vous pensez apporter à l'entreprise lors de votre
-                    alternance. Adaptez également votre CV.
-                    <br />
-                    <br />
-                    Pour cela, le service{" "}
-                    <DsfrLink href="https://diagoriente.beta.gouv.fr/" aria-label="Accéder au site de Diagoriente">
-                      Diagoriente
-                    </DsfrLink>{" "}
-                    vous aide à valoriser vos compétences sur votre CV sur la base de vos expériences et vos centres d’intérêt.
-                    <br />
-                    <br />
-                    Pour rendre votre CV plus beau et professionnel, vous pouvez utiliser ces outils gratuits :
-                  </Text>
-                  <UnorderedList pl={3} mt={3}>
-                    <ListItem mb={3}>
-                      <DsfrLink href="https://cv.clicnjob.fr/" aria-label="Accéder au site cv.clicnjob.fr">
-                        https://cv.clicnjob.fr/
-                      </DsfrLink>
-                    </ListItem>
-                    <ListItem mb={3}>
-                      <DsfrLink href="https://cvdesignr.com/fr" aria-label="Accéder au site cvdesignr.com">
-                        https://cvdesignr.com/fr
-                      </DsfrLink>
-                    </ListItem>
-                    <ListItem mb={3}>
-                      <DsfrLink href="https://www.canva.com/fr_fr/creer/cv/" aria-label="Accéder au site www.canva.com pour créer un cv">
-                        https://www.canva.com/fr_fr/creer/cv/
-                      </DsfrLink>
-                    </ListItem>
-                  </UnorderedList>
-                </AccordionPanel>
-              </>
-            )}
-          </AccordionItem>
-          <AccordionItem borderBottom="1px solid #E5E5E5" onClick={onClick}>
-            {({ isExpanded }) => (
-              <>
-                <AccordionButton fontSize="1rem" fontWeight={700} color="#161616">
-                  <Box as="span" flex="1" textAlign="left">
-                    3. Anticiper la suite
-                  </Box>
-                  {isExpanded ? <MinusIcon fontSize="10px" /> : <AddIcon fontSize="10px" />}
-                </AccordionButton>
-
-                <AccordionPanel pb={4}>
-                  <Text>
-                    Une fois votre candidature envoyée, notez-vous un rappel pour pouvoir relancer l’entreprise dans 10 jours si vous n’avez pas de réponse d’ici là.
-                    <br />
-                    <br />
-                    <strong>Vous ne recevez pas de réponse ?</strong>
-                    <br />
-                    Voici un exemple de relance par téléphone :
-                    <br />
-                    <Text as="span" py={4} fontStyle="italic" color="grey.425">
-                      “Bonjour,
-                      <br />
-                      Je suis [Prénom Nom]. Je vous appelle car je vous ai envoyé ma candidature par mail le [jour/mois] pour un poste d'apprenti [intitulé du poste visé]. N'ayant
-                      pas reçu de réponse, je me permets de vous relancer car je suis vraiment très intéressé·e par votre entreprise. Je serai heureux·se de vous expliquer plus en
-                      détail ma motivation lors d'un rendez-vous. Pourriez-vous me dire à qui je dois m’adresser pour savoir où en est ma candidature et quand puis-je espérer
-                      recevoir une réponse ?”
-                    </Text>
-                    <br />
-                    <br />
-                    <strong>Vous avez une proposition d’entretien ?</strong>
-                    <br />
-                    Préparez-vous avec ce quizz interactif :
-                  </Text>
-                  <UnorderedList pl={3} mt={3}>
-                    <ListItem mb={3}>
-                      <DsfrLink
-                        href="https://dinum-beta.didask.com/courses/demonstration/60d1adbb877dae00003f0eac"
-                        aria-label="Accéder aux site de conseils didask pour préparer un entretient avec un employeur"
-                      >
-                        Préparer un entretien avec un employeur
-                      </DsfrLink>
-                    </ListItem>
-                  </UnorderedList>
-                </AccordionPanel>
-              </>
-            )}
-          </AccordionItem>
+          <Text>
+            Avant de candidater, il est indispensable de prendre le temps de vous renseigner sur les activités de l’entreprise.{" "}
+            <DsfrLink href={getCompanyGoogleSearchLink(recruteurLba)} aria-label="Recherche de l'entreprise sur google.fr - nouvelle fenêtre">
+              Démarrez une recherche
+            </DsfrLink>{" "}
+            et visitez son site internet. Posez-vous les questions suivantes :
+          </Text>
+          <UnorderedList pl={3} mt={3}>
+            <ListItem>Est-ce que les activités de l’entreprise correspondent au métier que je souhaite exercer, en lien avec ma formation ?</ListItem>
+            <ListItem>Pourquoi cette entreprise plutôt que ses concurrents ?</ListItem>
+            <ListItem>Quelles compétences souhaiteriez-vous développer en intégrant cette entreprise ?</ListItem>
+            <ListItem>Parmi mes qualités, lesquelles pourraient être utiles à cette entreprise ?</ListItem>
+          </UnorderedList>
         </Accordion>
+        <Accordion label="2. Préparez votre candidature spontanée">
+          <Text>
+            Après une recherche approfondie sur l'entreprise, personnalisez votre lettre de motivation en précisant tout d'abord pourquoi elle vous intéresse particulièrement : son
+            domaine d'activité, ses valeurs, etc.
+            <br />
+            Mettez ensuite en avant vos qualités en lien avec le métier recherché. Puis terminez en exposant ce que vous pensez apporter à l'entreprise lors de votre alternance.
+            Adaptez également votre CV.
+            <br />
+            <br />
+            Pour cela, le service{" "}
+            <DsfrLink href="https://diagoriente.beta.gouv.fr/" aria-label="Accéder au site de Diagoriente">
+              Diagoriente
+            </DsfrLink>{" "}
+            vous aide à valoriser vos compétences sur votre CV sur la base de vos expériences et vos centres d’intérêt.
+            <br />
+            <br />
+            Pour rendre votre CV plus beau et professionnel, vous pouvez utiliser ces outils gratuits :
+          </Text>
+          <UnorderedList pl={3} mt={3}>
+            <ListItem mb={3}>
+              <DsfrLink href="https://cv.clicnjob.fr/" aria-label="Accéder au site cv.clicnjob.fr">
+                https://cv.clicnjob.fr/
+              </DsfrLink>
+            </ListItem>
+            <ListItem mb={3}>
+              <DsfrLink href="https://cvdesignr.com/fr" aria-label="Accéder au site cvdesignr.com">
+                https://cvdesignr.com/fr
+              </DsfrLink>
+            </ListItem>
+            <ListItem mb={3}>
+              <DsfrLink href="https://www.canva.com/fr_fr/creer/cv/" aria-label="Accéder au site www.canva.com pour créer un cv">
+                https://www.canva.com/fr_fr/creer/cv/
+              </DsfrLink>
+            </ListItem>
+          </UnorderedList>
+        </Accordion>
+        <Accordion label="3. Anticiper la suite">
+          {" "}
+          <Text>
+            Une fois votre candidature envoyée, notez-vous un rappel pour pouvoir relancer l’entreprise dans 10 jours si vous n’avez pas de réponse d’ici là.
+            <br />
+            <br />
+            <strong>Vous ne recevez pas de réponse ?</strong>
+            <br />
+            Voici un exemple de relance par téléphone :
+            <br />
+            <Text as="span" py={4} fontStyle="italic" color="grey.425">
+              “Bonjour,
+              <br />
+              Je suis [Prénom Nom]. Je vous appelle car je vous ai envoyé ma candidature par mail le [jour/mois] pour un poste d'apprenti [intitulé du poste visé]. N'ayant pas reçu
+              de réponse, je me permets de vous relancer car je suis vraiment très intéressé·e par votre entreprise. Je serai heureux·se de vous expliquer plus en détail ma
+              motivation lors d'un rendez-vous. Pourriez-vous me dire à qui je dois m’adresser pour savoir où en est ma candidature et quand puis-je espérer recevoir une réponse ?”
+            </Text>
+            <br />
+            <br />
+            <strong>Vous avez une proposition d’entretien ?</strong>
+            <br />
+            Préparez-vous avec ce quizz interactif :
+          </Text>
+          <UnorderedList pl={3} mt={3}>
+            <ListItem mb={3}>
+              <DsfrLink
+                href="https://dinum-beta.didask.com/courses/demonstration/60d1adbb877dae00003f0eac"
+                aria-label="Accéder aux site de conseils didask pour préparer un entretient avec un employeur"
+              >
+                Préparer un entretien avec un employeur
+              </DsfrLink>
+            </ListItem>
+          </UnorderedList>
+        </Accordion>
+
         <Box marginTop="10px">
           <ReportJobLink
             itemId={recruteurLba?.company?.siret}
