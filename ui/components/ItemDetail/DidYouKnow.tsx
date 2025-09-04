f1ffca4a01256bdc9f0e31796040ccca907823bb
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-09-04 13:31:05 +0200

    feat: migrate ask if

--- ui/components/ItemDetail/DidYouKnow.tsx
+++ ui/components/ItemDetail/DidYouKnow.tsx
@@ -14,7 +14,6 @@ const DidYouKnow = () => {
         padding: "16px 24px",
         maxWidth: "970px",
         mx: { xs: 0, sm: "30px", md: "30px", lg: "auto" },
-        mb: 8,
         mt: fr.spacing("3w"),
       }}
     >
