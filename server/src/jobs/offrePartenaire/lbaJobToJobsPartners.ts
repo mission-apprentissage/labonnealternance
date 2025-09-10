69853d7bf58fde400f45d2adaf1b07b5a3207bd5
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-10 11:03:54 +0200

    fix(lba-3012): correction affectation de data cas is_delegated pour api job search (#2293)
    * feat: migration et cas de synchro complète
    
    * feat: simplejob for full sync
    
    * fix: correction des données envoyés à l'api job si is_delegated
    
    * fix: model
    
    * fix: snapshots

--- server/src/jobs/offrePartenaire/lbaJobToJobsPartners.ts
+++ server/src/jobs/offrePartenaire/lbaJobToJobsPartners.ts
@@ -5,10 +5,18 @@ import { asyncForEach } from "@/common/utils/asyncUtils"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 import { updateJobsPartnersFromRecruiterById } from "@/services/formulaire.service"
 
-export const syncLbaJobsIntoJobsPartners = async () => {
-  const recruitersWithActiveJobs = await getDbCollection("recruiters").find({ status: RECRUITER_STATUS.ACTIF, "jobs.job_status": JOB_STATUS.ACTIVE }).toArray()
+export const FULL_RECRUITER_SYNC = "full"
+
+export const syncLbaJobsIntoJobsPartners = async (type?: "full") => {
+  const recruitersWithActiveJobs = await getDbCollection("recruiters")
+    .find(type === FULL_RECRUITER_SYNC ? {} : { status: RECRUITER_STATUS.ACTIF, "jobs.job_status": JOB_STATUS.ACTIVE })
+    .toArray()
 
   await asyncForEach(recruitersWithActiveJobs, async (recruiter) => {
     await updateJobsPartnersFromRecruiterById(recruiter._id)
   })
+}
+
+export const syncLbaJobsIntoJobsPartnersFull = async () => {
+  await syncLbaJobsIntoJobsPartners(FULL_RECRUITER_SYNC)
 }
