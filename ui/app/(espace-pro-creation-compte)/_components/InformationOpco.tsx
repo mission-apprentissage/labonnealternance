0d6762384ee5c46c9e2b9a15e62464828a5c8c79
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-14 09:44:04 +0200

    feat: clean

--- ui/app/(espace-pro-creation-compte)/_components/InformationOpco.tsx
+++ ui/app/(espace-pro-creation-compte)/_components/InformationOpco.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
+import { fr } from "@codegouvfr/react-dsfr"
+import { Box, Stack, Typography } from "@mui/material"
 import Image from "next/image"
 
 import { Edit2Fill, InfoCircle } from "@/theme/components/icons"
@@ -6,32 +8,32 @@ import { InfosOpco } from "@/theme/components/logos/infosOpcos"
 
 export const InformationOpco = ({ isUpdatable, infosOpco, resetOpcoChoice }: { isUpdatable: boolean; resetOpcoChoice: () => void; infosOpco: InfosOpco }) => {
   return (
-    <Box backgroundColor="#F5F5FE" p={3} mt={3}>
-      <SimpleGrid columns={1} spacing="20px">
-        <Flex align="center" justify="space-between">
-          <Text fontWeight="700" fontSize="20px">
+    <Box sx={{ backgroundColor: "#F5F5FE", p: fr.spacing("3v"), mt: fr.spacing("3v") }}>
+      <Stack direction="column" gap={fr.spacing("5v")}>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <Typography fontWeight="700" fontSize="20px">
             Votre OPCO
-          </Text>
+          </Typography>
           {isUpdatable && (
             <Button onClick={resetOpcoChoice} variant="pill" color="bluefrance.500" leftIcon={<Edit2Fill width={3} />}>
               Modifier
             </Button>
           )}
-        </Flex>
-        <Flex align="flex-start" direction={["column", "column", "row", "row"]} gap={[2, 2, 4, 4]}>
-          <Text>{infosOpco.description}</Text>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "flex-start", flexDirection: { xs: "column", md: "row" }, gap: { xs: fr.spacing("1w"), md: fr.spacing("2w") } }}>
+          <Typography>{infosOpco.description}</Typography>
           <Image src={infosOpco.image} alt="" width={80} />
-        </Flex>
-        <Flex alignItems="flex-start" gap="2px">
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "flex-start", gap: "2px" }}>
           <Box lineHeight={["16px", "16px", "16px", "20px"]}>
             <InfoCircle sx={{ color: "#000091", width: { xs: "14px", md: "20px" } }} />
           </Box>
-          <Text color="#000091">
+          <Typography color="#000091">
             Chaque entreprise est rattachée à un OPCO. C’est votre acteur de référence pour vous accompagner dans vos démarches liées à l’alternance (financement des contrats,
             formation, ...). Pour vous accompagner dans vos recrutements, votre OPCO accède à vos informations sur La bonne alternance.
-          </Text>
-        </Flex>
-      </SimpleGrid>
+          </Typography>
+        </Box>
+      </Stack>
     </Box>
   )
 }
