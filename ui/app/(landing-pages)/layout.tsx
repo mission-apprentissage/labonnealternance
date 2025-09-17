e3268b12528fbd205cc56934c4fce08c73970523
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-17 17:05:04 +0200

    feat: ok sur homes espace pro

--- ui/app/(landing-pages)/layout.tsx
+++ ui/app/(landing-pages)/layout.tsx
@@ -1,4 +1,5 @@
 import { fr } from "@codegouvfr/react-dsfr"
+import SkipLinks from "@codegouvfr/react-dsfr/SkipLinks"
 import { Box } from "@mui/material"
 import type { PropsWithChildren } from "react"
 
@@ -11,6 +12,13 @@ export default async function PublicLayout({ children }: PropsWithChildren) {
 
   return (
     <>
+      <SkipLinks
+        links={[
+          { label: "Menu", anchor: "#header-links" },
+          { label: "Contenu", anchor: "#home-pro-content" },
+          { label: "Pied de page", anchor: "#footer-links" },
+        ]}
+      />
       <PublicHeader user={user} />
       <Box sx={{ marginBottom: fr.spacing("4w") }}>{children}</Box>
       <Footer />
