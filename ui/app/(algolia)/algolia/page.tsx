38fc938e32072a2d646260e366523bfa876b6ffe
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-09-18 16:09:24 +0200

    feat: address input

--- ui/app/(algolia)/algolia/page.tsx
+++ ui/app/(algolia)/algolia/page.tsx
@@ -6,11 +6,13 @@ import { liteClient as algoliasearch } from "algoliasearch/lite"
 import React from "react"
 import { InstantSearch, Hits, Highlight, Configure } from "react-instantsearch"
 
+import CustomAddressInput from "@/app/(algolia)/_components/CustomAddressInput"
 import { CustomRefinementList } from "@/app/(algolia)/_components/CustomRefinementList"
 import { CustomSearchBox } from "@/app/(algolia)/_components/CustomSearchBox"
 import { TagFormation } from "@/components/ItemDetail/TagFormation"
 import { TagOffreEmploi } from "@/components/ItemDetail/TagOffreEmploi"
 import { publicConfig } from "@/config.public"
+import { searchAddress } from "@/services/baseAdresse"
 
 const { algoliaApiKey, algoliaAppId } = publicConfig
 
@@ -36,7 +38,21 @@ export default function AlogliaPage() {
       {/* set insight to true when going in user-testing phase or production */}
       <InstantSearch searchClient={searchClient} indexName="lba" insights={false} routing={true}>
         <Configure hitsPerPage={20} />
-        <CustomSearchBox />
+        <Box sx={{ display: "flex", gap: fr.spacing("2w"), marginBottom: fr.spacing("3w") }}>
+          <CustomSearchBox />
+          <CustomAddressInput
+            name="establishment_siret"
+            handleSearch={(search: string) => searchAddress(search)}
+            renderItem={({ label }) => <>{label}</>}
+            itemToString={({ label }) => label}
+            onInputFieldChange={() => {}}
+            onSelectItem={() => {}}
+            onError={() => {}}
+            allowHealFromError={false}
+            renderNoResult={undefined}
+            renderError={() => null}
+          />
+        </Box>
         <Box sx={{ display: "flex", gap: fr.spacing("2w"), marginBottom: fr.spacing("3w") }}>
           <CustomRefinementList attribute="type" title="Type" />
           <CustomRefinementList attribute="level" title="Niveau" />
