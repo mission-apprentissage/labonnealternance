be9a63a48dbaaf8714ef8c55a3d5bdabb39267ca
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-19 10:39:32 +0200

    feat: spacing dsfr

--- ui/components/espace_pro/common/components/BorderedBox.tsx
+++ ui/components/espace_pro/common/components/BorderedBox.tsx
@@ -1,5 +1,6 @@
+import { fr } from "@codegouvfr/react-dsfr"
 import { Box } from "@mui/material"
 
 export const BorderedBox = ({ children, sx }: Parameters<typeof Box>[0]) => (
-  <Box sx={{ border: "1px solid #000091", px: { xs: 1, lg: 2 }, py: { xs: 1, lg: 2 }, ...sx }}>{children}</Box>
+  <Box sx={{ border: "1px solid #000091", p: { xs: fr.spacing("1v"), lg: fr.spacing("1w") }, ...sx }}>{children}</Box>
 )
