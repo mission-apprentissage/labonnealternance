feee659c9a589c1318af87286a8368bd1934572c
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-19 10:41:08 +0200

    fix(lba-3104): correction affichage tag handi-engagé (#2324)
    * feat: ajout d'exclusion pour éviter d'afficher l'étiquette
    
    * feat: affectation valeur du tag
    
    * feat: disabledEngagement sur changeStream
    
    * feat: migration
    
    * feat: seed avec migration
    
    * feat: seed avec tags handi
    
    * fix: typing
    
    * feat: seed propre

--- server/src/services/referentielEngagementEntreprise.service.ts
+++ server/src/services/referentielEngagementEntreprise.service.ts
@@ -1,8 +1,10 @@
+import { EntrepriseEngagementSources } from "shared/models/referentielEngagementEntreprise.model"
+
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 export const getEntrepriseEngagement = async (siret: string): Promise<boolean> => {
   // 2025-08-07 : locked on france-travail sources for now
-  const reponse = await getDbCollection("referentiel_engagement_entreprise").findOne({ siret, sources: { $in: ["france-travail"] } })
+  const reponse = await getDbCollection("referentiel_engagement_entreprise").findOne({ siret, sources: { $in: [EntrepriseEngagementSources.FRANCE_TRAVAIL] } })
   if (!reponse) return false
   return true
 }
