6ccf82dc1d43a6ad0eea44b21a15eafcb18cf64c
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-22 14:54:31 +0200

    fix: style hint text

--- ui/app/_components/CustomInput.tsx
+++ ui/app/_components/CustomInput.tsx
@@ -15,9 +15,9 @@ const CustomInput = (props) => {
       <FormControl sx={{ width: "100%" }} error={meta.error && meta.touched} required={props.required ?? true}>
         {props.label && <FormLabel error={meta.error && meta.touched}>{props.label}</FormLabel>}
         {props.info && (
-          <FormHelperText className={fr.cx("fr-hint-text")} sx={{ pb: 1 }}>
+          <Box className={fr.cx("fr-hint-text")} sx={{ pb: 1 }}>
             {props.info}
-          </FormHelperText>
+          </Box>
         )}
         <Input sx={{ mt: "8px !important" }} className={fr.cx("fr-input")} {...field} {...props} />
         {props.helper && <FormHelperText>{props.helper}</FormHelperText>}
