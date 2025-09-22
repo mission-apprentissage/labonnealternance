cd3e2633de8669791261fe4c9f9057148ab85af6
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-22 18:16:25 +0200

    feat: meilleur padding borderedbox

--- ui/components/espace_pro/common/components/BorderedBox.tsx
+++ ui/components/espace_pro/common/components/BorderedBox.tsx
@@ -2,5 +2,5 @@ import { fr } from "@codegouvfr/react-dsfr"
 import { Box } from "@mui/material"
 
 export const BorderedBox = ({ children, sx }: Parameters<typeof Box>[0]) => (
-  <Box sx={{ border: "1px solid #000091", p: { xs: fr.spacing("1v"), lg: fr.spacing("1w") }, ...sx }}>{children}</Box>
+  <Box sx={{ border: "1px solid #000091", p: { xs: fr.spacing("1w"), lg: fr.spacing("2w") }, ...sx }}>{children}</Box>
 )
