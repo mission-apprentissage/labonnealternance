ce7cdaa956c8ac7c2b9c0a0d8a0395020cd1ff01
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-09-23 18:26:52 +0200

    feat: à revérifier

--- ui/components/DepotOffre/RomeDetailReadOnly.tsx
+++ ui/components/DepotOffre/RomeDetailReadOnly.tsx
@@ -1,24 +1,8 @@
-import { Accordion } from "@chakra-ui/react"
 import { fr } from "@codegouvfr/react-dsfr"
+import Accordion from "@codegouvfr/react-dsfr/Accordion"
 import styled from "@emotion/styled"
 import { Box, Typography } from "@mui/material"
 import { IReferentielRomeForJobJson } from "shared"
-
-import { CustomAccordion } from "./CustomAccordion"
-
-const AccordionHeader = styled.div`
-  font-size: 16px;
-  font-weight: 700;
-
-  .subtitle {
-    color: #666666;
-    font-style: italic;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 20px;
-    margin-left: 4px;
-  }
-`
 
 const CompetencesGroupDiv = styled.div`
   margin-bottom: 20px;
@@ -45,48 +29,47 @@ export const RomeDetailReadOnly = ({
       <Typography component="h2" sx={{ fontWeight: 700, mb: fr.spacing("2w") }}>
         {appellation}
       </Typography>
-      <Accordion defaultIndex={[0]} allowMultiple>
-        <CustomAccordion id="metier" header={<AccordionHeader>Descriptif du métier</AccordionHeader>}>
-          <Typography>{definition}</Typography>
-        </CustomAccordion>
-        {competences?.savoir_etre_professionnel && (
-          <CustomAccordion id="qualites" header={<AccordionHeader>Qualités souhaitées pour ce métier</AccordionHeader>}>
-            <CompetencesGroupDiv>
-              {competences.savoir_etre_professionnel.map(({ libelle }) => (
+      <Accordion style={{ marginBottom: fr.spacing("2w") }} defaultExpanded={true} id="metier" label="Descriptif du métier">
+        {definition}
+      </Accordion>
+      {competences?.savoir_etre_professionnel && (
+        <Accordion id="qualites" label="Qualités souhaitées pour ce métier">
+          <CompetencesGroupDiv>
+            {competences.savoir_etre_professionnel.map(({ libelle }) => (
+              <li key={libelle}>{libelle}</li>
+            ))}
+          </CompetencesGroupDiv>
+        </Accordion>
+      )}
+      {competences?.savoir_faire && (
+        <Accordion style={{ marginBottom: fr.spacing("2w") }} id="competences" label="Compétences qui seront acquises durant l’alternance">
+          {competences.savoir_faire.map(({ libelle, items = [] }) => (
+            <CompetencesGroupDiv key={libelle}>
+              <Typography className="competences-group-title">{libelle}</Typography>
+              {items.map(({ libelle }) => (
                 <li key={libelle}>{libelle}</li>
               ))}
             </CompetencesGroupDiv>
-          </CustomAccordion>
-        )}
-        {competences?.savoir_faire && (
-          <CustomAccordion id="competences" header={<AccordionHeader>Compétences qui seront acquises durant l’alternance</AccordionHeader>}>
-            {competences.savoir_faire.map(({ libelle, items = [] }) => (
-              <CompetencesGroupDiv key={libelle}>
-                <Typography className="competences-group-title">{libelle}</Typography>
-                {items.map(({ libelle }) => (
-                  <li key={libelle}>{libelle}</li>
-                ))}
-              </CompetencesGroupDiv>
-            ))}
-          </CustomAccordion>
-        )}
-        {competences?.savoirs && (
-          <CustomAccordion id="techniques" header={<AccordionHeader>Domaines et techniques de travail</AccordionHeader>}>
-            {competences.savoirs.map(({ libelle, items = [] }) => (
-              <CompetencesGroupDiv key={libelle}>
-                <Typography className="competences-group-title">{libelle}</Typography>
-                {items.map(({ libelle }) => (
-                  <li key={libelle}>{libelle}</li>
-                ))}
-              </CompetencesGroupDiv>
-            ))}
-          </CustomAccordion>
-        )}
+          ))}
+        </Accordion>
+      )}
+      {competences?.savoirs && (
+        <Accordion style={{ marginBottom: fr.spacing("2w") }} id="techniques" label="Domaines et techniques de travail">
+          {competences.savoirs.map(({ libelle, items = [] }) => (
+            <CompetencesGroupDiv key={libelle}>
+              <Typography className="competences-group-title">{libelle}</Typography>
+              {items.map(({ libelle }) => (
+                <li key={libelle}>{libelle}</li>
+              ))}
+            </CompetencesGroupDiv>
+          ))}
+        </Accordion>
+      )}
 
-        <CustomAccordion id="accessibilite" header={<AccordionHeader>À qui ce métier est-il accessible ?</AccordionHeader>}>
-          <Typography>{acces_metier}</Typography>
-        </CustomAccordion>
+      <Accordion style={{ marginBottom: fr.spacing("2w") }} id="accessibilite" label="À qui ce métier est-il accessible ?">
+        <Typography>{acces_metier}</Typography>
       </Accordion>
+
       <Typography sx={{ fontSize: "14px", color: "#3A3A3A", lineHeight: "24px" }}>La fiche métier se base sur la classification ROME de France Travail</Typography>
     </Box>
   )
