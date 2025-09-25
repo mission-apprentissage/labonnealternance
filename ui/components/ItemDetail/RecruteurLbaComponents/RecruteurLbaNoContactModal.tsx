6037e18b150fa52864f9bce2878016aa4b9798cc
Rémy Auricoste <remy.auricoste@gmail.com>
2025-09-25 14:44:12 +0200

    fix: ameliorations

--- ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaNoContactModal.tsx
+++ ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaNoContactModal.tsx
@@ -4,6 +4,7 @@ import { Box, Typography } from "@mui/material"
 import Image from "next/image"
 import { ILbaItemLbaCompanyJson, ILbaItemLbaJobJson, ILbaItemPartnerJobJson } from "shared"
 
+import { useToast } from "@/app/hooks/useToast"
 import { useDisclosure } from "@/common/hooks/useDisclosure"
 import { DsfrLink } from "@/components/dsfr/DsfrLink"
 import { ModalReadOnly } from "@/components/ModalReadOnly"
@@ -16,90 +17,109 @@ export const RecruteurLbaNoContactModal = ({
   item: ILbaItemLbaJobJson | ILbaItemLbaCompanyJson | ILbaItemPartnerJobJson
   modalControls: ReturnType<typeof useDisclosure>
 }) => {
+  const { toast, ToastComponent } = useToast()
+
   const { isOpen, onClose: onModalClose } = modalControls
   const raisonSociale = item.company?.name
+  const phone = item.contact?.phone
 
   const onAnswer = async (isPositive: boolean) => {
     const eventName = isPositive ? "Clic Je vais contacter - Fiche entreprise Algo" : "Clic Je ne vais pas contacter - Fiche entreprise Algo"
     await SendPlausibleEvent(eventName, {
       info_fiche: item.id,
     })
+    toast({
+      title: "Merci pour votre réponse !",
+      status: "success",
+    })
     onModalClose()
   }
 
   return (
-    <ModalReadOnly isOpen={isOpen} onClose={onModalClose}>
-      <Box
-        sx={{
-          px: "32px",
-          pt: fr.spacing("2w"),
-          pb: fr.spacing("4w"),
-          maxWidth: 620,
-        }}
-      >
-        <Typography
-          component="h1"
-          sx={{
-            fontWeight: 700,
-            fontSize: "24px",
-            lineHeight: "32px",
-            mb: fr.spacing("2w"),
-          }}
-        >
-          Pour envoyer votre candidature spontanée :
-        </Typography>
-        <Typography>Nous ne disposons pas de contact pour cette entreprise, mais peut-être que vous en trouverez un sur internet !</Typography>
-        <Box sx={{ display: "flex", alignItems: "center", gap: "6px", mt: fr.spacing("2w") }}>
-          <Image style={{ display: "inline-block" }} width={24} height={24} src="/images/icons/search-eye-line.svg" alt="" />
-          <Typography>
-            Lancer une recherche Google sur{" "}
-            <DsfrLink href={`https://www.google.com/search?q=${encodeURIComponent(raisonSociale)}`} external aria-label="Accéder au détail de l'astuce">
-              {raisonSociale}
-            </DsfrLink>
-          </Typography>
-        </Box>
-        <hr style={{ marginTop: 24, marginBottom: 23, paddingBottom: 1 }} />
-        <Typography
-          component="h2"
-          sx={{
-            fontWeight: 700,
-            fontSize: "24px",
-            lineHeight: "32px",
-            my: fr.spacing("2w"),
-          }}
-        >
-          Allez-vous contacter cette entreprise ?
-        </Typography>
+    <>
+      {ToastComponent}
+      <ModalReadOnly isOpen={isOpen} onClose={onModalClose}>
+        {ToastComponent}
         <Box
           sx={{
-            display: "flex",
-            flexDirection: {
-              xs: "column",
-              sm: "row",
-            },
-            gap: "16px",
-            button: {
-              px: "16px",
-              py: {
-                xs: "16px",
-                sm: "8px",
-              },
-              width: {
-                xs: "100%",
-                sm: "fit-content",
-              },
-              justifyContent: "center",
-            },
+            px: "32px",
+            pt: fr.spacing("2w"),
+            pb: fr.spacing("4w"),
+            maxWidth: 620,
           }}
         >
-          <Button onClick={() => onAnswer(false)} priority="secondary" aria-label="Je ne vais pas la contacter">
-            👎 Non, je ne vais pas la contacter
-          </Button>
-          <Button onClick={() => onAnswer(true)} aria-label="Je vais la contacter">
-            👍 Oui, je vais la contacter
-          </Button>
+          <Typography
+            component="h1"
+            sx={{
+              fontWeight: 700,
+              fontSize: "24px",
+              lineHeight: "32px",
+              mb: fr.spacing("2w"),
+            }}
+          >
+            Pour envoyer votre candidature spontanée :
+          </Typography>
+          {phone && (
+            <Typography my={fr.spacing("2w")}>
+              <span className="fr-icon-phone-line" style={{ marginRight: "8px" }}></span>
+              Vous pouvez contacter l'entreprise au <DsfrLink href={`tel:${phone}`}>{phone}</DsfrLink>
+            </Typography>
+          )}
+          <Typography>
+            Nous ne disposons pas de contact mail{!phone && " ni de numéro de téléphone"} pour cette entreprise, mais peut-être que vous en trouverez un sur internet !
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "6px", mt: fr.spacing("2w") }}>
+            <Image style={{ display: "inline-block" }} width={24} height={24} src="/images/icons/search-eye-line.svg" alt="" />
+            <Typography>
+              Lancer une recherche Google sur{" "}
+              <DsfrLink href={`https://www.google.com/search?q=${encodeURIComponent(raisonSociale)}`} external aria-label="Accéder au détail de l'astuce">
+                {raisonSociale}
+              </DsfrLink>
+            </Typography>
+          </Box>
+          <hr style={{ marginTop: 24, marginBottom: 23, paddingBottom: 1 }} />
+          <Typography
+            component="h2"
+            sx={{
+              fontWeight: 700,
+              fontSize: "24px",
+              lineHeight: "32px",
+              my: fr.spacing("2w"),
+            }}
+          >
+            Allez-vous contacter cette entreprise ?
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: {
+                xs: "column",
+                sm: "row",
+              },
+              gap: "16px",
+              button: {
+                px: "16px",
+                py: {
+                  xs: "16px",
+                  sm: "8px",
+                },
+                width: {
+                  xs: "100%",
+                  sm: "fit-content",
+                },
+                justifyContent: "center",
+              },
+            }}
+          >
+            <Button onClick={() => onAnswer(false)} priority="secondary" aria-label="Je ne vais pas la contacter">
+              👎 Non, je ne vais pas la contacter
+            </Button>
+            <Button onClick={() => onAnswer(true)} aria-label="Je vais la contacter">
+              👍 Oui, je vais la contacter
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </ModalReadOnly>
+      </ModalReadOnly>
+    </>
   )
 }
