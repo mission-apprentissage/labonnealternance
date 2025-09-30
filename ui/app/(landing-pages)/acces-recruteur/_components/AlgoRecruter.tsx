730e999c62db2c770d2735f681b774a3c8395d35
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-09-30 12:36:29 +0200

    feat: last components

--- ui/app/(landing-pages)/acces-recruteur/_components/AlgoRecruter.tsx
+++ ui/app/(landing-pages)/acces-recruteur/_components/AlgoRecruter.tsx
@@ -1,12 +1,14 @@
 import { fr } from "@codegouvfr/react-dsfr"
-import { Box, Grid2 as Grid, List, ListItem, ListItemIcon, Typography } from "@mui/material"
+import { Box, Grid2 as Grid, Typography, Stack } from "@mui/material"
 import Image from "next/image"
 
 import { DsfrLink } from "@/components/dsfr/DsfrLink"
 
 export const AlgoRecruteur = ({ withLinks = false }: { withLinks?: boolean }) => {
   return (
-    <Box sx={{ p: fr.spacing("5w"), borderRadius: "10px", backgroundColor: fr.colors.decisions.background.alt.blueFrance.default }}>
+    <Box
+      sx={{ p: { xs: fr.spacing("2w"), sm: fr.spacing("2w"), md: fr.spacing("5w") }, borderRadius: "10px", backgroundColor: fr.colors.decisions.background.alt.blueFrance.default }}
+    >
       <Grid container spacing={fr.spacing("8w")} sx={{ alignItems: "center", justifyContent: "center" }}>
         <Grid size={{ xs: 12, md: 7 }}>
           <Typography component="h2" variant="h2" sx={{ mb: 2 }}>
@@ -17,28 +19,20 @@ export const AlgoRecruteur = ({ withLinks = false }: { withLinks?: boolean }) =>
             </Typography>{" "}
           </Typography>
           <Box component="hr" sx={{ maxWidth: "93px", border: "none", borderBottom: "none", borderTop: "4px solid #0063CB", opacity: 1 }} />
-          <List>
-            <ListItem sx={{ alignItems: "flex-start", px: 0 }}>
-              <ListItemIcon sx={{ minWidth: "30px" }}>
-                <Box component={"span"} className={fr.cx("ri-circle-fill", "fr-icon--xs")} />
-              </ListItemIcon>
-              <Typography>
+          <Stack spacing={2} sx={{ my: fr.spacing("3w") }}>
+            <Box sx={{ display: "flex", gap: fr.spacing("2w"), flexDirection: "column" }}>
+              <Typography sx={{ display: "list-item", ml: fr.spacing("2w") }}>
                 <span className="fr-text--bold">Celles ayant émis un besoin en recrutement </span>sur notre plateforme ainsi que sur France Travail et ses sites partenaires
               </Typography>
-            </ListItem>
-            <ListItem sx={{ alignItems: "flex-start", px: 0 }}>
-              <ListItemIcon sx={{ minWidth: "30px" }}>
-                <Box component={"span"} className={fr.cx("ri-circle-fill", "fr-icon--xs")} />
-              </ListItemIcon>
-              <Typography>
+              <Typography sx={{ display: "list-item", ml: fr.spacing("2w") }}>
                 <span className="fr-text--bold">
                   Celles n&apos;ayant pas diffusé d&apos;offres, mais ayant été identifiées comme &quot;à fort potentiel d&apos;embauche en alternance&quot;
                 </span>{" "}
                 grâce à l'analyse de diverses données publiques (données de recrutement, données financières, etc.). La bonne alternance identifie ainsi chaque mois une liste
                 restreinte d'entreprises à fort potentiel d'embauche en alternance pour faciliter les démarches de candidatures spontanées de ses utilisateurs.
               </Typography>
-            </ListItem>
-          </List>
+            </Box>
+          </Stack>
           {withLinks && (
             <DsfrLink arrow="right" href="/desinscription" aria-label="Accès au formulaire de désinscription au service d'envoi de candidatures spontanées">
               Je ne souhaite plus recevoir de candidature spontanée
