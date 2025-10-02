855e31a73c5b9178d64c845e45145c703772e312
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-10-02 09:16:13 +0200

    feat(lba-2966): blocage des adresses emails de CFA ne répondants pas aux candidats (#2346)
    * feat: migration à tester
    
    * feat: typage

---
+++ server/src/migrations/20250929154955-bl-emails-sans-reponse.ts
@@ -0,0 +1,32 @@
+import { ObjectId } from "mongodb"
+
+import { logger } from "@/common/logger"
+import { s3ReadAsString } from "@/common/utils/awsUtils"
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+
+export const up = async () => {
+  const listFileKey = "non_answering_cfa_emails_to_bl_lba-2966.json"
+
+  const rawList = await s3ReadAsString("storage", listFileKey)
+
+  if (!rawList) {
+    logger.warn("No file found at the specified S3 location.")
+    return
+  }
+  const notAnsweringEmails = JSON.parse(rawList)
+
+  logger.info(`Found ${notAnsweringEmails.length} emails in the list.`)
+
+  const now = new Date()
+  const blackListAdditions: { _id: ObjectId; email: string; created_at: Date; blacklisting_origin: string }[] = notAnsweringEmails.map((email) => ({
+    _id: new ObjectId(),
+    email,
+    created_at: now,
+    blacklisting_origin: "campagne 04/25 - RDVA sans réponse",
+  }))
+
+  await getDbCollection("emailblacklists").insertMany(blackListAdditions)
+}
+
+// set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
+export const requireShutdown: boolean = false
