22dac4cfd6e5b442d1b1246281f6ae4ae8122609
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-10-09 12:05:35 +0200

    feat: optimize

--- server/src/jobs/partenaireExport/exportBlacklistAlgo.ts
+++ server/src/jobs/partenaireExport/exportBlacklistAlgo.ts
@@ -4,22 +4,26 @@ import { s3WriteString } from "@/common/utils/awsUtils"
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 import { notifyToSlack } from "@/common/utils/slackUtils"
 
-const exportBlacklistAlgo = async (): Promise<void> => {
-  const reportedCompanies = await getDbCollection("reported_companies")
-    .find({ reason: "Entreprise fermée", type: LBA_ITEM_TYPE.RECRUTEURS_LBA }, { projection: { _id: 0, siret: 1 } })
-    .toArray()
-  const unsubscribedCompanies = await getDbCollection("unsubscribedrecruteurslba")
-    .find({}, { projection: { _id: 0, siret: 1 } })
-    .toArray()
-  const uniqueSiret = [...new Set([...reportedCompanies, ...unsubscribedCompanies].map((company) => company.siret))]
-  const jsonContent = JSON.stringify(uniqueSiret, null, 2)
-  const fileKey = `blacklist-algo.json`
-
+const uploadJsonToS3 = async (fileKey: string, data: unknown): Promise<void> => {
   await s3WriteString("storage", fileKey, {
-    Body: jsonContent,
+    Body: JSON.stringify(data, null, 2),
     ContentType: "application/json",
   })
+}
 
+const exportBlacklistAlgo = async (): Promise<void> => {
+  const [reportedCompanies, unsubscribedCompanies] = await Promise.all([
+    getDbCollection("reported_companies")
+      .find({ reason: "Entreprise fermée", type: LBA_ITEM_TYPE.RECRUTEURS_LBA }, { projection: { _id: 0, siret: 1 } })
+      .toArray(),
+    getDbCollection("unsubscribedrecruteurslba")
+      .find({}, { projection: { _id: 0, siret: 1 } })
+      .toArray(),
+  ])
+
+  const uniqueSiret = [...new Set([...reportedCompanies, ...unsubscribedCompanies].map((company) => company.siret))]
+
+  await uploadJsonToS3("blacklist-algo.json", uniqueSiret)
   await notifyToSlack({ subject: "Fichier de blacklist pour l'algorithme", message: `${uniqueSiret.length} siret exportés` })
 }
 
@@ -27,17 +31,11 @@ const exportApplications = async (): Promise<void> => {
   const applications = await getDbCollection("applications")
     .find({ job_origin: LBA_ITEM_TYPE.RECRUTEURS_LBA }, { projection: { _id: 1, company_siret: 1, created_at: 1 } })
     .toArray()
-  const jsonContent = JSON.stringify(applications, null, 2)
-  const fileKey = `applications-algo.json`
-  await s3WriteString("storage", fileKey, {
-    Body: jsonContent,
-    ContentType: "application/json",
-  })
 
+  await uploadJsonToS3("applications-algo.json", applications)
   await notifyToSlack({ subject: "Fichier des candidatures pour l'algorithme", message: `${applications.length} candidatures exportées` })
 }
 
 export const exportFileForAlgo = async (): Promise<void> => {
-  await exportBlacklistAlgo()
-  await exportApplications()
+  await Promise.all([exportBlacklistAlgo(), exportApplications()])
 }
