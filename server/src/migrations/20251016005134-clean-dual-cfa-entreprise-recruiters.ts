6151a1be9c172ffd6d75016e559516ce545f70f5
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-10-16 01:08:48 +0200

    feat: archivage recruiters duals

---
+++ server/src/migrations/20251016005134-clean-dual-cfa-entreprise-recruiters.ts
@@ -0,0 +1,21 @@
+import { JOB_STATUS } from "shared"
+import { RECRUITER_STATUS } from "shared/constants/recruteur"
+
+import { logger } from "@/common/logger"
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+
+export const up = async () => {
+  logger.info("Starting migration: clean dual cfa entreprise recruiters")
+
+  const now = new Date()
+  await getDbCollection("recruiters").updateMany(
+    {
+      $expr: { $eq: ["$establishment_siret", "$cfa_delegated_siret"] },
+    },
+    { $set: { "jobs.job_status": JOB_STATUS.ANNULEE, updatedAt: now, status: RECRUITER_STATUS.ARCHIVE } }
+  )
+
+  logger.info("Migration completed: clean dual cfa entreprise recruiters")
+}
+
+export const requireShutdown: boolean = false
