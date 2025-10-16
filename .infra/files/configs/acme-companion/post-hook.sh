2e78a9c0db7ff82292765602175e5ada74db588a
mathieu <mathieu.borrell@beta.gouv.fr>
2025-10-17 01:08:53 +0200

    fix: debug

--- .infra/files/configs/acme-companion/post-hook.sh
+++ .infra/files/configs/acme-companion/post-hook.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-find /etc/nginx/certs -name key.pem -exec sh -c 'ln -sr $1 $(dirname $1)/privkey.pem' sh {} \;
+find /etc/nginx/certs -name key.pem -exec sh -c 'install -m 644 $1 $(dirname $1)/privkey.pem' sh {} \;
 find /etc/nginx/certs -type f -exec chmod +r {} \;
-find /etc/nginx/certs -name key.pem -exec chmod 444 {} \;
