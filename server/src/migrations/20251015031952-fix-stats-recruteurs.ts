48345587b7cbbf276b04a0b82cca49d661694d6a
Rémy Auricoste <remy.auricoste@gmail.com>
2025-10-23 15:26:57 +0200

    fix: migration

--- server/src/migrations/20251015031952-fix-stats-recruteurs.ts
+++ server/src/migrations/20251015031952-fix-stats-recruteurs.ts
@@ -1,7 +1,15 @@
-import { getDbCollection } from "@/common/utils/mongodbUtils"
+import { getDatabase } from "@/common/utils/mongodbUtils"
 
 export const up = async () => {
-  await getDbCollection("jobs_partners").updateMany({ stats_detail_view: null }, { $set: { stats_detail_view: 0 } })
+  await getDatabase()
+    .collection("jobs_partners")
+    .updateMany({ stats_detail_view: null }, { $set: { stats_detail_view: 0 } })
+  await getDatabase()
+    .collection("jobs_partners")
+    .updateMany({ stats_search_view: null }, { $set: { stats_search_view: 0 } })
+  await getDatabase()
+    .collection("jobs_partners")
+    .updateMany({ stats_postuler: null }, { $set: { stats_postuler: 0 } })
 }
 
 export const requireShutdown: boolean = true
