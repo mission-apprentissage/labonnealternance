0ee34e8034ddf04414231abbed4a4750ec54dd29
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-10-30 17:48:58 +0100

    fix: update vitest & snapshot

--- server/tests/integration/common/model.test.ts
+++ server/tests/integration/common/model.test.ts
@@ -7,19 +7,7 @@ import { createIndexes } from "../../../src/common/utils/mongodbUtils"
 describe("createMongoDBIndexes", () => {
   useMongo()
 
-  it(
-    "should create indexes for all models",
-    async () => {
-      await expect(createIndexes()).resolves.toBeUndefined()
-    },
-    { timeout: 20_000 }
-  )
-
-  it(
-    "should load all schemas",
-    async () => {
-      // TODO
-    },
-    { timeout: 20_000 }
-  )
+  it("should create indexes for all models", { timeout: 20_000 }, async () => {
+    await expect(createIndexes()).resolves.toBeUndefined()
+  })
 })
