a31238205408d7b8dc3848d39a61d97517413d15
Kevin Barnoin <k.barnoin@gmail.com>
2025-10-30 18:49:25 +0100

    fix: typo (#2412)

--- ui/components/ItemDetail/ItemDetailServices/getCompanySize.ts
+++ ui/components/ItemDetail/ItemDetailServices/getCompanySize.ts
@@ -5,7 +5,7 @@ import { endsWithNumber } from "@/utils/strutils"
 export const getCompanySize = (item: ILbaItemLbaJobJson | ILbaItemLbaCompanyJson | ILbaItemFtJobJson | ILbaItemFormation2Json) => {
   let companySize = item?.company?.size?.toLowerCase()
   if (!companySize || companySize === "non diffusé") {
-    companySize = "non renseigné"
+    companySize = "non renseignée"
   } else if (companySize.startsWith("0")) {
     companySize = "0 à 9 salariés"
   }
