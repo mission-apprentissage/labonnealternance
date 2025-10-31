290495a49422432368c4fe3c0306bf28cd46d0d7
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-10-31 12:17:43 +0100

    fix: comparaison

--- server/src/services/application.service.ts
+++ server/src/services/application.service.ts
@@ -1176,7 +1176,7 @@ const getJobOrCompany = async (application: IApplication): Promise<IJobOrCompany
     if (!recruiter) {
       throw internal(`inattendu: aucun recruiter avec jobs._id=${job_id}`)
     }
-    const job = recruiter?.jobs?.find((job) => job._id === job_id)
+    const job = recruiter?.jobs?.find((job) => job._id.toString() === job_id.toString())
     if (!job) {
       throw internal(`inattendu: aucun job recruiter avec id=${job_id}`)
     }
