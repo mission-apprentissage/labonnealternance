a36455a99baa53c610d80864be158b932933abc6
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-11-17 17:18:06 +0100

    fix: test job importer + sentry 1

--- server/src/jobs/offrePartenaire/rawToComputedJobsPartners.ts
+++ server/src/jobs/offrePartenaire/rawToComputedJobsPartners.ts
@@ -65,6 +65,7 @@ export const rawToComputedJobsPartners = async <ZodInput extends AnyZodObject>({
           const newError = internal(`error converting raw job to partner_label job for id=${document._id} partner_label=${partnerLabel}`)
           logger.error(newError.message, err)
           newError.cause = err
+          console.log("should have sent exception to sentry")
           //sentryCaptureException(newError)
         }
         callback()
