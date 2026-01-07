37179ed409e978e48d163028c1f8c226d7686ae7
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-01-07 10:32:42 +0100

    feat: progress

--- ui/app/(creation-compte-widget)/espace-pro/widget/entreprise/offre/page.tsx
+++ ui/app/(creation-compte-widget)/espace-pro/widget/entreprise/offre/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next"
 import { DepotSimplifieCreationOffre } from "@/app/(espace-pro-creation-compte)/_components/DepotSimplifieCreationOffre"
+
+export const metadata: Metadata = {
+  title: "Formulaire de dépôt d'offre - compte créé - La bonne alternance",
+}
 
 export default function Page() {
   return <DepotSimplifieCreationOffre />
