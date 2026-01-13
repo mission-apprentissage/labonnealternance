e8f5c9ce3abd1696832f3cfc4dd07b8308117186
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-01-13 14:59:17 +0100

    feat: layouts manquants

--- ui/app/(candidat)/postuler/layout.tsx
+++ ui/app/(candidat)/postuler/layout.tsx
@@ -1,14 +1,15 @@
 import type { PropsWithChildren } from "react"
 
 import { Box } from "@mui/material"
-import { DepotSimplifieLayout } from "@/components/espace_pro/common/components/DepotSimplifieLayout"
+import { Footer } from "@/app/_components/Footer"
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <DepotSimplifieLayout>
+    <>
       <Box role="main" component="main">
         {children}
       </Box>
-    </DepotSimplifieLayout>
+      <Footer />
+    </>
   )
 }
