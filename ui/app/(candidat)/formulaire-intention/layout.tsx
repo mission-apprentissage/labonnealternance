7b9f1184ae5512e923ed215399c3caa245647894
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-01-30 18:16:24 +0100

    fix: header, skiplinks in intention

--- ui/app/(candidat)/formulaire-intention/layout.tsx
+++ ui/app/(candidat)/formulaire-intention/layout.tsx
@@ -1,12 +1,23 @@
 import type { PropsWithChildren } from "react"
 
 import { Box } from "@mui/material"
+import SkipLinks from "@codegouvfr/react-dsfr/SkipLinks"
+import { fr } from "@codegouvfr/react-dsfr"
 import { Footer } from "@/app/_components/Footer"
+import { PublicHeader } from "@/app/_components/PublicHeader"
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <Box role="main" component="main">
+      <SkipLinks
+        links={[
+          { label: "Menu", anchor: "#header-links" },
+          { label: "Contenu", anchor: "#intention-content-container" },
+          { label: "Pied de page", anchor: "#footer-links" },
+        ]}
+      />
+      <PublicHeader hideConnectionButton={false} />
+      <Box role="main" sx={{ py: fr.spacing("10v") }} component="main" tabIndex={-1} id="intention-content-container">
         {children}
       </Box>
       <Footer />
