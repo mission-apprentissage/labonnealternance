02a34a1a151442ad4f16a40295be9c5af961475b
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-02-12 11:19:39 +0100

    fix: html element

--- ui/app/(home)/_components/AlgoHome.tsx
+++ ui/app/(home)/_components/AlgoHome.tsx
@@ -35,15 +35,17 @@ export const AlgoHome = () => (
         <Box sx={{ display: "flex", flexDirection: "column", gap: fr.spacing("3w") }}>
           <Typography className={fr.cx("fr-text--lg")}>La bonne alternance expose différents types d&apos;opportunités d&apos;emplois :</Typography>
           <Box sx={{ display: "flex", flexDirection: "column", gap: fr.spacing("3w") }}>
-            <Typography component="div" className={fr.cx("fr-text--lg")}>
-              <strong>Les offres d&apos;emploi</strong> identifiables grâce au tag <TagOffreEmploi /> qui sont de 3 types :
+            <Box className={fr.cx("fr-text--lg")}>
+              <Typography>
+                <strong>Les offres d&apos;emploi</strong> identifiables grâce au tag <TagOffreEmploi /> qui sont de 3 types :
+              </Typography>
               <ul>
                 <li>celles publiées directement sur notre plateforme</li>
                 <li>celles issues de nos partenaires : France Travail, Hellowork, MétéoJob et bien d’autres,</li>
                 <li>celles publiées par des écoles qui recrutent pour le compte des entreprises de leur réseau.</li>
               </ul>
-            </Typography>
-            <Typography component="div" className={fr.cx("fr-text--lg")}>
+            </Box>
+            <Typography className={fr.cx("fr-text--lg")}>
               <strong>Les candidatures spontanées :</strong> correspondant au marché caché de l'emploi. Chaque mois, un algorithme prédictif de France Travail analyse les
               recrutements des 6 années passées pour prédire ceux des 6 mois à venir. Grâce à ces données, il identifie une liste restreinte d'entreprises "à fort potentiel
               d'embauche en alternance" pour faciliter vos démarches de candidatures spontanées. Elles sont identifiées grâce au tag <TagCandidatureSpontanee />
