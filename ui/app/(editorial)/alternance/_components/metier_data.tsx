6c304a547770ab07c08b0e72425f5c4ba6d16f30
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-02-12 11:33:08 +0100

    feat: listing metiers base

---
+++ ui/app/(editorial)/alternance/_components/metier_data.tsx
@@ -0,0 +1,52 @@
+export const metierData = [
+  {
+    metier: "Assistant administratif",
+    slug: "assistant-administratif",
+    romes: ["M1607"],
+  },
+  {
+    metier: "Assistant marketing",
+    slug: "assistant-marketing",
+    romes: ["M1705"],
+  },
+  {
+    metier: "Assistant ressources humaines",
+    slug: "assistant-ressources-humaines",
+    romes: ["M1501"],
+  },
+  {
+    metier: "Chargé de communication",
+    slug: "charge-de-communication",
+    romes: ["E1103"],
+  },
+  {
+    metier: "Chargé de recrutement",
+    slug: "charge-de-recrutement",
+    romes: ["M1502"],
+  },
+  {
+    metier: "Comptable",
+    slug: "comptable",
+    romes: ["M1203"],
+  },
+  {
+    metier: "Contrôleur de gestion",
+    slug: "controleur-de-gestion",
+    romes: ["M1204"],
+  },
+  {
+    metier: "Data analyst",
+    slug: "data-analyst",
+    romes: ["M1403"],
+  },
+  {
+    metier: "Développeur cybersécurité",
+    slug: "developpeur-cybersecurite",
+    romes: ["M1802"],
+  },
+  {
+    metier: "Développeur web",
+    slug: "developpeur-web",
+    romes: ["M1805"],
+  },
+]
