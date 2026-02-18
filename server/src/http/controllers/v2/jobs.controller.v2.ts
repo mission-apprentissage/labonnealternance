eaa08ad180f7c370f00dac73de7cf574b6d6fbc8
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-02-18 10:48:17 +0100

    feat(lba-3584): remove jobs api v1 & v2

--- server/src/http/controllers/v2/jobs.controller.v2.ts
+++ server/src/http/controllers/v2/jobs.controller.v2.ts
@@ -1,14 +1,7 @@
-import { badRequest, internal } from "@hapi/boom"
+import { badRequest } from "@hapi/boom"
 import { JOB_STATUS_ENGLISH, zRoutes } from "shared"
-import { LBA_ITEM_TYPE } from "shared/constants/lbaitem"
-
-import dayjs from "shared/helpers/dayjs"
-import { s3SignedUrl } from "@/common/utils/awsUtils"
-import { trackApiCall } from "@/common/utils/sendTrackingEvent"
-import { sentryCaptureException } from "@/common/utils/sentryUtils"
-import { addExpirationPeriod, getFormulaires } from "@/services/formulaire.service"
 import type { Server } from "@/http/server"
-import { getUserFromRequest } from "@/security/authenticationService"
+
 import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 const config = {
@@ -19,92 +12,6 @@ const config = {
 }
 
 export default (server: Server) => {
-  server.get(
-    "/v2/jobs/establishment",
-    {
-      schema: zRoutes.get["/v2/jobs/establishment"],
-      config,
-      onRequest: server.auth(zRoutes.get["/v2/jobs/establishment"]),
-    },
-    async (req, res) => {
-      const { establishment_siret, email } = req.query
-
-      const establishment = await getDbCollection("recruiters").findOne({ establishment_siret, email })
-
-      if (!establishment) {
-        return res.status(400).send({ error: true, message: "Establishment not found" })
-      }
-
-      return res.status(200).send(establishment.establishment_id)
-    }
-  )
-
-  server.get(
-    "/v2/jobs/bulk",
-    {
-      schema: zRoutes.get["/v2/jobs/bulk"],
-      config,
-      onRequest: server.auth(zRoutes.get["/v2/jobs/bulk"]),
-    },
-    async (req, res) => {
-      const { query, select, page, limit } = req.query
-
-      const user = getUserFromRequest(req, zRoutes.get["/v2/jobs/bulk"]).value
-
-      const qs = query ? JSON.parse(query) : {}
-      const slt = select ? JSON.parse(select) : {}
-
-      const jobs = await getFormulaires({ ...qs, opco: user.organisation }, slt, { page, limit })
-
-      res.status(200)
-      return res.send(jobs)
-    }
-  )
-
-  server.post(
-    "/v2/jobs/provided/:id",
-    {
-      schema: zRoutes.post["/v2/jobs/provided/:id"],
-      onRequest: server.auth(zRoutes.post["/v2/jobs/provided/:id"]),
-      config,
-    },
-    async (req, res) => {
-      const { id } = req.params
-      const job = await getDbCollection("jobs_partners").findOne({ _id: id })
-      if (!job) {
-        throw badRequest("Job does not exist")
-      }
-
-      if (job.offer_status === JOB_STATUS_ENGLISH.POURVUE) {
-        throw badRequest("Job is already provided")
-      }
-      await getDbCollection("jobs_partners").findOneAndUpdate({ _id: id }, { $set: { offer_status: JOB_STATUS_ENGLISH.POURVUE } })
-      return res.status(204).send()
-    }
-  )
-
-  server.post(
-    "/v2/jobs/canceled/:id",
-    {
-      schema: zRoutes.post["/v2/jobs/canceled/:id"],
-      onRequest: server.auth(zRoutes.post["/v2/jobs/canceled/:id"]),
-      config,
-    },
-    async (req, res) => {
-      const { id } = req.params
-      const job = await getDbCollection("jobs_partners").findOne({ _id: id })
-      if (!job) {
-        throw badRequest("Job does not exists")
-      }
-
-      if (job.offer_status === JOB_STATUS_ENGLISH.ANNULEE) {
-        throw badRequest("Job is already canceled")
-      }
-      await getDbCollection("jobs_partners").findOneAndUpdate({ _id: id }, { $set: { offer_status: JOB_STATUS_ENGLISH.ANNULEE } })
-      return res.status(204).send()
-    }
-  )
-
   server.post(
     "/v2/_private/jobs/provided/:id",
     {
@@ -146,70 +53,6 @@ export default (server: Server) => {
       }
       await getDbCollection("jobs_partners").findOneAndUpdate({ _id: id }, { $set: { offer_status: JOB_STATUS_ENGLISH.ANNULEE } })
       return res.status(200).send({})
-    }
-  )
-
-  server.post(
-    "/v2/jobs/extend/:id",
-    {
-      schema: zRoutes.post["/v2/jobs/extend/:id"],
-      onRequest: server.auth(zRoutes.post["/v2/jobs/extend/:id"]),
-      config,
-    },
-    async (req, res) => {
-      const { id } = req.params
-      const job = await getDbCollection("jobs_partners").findOne({ _id: id })
-      if (!job) {
-        throw badRequest("Job does not exists")
-      }
-      if (addExpirationPeriod(dayjs()).isSame(dayjs(job.offer_expiration), "day")) {
-        throw badRequest("Job is already extended up to two month")
-      }
-
-      if (job.offer_status !== JOB_STATUS_ENGLISH.ACTIVE) {
-        throw badRequest("Job cannot be extended as it is not active")
-      }
-      await getDbCollection("jobs_partners").findOneAndUpdate({ _id: id }, { $set: { offer_expiration_date: addExpirationPeriod(dayjs()).toDate() } })
-      return res.status(204).send()
-    }
-  )
-
-  server.get(
-    "/v2/jobs/export",
-    {
-      schema: zRoutes.get["/v2/jobs/export"],
-      onRequest: server.auth(zRoutes.get["/v2/jobs/export"]),
-      config: {
-        rateLimit: {
-          max: 1,
-          timeWindow: "1s",
-        },
-      },
-    },
-    async (req, res) => {
-      const user = getUserFromRequest(req, zRoutes.get["/v2/jobs/export"]).value
-      const { source } = req.query
-      if (source === LBA_ITEM_TYPE.OFFRES_EMPLOI_LBA) {
-        try {
-          const key = `${LBA_ITEM_TYPE.OFFRES_EMPLOI_LBA}.json`
-          const url = await s3SignedUrl("storage", key, { expiresIn: 120 })
-          trackApiCall({ caller: user._id.toString(), api_path: `${zRoutes.get["/v2/jobs/export"].path}/${source}`, response: "OK" })
-          return res.send(url)
-        } catch (error) {
-          sentryCaptureException(error)
-          throw internal("Une erreur est survenue lors de la génération du lien de téléchargement.")
-        }
-      } else {
-        try {
-          const key = `${LBA_ITEM_TYPE.RECRUTEURS_LBA}.json`
-          const url = await s3SignedUrl("storage", key, { expiresIn: 120 })
-          trackApiCall({ caller: user._id.toString(), api_path: `${zRoutes.get["/v2/jobs/export"].path}/${source}`, response: "OK" })
-          return res.send(url)
-        } catch (error) {
-          sentryCaptureException(error)
-          throw internal("Une erreur est survenue lors de la génération du lien de téléchargement.")
-        }
-      }
     }
   )
 }
