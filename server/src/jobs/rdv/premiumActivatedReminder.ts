f592dfdc4c9ee41876a9809731c89858f25c9dda
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-02-24 19:20:43 +0100

    feat: adaptation du modèle wip

--- server/src/jobs/rdv/premiumActivatedReminder.ts
+++ server/src/jobs/rdv/premiumActivatedReminder.ts
@@ -57,6 +57,7 @@ export const premiumActivatedReminder = async () => {
         subject: "[Rappel] Trouvez et recrutez vos candidats sur Parcoursup avec La bonne alternance",
         template: getStaticFilePath("./templates/mail-cfa-premium-activated-reminder.mjml.ejs"),
         data: {
+          isParcoursup: true,
           images: {
             logoLba: `${config.publicUrl}/images/emails/logo_LBA.png?raw=true`,
             logoRf: `${config.publicUrl}/images/emails/logo_rf.png?raw=true`,
@@ -73,4 +74,70 @@ export const premiumActivatedReminder = async () => {
   }
 
   logger.info("Cron #premiumActivatedReminder done.")
+}
+
+export const premiumActivatedReminderAffelnet = async () => {
+  logger.info("Cron #premiumActivatedReminderAffelnet started.")
+
+  const [etablissementsActivated, eligibleTrainingsForAppointmentsFound] = await Promise.all([
+    getDbCollection("etablissements")
+      .find({
+        gestionnaire_email: {
+          $ne: null,
+        },
+        premium_activation_date: {
+          $ne: null,
+        },
+      })
+      .toArray(),
+    eligibleTrainingsForAppointmentService.find({ parcoursup_id: { $ne: null }, lieu_formation_email: { $ne: null } }),
+  ])
+
+  const etablissementWithParcoursup = etablissementsActivated.filter((etablissement) =>
+    eligibleTrainingsForAppointmentsFound.find((eligibleTrainingsForAppointment) => eligibleTrainingsForAppointment.etablissement_formateur_siret === etablissement.formateur_siret)
+  )
+
+  let targetedEmails: string[] = []
+
+  for (const etablissement of etablissementWithParcoursup) {
+    // Retrieve all emails
+    let establishmentEmails = eligibleTrainingsForAppointmentsFound.flatMap((eligibleTrainingsForAppointment) => {
+      if (eligibleTrainingsForAppointment.etablissement_formateur_siret === etablissement.formateur_siret) {
+        const email = eligibleTrainingsForAppointment.lieu_formation_email
+        return email ? [email] : []
+      } else {
+        return []
+      }
+    })
+
+    establishmentEmails = [...new Set(establishmentEmails)]
+
+    targetedEmails.push(...establishmentEmails)
+  }
+
+  targetedEmails = [...new Set(targetedEmails)]
+
+  for (const email of targetedEmails) {
+    try {
+      await mailer.sendEmail({
+        to: email,
+        subject: "[Rappel] Trouvez et recrutez vos candidats sur Choisir son affectation après la 3e avec La bonne alternance",
+        template: getStaticFilePath("./templates/mail-cfa-premium-activated-reminder.mjml.ejs"),
+        data: {
+          isAffelnet: true,
+          images: {
+            logoLba: `${config.publicUrl}/images/emails/logo_LBA.png?raw=true`,
+            logoRf: `${config.publicUrl}/images/emails/logo_rf.png?raw=true`,
+            optoutCfa: `${config.publicUrl}/images/emails/optout_cfa.png?raw=true`,
+          },
+          publicEmail: config.publicEmail,
+          utmParams: "utm_source=lba&utm_medium=email&utm_campaign=lba_cfa_rdva-premium-affelnet-rappel-annuel",
+        },
+      })
+    } catch (error) {
+      logger.error(error)
+    }
+  }
+
+  logger.info("Cron #premiumActivatedReminderAffelnet done.")
 }
