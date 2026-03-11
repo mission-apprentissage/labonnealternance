b8c8b2b9a3f7ba1fb1408240dd0c2f8f6e88e2d1
Lucas Détré <lucas.detre@beta.gouv.fr>
2026-03-11 13:59:25 +0100

    feat: guide cfa - la carte étudiant des métiers

--- ui/app/(editorial)/guide-cfa/la-carte-etudiant-des-metiers/page.tsx
+++ ui/app/(editorial)/guide-cfa/la-carte-etudiant-des-metiers/page.tsx
@@ -4,6 +4,9 @@ import { Paragraph } from "@/app/(editorial)/_components/Paragraph"
 import { UpdatedAtSection } from "@/app/(editorial)/_components/UpdatedAtSection"
 import { ARTICLES } from "@/app/(editorial)/guide-cfa/const"
 import { PAGES } from "@/utils/routes.utils"
+import { Section } from "@/app/(editorial)/_components/Section"
+import { ParagraphList } from "@/app/(editorial)/_components/ParagraphList"
+import { DsfrLink } from "@/components/dsfr/DsfrLink"
 
 export const metadata: Metadata = PAGES.static.guideCFALaCarteEtudiantDesMetiers.getMetadata()
 
@@ -18,7 +21,32 @@ const LaCarteEtudiantDesMetiersPage = () => {
       description={<></>}
       allerPlusLoinItems={[]}
     >
-      <Paragraph>Contenu en cours de rédaction.</Paragraph>
+      <Section>
+        <Paragraph>
+          La carte d’étudiant des métiers permet d’ouvrir droit à de nombreuses réductions (restaurant universitaire, cinéma, transports, musées, etc.) à l’instar des cartes
+          d’étudiant de l’enseignement supérieur.
+        </Paragraph>
+        <Paragraph>
+          Elle est valable sur l’ensemble du territoire national. L’établissement de formation doit délivrer la carte d’étudiant des métiers dans les 30 jours suivant votre
+          inscription.
+        </Paragraph>
+        <Paragraph>En cas de rupture du contrat en alternance, elle doit être récupérée et détruite par l’établissement de formation qui l’a délivrée.</Paragraph>
+        <Paragraph>Elle est systématiquement délivrée en contrat d’apprentissage. En contrat de professionnalisation, il faut en revanche remplir certaines conditions :</Paragraph>
+        <ParagraphList
+          listItems={[
+            "être inscrit dans une qualification enregistrée au Répertoire national des certifications professionnelles (RNCP) ;",
+            "être âgé de 16 à 25 ans ;",
+            "la formation doit durer au minimum 12 mois.",
+          ]}
+        />
+        <Paragraph>
+          Si vous êtes un organisme de formation{" "}
+          <DsfrLink href={PAGES.static.espaceProCreationCfa.getPath()} aria-label="Accédez à votre espace CFA connecté">
+            accédez à votre espace connecté
+          </DsfrLink>{" "}
+          pour télécharger le modèle de la carte étudiant des métiers.
+        </Paragraph>
+      </Section>
     </LayoutArticle>
   )
 }
