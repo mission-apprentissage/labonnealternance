589034b732e90c6c8cb1735f4aed7bd5b8e3cb78
Lucas Détré <lucas.detre@beta.gouv.fr>
2026-03-12 19:47:50 +0100

    feat: navigation CFA connecté

---
+++ ui/app/(espace-pro)/espace-pro/(connected)/_components/ConnectedHeaderNavigation.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import MainNavigation from "@codegouvfr/react-dsfr/MainNavigation"
+import { usePathname } from "next/navigation"
+import { PAGES } from "@/utils/routes.utils"
+
+type NavItem = {
+  text: string
+  linkProps: {
+    href: string
+  }
+}
+
+const CFA_NAV_ITEMS: NavItem[] = [
+  {
+    text: "Mes entreprises",
+    linkProps: {
+      href: PAGES.static.backCfaHome.getPath(),
+    },
+  },
+  {
+    text: "Carte d'étudiant des métiers",
+    linkProps: {
+      href: PAGES.static.espaceProCfaCarteDEtudiantDesMetiers.getPath(),
+    },
+  },
+]
+
+function isLinkActive(pathname: string, href: string): boolean {
+  if (href === "/") {
+    return pathname === "/"
+  }
+  return pathname.startsWith(href)
+}
+
+export function ConnectedHeaderNavigation() {
+  const pathname = usePathname()
+
+  const items = CFA_NAV_ITEMS.map((item) => ({
+    text: item.text,
+    linkProps: {
+      href: item.linkProps.href,
+      isActive: isLinkActive(pathname, item.linkProps.href),
+    },
+  }))
+
+  return <MainNavigation items={items} />
+}
