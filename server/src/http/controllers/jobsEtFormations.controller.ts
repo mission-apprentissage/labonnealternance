7796b2756c782989597efcca3bc5978e033135f1
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-03-20 10:20:51 +0100

    fix: rate limit

--- server/src/http/controllers/jobsEtFormations.controller.ts
+++ server/src/http/controllers/jobsEtFormations.controller.ts
@@ -1,11 +1,21 @@
 import type { Server } from "@/http/server"
 
 export default (server: Server) => {
-  server.get("/v1/jobsEtFormations", async (_req, res) =>
-    res.status(410).send({
-      error: "API_DECOMMISSIONED",
-      message: "Cette API n'est plus disponible. Veuillez utiliser la nouvelle adresse.",
-      new_endpoint: "https://api.apprentissage.beta.gouv.fr/fr",
-    })
+  server.get(
+    "/v1/jobsEtFormations",
+    {
+      config: {
+        rateLimit: {
+          max: 5,
+          timeWindow: 1000,
+        },
+      },
+    },
+    async (_req, res) =>
+      res.status(410).send({
+        error: "API_DECOMMISSIONED",
+        message: "Cette API n'est plus disponible. Veuillez utiliser la nouvelle adresse.",
+        new_endpoint: "https://api.apprentissage.beta.gouv.fr/fr",
+      })
   )
 }
