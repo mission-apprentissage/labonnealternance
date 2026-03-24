722a50894a15f6ac42efd484c0c878ebc796fb94
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-03-24 20:44:47 +0100

    fix: preview p1000

--- server/src/migrations/20251110154539-clean-migration.ts
+++
@@ -1,8 +0,0 @@
-import { getDatabase } from "@/common/utils/mongodbUtils"
-
-export const up = async () => {
-  const db = getDatabase()
-  await db.collection("changelog").deleteMany({})
-}
-
-export const requireShutdown: boolean = false
