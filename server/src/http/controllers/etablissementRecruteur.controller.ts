313ed7a590d15f95d909e822e058db7474e3fbfc
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-03-26 17:06:11 +0100

    feat: traitement cas spécifique

--- server/src/http/controllers/etablissementRecruteur.controller.ts
+++ server/src/http/controllers/etablissementRecruteur.controller.ts
@@ -78,6 +78,10 @@ export default (server: Server) => {
       }
       if (!entrepriseOpt) {
         const siretResponse = await getEntrepriseDataFromSiret({ siret, type: cfa_delegated_siret ? CFA : ENTREPRISE })
+        if ("errorCode" in siretResponse && siretResponse.errorCode === BusinessErrorCodes.IS_CFA) {
+          // pas d'enregistrement de la réponse dans entreprise pour ce code d'erreur
+          throw badRequest(siretResponse.message, siretResponse)
+        }
         entrepriseOpt = await upsertEntrepriseData(siret, "création de compte entreprise", siretResponse, false)
         if ("error" in siretResponse) {
           throw badRequest(siretResponse.message, siretResponse)
