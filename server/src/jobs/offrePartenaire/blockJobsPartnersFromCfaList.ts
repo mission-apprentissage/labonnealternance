a1fc16fe55a6a5e157dbcda6067061a6dad8c879
Rémy Auricoste <remy.auricoste@gmail.com>
2026-03-27 14:31:34 +0100

    feat(lbac-3653): classification cfa: ajout d une white list (#2764)
    Co-authored-by: Kevin Barnoin <k.barnoin@gmail.com>
    Co-authored-by: kevbarns <kevin.barnoin@ext.beta.gouv.fr>

--- server/src/jobs/offrePartenaire/blockJobsPartnersFromCfaList.ts
+++ server/src/jobs/offrePartenaire/blockJobsPartnersFromCfaList.ts
@@ -1,5 +1,6 @@
+import type { Filter } from "mongodb"
 import type { IComputedJobsPartners } from "shared/models/jobsPartnersComputed.model"
-import { COMPUTED_ERROR_SOURCE, JOB_PARTNER_BUSINESS_ERROR } from "shared/models/jobsPartnersComputed.model"
+import { COMPUTED_ERROR_SOURCE, JOB_PARTNER_BUSINESS_ERROR, PARTNER_WHITELIST } from "shared/models/jobsPartnersComputed.model"
 import type { FillComputedJobsPartnersContext } from "./fillComputedJobsPartners"
 import { fillFieldsForComputedPartnersFactory } from "./fillFieldsForPartnersFactory"
 import { isCompanyInBlockedCfaList } from "./isCompanyInBlockedCfaList"
@@ -21,12 +22,19 @@ const hasBlockedCfaMention = ({
 export const blockJobsPartnersFromCfaList = async ({ addedMatchFilter }: FillComputedJobsPartnersContext) => {
   const filledFields = ["business_error"] as const satisfies (keyof IComputedJobsPartners)[]
 
+  const filters: Filter<IComputedJobsPartners>[] = [{ partner_label: { $nin: PARTNER_WHITELIST } }]
+  if (addedMatchFilter) {
+    filters.push(addedMatchFilter)
+  }
+
   return fillFieldsForComputedPartnersFactory({
     job: COMPUTED_ERROR_SOURCE.BLOCK_CFA_NAME,
     sourceFields,
     filledFields,
     groupSize: 500,
-    addedMatchFilter,
+    addedMatchFilter: {
+      $and: filters,
+    },
     getData: async (documents) => {
       return documents.map((document) => {
         const { _id, workplace_name, offer_description, workplace_description, business_error } = document
