08df824690df5b0af1890d4dafb66cc0245eb90d
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-01 14:48:48 +0200

    feat(lba-3757): ajustements accessibilité balises navigation (#2811)

--- ui/app/(espace-pro)/espace-pro/(from-mail)/layout.tsx
+++ ui/app/(espace-pro)/espace-pro/(from-mail)/layout.tsx
@@ -1,7 +1,7 @@
 import { fr } from "@codegouvfr/react-dsfr"
+import SkipLinks from "@codegouvfr/react-dsfr/SkipLinks"
 import { Box, Container } from "@mui/material"
 import type { PropsWithChildren } from "react"
-
 import { Footer } from "@/app/_components/Footer"
 import { PublicHeader } from "@/app/_components/PublicHeader"
 import { getSession } from "@/utils/getSession"
@@ -11,8 +11,15 @@ export default async function RecruteurLayout({ children }: PropsWithChildren) {
 
   return (
     <>
+      <SkipLinks
+        links={[
+          { label: "Menu", anchor: "#header-links" },
+          { label: "Contenu", anchor: "#main-content" },
+          { label: "Pied de page", anchor: "#footer-links" },
+        ]}
+      />
       <PublicHeader user={user} />
-      <Box component="main" role="main" sx={{ marginBottom: fr.spacing("8v") }}>
+      <Box component="main" role="main" id="main-content" tabIndex={-1} sx={{ marginBottom: fr.spacing("8v") }}>
         <Container maxWidth="xl" sx={{ marginTop: fr.spacing("4v") }}>
           {children}
         </Container>
