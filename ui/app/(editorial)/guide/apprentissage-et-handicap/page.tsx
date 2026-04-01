09d9ba9012c52cce81e7f8b2574bd38a9712ac73
Lucas Détré <lucas.detre.ext@beta.gouv.fr>
2026-04-01 11:32:01 +0200

    feat: modification de l'article handicap

--- ui/app/(editorial)/guide/apprentissage-et-handicap/page.tsx
+++ ui/app/(editorial)/guide/apprentissage-et-handicap/page.tsx
@@ -86,17 +86,16 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
             listItems={[
               <>
                 <Typography component="span" fontWeight={"bold"}>
-                  dès l’âge de 16 ans (sauf dérogation) et sans limite d’âge maximal
-                </Typography>{" "}
-                (contrairement au contrat d’apprentissage de droit commun) pour :
+                  dès l’âge de 16 ans (sauf dérogation) et sans limite d’âge maximal (contrairement au contrat d’apprentissage de droit commun) pour :
+                </Typography>
               </>,
             ]}
           />
           <Box sx={{ ml: fr.spacing("10v"), mt: 0 }}>
             <ParagraphList
               listItems={[
-                "Les personnes qui bénéficient de la reconnaissance de la qualité de travailleur handicapé (RQTH) ; ",
-                "les personnes bénéficiaires de l'obligation d'emploi (BOE) (2), à l’exception des ayants droit de victimes ou pensionnés de guerre ; ",
+                "les personnes qui bénéficient de la reconnaissance de la qualité de travailleur handicapé (RQTH) ;",
+                "les personnes bénéficiaires de l'obligation d'emploi (BOE), à l’exception des ayants droit de victimes ou pensionnés de guerre ;",
               ]}
             />
           </Box>
@@ -104,16 +103,15 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
             listItems={[
               <>
                 <Typography component="span" fontWeight={"bold"}>
-                  aux jeunes âgés de 15 à 20 ans (“équivalences jeunes”)
+                  aux jeunes âgés de 15 à 20 ans (“équivalences jeunes”), qui bénéficient soit :
                 </Typography>
-                , qui bénéficient soit :{" "}
               </>,
             ]}
           />
           <Box sx={{ ml: fr.spacing("10v"), mt: 0 }}>
             <ParagraphList
               listItems={[
-                "D’un projet personnalisé de scolarisation (PPS) ;",
+                "d’un projet personnalisé de scolarisation (PPS) ;",
                 "de l’attribution de l’allocation d’éducation de l’enfant handicapé (AEEH) ;",
                 "de la prestation de compensation (PCH).",
               ]}
@@ -129,13 +127,15 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
       <Section title="Quelle est la durée du contrat ?">
         <Paragraph>En principe, la durée d’un contrat d’apprentissage varie, selon la qualification préparée, entre 6 mois et 3 ans.</Paragraph>
         <Paragraph>
-          La durée maximale du contrat d'apprentissage aménagé peut, elle, être portée jusqu’à 4 ans maximum. En effet, en fonction des besoins de l’apprenti en situation de
-          handicap, l’enseignement délivré par le CFA en vue de conduire au diplôme prévu au contrat, est réparti sur une période égale à la durée habituelle d’apprentissage pour
-          la formation considérée, augmentée d’un an au plus.
+          <Typography fontWeight={"bold"} component={"span"}>
+            La durée maximale du contrat d'apprentissage aménagé peut, elle, être portée jusqu’à 4 ans maximum pour les personnes en situation de handicap.
+          </Typography>{" "}
+          En effet, en fonction des besoins de l’apprenti en situation de handicap, l’enseignement délivré par le CFA en vue de conduire au diplôme prévu au contrat, est réparti
+          sur une période égale à la durée habituelle d’apprentissage pour la formation considérée, augmentée d’un an au plus.
         </Paragraph>
       </Section>
       <Section title="Comment se déroule la formation ?">
-        <Paragraph>La formation peut se dérouler dans tout centre de formation d’apprentis (CFA).</Paragraph>
+        <Paragraph>La formation peut se dérouler dans l’ensemble des centres de formation d’apprentis (CFA).</Paragraph>
         <Paragraph>
           Les CFA doivent tenir compte des besoins particuliers des apprentis en situation de handicap en procédant aux aménagements nécessaires pour le suivi de la formation, y
           compris pédagogiques.
@@ -150,15 +150,24 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
           La loi prévoit une majoration du niveau de prise en charge des contrats d’apprentissage aménagés, au bénéfice des CFA pour les aider à financer les aménagements. Cette
           majoration est limitée à 4000 euros par année d’exécution du contrat.
         </Paragraph>
+        <Paragraph>
+          Par ailleurs, une aide au recrutement d’un apprenti en situation de handicap est prévue. Son montant est fixé par décret :{" "}
+          <DsfrLink
+            href="https://travail-emploi.gouv.fr/laide-aux-employeurs-qui-recrutent-en-apprentissage"
+            aria-label="Consulter le site du Ministère du Travail et des Solidarités"
+          >
+            L'aide aux employeurs qui recrutent en apprentissage | Ministère du Travail et des Solidarités
+          </DsfrLink>
+        </Paragraph>
       </Section>
       <Section title="Quelle est la rémunération ?">
         <Paragraph>
-          Comme tout apprenti, la personne en situation de handicap est rémunérée en pourcentage du Smic ou du SMC (salaire minimum conventionnel de l’emploi occupé pour les 21 ans
-          et plus), variable selon son âge et sa progression dans le ou les cycles de formation faisant l’objet de l’apprentissage.
+          Comme tous les apprentis, la personne en situation de handicap est rémunérée en pourcentage du Smic ou du SMC (salaire minimum conventionnel de l’emploi occupé pour les
+          21 ans et plus), variable selon son âge et sa progression dans le ou les cycles de formation faisant l’objet de l’apprentissage.
         </Paragraph>
         <Paragraph>
           L’apprenti bénéficie d’une rémunération variant en fonction de son âge ; en outre, sa rémunération progresse chaque nouvelle année d’exécution de son contrat. Le salaire
-          minimum perçu par l’apprenti correspond à un pourcentage du Smic ou du SMC.
+          minimum perçu par l’apprenti correspond à un pourcentage du Smic ou du salaire minimum conventionnel.
         </Paragraph>
         <TableArticle
           headers={["Année d’exécution du contrat", "Apprenti de moins de 18 ans", "Apprenti de 18 ans à 20 ans", "Apprenti de 21 ans à 25 ans", "Apprenti de 26 ans et plus"]}
@@ -197,7 +206,7 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
                 <Typography component={"span"} fontWeight={"bold"}>
                   Pour les employeurs du secteur privé :
                 </Typography>{" "}
-                l’aide à l’embauche en contrat d’apprentissage d’une personne handicapée proposée par l’
+                l’aide à l’embauche en contrat d’apprentissage d’une personne handicapée proposée par l'
                 <DsfrLink href="https://www.agefiph.fr/" aria-label="Consulter le site de l'Agefiph">
                   Agefiph
                 </DsfrLink>
@@ -250,10 +259,10 @@ const ApprentissageEtHandicapPage = async ({ searchParams }: { searchParams: Pro
           d’apprentissage (décret n° 2020-530 du 5 mai 2020).
         </Paragraph>
         <Paragraph>
-          La titularisation des apprentis BOETH à l’issue de leur contrat d’apprentissage dans la fonction publique n’est pas automatique : il est nécessaire que l’apprenti fasse
-          acte de candidature et que l’employeur public décide de le titulariser en tenant compte notamment des capacités du candidat à exercer les missions, de sa motivation, du
-          bilan de la période d'apprentissage, de son parcours professionnel ainsi que de ses connaissances sur l'environnement professionnel de l'emploi ou des emplois faisant
-          l'objet de sa candidature.
+          La titularisation des apprentis BOETH à l’issue de leur contrat d’apprentissage dans la fonction publique n’est pas automatique : il est nécessaire que l’apprenti se
+          porte candidat et que l’employeur public décide de le titulariser en tenant compte notamment des capacités du candidat à exercer les missions, de sa motivation, du bilan
+          de la période d'apprentissage, de son parcours professionnel ainsi que de ses connaissances sur l'environnement professionnel de l'emploi ou des emplois faisant l'objet
+          de sa candidature.
         </Paragraph>
         <Paragraph>
           Ces recrutements sont pris en compte dans la proportion minimale de 6% des postes offerts aux concours dans un corps, réservée aux modalités spécifiques de recrutement
