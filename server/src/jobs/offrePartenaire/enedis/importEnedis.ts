9c19b67262abb2ef7d7e77d4246eab89acb5a501
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-01 17:16:16 +0200

    feat: fondations

---
+++ server/src/jobs/offrePartenaire/enedis/importEnedis.ts
@@ -0,0 +1,39 @@
+import { JOBPARTNERS_LABEL } from "shared/models/jobsPartners.model"
+import config from "@/config"
+import { importFromStreamInXml } from "@/jobs/offrePartenaire/importFromStreamInXml"
+import { importFromUrlInXml } from "@/jobs/offrePartenaire/importFromUrlInXml"
+import { rawToComputedJobsPartners } from "@/jobs/offrePartenaire/rawToComputedJobsPartners"
+
+//const rawCollectionName = rawEnedis.collectionName
+const rawCollectionName = "raw_enedis"
+const offerXmlTag = "job"
+
+export const importEnedisRaw = async (sourceStream?: NodeJS.ReadableStream) => {
+  if (sourceStream) {
+    await importFromStreamInXml({
+      destinationCollection: rawCollectionName,
+      offerXmlTag,
+      stream: sourceStream,
+      importName: JOBPARTNERS_LABEL.ENEDIS,
+      conflictingOpeningTagWithoutAttributes: true,
+    })
+  } else {
+    await importFromUrlInXml({
+      destinationCollection: rawCollectionName,
+      url: config.enedisUrl,
+      offerXmlTag,
+      partnerLabel: JOBPARTNERS_LABEL.ENEDIS,
+      conflictingOpeningTagWithoutAttributes: true,
+    })
+  }
+}
+
+export const importEnedisToComputed = async () => {
+  await rawToComputedJobsPartners({
+    collectionSource: rawCollectionName,
+    partnerLabel: JOBPARTNERS_LABEL.ENEDIS,
+    zodInput: ZEnedisJob,
+    mapper: enedisJobToJobsPartners,
+    documentJobRoot: offerXmlTag,
+  })
+}
