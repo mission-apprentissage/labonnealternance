3edf1bd7924e318d87d2fbb62710786527e02f3e
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-07 15:35:37 +0200

    feat: logos en-tête non optimisés

--- ui/app/(candidat)/(1jeune1solution)/components/Header1J1S.tsx
+++ ui/app/(candidat)/(1jeune1solution)/components/Header1J1S.tsx
@@ -18,10 +18,11 @@ export const DsfrHeaderProps1J1S: Omit<HeaderProps, "navigation"> = {
     "aria-label": "Accueil - La bonne alternance",
   },
   quickAccessItems: [
-    <Box>1jeune1solution</Box>,
-    <Box>Gros plus jaune</Box>,
-
-    <Image width="155" height="40" src="/images/logo_LBA.svg" alt="La bonne alternance" />,
+    <Box display="flex" flexDirection="row" alignItems="center" gap={0.5} key="header-1j1s-title">
+      <Image width="207" height="25" src="/images/1j1s/logo-1j1s.svg" alt="Logo un jeune une solution" />
+      <Image width="83" height="83" src="/images/1j1s/plus-jaune.svg" aria-hidden="true" alt="" />
+      <Image width="155" height="40" src="/images/logo_LBA.svg" aria-label="La bonne alternance" alt="La bonne alternance" />
+    </Box>,
   ],
   id: "header-1j1s-links",
 }
