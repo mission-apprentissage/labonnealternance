84cdcea3f56350aa4fd67f5a21d55db8c8c77e1c
Lucas Détré <lucas.detre.ext@beta.gouv.fr>
2026-04-09 15:40:43 +0200

    fix(lba-3512): ui espace cfa / entreprises partenaires (#2784)

--- ui/app/(espace-pro)/espace-pro/(connected)/_components/CfaHomeEntrepriseMenu.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/_components/CfaHomeEntrepriseMenu.tsx
@@ -1,7 +1,7 @@
 import type { IRecruiterJson } from "shared"
-
 import type { PopoverMenuAction } from "@/app/(espace-pro)/_components/PopoverMenu"
 import { PopoverMenu } from "@/app/(espace-pro)/_components/PopoverMenu"
+import { DsfrIcon } from "@/components/DsfrIcon"
 import { PAGES } from "@/utils/routes.utils"
 
 export const CfaHomeEntrepriseMenu = ({
@@ -19,6 +19,7 @@ export const CfaHomeEntrepriseMenu = ({
       ariaLabel: `Voir les offres de l'entreprise ${row.establishment_raison_sociale}`,
       link: PAGES.dynamic.backCfaPageEntreprise(row.establishment_id).getPath(),
       type: "link",
+      icon: <DsfrIcon name="fr-icon-briefcase-line" size={16} />,
     },
     {
       label: "Supprimer l'entreprise",
@@ -28,6 +29,7 @@ export const CfaHomeEntrepriseMenu = ({
         setCurrentEntreprise(row)
       },
       type: "button",
+      icon: <DsfrIcon name="fr-icon-delete-line" size={16} />,
     },
   ]
   return <PopoverMenu actions={actions} title={`Actions sur l'entreprise ${row.establishment_raison_sociale}`} />
