530a1186d91a5386fce993baa2b05741dbf80189
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-15 11:29:44 +0200

    feat: bloc triple

--- ui/app/(1jeune1solution)/1jeune1solution-recruteurs/page.tsx
+++ ui/app/(1jeune1solution)/1jeune1solution-recruteurs/page.tsx
@@ -1,5 +1,6 @@
 import { fr } from "@codegouvfr/react-dsfr"
-import { Container } from "@mui/material"
+import { Box, Container, Typography } from "@mui/material"
+import NextImage from "next/image"
 import Social from "@/app/(1jeune1solution)/components/Social"
 
 export default async function unJeune1Solution({ searchParams }: { searchParams: Promise<Record<string, string>> }) {
@@ -16,6 +17,49 @@ export default async function unJeune1Solution({ searchParams }: { searchParams:
       }}
       maxWidth="xl"
     >
+      <Box
+        sx={{ display: "flex", flexDirection: { xs: "column", md: "row" }, gap: fr.spacing("6v"), px: { xs: fr.spacing("3v"), md: fr.spacing("6v"), lg: 0 }, py: fr.spacing("8v") }}
+      >
+        <Box sx={{ flex: 1 }}>
+          <NextImage src="/images/1j1s/graph.svg" width={89} height={80} fetchPriority="low" alt="" unoptimized aria-hidden="true" />
+
+          <Typography
+            sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, fontSize: "50px", lineHeight: "60px", mt: fr.spacing("6v"), mb: fr.spacing("3v") }}
+          >
+            3,5 M
+          </Typography>
+          <Typography sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, lineHeight: "32px", fontSize: "20px", mb: fr.spacing("3v") }}>
+            DE VISITEURS
+          </Typography>
+          <Typography sx={{ fontSize: "20px", lineHeight: "28px", fontWeight: 700 }}>Sur La bonne alternance en 2025</Typography>
+        </Box>
+
+        <Box sx={{ flex: 1 }}>
+          <NextImage src="/images/1j1s/heart.svg" width={70} height={80} fetchPriority="low" alt="" unoptimized aria-hidden="true" />
+          <Typography
+            sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, fontSize: "50px", lineHeight: "60px", mt: fr.spacing("6v"), mb: fr.spacing("3v") }}
+          >
+            200 000
+          </Typography>
+          <Typography sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, lineHeight: "32px", fontSize: "20px", mb: fr.spacing("3v") }}>
+            CANDIDATS
+          </Typography>
+          <Typography sx={{ fontSize: "20px", lineHeight: "28px", fontWeight: 700 }}>postulent aux offres chaque année</Typography>
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <NextImage src="/images/1j1s/scroll.svg" width={71} height={80} fetchPriority="low" alt="" unoptimized aria-hidden="true" />
+          <Typography
+            sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, fontSize: "50px", lineHeight: "60px", mt: fr.spacing("6v"), mb: fr.spacing("3v") }}
+          >
+            30%
+          </Typography>
+          <Typography sx={{ background: "#FFE817", width: "fit-content", padding: "0", fontWeight: 800, lineHeight: "32px", fontSize: "20px", mb: fr.spacing("3v") }}>
+            1 APPRENTI SUR 3
+          </Typography>
+          <Typography sx={{ fontSize: "20px", lineHeight: "28px", fontWeight: 700 }}>reste dans l'entreprise qui l'a formé</Typography>
+        </Box>
+      </Box>
+
       <Social />
     </Container>
   )
