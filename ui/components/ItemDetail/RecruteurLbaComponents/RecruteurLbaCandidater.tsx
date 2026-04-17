6b105815f836b04d1f2c418806ebeec386c62505
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-17 18:24:31 +0200

    feat: ok sur recruteurs_lba

--- ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaCandidater.tsx
+++ ui/components/ItemDetail/RecruteurLbaComponents/RecruteurLbaCandidater.tsx
@@ -16,11 +16,15 @@ export function RecruteurLbaCandidater({ item }: { item: ILbaItemLbaCompanyJson 
           backgroundColor: "#FEECC2", // fr introuvable.
           color: fr.colors.decisions.background.actionHigh.greenTilleulVerveine.default,
           fontStyle: "italic",
+          px: "6px",
+          py: "2px",
         }}
-        className={fr.cx("ri-spy-line", "fr-text--sm", "fr-icon--sm")}
+        className={fr.cx("fr-text--sm")}
       >
-        {" "}
-        Nous n’avons pas de contact pour cette entreprise, peut-être que vous en trouverez un sur internet !
+        🕵️{" "}
+        {item.contact.phone
+          ? `Nous n’avons pas d’email pour cette entreprise, mais vous pouvez l’appeler au ${item.contact.phone} et demander s’ils recrutent des alternants !`
+          : "Nous n’avons pas de contact pour cette entreprise, peut-être que vous en trouverez un sur internet !"}
       </Typography>
     )
   }
