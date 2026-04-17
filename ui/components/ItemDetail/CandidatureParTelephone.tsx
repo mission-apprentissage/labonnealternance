15896ed3dd792fac04cecd6c5ed1e66c950a7b62
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-04-17 18:42:13 +0200

    feat: message pas de contact autre que téléphone

--- ui/components/ItemDetail/CandidatureParTelephone.tsx
+++ ui/components/ItemDetail/CandidatureParTelephone.tsx
@@ -1,39 +1,24 @@
 import { fr } from "@codegouvfr/react-dsfr"
-import Button from "@codegouvfr/react-dsfr/Button"
 import { Box, Typography } from "@mui/material"
-import { useDisclosure } from "@/common/hooks/useDisclosure"
-import { DsfrLink } from "@/components/dsfr/DsfrLink"
-import { ModalReadOnly } from "@/components/ModalReadOnly"
 
-export default function CandidatureParTelephone({ companyName, contactPhone, contactName }: { companyName: string; contactPhone: string; contactName: string }) {
-  const { isOpen, onClose, onOpen } = useDisclosure()
+export default function CandidatureParTelephone({ contactPhone }: { contactPhone: string }) {
   return (
     <>
       <Box sx={{ my: fr.spacing("4v") }}>
-        <Button data-tracking-id="postuler-offre-partenaire" onClick={onOpen}>
-          Contacter le recruteur
-        </Button>
+        <Typography
+          component="span"
+          sx={{
+            backgroundColor: "#FEECC2", // fr introuvable.
+            color: fr.colors.decisions.background.actionHigh.greenTilleulVerveine.default,
+            fontStyle: "italic",
+            px: "6px",
+            py: "2px",
+          }}
+          className={fr.cx("fr-text--sm")}
+        >
+          🕵️{` Nous n’avons pas d’email pour cette entreprise, mais vous pouvez l’appeler au ${contactPhone} et demander s’ils recrutent des alternants !`}
+        </Typography>
       </Box>
-      <ModalReadOnly isOpen={isOpen} onClose={onClose} size="md">
-        <Box sx={{ p: fr.spacing("6v") }}>
-          <Typography variant="h2" sx={{ mb: fr.spacing("4v") }}>
-            Postuler à l'offre de {companyName}
-          </Typography>
-          <Typography>L'entreprise préfère être contactée par téléphone. Pour proposer votre candidature, appelez directement le numéro suivant :</Typography>
-          <Typography variant="h6" sx={{ fontWeight: 700, textAlign: "center" }}>
-            {contactName}
-          </Typography>
-          <Box sx={{ display: "flex", justifyContent: "center", my: fr.spacing("6v") }}>
-            <Box sx={{ backgroundColor: "#ECECFE", p: fr.spacing("6v") }}>
-              <DsfrLink href={`tel:${contactPhone}`}>
-                <Typography variant="h6" sx={{ color: "#000091", fontWeight: 700 }}>
-                  {contactPhone}
-                </Typography>
-              </DsfrLink>
-            </Box>
-          </Box>
-        </Box>
-      </ModalReadOnly>
     </>
   )
 }
--- ui/components/ItemDetail/PartnerJobComponents/PartnerJobPostuler.tsx
+++ ui/components/ItemDetail/PartnerJobComponents/PartnerJobPostuler.tsx
@@ -39,7 +39,7 @@ export const PartnerJobPostuler = ({ job }: { job: ILbaItemPartnerJobJson }) => 
   }
 
   if (job.contact?.phone) {
-    return <CandidatureParTelephone companyName={job.company.name || null} contactName={job.contact.name || null} contactPhone={job.contact.phone || null} />
+    return <CandidatureParTelephone contactPhone={job.contact.phone} />
   }
 
   return null
