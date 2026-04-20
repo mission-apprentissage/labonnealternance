ec013c45e3f755760035f8e8e43d9517eee17925
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-04-20 20:39:17 +0200

    fix(lbac-3944): upsertCfa atomic findOneAndUpdate

--- server/src/services/cfa.service.ts
+++ server/src/services/cfa.service.ts
@@ -5,21 +5,14 @@ import { getDbCollection } from "@/common/utils/mongodbUtils"
 
 export const upsertCfa = async (siret: string, cfaFields: Omit<ICFA, "_id" | "createdAt" | "updatedAt" | "origin" | "siret">, origin: string): Promise<ICFA> => {
   const now = new Date()
-  let cfa = await getDbCollection("cfas").findOne({ siret })
-  if (cfa) {
-    cfa = await getDbCollection("cfas").findOneAndUpdate({ siret }, { $set: { ...cfaFields, siret } }, { returnDocument: "after" })
-    if (!cfa) throw new Error("inattendu: pas de cfa")
-    return cfa
-  } else {
-    const createCfaFields: ICFA = {
-      ...cfaFields,
-      origin,
-      siret,
-      _id: new ObjectId(),
-      createdAt: now,
-      updatedAt: now,
-    }
-    await getDbCollection("cfas").insertOne(createCfaFields)
-    return createCfaFields
-  }
+  const cfa = await getDbCollection("cfas").findOneAndUpdate(
+    { siret },
+    {
+      $set: { ...cfaFields, siret, updatedAt: now },
+      $setOnInsert: { _id: new ObjectId(), createdAt: now, origin },
+    },
+    { upsert: true, returnDocument: "after" }
+  )
+  if (!cfa) throw new Error("inattendu: pas de cfa")
+  return cfa
 }
