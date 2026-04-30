e09e994bb2091327a9cbbd0890a08ef0f7ac50c2
Rémy Auricoste <remy.auricoste@gmail.com>
2026-04-30 14:18:01 +0200

    fix(lbac-3962): seo google search: fix valeurs vides

--- ui/components/ItemDetail/JobPostingSchema.tsx
+++ ui/components/ItemDetail/JobPostingSchema.tsx
@@ -37,16 +37,16 @@ const buildJobPostingSchema = ({ title, description, id, job }: JobPostingSchema
     employmentType: "FULL_TIME",
     hiringOrganization: {
       "@type": "Organization",
-      name: job?.company?.name,
+      name: job?.company?.name ?? "Non renseigné",
     },
     jobLocation: {
       "@type": "Place",
       address: {
         "@type": "PostalAddress",
-        streetAddress: job?.place?.numberAndStreet ?? null,
-        addressLocality: job?.place?.city ?? null,
-        addressRegion: null,
-        postalCode: job?.place?.zipCode ?? null,
+        streetAddress: job?.place?.numberAndStreet ?? "Non renseigné",
+        addressLocality: job?.place?.city ?? "Non renseigné",
+        addressRegion: "Non renseigné",
+        postalCode: job?.place?.zipCode ?? "Non renseigné",
         addressCountry: "France",
       },
     },
