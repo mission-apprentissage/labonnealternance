90f4a277b87e48e89cd422480d374393e2cc9360
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-05-06 13:01:46 +0200

    feat: some improvements

--- ui/app/(editorial)/guide-recruteur/page.tsx
+++ ui/app/(editorial)/guide-recruteur/page.tsx
@@ -37,10 +37,10 @@ const GuideRecruteurPage = () => {
       />
       <Breadcrumb pages={[PAGES.static.guideRecruteur]} />
       <DefaultContainer>
-        <Box display={{ md: "flex", xs: "none" }} id="editorial-content-container">
+        <Box sx={{ display: { md: "flex", xs: "none" }, position: "absolute", overflow: "visible" }}>
           <GuideHeaderIllustration />
         </Box>
-        <Box my={{ md: fr.spacing("4v") }}>
+        <Box id="editorial-content-container" my={{ md: fr.spacing("4v") }}>
           <Grid container spacing={fr.spacing("8v")} p={{ md: fr.spacing("6v") }} pt={{ md: fr.spacing("8v") }} sx={{ position: "relative", zIndex: 2 }}>
             <Grid size={{ md: 8, xs: 12 }} gap={fr.spacing("4v")} display={"flex"} flexDirection={"column"}>
               <Typography component={"h1"} variant="h1" sx={{ color: fr.colors.decisions.text.default.info.default }} gutterBottom>
