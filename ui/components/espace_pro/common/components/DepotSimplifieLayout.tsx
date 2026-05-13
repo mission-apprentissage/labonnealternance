8e2c6433b9bb40ce4d5d538287a34fd49c08d3fc
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-05-13 13:34:51 +0200

    feat: identification css fautif

--- ui/components/espace_pro/common/components/DepotSimplifieLayout.tsx
+++ ui/components/espace_pro/common/components/DepotSimplifieLayout.tsx
@@ -15,11 +15,11 @@ export const DepotSimplifieLayout = ({ children }: { children: React.ReactNode }
 export const DepotSimplifieStyling = ({ children }: { children: React.ReactNode }) => (
   <Box
     sx={{
-      "& p": {
-        fontSize: ["12px", "12px", "12px", "0.75rem"],
-        lineHeight: ["20px", "20px", "20px", "24px"],
-        marginBottom: 0,
-      },
+      // "& p": {
+      //   fontSize: ["12px", "12px", "12px", "0.75rem"],
+      //   lineHeight: ["20px", "20px", "20px", "24px"],
+      //   marginBottom: 0,
+      // },
       "& p.big": {
         fontSize: ["12px", "12px", "12px", "20px"],
         lineHeight: ["20px", "20px", "20px", "32px"],
