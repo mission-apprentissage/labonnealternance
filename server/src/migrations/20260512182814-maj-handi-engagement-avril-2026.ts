7bef6006daa14363066728ab61dd1145f222b263
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-05-13 12:30:29 +0200

    feat: ajout de log

--- server/src/migrations/20260512182814-maj-handi-engagement-avril-2026.ts
+++ server/src/migrations/20260512182814-maj-handi-engagement-avril-2026.ts
@@ -1,8 +1,11 @@
 import { addJob } from "job-processor"
+import { logger } from "@/common/logger"
 
 export const up = async () => {
+  logger.info("Scheduling jobs for refreshEntrepriseEngagementJobsPartners migration")
   await addJob({ name: "refreshReferentielEngagementFranceTravail", queued: true })
   await addJob({ name: "refreshEntrepriseEngagementJobsPartners", queued: true })
+  logger.info("Jobs scheduled for refreshEntrepriseEngagementJobsPartners migration")
 }
 
 // set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
