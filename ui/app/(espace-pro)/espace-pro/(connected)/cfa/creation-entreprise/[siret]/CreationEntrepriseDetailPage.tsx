aa8eac12df3e39245f299c8a3a761af2f236669f
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-05-21 13:09:54 +0200

    fix: gap dans flex

--- ui/app/(espace-pro)/espace-pro/(connected)/cfa/creation-entreprise/[siret]/CreationEntrepriseDetailPage.tsx
+++ ui/app/(espace-pro)/espace-pro/(connected)/cfa/creation-entreprise/[siret]/CreationEntrepriseDetailPage.tsx
@@ -119,7 +119,7 @@ function CreationEntrepriseDetail({ siret }: { siret: string }) {
           <Typography component="h2" sx={{ fontSize: "24px", fontWeight: "bold" }}>
             Informations de contact
           </Typography>
-          <Typography sx={{ fontSize: "20px", textAlign: "justify", mt: fr.spacing("2v") }}>
+          <Typography sx={{ fontSize: "20px", mt: fr.spacing("2v") }}>
             Il s’agit des informations de contact de votre entreprise partenaire. Ces informations ne seront pas visibles sur l’offre.
           </Typography>
         </Box>
