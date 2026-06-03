13931b8d1930077f98d2d8411b8e2327f5ba1482
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-06-03 10:29:13 +0200

    feat: supprimer le champ establishment_id de jobs_partners
    - supprime la branche DB morte dans establishmentIdToUserIdAndSiret
    - rend la fonction synchrone
    - retire le champ du modèle Zod et l'index associé
    - ajoute une migration $unset sur la collection
    - retire la projection d'exclusion dans exportJobsToS3

---
+++ server/src/migrations/20260603120000-remove-establishment-id-from-jobs-partners.ts
@@ -0,0 +1,11 @@
+import { logger } from "@/common/logger"
+import { getDbCollection } from "@/common/utils/mongodbUtils"
+
+export const up = async () => {
+  logger.info("Removing establishment_id field from jobs_partners collection")
+  await getDbCollection("jobs_partners").updateMany({}, { $unset: { establishment_id: "" } })
+  logger.info("establishment_id field removed from jobs_partners collection")
+}
+
+// set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
+export const requireShutdown: boolean = false
