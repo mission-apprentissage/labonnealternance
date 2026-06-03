26e57a065b137360fa58d1ba1f56ceb1bf16193e
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-06-03 21:01:09 +0200

    chore: suppression des migrations déjà appliquées

--- server/src/migrations/20251120025212-update-rome.ts
+++
@@ -1,9 +0,0 @@
-import type { Db } from "mongodb"
-import { importReferentielRome } from "@/jobs/referentielRome/referentielRome"
-
-export const up = async (_db: Db) => {
-  await importReferentielRome()
-}
-
-// set to false ONLY IF migration does not imply a breaking change (ex: update field value or add index)
-export const requireShutdown: boolean = true
