941ad2ea936ac51f34a08a9ea14e62ee81e03ee1
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-06-19 09:59:06 +0200

    fix: retourner l'id du document en cas d'absence de géolocalisation ou de siret

--- server/src/jobs/offrePartenaire/fillSiretInfosForPartners.ts
+++ server/src/jobs/offrePartenaire/fillSiretInfosForPartners.ts
@@ -38,7 +38,7 @@ export const fillSiretInfosForPartners = async ({ addedMatchFilter }: FillComput
       const response = await getSiretInfos(siret)
 
       if (!response) {
-        return []
+        return [{ _id: document._id }] as Pick<IComputedJobsPartners, (typeof filledFields)[number] | "_id">[]
       }
       if (isEnum(BusinessErrorCodes, response)) {
         return [{ _id: document._id, business_error: response }]
