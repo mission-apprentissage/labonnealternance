ff0994d9e125e84db0559bab7c8f8eaad9222c5f
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-06-25 12:29:07 +0200

    feat: action déclenchables par un admin

--- server/src/http/controllers/_private/admin/processor.admin.routes.ts
+++ server/src/http/controllers/_private/admin/processor.admin.routes.ts
@@ -1,4 +1,4 @@
-import { getProcessorStatus } from "job-processor"
+import { addJob, getProcessorStatus } from "job-processor"
 import { zRoutes } from "shared"
 
 import type { Server } from "@/http/server"
@@ -12,6 +12,19 @@ export function processorAdminRoutes(server: Server) {
     },
     async (_request, response) => {
       return response.status(200).send(await getProcessorStatus())
+    }
+  )
+
+  server.post(
+    "/_private/admin/processor/trigger",
+    {
+      schema: zRoutes.post["/_private/admin/processor/trigger"],
+      onRequest: [server.auth(zRoutes.post["/_private/admin/processor/trigger"])],
+    },
+    async (request, response) => {
+      const { job } = request.body as { job: string }
+      await addJob({ name: job, queued: true, payload: {} })
+      return response.status(200).send({})
     }
   )
 }
