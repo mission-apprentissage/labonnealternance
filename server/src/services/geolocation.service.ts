c87a6207145936fc7cabf99d727aaf33fd93ca19
Kevin Barnoin <k.barnoin@gmail.com>
2026-09-14 15:37:38 +0200

    fix: supprimer les fonctions de géocodage Géoplateforme sans appelant (#5467)

--- server/src/services/geolocation.service.ts
+++ server/src/services/geolocation.service.ts
@@ -1,6 +1,5 @@
 import { internal, isBoom } from "@hapi/boom"
 import type { AxiosResponse } from "axios"
-import type FormData from "form-data"
 import type { IAdresseV3, IAPIAdresse, IGeometry, IGeoPoint, IPointFeature } from "shared/models/index"
 import { ZPointFeature, ZPointGeometry } from "shared/models/index"
 import { joinNonNullStrings } from "shared/utils/index"
@@ -51,35 +50,6 @@ export const getGeolocationFromApiAdresse = async (address: string) => {
       throw error
     }
     const newError = internal(`getGeolocationFromApiAdresse: erreur de récupération des geo coordonnées`, { address })
-    newError.cause = error
-    throw newError
-  }
-}
-
-export const _getReverseGeolocationFromApiAdresse = async (lon: number, lat: number): Promise<IPointFeature | null> => {
-  try {
-    let response: AxiosResponse<IAPIAdresse> | null = null
-    let trys = 0
-    while (trys < 3) {
-      response = await client.get(`${API_ADRESSE_URL}/reverse/?lon=${lon}&lat=${lat}&type=municipality&limit=1`)
-      if (response?.status === 429) {
-        console.warn("429 ", new Date())
-        trys++
-        await new Promise((resolve) => setTimeout(resolve, 1000))
-      } else {
-        break
-      }
-    }
-    if (response?.data?.features?.length) {
-      return response.data.features.at(0)!
-    } else {
-      return null
-    }
-  } catch (error: any) {
-    if (isBoom(error)) {
-      throw error
-    }
-    const newError = internal(`getReverseGeolocationFromApiAdresse: erreur de récupération des geo coordonnées`, { lat, lon })
     newError.cause = error
     throw newError
   }
@@ -195,14 +165,6 @@ export const addressDetailToString = (address: IAdresseV3): string => {
 
 export const addressDetailToStreetLabel = (address: IAdresseV3): string | null => {
   return joinNonNullStrings([address.numero_voie, address.type_voie, address.libelle_voie])
-}
-
-export const getBulkGeoLocation = async (form: FormData) => {
-  return await client.post("https://data.geopf.fr/geocodage/search/csv/", form, {
-    headers: {
-      ...form.getHeaders(),
-    },
-  })
 }
 
 export const getGeolocationFromCodeInsee = async (city: string, codeInsee: string) => {
