3e75cfb30a639bef669a2ffd87b4e7318bbef186
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2026-09-17 01:25:22 +0200

    feat: factorisation bloc didask

--- ui/components/ItemDetail/LbaJobComponents/LbaJobCfaDetail.tsx
+++ ui/components/ItemDetail/LbaJobComponents/LbaJobCfaDetail.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { fr } from "@codegouvfr/react-dsfr"
-import { Box, Link, Stack, Typography } from "@mui/material"
-import Image from "next/image"
+import { Box, Stack, Typography } from "@mui/material"
 import { useEffect } from "react"
 import type { ILbaItemPartnerJobJson } from "shared"
 import { DsfrLink } from "@/components/dsfr/DsfrLink"
+import { ConseilsEntretienBlock } from "@/components/ItemDetail/ConseilsEntretienBlock"
 import { ContratBlock } from "@/components/ItemDetail/ItemDetailServices/ContratBlock"
 import { EmployeurPresentationBlock } from "@/components/ItemDetail/ItemDetailServices/EmployeurPresentationBlock"
 import ItemGoogleSearchLink from "@/components/ItemDetail/ItemDetailServices/ItemGoogleSearchLink"
@@ -86,20 +86,7 @@ export const LbaJobCfaDetail = ({ job, title, jobSearchedByUser }: { job: ILbaIt
         </Box>
       </Box>
 
-      <Stack spacing={2} direction="row" sx={{ alignItems: "center", my: fr.spacing("6v"), mx: { xs: 2, sm: 2, md: "auto" } }}>
-        <Image src="/images/whisper.svg" alt="" aria-hidden={true} width={34} height={39} style={{ marginTop: "2px" }} />
-        <Box>
-          <Typography component="div" sx={{ fontWeight: 700, fontSize: "20px", color: "#3a3a3a" }}>
-            Psst !
-          </Typography>
-          <Box sx={{ color: "grey.700" }}>
-            Pour convaincre l'entreprise de vous embaucher,{" "}
-            <Link href="https://dinum.didask.com/courses/demonstration/60d21bf5be76560000ae916e" target="_blank" rel="noopener noreferrer" underline="always">
-              on vous donne des conseils ici pour vous aider !<span className="fr-sr-only"> - nouvelle fenêtre</span>
-            </Link>
-          </Box>
-        </Box>
-      </Stack>
+      <ConseilsEntretienBlock />
     </>
   )
 }
